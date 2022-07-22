d583c2a5ca7c1c024616319fd62a92af61af12b4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-21 20:14:36 -0500

    Love con mención/tag 🔖

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,12 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
-conn.reply(m.chat, `
-*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
-`.trim(), m, m.mentionedJid ? {
-contextInfo: {
-mentionedJid: m.mentionedJid
-}} : {})}
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })
+}
 handler.help = ['love']
 handler.tags = ['fun']
 handler.command = /^(love)$/i
