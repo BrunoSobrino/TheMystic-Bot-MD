706db5e326c26330243f6cfb40eafc25f76584e4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-22 08:15:21 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/herramientas-whatmusic.js
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import acrcloud from 'acrcloud'
+let acr = new acrcloud({
+host: 'identify-eu-west-1.acrcloud.com',
+access_key: 'c33c767d683f78bd17d4bd4991955d81',
+access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
+})
+
+let handler = async (m) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (/audio|video/.test(mime)) {
+let media = await q.download()
+let ext = mime.split('/')[1]
+fs.writeFileSync(`./${m.sender}.${ext}`, media)
+let res = await acr.identify(fs.readFileSync(`./${m.sender}.${ext}`))
+let { code, msg } = res.status
+if (code !== 0) throw msg
+let { title, artists, album, genres, release_date } = res.metadata.music[0]
+let txt = `
+𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰
+
+• 📌 𝚃𝙸𝚃𝚄𝙻𝙾: ${title}
+• 👨‍🎤 𝙰𝚁𝚃𝙸𝚂𝚃𝙰: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'No encontrado'}
+• 💾 𝙰𝙻𝙱𝚄𝙼: ${album.name || 'No encontrado'}
+• 🌐 𝙶𝙴𝙽𝙴𝚁𝙾: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'No encontrado'}
+• 📆 𝙵𝙴𝙲𝙷𝙰 𝙳𝙴 𝙻𝙰𝙽𝚉𝙰𝙼𝙸𝙴𝙽𝚃𝙾: ${release_date}
+`.trim()
+fs.unlinkSync(`./${m.sender}.${ext}`)
+m.reply(txt)
+} else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙰𝚄𝙳𝙸𝙾*'
+}
+handler.command = /^quemusica|quemusicaes|whatmusic$/i
+export default handler
