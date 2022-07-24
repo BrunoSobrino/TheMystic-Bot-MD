57209f3905637eecc05283cbc25987c410272821
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-24 16:10:33 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`
-let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
+let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
 let json = await res.json()
-let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
-m.reply(`${tes}`) 
-}
-handler.help = ['simsimi']
-handler.tags = ['General']
-handler.command = ['bot', 'simi', 'simsimi'] 
+if (json.success) m.reply(json.success)
+else throw json }
+handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
+handler.tags = ['fun']
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i
 export default handler
