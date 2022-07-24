b26434ba0dc12036b0bfbeaf4e9f7ea605cf874d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-24 11:31:32 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
 let full = /f$/i.test(command)
-if (!args[0]) return conn.reply(m.chat, '*Porfavor ingresa un url de la página a la que se le tomara captura🔎*', m)
+if (!args[0]) return conn.reply(m.chat, '*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*', m)
 let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
 let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
 conn.sendFile(m.chat, ss, 'error.png', url, m)
