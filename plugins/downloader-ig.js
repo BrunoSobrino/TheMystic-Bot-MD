01ee13fc461afda948de2d77a38b7b64f6a6383f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-25 09:52:17 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,13 +1,13 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+import hx from 'hxz-api'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-const results = await instagramdl(args[0])
-.catch(async _ => await instagramdlv2(args[0]))
-.catch(async _ => await instagramdlv3(args[0]))
-.catch(async _ => await instagramdlv4(args[0]))
-for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
-}
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command =/^(instagram|ig(dl)?)$/i
+if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
+hx.igdl(args[0]).then(async (r) => {
+for (let i = 0; i < r.medias.length; i++) {
+let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt = `🔗 *Url:* ${shortUrl}`.trim()
+await conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
+}})}
+handler.command = /^instagramdl|instagram|igdl|ig$/i
+handler.dfail = null
 export default handler
