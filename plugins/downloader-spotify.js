75dd10b098ddd45bcb2777db9819ca5efa73a92f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-24 19:57:46 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -2,33 +2,19 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
 try {
-let res = await fetch(global.API('zeks', '/api/spotify', { q: text }, 'apikey'))
-if (!res.ok) throw await res.text()
+let res = await fetch(`https://hadi-api.herokuapp.com/api/soundcloud/play?query=${text}`)
 let json = await res.json()
-if(!json.data[0]) throw json
-let { title, artists, album, thumb, url, preview_mp3 } = json.data[0]
-let spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒
+let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${json.result.download}`)).text()
+let soundcloudt = `❒═══❬ 𝐒𝐎𝐔𝐍𝐃𝐂𝐋𝐎𝐔𝐃 ❭═══╾❒
 ┬
-├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${json.result.title}
 ┴
 ┬
-├‣🗣️ *𝙰𝚁𝚃𝙸𝚂𝚃𝙰:* ${artists}
-┴
-┬
-├‣🎆 *𝙰𝙻𝙱𝚄𝙼:* ${album}
-┴
-┬
-├‣🌐 *𝚄𝚁𝙻*: ${url}
-┴
-┬
-├‣💚 *𝚄𝚁𝙻 𝙳𝙸𝚁𝙴𝙲𝚃𝙾:* ${preview_mp3}\n┴\n\n*_- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚖𝚞𝚜𝚒𝚌𝚊 𝚍𝚎 𝚙𝚛𝚎𝚟𝚒𝚜𝚞𝚊𝚕𝚒𝚣𝚊𝚌𝚒𝚘𝚗_*\n\n_﹫ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ_`
-
-conn.sendFile(m.chat, thumb, '', spotifyi, m)
-conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyi, m)
+├‣💚 *𝚄𝚁𝙻 𝙳𝙸𝚁𝙴𝙲𝚃𝙾:* ${shortUrl}\n┴\n\n*- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚖𝚞𝚜𝚒𝚌𝚊...*\n\n_﹫ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ_`
+conn.sendFile(m.chat, json.result.thumbnail, '', soundcloudt, m)
+conn.sendFile(m.chat, json.result.download, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })
 } catch (e) {
 throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙻𝙾𝙶𝚁𝙾 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*'
 }}
-handler.command = /^(spotify|music)$/i
-handler.help = ['spotify']
-handler.tags = ['general']
+handler.command = /^(spotify|music|soundcloud|cover)$/i
 export default handler
