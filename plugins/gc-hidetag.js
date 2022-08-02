ed2a41c69fff287c48cba9dc24fb82c801f28bac
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-02 12:39:30 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,8 +1,8 @@
 import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
 let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m || m.text
-let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text
+let q = m.quoted ? m.quoted : m || m.text || m.sender
+let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
 let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: m, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
