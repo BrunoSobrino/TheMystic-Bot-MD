945de60f3d47247dca3e19e56955edfbce39a5c7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-05 10:27:33 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-ppcp.js
+++ plugins/random-ppcp.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-let res = await fetch('https://api.lolhuman.xyz/api/random/ppcouple?apikey=9b817532fadff8fc7cb86862')
+let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkeysapi}`)
 if (res.status != 200) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
