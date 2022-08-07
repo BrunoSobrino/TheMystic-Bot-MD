02cfeb5d6e2c51ecf092caa3a195e761601746e4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-07 09:12:52 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -6,10 +6,19 @@ let text = `
 > Pagina Oficial: https://boxmineworld.com
 > Dashboard: https://dash.boxmineworld.com/home
 > Panel: https://panel.boxmineworld.com
+> Soporte 
+
+------------------------------------
+
+*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂 𝙷𝙾𝚂𝚃*
+> Tutorial: https://youtu.be/nbjvreJ0tUk
+> Pagina: https://billing.acidicnodes.ml/register?ref=ADII104p
+> Soporte: https://whatsapp.acidicnodes.com
 
 ------------------------------------
 
 *—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+*[❗] NO DISPONIBLE POR EL MOMENTO [❗]*
 > cd
 > termux-setup-storage
 > apt update 
