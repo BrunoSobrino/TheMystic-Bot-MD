51f1685979b0e8334e7d114e3cff4edd396bfe0f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-09 19:40:35 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -3,7 +3,7 @@ import path from 'path'
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/a.mp3'
-this.sendPresenceUpdate('recording', m.chat)
+conn.sendPresenceUpdate('recording', m.chat)
 conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 handler.customPrefix = /ª|a|A/
