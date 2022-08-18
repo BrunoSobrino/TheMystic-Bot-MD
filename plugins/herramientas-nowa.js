91830047e5c5cead45a85065084cbaf054a13ec2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-18 00:39:10 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/herramientas-nowa.js
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------
+  🍀 • By https://github.com/ALBERTO9883
+  🍀 • ⚘Alberto Y Ashly⚘
+-----------------------------------------------------------------------------------------*/
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let regex = /x/g
+if (!text) throw '⚠️ Falto el número.'
+if (!text.match(regex)) throw `*Ejemplo de uso: ${usedPrefix + command} 521999340434x*`
+let random = text.match(regex).length, total = Math.pow(10, random), array = []
+for (let i = 0; i < total; i++) {
+let list = [...i.toString().padStart(random, '0')]
+let result = text.replace(regex, () => list.shift()) + '@s.whatsapp.net'
+if (await conn.onWhatsApp(result).then(v => (v[0] || {}).exists)) {
+let info = await conn.fetchStatus(result).catch(_ => {})
+array.push({ exists: true, jid: result, ...info })
+} else {
+array.push({ exists: false, jid: result })
+}}
+let txt = 'Registrados\n\n' + array.filter(v => v.exists).map(v => `• Nro: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'Sin descripcion'}\n*• Fecha:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*No registrados*\n\n' + array.filter(v => !v.exists).map(v => v.jid.split('@')[0]).join('\n')
+m.reply(txt)
+}
+handler.command = /^nowa$/i
+export default handler
+function formatDate(n, locale = 'id') {
+let d = new Date(n)
+return d.toLocaleDateString(locale, { timeZone: 'Asia/Jakarta' })}
