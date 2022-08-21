9c8fb6f42cc179a41379dd3f410482a122061879
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-08-21 00:17:03 -0500

    Anti privado 💬

---
+++ plugins/_antiprivado.js
@@ -0,0 +1,18 @@
+/*export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (m.isGroup)
+       return !1
+    if (!m.message)
+       return !0
+    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA'))
+       return !0
+    let chat = global.db.data.chats[m.chat]
+    let bot = global.db.data.settings[this.user.jid] || {}
+    
+    if (bot.antiPrivate && !isOwner) {
+       await m.reply(`Hola *@${m.sender.split`@`[0]}*, está prohibido hablar al privado de la bot serás bloqueado.`, false, { mentions: [m.sender] })
+       await this.updateBlockStatus(m.chat, 'block')
+    }
+    return !1
+}*/
