ad20127bbcafe18f6be99c89d98492edd3525574
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-21 13:13:55 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,4 @@
-export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
+export async function before(m, { isAdmin, isBotAdmin, isOwner, isROwner }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (m.isGroup)
@@ -10,7 +10,7 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     
-    if (bot.antiPrivate && !isOwner) {
+    if (bot.antiPrivate && !isOwner && !isROwner) {
        await m.reply(`Hola *@${m.sender.split`@`[0]}*, está prohibido hablar al privado del bot serás bloqueado.`, false, { mentions: [m.sender] })
        await this.updateBlockStatus(m.chat, 'block')
     }
