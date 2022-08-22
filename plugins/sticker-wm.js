00f5b9e8d5abff345d8384d4a18cff9b2cee2751
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-22 02:03:39 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -19,5 +19,5 @@ else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙰�
 }}
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
-handler.command = /^wm$/i
+handler.command = /^robar|wm$/i
 export default handler
