1c595f3ecc7b73c5eff4111c07b590549cb5d820
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-08-22 15:36:46 -0500

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,4 +1,6 @@
-export async function before(m, { isAdmin, isBotAdmin, isOwner, isROwner }) {
+const toxicRegex = /puto|puta|rata|mrd|verga|vrga/i
+
+export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup)
@@ -6,11 +8,11 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner, isROwner }) {
     let user = global.db.data.users[m.sender]
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
+    const isToxic = toxicRegex.exec(m.text)
     
-    let toxic = ['Puto', 'puto', 'Puta', 'puta', 'Rata', 'rata', 'mrd', 'vrga', 'verga'] // Aqui puedes agregarle mas palabras si quieres
-    if (toxic.includes(m.text) && chat.antiToxic && !isOwner && !isROwner) {
+    if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
        user.warn += 1
-       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${toxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
+       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
     }
     
     if (user.warn >= 5) {
