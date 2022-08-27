2ba994d6ad4d07d4ad640a277b0d1551561be7f3
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-08-27 13:18:45 -0500

    😸🌌
    Un simple Antitrabas/antiVirtex o como se le llamé x'd

---
+++ plugins/_antitrabas.js
@@ -0,0 +1,29 @@
+//
+//By @NeKosmic || https://github.com/NeKosmic/
+//
+import * as fs from 'fs'
+
+export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = global.db.data.chats[m.chat]
+    let bot = global.db.data.settings[this.user.jid] || {}
+    let name = await conn.getName(m.sender)
+
+    if (chat.antiTraba && m.text.length > 1200) { //Cantidad máxima de caracteres aceptados en un mensaje//
+    	if (isAdmin) return conn.sendMessage(m.chat, { text: `El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝', 'jpegThumbnail': null}}} })
+    
+    await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[ DESACTIVAR ANTI TRABAS ]', usedPrefix+'disable antitraba'], m)
+    
+        if (isBotAdmin && bot.restrict) {
+        	setTimeout(() => { 
+        	conn.sendMessage(m.chat, { text: `Marcar el chat como leido ✓\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\n[ ! ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616169743@g.us","inviteCode": "m","groupName": "P", "caption": '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝', 'jpegThumbnail': null}}} })
+        }, 0)
+        setTimeout(() => { 
+        	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+            }, 1000)
+        } else if (!bot.restrict) return m.reply('[ ! ] Para realizar acciones de eliminación, mi dueño tiene que encender el modo restringido!')
+    }
+    return !0
+}
