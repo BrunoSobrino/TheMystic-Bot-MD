a7b1cd2407917385735928ad9d3bfd6fca8a7953
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-08-28 13:43:57 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/frase-frases.js
+++ plugins/frase-frases.js
@@ -5,25 +5,34 @@
 
 import translate from 'translate-google-api'
 import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
+let handler = async (m, { conn, command }) => { 
 
 if (command == 'consejo') {
+    
 let res = await fetch("https://supra-api.herokuapp.com/api/conselho?apikey=supraz")
+try {
 let json = await res.json()
 let { frase } = json
 const tld = 'cn'
 let frase1 = await translate(`${frase}`, { tld, to: 'es' })
-m.reply(`*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${frase1}*\n*┗━━━━━━━━━━━━━━━━┚*`)}
+m.reply(`*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${frase1}*\n*┗━━━━━━━━━━━━━━━━┚*`)
+} catch {
+m.reply(`*[❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚂𝙸𝙱𝙻𝙴𝙼𝙴𝙽𝚃𝙴 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 (𝙰𝙿𝙸) 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙴𝚂𝚃𝙴 𝙲𝙰𝙸𝙳𝙰 𝚃𝙴𝙼𝙿𝙾𝚁𝙰𝙻𝙼𝙴𝙽𝚃𝙴, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*`)}}
+    
 
 if (command == 'fraseromantica') {
+try {
 let res = await fetch("https://supra-api.herokuapp.com/api/romanticafrase?apikey=supraz")
 let json = await res.json()
 let { frase } = json
 const tld = 'cn'
 let frase1 = await translate(`${frase}`, { tld, to: 'es' })
-m.reply(`*╭─◆────◈⚘◈─────◆─╮*\n*❥  ${frase1}*\n*╰─◆────◈⚘◈─────◆─╯*`)}
+m.reply(`*╭─◆────◈⚘◈─────◆─╮*\n*❥  ${frase1}*\n*╰─◆────◈⚘◈─────◆─╯*`)
+} catch {
+m.reply(`*[❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚂𝙸𝙱𝙻𝙴𝙼𝙴𝙽𝚃𝙴 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 (𝙰𝙿𝙸) 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙴𝚂𝚃𝙴 𝙲𝙰𝙸𝙳𝙰 𝚃𝙴𝙼𝙿𝙾𝚁𝙰𝙻𝙼𝙴𝙽𝚃𝙴, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*`)}}
 
 if (command == 'historiaromantica') {
+try {
 let res = await fetch("https://api-xcoders.xyz/api/random/cerpen/cinta?apikey=xcoders")
 let json = await res.json()
 let { story, title, author_name } = json.result
@@ -33,7 +42,10 @@ let titletime = await translate(`${title}`, { tld, to: 'es' })
 conn.reply(m.chat, `᭥🫐᭢ Título: ${titletime}
 ᭥🍃᭢ Autor: ${author_name}
 ────────────────
-${storytime}`, m)}
+${storytime}`, m)
+} catch {    
+m.reply(`*[❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚂𝙸𝙱𝙻𝙴𝙼𝙴𝙽𝚃𝙴 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 (𝙰𝙿𝙸) 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙴𝚂𝚃𝙴 𝙲𝙰𝙸𝙳𝙰 𝚃𝙴𝙼𝙿𝙾𝚁𝙰𝙻𝙼𝙴𝙽𝚃𝙴, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*`)}}
+      
 }
 handler.tags = ['frases']
 handler.command = handler.help = ['consejo', 'fraseromantica', 'historiaromantica']
