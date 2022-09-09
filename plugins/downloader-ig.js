9e591d08198c46f990b3344ce0608fc267ab5a85
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 22:20:14 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,4 +1,17 @@
-import hx from 'hxz-api'
+/* Creado por https://github.com/FG98F */
+
+import instagramGetUrl from 'fg-ig'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
+let results = (await instagramGetUrl(args[0])).url_list[0]
+let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt = `🔗 *Url:* ${shortUrl}`.trim()
+await conn.sendFile(m.chat, results, 'instagram.mp4', txt, m)}
+handler.command =/^(instagram|ig(dl)?)$/i
+handler.dfail = null
+export default handler
+
+/*import hx from 'hxz-api'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
@@ -10,4 +23,4 @@ await conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
 }})}
 handler.command = /^instagramdl|instagram|igdl|ig$/i
 handler.dfail = null
-export default handler
+export default handler*/
