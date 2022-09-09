bae83b35463e5b8bf05f62c82e49b71a5b608042
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 21:18:32 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -2,7 +2,7 @@ import cp from 'child_process'
 import { promisify } from 'util'
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (m) => {
-	await conn.reply(m.chat, wait, m)
+	await conn.reply(m.chat, global.wait, m)
     let o
     try {
         o = await exec('python speed.py')
