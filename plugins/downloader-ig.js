b3b29a0dd40320546db0b7b0fd37a4bbf619cf3e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 23:18:16 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -5,9 +5,9 @@ import instagramGetUrl from 'fg-ig'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
 let results = (await instagramGetUrl(args[0])).url_list[0]
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${results}`)).text()
 let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, results, 'instagram.mp4', txt, m)}
+await conn.sendFile(m.chat, results, 'error.mp4', txt, m)}
 handler.command =/^(instagram|ig(dl)?)$/i
 handler.dfail = null
 export default handler
