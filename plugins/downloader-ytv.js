1cf867da351f7a76db6d8aec29c66d21d59de5d1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-09 00:58:02 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -7,7 +7,7 @@ try {
 let qu = args[1] || '360'
 let q = qu + 'p'
 let v = args[0]
-const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
