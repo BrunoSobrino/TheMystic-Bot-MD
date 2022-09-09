06121277a3479d46da509288e736e697f1a820c9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 23:16:37 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,5 +1,6 @@
 /* Creado por https://github.com/FG98F */
 
+import fetch from 'node-fetch'
 import instagramGetUrl from 'fg-ig'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
