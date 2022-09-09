50d15b77211fb2b4099bad7c8efc01f3f0461769
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 21:32:24 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- speed.py
+++ speed.py
@@ -14,6 +14,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+#    FOR THE MYSTIC-BOT   #
 
 import csv
 import datetime
