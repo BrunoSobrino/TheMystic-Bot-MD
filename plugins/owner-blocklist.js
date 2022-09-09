38e5d1be253db01a8f46bdae9c1277929584bc94
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 22:32:45 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -4,14 +4,12 @@ let handler = async (m, { conn }) => {
 await conn.fetchBlocklist().then(async data => {
 let txt = `*≡ Lista de bloqueados*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
 for (let i of data) {
-txt += `▢ @${i.split("@")[0]}\n`
-}
+txt += `▢ @${i.split("@")[0]}\n`}
 txt += "└───────────"
 return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 }).catch(err => {
 console.log(err);
-throw 'No hay números bloqueados'
-})}
+throw 'No hay números bloqueados'})}
 handler.help = ['blocklist']
 handler.tags = ['main']
 handler.command = ['blocklist', 'listblock'] 
