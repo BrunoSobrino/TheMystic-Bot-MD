32a99989b15dbae7e51123b6499fc7a39632c178
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 21:26:07 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-sremovebg.js
+++ plugins/sticker-sremovebg.js
@@ -6,7 +6,7 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 let img = await q.download()
 let url = await uploadImage(img)
-let sremovebg = global.API(`https://violetics.pw/api/media/removebg?apikey=beta&img=${url}`) 
+let sremovebg = global.API(`https://api.lolhuman.xyz/api/removebg?apikey=85faf717d0545d14074659ad&img=${url}`) 
 let stickerr = await sticker(false, sremovebg, global.packname, global.author)
 conn.sendFile(m.chat, stickerr, 'sticker.webp', '', m, { asSticker: true })
 } catch (e) {
