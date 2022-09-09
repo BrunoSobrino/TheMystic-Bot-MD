0a7f1b39a9254766b79597af87438036d5e3a1d6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-08 21:10:42 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, '*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*', m)
-let ss = await (await fetch(`https://violetics.pw/api/media/ssweb?apikey=beta&url=${args[0]}`)).buffer()
+let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
