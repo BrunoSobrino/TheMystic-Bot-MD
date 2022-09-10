0ff6358c916fd13332f84b67a075f9a53948aa47
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-10 16:25:45 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,19 +1,9 @@
 let handler = async (m, { conn }) => {
-    conn.reply(m.chat, `
-「 *LISTA DE  COMANDOS* 」
-▢ Info: Si esta en *negrita*  esta bloqueado
+conn.reply(m.chat, `
+*< 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 / 𝚃𝙴𝚇𝚃𝙾𝚂 𝙰𝚂𝙸𝙶𝙰𝙽𝙳𝙾𝚂 />*
 
-❉─────────────────────❉ 
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `*(bloqueado)* ${key}` : key} : ${value.text}`).join('\n')}
-
-`.trim(), null, {
-        mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
-    })
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `*(bloqueado)* ${key}` : key} : ${value.text}`).join('\n\n')}
+`.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
 }
-
-
-handler.help = ['listcmd']
-handler.tags = ['database']
-handler.command = ['listcmd']
-
+handler.command = ['listcmd', 'cmdlist']
 export default handler
