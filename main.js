9a8d5d773033744ae0a61c55bed736c03967fe97
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-09-10 23:54:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-import './config.js';
+import config from './config.js';
 import { createRequire } from "module"; 
 import path, { join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
