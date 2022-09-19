9fa0745d50205c3eda34db9fc9ed31607273491f
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-19 17:39:45 -0500

    😸✍️
    Simple buscador de playstore :D

---
+++ plugins/buscador-playstore.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, text, args }) => {
+	if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+	let enc = encodeURIComponent(text)
+try {
+let gPlay = await fetchJson(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
+if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
+conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 Resultado: ${gPlay.titulo}
+🧬 Identificador: ${gPlay.id}
+⛓️ Link: ${gPlay.link}
+🖼️ Imagen: ${gPlay.imagen}
+✍️ Desarrollador: ${gPlay.desarrollador}
+📜 Descripcion: ${gPlay.descripcion}
+💲 Moneda: ${gPlay.moneda}
+🎭 Gratis?: ${gPlay.gratis}
+💸 Precio: ${gPlay.precio}
+📈 Puntuacion: ${gPlay.puntuacion}`},{quoted:m})
+} catch (e) {
+m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')
+console.log(e)
+}
+}
+
+handler.help = ['playstore <aplicacion>']
+handler.tags = ['internet']
+handler.command = /^(playstore)$/i
+
+export default handler
