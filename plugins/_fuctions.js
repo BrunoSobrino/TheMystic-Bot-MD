9338d46925b368dc7341af5d64d681e844c2e2d9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-21 16:56:16 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_fuctions.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-  
-function message1(text) {
-let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: text, contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD' }, mentionedJid: [m.sender] }}}, { quoted: m })
-conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] })}
-global.replyAdd = message1
-}
-handler.command = /^(prueba)$/i
-export default handler
