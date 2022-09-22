22dc8bbd0955071dddc56400beaede8404611952
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-22 18:29:53 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-covid.js
+++ plugins/info-covid.js
@@ -1,17 +1,18 @@
 import fetch from 'node-fetch'
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.confirmed) throw 'País?'
-  if (json.confirmed) m.reply(`
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝙿𝙰𝙸𝚂, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} Mexico*`
+let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.confirmed) throw 'País?'
+if (json.confirmed) m.reply(`
 🌏 País : ${text}
 ✅Confirmado : ${json.confirmed.value}
 📉curado : ${json.recovered.value}
 ☠️Muertes : ${json.deaths.value}
 💌Info Actualizada : ${json.lastUpdate}
 `.trim())
-  else throw json
+else throw json
 }
 handler.help = ['covid'].map(v => v + ' <país>')
 handler.tags = ['info']
