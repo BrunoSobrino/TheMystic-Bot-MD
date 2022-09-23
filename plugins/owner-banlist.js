ea7071dcefa78402c410480d25f8a25b08fc1166
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-22 20:22:09 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/owner-banlist.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, isOwner }) => {
+let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
+let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
+let caption = `
+┌〔 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐁𝐀𝐍𝐄𝐀𝐃𝐎𝐒 〕
+├ Total : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
+├ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+└────
+
+┌〔 𝐂𝐇𝐀𝐓𝐒 𝐁𝐀𝐍𝐄𝐀𝐃𝐎𝐒 〕
+├ Total : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
+├ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+└────
+`.trim()
+m.reply(caption, null, {mentions: conn.parseMention(caption)})}
+handler.command = /^banlist(ned)?|ban(ned)?list|daftarban(ned)?$/i
+handler.rowner = true
+export default handler
