667186bdc57b8ade27e44e2a9303152da4053bc6
GeneradorVIP <89824328+GeneradorVIP@users.noreply.github.com>
2022-09-26 10:03:30 -0500

    Update buscador-playstore.js

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,17 +1,19 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args }) => {
 if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+const tld = 'cn'
 let enc = encodeURIComponent(text)
 try {
 let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
 let gPlay = await json.json()
+let Mystic = await translate(`${gPlay.descripcion}`, { tld, to: 'es' })
 if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
 conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 Resultado: ${gPlay.titulo}
 🧬 Identificador: ${gPlay.id}
 ⛓️ Link: ${gPlay.link}
 🖼️ Imagen: ${gPlay.imagen}
 ✍️ Desarrollador: ${gPlay.desarrollador}
-📜 Descripcion: ${gPlay.descripcion}
+📜 Descripcion: ${Mystic}
 💲 Moneda: ${gPlay.moneda}
 🎭 Gratis?: ${gPlay.gratis}
 💸 Precio: ${gPlay.precio}
