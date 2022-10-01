f4e4b7ff53d258c193eb523d60ab5b35bbc8811d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-01 12:02:38 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_wel&bye.js
+++ plugins/_wel&bye.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, usedPrefix, command, customPrefix }) => {
 let stikerwelgc = "./src/welgc.webp"
 let stikerbyegc = "./src/byegc.webp"
 if (command == 'welcomegc') {
-conn.sendFile(m.chat, stikerwelgc, null, { asSticker: true })
+conn.sendFile(m.chat, stikerwelgc, null, m, null, { asSticker: true })
 }
 if (command == 'byegc') {
-conn.sendFile(m.chat, stikerbyegc, null, { asSticker: true })
+conn.sendFile(m.chat, stikerbyegc, null, m, null, { asSticker: true })
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
