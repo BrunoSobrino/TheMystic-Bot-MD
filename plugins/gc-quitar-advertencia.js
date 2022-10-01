46d22625b1535064b6b25dfd86f049bb19725b24
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-01 12:56:15 -0400

    Create gc-quitar-advertencia.js

---
+++ plugins/gc-quitar-advertencia.js
@@ -0,0 +1,37 @@
+let handler = async (m, { conn, text, command, usedPrefix }) => {//prems 
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}  
+if (!db.data.chats[m.chat].antitoxic && m.isGroup) return conn.reply(m.chat, `[❗𝐈𝐍𝐅𝐎❗]𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 *#on antitoxicos* 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝘼, 𝘿𝙀𝘽𝙀 𝘿𝙀 𝘼𝘾𝙏𝙄𝙑𝘼𝙍 𝘿𝙄𝘾𝙃𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝙍 𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊`, fkontak, m) 
+let who
+let pp = './Menu2.jpg'
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+let user = global.db.data.users[who]
+if (!who) throw `[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝚀𝚄𝙴 𝙻𝙴 𝚅𝙰 𝙰 𝚀𝚄𝙸𝚃𝙰𝚁 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰 ⚠️\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*`
+user.warn -= 1
+  
+await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} 𝚂𝙴 𝙻𝙴 𝚀𝚄𝙸𝚃𝙾 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰 `, `*ADVERTENCIA : WARNING*\n⚠️ *Antes: ${user.warn + 1}/4*\n⚠️ *Ahora: ${user.warn}/4*\n\n${wm}`, pp, [
+['🌱 𝙱𝙰𝙻𝙰𝙽𝙲𝙴', '.inventario'],
+['📦 𝙼𝙴𝙽𝚄', '/menu']], false, { mentions: [who] }) //[m.sender]
+	
+}
+handler.help = ['addprem <@user>']
+handler.tags = ['owner']
+//handler.command = /^(delwarn|deladvertir|deladvertencia|delwarning|)$/i
+handler.command = /^(del|delete|eliminar|\-)advertir|advertencia|warn(ing)?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
