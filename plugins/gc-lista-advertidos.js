d63018b50a90bf4b51cd53a1e5e17636d0842bc8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-01 12:48:11 -0400

    Create gc-lista-advertidos.js

---
+++ plugins/gc-lista-advertidos.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, isOwner }) => {
+let adv = Object.entries(global.db.data.users).filter(user => user[1].warn)
+let warns = global.db.data.users.warn
+let user = global.db.data.users
+
+let caption = `⚠️ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝙳𝙾𝚂 
+*╔═══════════════════·•*
+║ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
+║
+║ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${user.warn}/4)*`}
+║ ${isOwner ? '@' + jid.split`@`[0] : jid}\n║ - - - - - - - - -`.trim()).join('\n') : ''}
+*╚═══════════════════·•*`
+await conn.sendButton(m.chat, caption, `⚠️ 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂 ⇢ ${warns ? `*${warns}/4*` : '*0/4*'}\n${wm}`, null, [ 
+['𝐌 𝐄 𝐍 𝐔 🌠', '/menu']], m, { mentions: await conn.parseMention(caption) })}
+
+handler.command = /^(listaadv|listadv|adv|advlist|advlista)$/i 
+
+export default handler
