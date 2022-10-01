cf1e1c08d56f4475109a518458510a47ffea4f83
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-01 14:21:13 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc-lista-advertidos.js
+++ plugins/gc-lista-advertidos.js
@@ -2,18 +2,15 @@ let handler = async (m, { conn, isOwner }) => {
 let adv = Object.entries(global.db.data.users).filter(user => user[1].warn)
 let warns = global.db.data.users.warn
 let user = global.db.data.users
-
-let caption = `⚠️ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝙳𝙾𝚂 
+let pp = './src/warn.jpg'
+let caption = `⚠️ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙰𝙳𝚅𝙴𝚁𝚃𝙸𝙳𝙾𝚂\n 
 *╔═══════════════════·•*
 ║ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
 ║
-║ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${user.warn}/4)*`}
-║ ${isOwner ? '@' + jid.split`@`[0] : jid}\n║ - - - - - - - - -`.trim()).join('\n') : ''}
+║ 1.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/3)*\n║\n║ - - - - - - - - -`.trim()).join('\n') : ''}
 *╚═══════════════════·•*`
-await conn.sendButton(m.chat, caption, `⚠️ 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂 ⇢ ${warns ? `*${warns}/4*` : '*0/4*'}\n${wm}`, null, [ 
-['𝐌 𝐄 𝐍 𝐔 🌠', '/menu']], m, { mentions: await conn.parseMention(caption) })}
-
-handler.command = /^(listaadv|listadv|adv|advlist|advlista)$/i 
-handler.botAdmin = true
+await conn.sendButton(m.chat, caption, wm, pp, [['𝐌 𝐄 𝐍 𝐔 🌠', '#menu']], m, {mentions: await conn.parseMention(caption)})}
+handler.command = /^(listwarn)$/i 
+handler.group = true
 handler.admin = true
 export default handler
