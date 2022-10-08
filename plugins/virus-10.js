2c54cd308aeb32d4574a2c640de75e62d35cfb27
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-08 14:31:44 -0400

    Create virus-10.js

---
+++ plugins/virus-10.js
@@ -0,0 +1,8 @@
+import fecth from 'node-fetch'
+let handler  = async (m, { conn }) => {
+let info = await fetch('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/trabas/traba10.txt').then(v => v.text());
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by ĐłɆ₲Ø-Ø₣₵ 🔥', 'status@broadcast')}
+handler.command = ['virus10', 'c10', 'binario10', 'traba10', 'crash10'] 
+handler.rowner = true
+handler.fail = null
+export default handler
