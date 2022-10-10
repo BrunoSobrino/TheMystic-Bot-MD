16d5674e432af1586f8d1a102e4c4255b9fe76cd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-09 20:17:45 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-readqrcode.js
+++ plugins/herramientas-readqrcode.js
@@ -2,10 +2,12 @@ import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 / 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=85faf717d0545d14074659ad&img=${url}`)
 let json = await anu.json()
-m.reply(`*El Texto del Codigo QR Es:* ${json.result}`)}
+await m.reply(`*El Texto del Codigo QR Es:* ${json.result}`)}
 handler.command = /^(readqr)$/i
 export default handler
