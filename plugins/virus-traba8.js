b65d86d6dbb3b2092aee1b20f28f0fd012f6d963
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-09 21:37:03 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/virus-traba8.js
+++ plugins/virus-traba8.js
@@ -1,4 +1,4 @@
-import fecth from 'node-fetch'
+import fetch from 'node-fetch'
 let handler  = async (m, { conn }) => {
 let info = await fetch('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/trabas/traba5.txt').then(v => v.text());
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by ĐłɆ₲Ø-Ø₣₵ 🔥', 'status@broadcast')}
