aa685d498a5a2d4c332f91e32734535e28a9f9ca
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-13 20:01:21 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-translate.js
+++ plugins/herramientas-translate.js
@@ -1,24 +1,26 @@
 import translate from 'translate-google-api'
-const defaultLang = 'es'
-const tld = 'cn'
 import fetch from 'node-fetch'
 let handler = async (m, { args, usedPrefix, command }) => {
 let msg = `*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} (idioma) (texto)*\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} es Hello*\n\n*𝙲𝙾𝙽𝙾𝙲𝙴 𝙻𝙾𝚂 𝙸𝙳𝙸𝙾𝙼𝙰𝚂 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾𝚂 𝙴𝙽:*\n*- https://cloud.google.com/translate/docs/languages*`
-if (!args || !args[0]) return m.reply(msg)
+if (!args || !args[0]) return m.reply(msg)  
 let lang = args[0]
 let text = args.slice(1).join(' ')
+const defaultLang = 'es'
 if ((args[0] || '').length !== 2) {
 lang = defaultLang
-text = args.join(' ') }
+text = args.join(' ')}
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-let result
-try {
-result = await translate(`${text}`, { tld, to: lang, })
+try {      
+let result = await translate(`${text}`, { tld, to: lang, })
+await m.reply('*Traducción:* ' + result)
 } catch {
+try {    
 let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
 let loll = await lol.json()
-result = loll.result.translated
-} finally {
-m.reply('*Traducción:* ' +  result) }}
-handler.command = /^(tr(anslate)|traducir?)$/i
+let result2 = loll.result.translated
+await m.reply('*Traducción:* ' + result2)
+} catch { 
+await m.reply('*[❗𝐈𝐍𝐅𝐎❗] ERROR, VUELVA A INTENTARLO*')    
+}}}
+handler.command = /^(translate|traducir|trad)$/i
 export default handler
