1f2eb2b36628264cdc369ee25e0c308085fbd298
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-21 04:59:19 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-lolivid.js
+++ plugins/random-lolivid.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let res = "https://recoders-area.caliph.repl.co/api/lolivid"
-conn.sendHydrated(m.chat, null, null, res, null, null, null, null, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
+conn.sendButton(m.chat, `ʟᴏʟɪ ɪѕ ᴄᴜᴛᴇ 🥺`, wm, res, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
 handler.help = ['lolivid']
 handler.tags = ['random']
 handler.command = /^(lolivid|lolivideos|lolívid)$/i
