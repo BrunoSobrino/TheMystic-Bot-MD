a8494ff7e27be8b8508ced40cbfaa8495bc93e24
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-23 19:52:51 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-stickerly.js
+++ plugins/downloader-stickerly.js
@@ -9,9 +9,17 @@ for (let data of (json.result.stickerlist || json)) {
 const stikers = await sticker(false, data, global.packname, global.author)
 conn.sendFile(m.chat, stikers, 'sticker.webp', '', m, { asSticker: true })}
 //await delay(1500)
-} catch {  
+} catch { 
+try {
+let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=5bd33b276d41d6b4`)
+let json = await res.json()
+for (let data of (json.result.stickerlist || json)) {
+const stikers = await sticker(false, data, global.packname, global.author)
+conn.sendFile(m.chat, stikers, 'sticker.webp', '', m, { asSticker: true })}
+//await delay(1500)  
+} catch {   
 await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*')  
-}}
+}}}
 handler.command = /^stickerly|stickerpack$/i
 export default handler
 //const delay = time => new Promise(res => setTimeout(res, time))
