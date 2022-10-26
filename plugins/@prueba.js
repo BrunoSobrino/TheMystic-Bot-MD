b7f6de245c3baae9b0c92b7bc11ce194a6577ea9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-26 01:19:52 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/@prueba.js
+++ plugins/@prueba.js
@@ -1,9 +1,1 @@
-import getLastMessageInChat from '@adiwajshing/baileys'.default 
-let handler = async (m, { conn }) => {
- 
-const lastMsgInChat = await getLastMessageInChat('5219996125657@s.whatsapp.net') // implement this on your end
-await conn.chatModify({ delete: true, lastMessages: [{ key: lastMsgInChat.key, messageTimestamp: lastMsgInChat.messageTimestamp }]}, '5219996125657@s.whatsapp.net')
 
-}
-handler.command = /^(clearchat)$/i
-export default handler
