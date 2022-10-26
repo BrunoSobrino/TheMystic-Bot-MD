d782fec72856a33feb9e943c3b80950e97f4e5d3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-26 00:43:43 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc-setpp.js
+++ plugins/gc-setpp.js
@@ -1,8 +1,21 @@
-import { webp2png } from '../lib/webp2mp4.js'
+let handler = async (m, { conn, usedPrefix, command }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/image/.test(mime)) {
+let img = await q.download()
+if (!img) throw '*⚠️️ Responde a una imagen.*'
+await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('⚘ *_Imagen actualizada con éxito._*'))
+} else throw '*⚠️️ Responde a una imagen.*'}
+handler.command = /^setpp(group|grup|gc)?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
+
+/*import { webp2png } from '../lib/webp2mp4.js'
 import { URL_REGEX } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
 let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let url = await webp2png(await q.download())
 await conn.updateProfilePicture(m.chat, { url }).then(_ => m.reply('⚘ *_Imagen actualizada con éxito._*'))
@@ -15,4 +28,4 @@ handler.tags = ['group']
 handler.alias = ['setppgc', 'setppgrup', 'setppgroup']
 handler.command = /^setpp(gc|grup|group)$/i
 handler.group = handler.admin = handler.botAdmin = true
-export default handler
+export default handler*/
