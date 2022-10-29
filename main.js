0a7b1cf099e6e37683a686a96308894279a8c572
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-29 01:25:50 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- main.js
+++ main.js
@@ -93,9 +93,9 @@ tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(d
 const ignoreDir = (filePath) => filePath.includes('creds.js');
 readdirSync("./jadibts", null, null, ignoreDir).forEach(file => {
     rmSync("./jadibts/" + file, { recursive: true, force: true })})
-const ignoreDir2 = (filePath) => filePath.includes('creds.js');    
-readdirSync("./MysticSession", null, null, ignoreDir2).forEach(file => {
-    rmSync("./MysticSession/" + file, { recursive: true, force: true })})    
+//const ignoreDir2 = (filePath) => filePath.includes('creds.js');    
+//readdirSync("./MysticSession", null, null, ignoreDir2).forEach(file => {
+//    rmSync("./MysticSession/" + file, { recursive: true, force: true })})    
 return filename.map(file => {
 const stats = statSync(file)
 if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) return unlinkSync(file) // 3 minutes
