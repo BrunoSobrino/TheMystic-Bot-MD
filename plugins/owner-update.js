48a05e24fc9d730223fb3d52074176d24b6c2014
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-29 01:54:30 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,8 +1,12 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
+try {  
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)
+conn.reply(m.chat, stdout.toString(), m)}
+} catch {
+var update = execSync('git remote set-url origin https://github.com/BrunoSobrino/TheMystic-Bot-MD.git && git pull')
+await m.reply(update.toString())
 }}
 handler.help = ['update']
 handler.tags = ['owner']
