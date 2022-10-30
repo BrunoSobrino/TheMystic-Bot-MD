8f3032bfb4025bf1769b74e045a71c166e2d9d4b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 10:28:00 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,7 +5,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
        return !1
     if (!m.message)
        return !0
-    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || || m.text.includes('serbot') || || m.text.includes('jadibot'))
+    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || || m.text.includes('serbot') || m.text.includes('jadibot'))
        return !0
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
