1144cc627d1094a28cce8477393c7f7ad73efc3a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 00:25:09 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/adult-videoxxxlesbi.js
@@ -0,0 +1,52 @@
+/* By https://github.com/GataNina-Li/GataBot-MD */
+let handler  = async (m, { conn, usedPrefix, command }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'   
+let res = await pickRandom(asupan)
+await conn.sendButton(m.chat, `*ᴅɪsғʀᴜᴛᴀ ᴅᴇʟ ᴠɪᴅᴇᴏ 🥵*`, wm, res, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
+handler.help = ['videoxxxlesbi']
+handler.tags = ['random']
+handler.command = /^(videoxxxlesbi|videolesbixxx|pornolesbivid|pornolesbianavid|pornolesbiv|pornolesbianav|Pornolesbivid|Pornolesbianavid|pornolesv|Pornolesv)$/i
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(list.length * Math.random())]}
+
+const asupan = [
+"https://l.top4top.io/m_2257y4pyl0.mp4",
+"https://c.top4top.io/m_2274woesg0.mp4",
+"https://k.top4top.io/m_2257pdwjy0.mp4",
+"https://a.top4top.io/m_2257qulmx0.mp4",
+"https://a.top4top.io/m_2257vxzr62.mp4",
+"https://b.top4top.io/m_2257wjmbh3.mp4",
+"https://b.top4top.io/m_2257sen2a1.mp4",
+"https://c.top4top.io/m_2257hpo9v3.mp4",
+"https://e.top4top.io/m_2257pye7u1.mp4",
+"https://c.top4top.io/m_2257p7xg14.mp4",
+"https://c.top4top.io/m_2257p4v9i3.mp4",
+"https://l.top4top.io/m_2257jvkrv3.mp4",
+"https://b.top4top.io/m_2257pl7wh1.mp4",
+"https://e.top4top.io/m_2257fiwnp2.mp4",
+"https://b.top4top.io/m_22578b1nk1.mp4",
+"https://k.top4top.io/m_22572gv7q1.mp4",
+"https://i.top4top.io/m_2257pu90l2.mp4",
+"https://d.top4top.io/m_2257vcwiw3.mp4",
+"https://j.top4top.io/m_2258joebc2.mp4",
+"https://g.top4top.io/m_2258kvnba4.mp4",
+"https://f.top4top.io/m_2258nm8pe1.mp4",
+"https://g.top4top.io/m_2258af7bc2.mp4",
+"https://l.top4top.io/m_2258f0ci92.mp4",
+"https://j.top4top.io/m_2258ehqpb2.mp4",
+"https://h.top4top.io/m_2258pckkf3.mp4",
+"https://e.top4top.io/m_225857rs20.mp4",
+"https://k.top4top.io/m_225863kpa0.mp4",
+"https://j.top4top.io/m_2258s6we62.mp4",
+"https://i.top4top.io/m_2258if6l13.mp4",
+"https://b.top4top.io/m_2258lmd2h1.mp4",
+"https://j.top4top.io/m_2258a9oah2.mp4",
+"https://i.top4top.io/m_22588w3xh0.mp4",
+"https://g.top4top.io/m_225885lm14.mp4",
+"https://e.top4top.io/m_2258buxc30.mp4",
+"https://e.top4top.io/m_2258fvra62.mp4",
+"https://l.top4top.io/m_22588mx7k4.mp4",
+"https://g.top4top.io/m_2258zhldg1.mp4", 
+]
