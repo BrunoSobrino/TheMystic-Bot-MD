b1ace40e92cc854a989e78fc175299e22047b458
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 00:01:22 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/@prueba.js
+++ plugins/@prueba.js
@@ -1,14 +1,32 @@
-/* By https://github.com/FG98F/dylux-fg */
-import twitterDl from 'fg-twitter'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `✳️ Ejemplo : \n${usedPrefix + command} https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
-let res = await twitterDl(args[0])
-try {
-for (let result of res.download) {
-let res = result.url.replace('https://ssstwitter.com', '')
-conn.sendFile(m.chat, res, 'error.mp4', null, m)
-}} catch {
-await m.reply('✳️ Revise el link sea de un video de Twitter')
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+if (!text) throw 'Input URL'
+let res = await twitterDl(text)
+await m.reply('_In progress, please wait..._')
+for (let x = 0; x < res.media.length; x++) {
+let caption = x === 0 ? res.caption.replace(/https:\/\/t.co\/[a-zA-Z0-9]+/gi, '').trim() : ''
+await conn.sendFile(m.chat, res.media[x].url, 'error.mp4', caption, m)
 }}
-handler.command = /^(twitter|tw)$/i
+handler.command = /^((twt|twitter)(dl)?)$/i
 export default handler
+
+async function twitterDl(url) {
+let id = /twitter\.com\/[^/]+\/status\/(\d+)/.exec(url)?.[1]
+if (!id) throw 'Invalid URL'
+let res = await fetch(`https://tweetpik.com/api/tweets/${id}`)
+if (res.status !== 200) throw res.statusText
+let json = await res.json()
+if (json.media) {
+let media = []
+for (let i of json.media) {
+if (/video|animated_gif/.test(i.type)) {
+let vid = await (await fetch(`https://tweetpik.com/api/tweets/${id}/video`)).json()
+vid = vid.variants.pop()
+media.push({ url: vid.url, type: i.type })
+} else {
+media.push({ url: i.url, type: i.type })
+}}
+return {
+caption: json.text, media 
+}} else throw 'No media found' }
