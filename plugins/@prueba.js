638df19182e51c35b3e7a7bf0bf7a9bd9c85c30b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 00:11:44 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/@prueba.js
+++ plugins/@prueba.js
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-if (!text) throw 'Input URL'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚃𝚆𝙸𝚃𝚃𝙴𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw` 
 let res = await twitterDl(text)
-await m.reply('_In progress, please wait..._')
+await m.reply(global.wait)
 for (let x = 0; x < res.media.length; x++) {
-let caption = x === 0 ? res.caption.replace(/https:\/\/t.co\/[a-zA-Z0-9]+/gi, '').trim() : ''
+let caption = x === 0 ? res.caption.replace(/https:\/\/t.co\/[a-zA-Z0-9]+/gi, '*𝙰𝚀𝚄𝙸 𝙴𝚂𝚃𝙰 𝚂𝚄 𝚅𝙸𝙳𝙴𝙾*').trim() : ''
 await conn.sendFile(m.chat, res.media[x].url, 'error.mp4', caption, m)
 }}
-handler.command = /^((twt|twitter)(dl)?)$/i
+handler.command = /^((twdl|tw|twt|twitter)(dl)?)$/i
 export default handler
 
 async function twitterDl(url) {
 let id = /twitter\.com\/[^/]+\/status\/(\d+)/.exec(url)?.[1]
-if (!id) throw 'Invalid URL'
+if (!id) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝚃𝚆𝙸𝚃𝚃𝙴𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: #twitter* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw'
 let res = await fetch(`https://tweetpik.com/api/tweets/${id}`)
 if (res.status !== 200) throw res.statusText
 let json = await res.json()
@@ -29,4 +29,4 @@ media.push({ url: i.url, type: i.type })
 }}
 return {
 caption: json.text, media 
-}} else throw 'No media found' }
+}} else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*' }
