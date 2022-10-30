3e7f933ea49204fe6da9568548084a53f5009c55
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-29 23:16:45 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -24,5 +24,5 @@ await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimet
 } catch {
 await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*', m)}
 }}
-handler.command = /^getvid|yt(v|mp4)?$/i
+handler.command = /^fgmp4|dlmp4|getvid|yt(v|mp4)?$/i
 export default handler
