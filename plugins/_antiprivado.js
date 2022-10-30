9bf0aee5665a750d439c85f000fafd7464843e11
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 00:21:01 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,13 +5,13 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
        return !1
     if (!m.message)
        return !0
-    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA'))
+    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || || m.text.includes('serbot') || || m.text.includes('jadibot'))
        return !0
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     
     if (bot.antiPrivate && !isOwner && !isROwner) {
-       await m.reply(`Hola *@${m.sender.split`@`[0]}*, está prohibido hablar al privado del bot serás bloqueado.`, false, { mentions: [m.sender] })
+       await m.reply(`*Hola @${m.sender.split`@`[0]}, está prohibido hablar al privado del bot serás bloqueado.*`, false, { mentions: [m.sender] })
        await this.updateBlockStatus(m.chat, 'block')
     }
     return !1
