11a2815a0ef492594805fc545e6edc0c056fae7c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-30 23:03:01 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/@prueba.js
+++ plugins/@prueba.js
@@ -1,12 +1,1 @@
-import * as e from "fs";
-let handler = async (m, { conn, usedPrefix: t }) => {
-let n = await conn.getName(m.sender),
-l = global.owner.filter(([m, conn]) => m && conn);
-for (let [r, i] of l) {
-let o = "BEGIN:VCARD\nVERSION:3.0\nFN:" + 'BrunoSobrino' + "\nORG:The Shadow Brokers - TEAM;\nitem1.TEL;waid=5219992095479:+52 1 999 209 5479\nitem1.X-ABLabel:Creador Oficial 👑\nitem2.TEL;waid=" + r.replace(/[^0-9]/g, "") + ":+" + r.replace(/[^0-9]/g, "") + "\nitem2.X-ABLabel:\uD83E\uDD1D Due\xf1o actual 🤖\nitem3.EMAIL;type=INTERNET:theshadowbrokers133@gmail.com\nitem3.X-ABLabel:Email\nitem4.URL;Web:https://www.paypal.me/theshadowbrokers133\nitem4.ADR:;;\uD83C\uDDF5\uD83C\uDDEA México;;;;\nitem5.X-ABLabel:\nEND:VCARD";
-conn.sendMessage(m.chat, {
-contacts: {
-displayName: "BrunoSobrino",
-contacts: [{ vcard: o }]}}, { quoted: m })}}
-handler.command = /^(prueba)$/i
-export default handler
+
