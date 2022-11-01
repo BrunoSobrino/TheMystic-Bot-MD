d02f86fd30c43f60fb0293f980edd65cb41f3faf
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-31 18:31:13 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- main.js
+++ main.js
@@ -75,14 +75,15 @@ logger: P({ level: 'silent'}),
 browser: ['TheMystic-Bot','Safari','1.0.0']
 }
 
-//makeWASocket(connectionOptions)
+global.conn = makeWASocket(connectionOptions)
 /* Solucion mensajes en espera */
-global.conn = makeWASocket({ ...connectionOptions, ...opts.connectionOptions,
-getMessage: async (key) => (
-opts.store.loadMessage(/** @type {string} */(key.remoteJid), key.id) ||
-opts.store.loadMessage(/** @type {string} */(key.id)) || {}
-).message || { conversation: 'Please send messages again' },
-})
+//global.conn = makeWASocket({ ...connectionOptions, ...opts.connectionOptions,
+//getMessage: async (key) => (
+//opts.store.loadMessage(/** @type {string} */(key.remoteJid), key.id) ||
+//opts.store.loadMessage(/** @type {string} */(key.id)) || {}
+//).message || { conversation: 'Please send messages again' },
+//})
+
 conn.isInit = false
 
 if (!opts['test']) {
