47ea5e7e212addf2d7f7af86032cab039e4377ce
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-03 19:19:54 -0400

    Create _premium.js

---
+++ plugins/_premium.js
@@ -0,0 +1,10 @@
+let handler = m => m
+export async function all(m) {
+let user = global.db.data.users[m.sender]
+if (m.chat.endsWith('broadcast')) return
+if (user.premiumTime != 0 && user.premium) {
+if (new Date() * 1 >= user.premiumTime) {
+await m.reply(`*@${m.sender.split`@`[0]} ¡𝚂𝙴 𝙰𝙲𝙰𝙱𝙾 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙳𝙴 𝙿𝚁𝙴𝙼𝙸𝚄𝙼!*\n𝚂𝙸 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙾𝙱𝚃𝙴𝙽𝙴𝚁 𝚄𝙽 𝙽𝚄𝙴𝚅𝙾 𝙿𝙰𝚂𝙴 𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾\n*#pase premium*`, false, { mentions: [m.sender] })
+user.premiumTime = 0
+user.premium = false
+}}}
