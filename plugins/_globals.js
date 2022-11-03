5ab3ca494d465f1ff662c8d00ed4a7c760f9e6bb
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-03 19:46:26 -0400

    Update _globals.js

--- plugins/_globals.js
+++ plugins/_globals.js
@@ -403,7 +403,7 @@ global.fgif = {
                         'seconds': '999999999', 
                         'gifPlayback': 'true', 
                         'caption': bottime,
-                        'jpegThumbnail': fs.readFileSync('./media/menus/Menu3.jpg')
+                        'jpegThumbnail': fs.readFileSync('./Menu2.jpg')
                                }
                               }
                              }
