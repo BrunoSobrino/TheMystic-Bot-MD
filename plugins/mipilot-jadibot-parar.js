cd95d1a053c8b7e4c9600db4c5ee2ce55ed6fc13
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-04 22:17:14 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/mipilot-jadibot-parar.js
+++ plugins/mipilot-jadibot-parar.js
@@ -1,22 +1,9 @@
 let handler  = async (m, { conn }) => {
-    if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Por qué no vas directamente con el numero del Bot?', m)
-    else {
-      await conn.reply(m.chat, 'Adiós Bot :\')', m)
-      conn.ws.close()
-    }
-  }
-  handler.help = ['berhenti','stop']
-  handler.tags = ['General']
-  handler.command = /^(berhenti|stop)$/i
-  handler.owner = true
-  handler.mods = false
-  handler.premium = false
-  handler.group = false
-  handler.private = false
-  
-  handler.admin = false
-  handler.botAdmin = false
-  
-  handler.fail = null
-  
-  export default handler
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Por qué no vas directamente con el numero del Bot?', m)
+else {
+await conn.reply(m.chat, 'Adiós Bot :\')', m)
+conn.ws.close()}}
+handler.command = /^(berhenti|stop)$/i
+handler.owner = true  
+handler.fail = null
+export default handler
