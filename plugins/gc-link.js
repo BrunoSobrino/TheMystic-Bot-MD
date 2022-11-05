236ea91ad939775fe6e44b36814ccf1f14a48906
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-05 17:21:24 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -2,23 +2,7 @@ import fs from 'fs'
 let handler = async (m, { conn, args }) => {
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.sendMessage(m.chat, {text: link, "contextInfo": {
- mimetype: "image/jpeg",
- text: 'lol',
- "forwardingScore": 1000000000,
- isForwarded: true,
- sendEphemeral: true,
- "externalAdReply": {
- "title": 'lol2',
- "body": 'lol3',
- "previewType": "PHOTO",
- "thumbnailUrl": imagen4,
- "thumbnail": imagen4,
- "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD`
- }}}, { quoted: m, detectLink: true })
-  
-  
-  
+conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
