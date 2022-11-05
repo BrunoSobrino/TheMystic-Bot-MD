d9919bdac5433e34b28b0518ab4910ae4d8c3e4f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-04 22:13:16 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/virus-traba7.js
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch'
-let handler  = async (m, { conn }) => {
-let info = await fetch('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/trabas/traba4.txt').then(v => v.text());
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by 𝙲𝚛𝚕𝚜҉ꪶ͢sєrꫂ⁩* 🔥', 'status@broadcast')}
-handler.command = ['virus7', 'c7', 'binario7', 'traba7', 'crash7', 'bughole'] 
-handler.rowner = true
-handler.fail = null
-export default handler 
