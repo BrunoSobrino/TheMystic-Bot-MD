0d74aefb7049fadea141538d1912da55bcb71677
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-05 19:22:58 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,7 +1,9 @@
 let handler = async (m, { conn, args }) => {
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })}
+conn.reply(m.chat, link, m, {detectLink: true})
+//conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
+}
 handler.command = /^link(gro?up)?$/i
 handler.group = true
 handler.botAdmin = true
