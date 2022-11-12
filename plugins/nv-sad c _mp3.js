3da8f2caa4de8ad6b2ed201678d448d7b6b552fa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-11 19:45:10 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/nv-sad c _mp3.js
+++ plugins/nv-sad c _mp3.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/toma.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3` }, { quoted: m })}
+conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3` }, { quoted: m })}
 handler.command = /^(:c|c)$/i
 handler.fail = null
 handler.exp = 100
