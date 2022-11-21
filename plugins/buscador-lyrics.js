3d7a4fe8a76b4da14bdcf3c5a3342db0fd30a5b4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-21 05:31:37 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -10,8 +10,8 @@ title: result.author + result.title}))
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.thumbnail.genius) throw json
-let letratexto =`𝐓𝐈𝐓𝐔𝐋𝐎: *${result.title}*\n𝐀𝐔𝐓𝐎𝐑: *${result.author}*\n\n\n𝐋𝐄𝐓𝐑𝐀: ${result.lyrics}`.trim()
-conn.sendButton(m.chat, letratexto, `𝐔𝐑𝐋: ${result.link}\n` + wm, json.thumbnail.genius, [['🎵 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙰𝚄𝙳𝙸𝙾 🎵', `#play.1 ${text}`], ['🎥 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝚅𝙸𝙳𝙴𝙾 🎥', `#play.2 ${text}`]], m)
+let letratexto =`𝚃𝙸𝚃𝚄𝙻𝙾: *${result.title}*\n𝙰𝚄𝚃𝙾𝚁: *${result.author}*\n\n\n𝙻𝙴𝚃𝚁𝙰: ${result.lyrics}`.trim()
+conn.sendButton(m.chat, letratexto, `\n𝚄𝚁𝙻: ${result.link}\n` + wm, json.thumbnail.genius, [['🎵 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙰𝚄𝙳𝙸𝙾 🎵', `#play.1 ${text}`], ['🎥 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝚅𝙸𝙳𝙴𝙾 🎥', `#play.2 ${text}`]], m)
 } catch {
 await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')}}
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
