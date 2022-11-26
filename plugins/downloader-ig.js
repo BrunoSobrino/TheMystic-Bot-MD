8c6c90868fc1a8a1ed6232fce0a25004c56ae971
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-25 21:04:03 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,27 +1,44 @@
-/* Creado por https://github.com/FG98F */
-
+import hx from 'hxz-api'
 import fetch from 'node-fetch'
-import instagramGetUrl from 'fg-ig'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-let results = (await instagramGetUrl(args[0])).url_list[0]
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${results}`)).text()
-let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, results, 'error.mp4', txt, m)}
-handler.command =/^(instagram|ig(dl)?)$/i
-handler.dfail = null
-export default handler
-
-/*import hx from 'hxz-api'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
+import instagramGetUrl from 'instagram-url-direct'
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link` 
+try {
+await m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)
+switch (command) {        
+case "instagramdl": case "instagram": case "igdl": case "ig": 
+let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=85faf717d0545d14074659ad&url=${args[0]}`)
+let json = await human.json()
+let videoig = json.result
+let human2 = await fetch(`https://api.lolhuman.xyz/api/instagram2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)
+let json2 = await human2.json()
+if (!json.result || json.result == '') videoig = json2.result.media
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+break  
+case "instagramdl2": case "instagram2": case "igdl2": case "ig2":        
+const resultss = (await instagramGetUrl(args[0])).url_list[0]
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+break               
+case "instagramdl3": case "instagram3": case "igdl3": case "ig3":        
 hx.igdl(args[0]).then(async (r) => {
 for (let i = 0; i < r.medias.length; i++) {
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
-let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
-}})}
-handler.command = /^instagramdl|instagram|igdl|ig$/i
-handler.dfail = null
-export default handler*/
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
+await conn.sendFile(m.chat, r.medias[i].url, '', txt3, m)}})       
+break            
+case "instagramdl4": case "instagram4": case "igdl4": case "ig4":        
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl4 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt4 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+break       
+}} catch {
+await await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾, 𝚂𝙸 𝙴𝙻 𝙴𝚁𝚁𝙾𝚁 𝚂𝙸𝙶𝚄𝙴, 𝙿𝚁𝚄𝙴𝙱𝙴 𝙲𝙾𝙽 𝙾𝚃𝚁𝙰 𝙾𝙿𝙲𝙸𝙾𝙽 (${usedPrefix}ig, ${usedPrefix}ig2, ${usedPrefix}ig3, ${usedPrefix}ig4)*`)
+}}
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3|instagramdl4|instagram4|igdl4|ig4)$/i
+export default handler
