c8408b742ef2e45dc648e85dd6d0a01c5cbb0d89
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-27 13:21:15 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,18 +1,12 @@
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (m.isGroup)
-       return !1
-    if (!m.message)
-       return !0
-    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot'))
-       return !0
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    
-    if (bot.antiPrivate && !isOwner && !isROwner) {
-       await m.reply(`*Hola @${m.sender.split`@`[0]}, está prohibido hablar al privado del bot serás bloqueado.*`, false, { mentions: [m.sender] })
-       await this.updateBlockStatus(m.chat, 'block')
-    }
-    return !1
+if (m.isBaileys && m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
+let chat = global.db.data.chats[m.chat]
+let bot = global.db.data.settings[this.user.jid] || {}
+if (bot.antiPrivate && !isOwner && !isROwner) {
+await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, ᴇsᴛᴀ ᴘʀᴏʜɪʙɪᴅᴏ ʜᴀʙʟᴀʀ ᴀʟ ᴘʀɪᴠᴀᴅᴏ ᴅᴇʟ ʙᴏᴛ ᴘᴏʀ ʟᴏ ᴄᴜᴀʟ sᴇʀᴀs ʙʟᴏϙᴜᴇᴀᴅᴏ.*`, false, { mentions: [m.sender] })
+await this.updateBlockStatus(m.chat, 'block')}
+return !1
 }
