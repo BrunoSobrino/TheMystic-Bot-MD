2d82247eaee74a23d83944f015c1a168d59d4d7f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-29 03:27:46 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,13 +1,14 @@
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { text }) => {
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
+try {
 const { video, channel } = await youtubeSearch(text)
 let teks = [...video, ...channel].map(v => {
 switch (v.type) {
 case 'video': return `
 📌 *${v.title}* (${v.url})
 ⌚ 𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗: ${v.durationH}
-⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘 ${v.publishedTime}
+⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘: ${v.publishedTime}
 👁️ ${v.view} 𝚟𝚒𝚜𝚝𝚊𝚜
 `.trim()
 case 'channel': return `
@@ -18,8 +19,27 @@ case 'channel': return `
 }
 }).filter(v => v).join('\n\n========================\n\n')
 m.reply(teks)
+} catch {
+let get_result = await fetchJson(`https://api.lolhuman.xyz/api/ytsearch?apikey=85faf717d0545d14074659ad&query=${text}`)
+let get_result2 = get_result.result
+let ini_txt = ""
+for (var x of get_result2) {
+ini_txt += `📌 *${x.title}* (https://www.youtube.com/watch?v=${x.videoId})\n`
+ini_txt += `👁️ 𝚟𝚒𝚜𝚝𝚊𝚜: ${x.views}\n`
+ini_txt += `⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘: ${x.published}`
+ini_txt += `\n\n========================\n\n`
 }
+m.reply(ini_txt)
+}}   
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
 handler.command = /^yts(earch)?$/i
 export default handler
+async function fetchJson(url, options) {
+try {
+options ? options : {}
+const res = await axios({ method: 'GET', url: url, headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}, ...options })
+return res.data
+} catch (err) {
+return err
+}}
