86faf49c83bd732fd5ef7c349b3c11ca49cac135
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-11-29 03:04:45 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -1,16 +1,16 @@
-/* CREDITOS A https://github.com/FG98F */
-import axios from 'axios'
-import hx from 'hxz-api' 
-let handler = async (m, { conn, args, usedPrefix, command, text}) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`        
-hx.igstory(text).then(async (result) => {
-for (let i of result.medias) {
-if (i.url.includes("mp4")) {            
-conn.sendFile(m.chat, i.url, 'igstory.mp4', null, m)
-} else {     
-conn.sendFile(m.chat, i.url, '', '', m)
-}}});
-}
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`
+await m.reply(global.wait)    
+const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=85faf717d0545d14074659ad`)
+var anu = await res.json()
+var anuku = anu.result
+if (anuku == '') return m.reply('*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾 𝚂𝙸𝙽 𝙷𝙸𝚂𝚃𝙾𝚁𝙸𝙰𝚂*')  
+for (var i of anuku) {
+let res = await axios.head(i)
+let mime = res.headers['content-type']
+if (/image/.test(mime)) await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => { return m.reply('*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾 𝚂𝙸𝙽 𝙷𝙸𝚂𝚃𝙾𝚁𝙸𝙰𝚂*')})
+if (/video/.test(mime)) await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => { return m.reply('*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾 𝚂𝙸𝙽 𝙷𝙸𝚂𝚃𝙾𝚁𝙸𝙰𝚂*')})
+}}
 handler.help = ['igstory <username>']
 handler.tags = ['downloader']
 handler.command = ['igstory', 'ighistoria' ]
