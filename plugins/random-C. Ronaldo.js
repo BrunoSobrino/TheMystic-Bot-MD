3eb75230f6229e58ae46974d0d28d7fb86a139c5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-02 16:31:49 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = await axios("https://meme-api.herokuapp.com/gimme/Cristianoronaldo")
+let Cristiano = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/nsfw/xvid.json`)).data  
+/*let res = await axios("https://meme-api.herokuapp.com/gimme/Cristianoronaldo")
 let json = res.data
-let url = json.url
-conn.sendButton(m.chat, "*Siiiuuuuuu*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+let url = json.url*/
+let Ronaldo = Cristiano[Math.floor(Cristiano.length * Math.random())]
+conn.sendButton(m.chat, "*Siiiuuuuuu*", author, Ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo']
 handler.tags = ['internet']
 handler.command = /^(cristianoronaldo)$/i
