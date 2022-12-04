5688d18335e294d18586e5c6a6501067d4f05f74
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-04 03:56:45 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data  
-console.log(cristiano)
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)
-}
+conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['internet']
 handler.command = /^(cristianoronaldo|cr7)$/i
