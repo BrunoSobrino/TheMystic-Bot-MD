0bb1d4234696adf5c91c02b2af0b2455559a960c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-04 13:45:19 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-neko.js
+++ plugins/random-neko.js
@@ -1,11 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-/*let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
+let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
 let nek = ne.split('\n')
-let neko = await nek[Math.floor(Math.random() * nek.length)]*/
-let res = await fetch(`https://api.anythom.project.theh2so4.xyz/api/sfw/neko`)
-let json = await res.json()
-let neko = json.result  
+let neko = await nek[Math.floor(Math.random() * nek.length)]
 if (neko == '') throw 'Error'
 conn.sendButton(m.chat, 'Nyaww~ 🐾💗', wm, neko, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]],m)}
 handler.command = /^(neko)$/i
