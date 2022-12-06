f22825aa876c4b622ca00829a30a51244944a0a6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-05 20:17:24 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -6,19 +6,11 @@ let text = args.join` `
 if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m)
 let url = 'https://google.com/search?q=' + encodeURIComponent(text)
 let search = await googleIt(text)
-let msg = search.articles.map(({
-// header,
-title,
-url,
-description
-}) => {
-return `*${title}*\n_${url}_\n_${description}_`
-}).join('\n\n')
+let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
-let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).arrayBuffer()
-if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
+let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=85faf717d0545d14074659ad&url=${url}`).arrayBuffer()
 await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
-} catch (e) {
+} catch {
 m.reply(msg)
 }}
 handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
