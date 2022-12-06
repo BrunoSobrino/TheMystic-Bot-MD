47e698141d2210ceed51fab3ba124c456e2dc5eb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-05 20:38:11 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-readqrcode.js
+++ plugins/herramientas-readqrcode.js
@@ -6,7 +6,7 @@ let mime = (q.msg || q).mimetype || ''
 if (!mime) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 / 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*'
 let img = await q.download?.()
 let url = await uploadImage(img)
-let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=85faf717d0545d14074659ad&img=${url}`)
+let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
 let json = await anu.json()
 await m.reply(`*El Texto del Codigo QR Es:* ${json.result}`)}
 handler.command = /^(readqr)$/i
