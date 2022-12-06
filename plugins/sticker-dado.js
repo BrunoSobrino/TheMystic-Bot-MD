3c4d899c4b5b70055415c5aa02820c6aba819489
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-05 20:37:27 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-dado.js
+++ plugins/sticker-dado.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let dir = 'https://api.lolhuman.xyz/api/sticker/dadu?apikey=85faf717d0545d14074659ad'
+let dir = 'https://api.lolhuman.xyz/api/sticker/dadu?apikey=${lolkeysapi}'
 conn.sendFile(m.chat, dir, 'error.webp', null, m, { asSticker: true })}
 handler.command = ['dado', 'dados', 'dadu'] 
 export default handler
