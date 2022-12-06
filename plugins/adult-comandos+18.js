36ff7111fc8fc633a8d3b175aa9ed622250ef855
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-05 19:43:16 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -115,8 +115,8 @@ conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶
 if (command == 'yuri2') {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/yuri2.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-//conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
-conn.sendMessage(m.chat, { video: { url: url }, caption: `_${command}_`.trim(), gifPlayback: true }, { quoted: m })}
+conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
+//conn.sendMessage(m.chat, { video: { url: url }, caption: `_${command}_`.trim(), gifPlayback: true }, { quoted: m })}
   
   
   
