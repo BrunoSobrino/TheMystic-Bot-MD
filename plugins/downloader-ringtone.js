8ac0c5c5e31dec6a92da274046f19042df6fc3dd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-07 15:04:22 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ringtone.js
+++ plugins/downloader-ringtone.js
@@ -1,9 +1,17 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
 if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola*`
-let vn = await fetch(`https://fatiharridho.herokuapp.com/api/search/ringtone?query=${text}`)
-let x = await vn.json()
-conn.sendMessage(m.chat, { audio: { url: x.result[0].audio }, mimetype: 'audio/mp4' }, {quoted: m})
-}
+let anu = await ringtone(text)
+let result = anu[Math.floor(Math.random() * anu.length)]
+conn.sendMessage(m.chat, { audio: { url: result.audio }, fileName: result.title+'.mp3', mimetype: 'audio/mpeg' }, { quoted: m })}
 handler.command  = ['ringtone']
 export default handler
+async function ringtone(title) {
+return new Promise((resolve, reject) => {
+axios.get('https://meloboom.com/en/search/'+title).then((get) => {
+let $ = cheerio.load(get.data)
+let hasil = []
+$('#__next > main > section > div.jsx-2244708474.container > div > div > div > div:nth-child(4) > div > div > div > ul > li').each(function (a, b) {
+hasil.push({ title: $(b).find('h4').text(), source: 'https://meloboom.com/'+$(b).find('a').attr('href'), audio: $(b).find('audio').attr('src') })
+})
+resolve(hasil)})})}
