85d3bddbbef2eef8a3c69ce96635df4e27739134
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-07 15:12:49 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/random-wallprandoms.js
+++ plugins/random-wallprandoms.js
@@ -102,3 +102,16 @@ conn.sendButton(m.chat, `_${command}_`.trim(), `*◈•@${who.split("@s.whatsapp
 handler.command = ['wpmontaña', 'pubg', 'wpgaming', 'wpaesthetic', 'wprandom', 'coffee', 'pentol', 'caricatura', 'ciberespacio', 'technology', 'doraemon', 'hacker', 'planeta', 'randomprofile', 'wpaesthetic2', 'wpvehiculo', 'wallhp', 'wpmoto']
 export default handler
 
+async function wallpaper(title, page = '1') {
+return new Promise((resolve, reject) => {
+axios.get(`https://www.besthdwallpaper.com/search?CurrentPage=${page}&q=${title}`).then(({ data }) => {
+let $ = cheerio.load(data)
+let hasil = []
+$('div.grid-item').each(function (a, b) {
+hasil.push({
+title: $(b).find('div.info > a > h3').text(),
+type: $(b).find('div.info > a:nth-child(2)').text(),
+source: 'https://www.besthdwallpaper.com/'+$(b).find('div > a:nth-child(3)').attr('href'),
+image: [$(b).find('picture > img').attr('data-src') || $(b).find('picture > img').attr('src'), $(b).find('picture > source:nth-child(1)').attr('srcset'), $(b).find('picture > source:nth-child(2)').attr('srcset')]
+})})
+resolve(hasil)})})}
