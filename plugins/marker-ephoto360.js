ce7eb1e98278c54ee193e6c26c1319b12dc59eb4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-07 14:19:48 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -19,14 +19,6 @@ let res = `https://violetics.pw/api/ephoto360/christmas-snow?apikey=beta&text=${
 await conn.sendFile(m.chat, res, 'error.jpg', null, m)
 } catch {
 await conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*', m)}}
-if (command == 'mensajefalso') {
-if (!response[1]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾 𝚃𝙴𝚇𝚃𝙾, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} MysticBot|Hola como puedo ayudarte?*`  
-await conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)  
-try {  
-let res = `https://api-anubiskun.herokuapp.com/api/fakewa?name=${response[0]}&pesan=${response[1]}`
-await conn.sendFile(m.chat, res, 'error.jpg', null, m)
-} catch {
-await conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*', m)}}
 }
-handler.command = /^mensajefalso|logocorazon|logochristmas/i
+handler.command = /^logocorazon|logochristmas/i
 export default handler
