814895ed19da67f9e67c2fb48773cc524080d7aa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-07 19:44:58 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,7 +1,23 @@
 import { mediafiredl } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/pbabuzyc7i8ord5/TheMystic-Bot-MD-master_%25285%2529.zip/file*`
 try {
+let res2 = await fetch(`https://api.lolhuman.xyz/api/mediafire?apikey=${lolkeysapi}&url=${args[0]}`)
+let res3 = await res2.json()
+let res4 = res3.result
+let { filename, filetype, filesize, uploaded, link } = await res4  
+let caption2 = `
+*📓 𝙽𝙾𝙼𝙱𝚁𝙴:* ${filename}
+*📁 𝙿𝙴𝚂𝙾:* ${filesize}
+*📄 𝚃𝙸𝙿𝙾:* ${filetype}
+
+*⏳ 𝙴𝚂𝙿𝙴𝚁𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾. . . .* 
+`.trim()
+m.reply(caption2)
+await conn.sendFile(m.chat, link, filename, null, m, null, { mimetype: filetype, asDocument: true })   
+} catch {
+try { 
 let res = await mediafiredl(args[0])
 let { url, url2, filename, ext, aploud, filesize, filesizeH } = await res
 let caption = `
@@ -13,9 +29,9 @@ let caption = `
 `.trim()
 m.reply(caption)
 await conn.sendFile(m.chat, url2, filename, '', m, null, { mimetype: ext, asDocument: true })
-} catch {
+} catch {  
 await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/pbabuzyc7i8ord5/TheMystic-Bot-MD-master_%25285%2529.zip/file*')
-}}
+}}}
 handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
