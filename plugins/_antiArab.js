a58ccd5ced03400dcc3c60cd7f36e253b41deb0d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-07 20:35:51 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -1,22 +1,27 @@
-/* Creditos a https://github.com/FG98F */
-
 let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
-	
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
-
-if (isBotAdmin && chat.antiArab) {
+let bot = global.db.data.settings[conn.user.jid] || {}
+if (isBotAdmin && chat.antiArab && !isAdmin && !isOwner && !isROwner && bot.restrict) {
 		
 if (m.sender.startsWith('212' || '212')) {
-global.db.data.users[m.sender].banned = true
-m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return 
+}
    
 if (m.sender.startsWith('265' || '265')) {
-global.db.data.users[m.sender].banned = true
-m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 
+	
+if (m.sender.startsWith('92' || '92')) {
+m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} 	
    
 }}
 export default handler
