8a2a93eaca793badda1da5cc7c8d9beb5b7d27b4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-10 02:17:49 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -1,4 +1,5 @@
-import { sticker } from '../lib/sticker.js'
+import { sticker, addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
 import fetch from 'node-fetch'
 
 let handler = m => m
@@ -15,14 +16,16 @@ if (/webp/g.test(mime)) return
 if (/image/g.test(mime)) {
 let img = await q.download?.()
 if (!img) return
-stiker = await sticker(img, false, packname, author)
+stiker = await createSticker(img, false, packname || global.packname, author || global.author)
+//stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
 if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
 let img = await q.download()
 if (!img) return
-stiker = await sticker(img, false, packname, author)
+stiker = await mp4ToWebp(img, { pack: packname || global.packname, author: author || global.author })
+//stiker = await sticker(img, false, packname, author)
 } else if (m.text.split(/\n| /i)[0]) {
-if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author)
+if (isUrl(m.text)) stiker = await createSticker(false, args[0], '', author, 20)
 else return
 }
 if (stiker) {
@@ -35,3 +38,48 @@ export default handler
 
 const isUrl = (text) => {
 return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
+async function createSticker(img, url, packName, authorName, quality) {
+let stickerMetadata = { type: 'full', pack: packName, author: authorName, quality }
+return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()}
+async function mp4ToWebp(file, stickerMetadata) {
+if (stickerMetadata) {
+if (!stickerMetadata.pack) stickerMetadata.pack = '‎'
+if (!stickerMetadata.author) stickerMetadata.author = '‎'
+if (!stickerMetadata.crop) stickerMetadata.crop = false
+} else if (!stickerMetadata) { stickerMetadata = { pack: '‎', author: '‎', crop: false }}
+let getBase64 = file.toString('base64')
+const Format = { file: `data:video/mp4;base64,${getBase64}`, processOptions: { crop: stickerMetadata?.crop, startTime: '00:00:00.0', endTime: '00:00:7.0', loop: 0
+}, stickerMetadata: { ...stickerMetadata },
+sessionInfo: {
+WA_VERSION: '2.2106.5',
+PAGE_UA: 'WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+WA_AUTOMATE_VERSION: '3.6.10 UPDATE AVAILABLE: 3.6.11',
+BROWSER_VERSION: 'HeadlessChrome/88.0.4324.190',
+OS: 'Windows Server 2016',
+START_TS: 1614310326309,
+NUM: '6247',
+LAUNCH_TIME_MS: 7934,
+PHONE_VERSION: '2.20.205.16'
+},
+config: {
+sessionId: 'session',
+headless: true,
+qrTimeout: 20,
+authTimeout: 0,
+cacheEnabled: false,
+useChrome: true,
+killProcessOnBrowserClose: true,
+throwErrorOnTosBlock: false,
+chromiumArgs: [
+'--no-sandbox',
+'--disable-setuid-sandbox',
+'--aggressive-cache-discard',
+'--disable-cache',
+'--disable-application-cache',
+'--disable-offline-load-stale-cache',
+'--disk-cache-size=0'
+],
+executablePath: 'C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe',
+skipBrokenMethodsCheck: true,
+stickerServerEndpoint: true
+}}
