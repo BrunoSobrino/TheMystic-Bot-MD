1183024c3f0207e3a087152f9176f6e14aa28ba1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-10 01:22:36 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,18 +1,15 @@
 import gtts from 'node-gtts'
 import { readFileSync, unlinkSync } from 'fs'
 import { join } from 'path'
-
 const defaultLang = 'es'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
 let lang = args[0]
 let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) {
-lang = defaultLang
+if ((args[0] || '').length !== 2) { 
+lang = defaultLang 
 text = args.join(' ')
 }
 if (!text && m.quoted?.text) text = m.quoted.text
-
 let res
 try { res = await tts(text, lang) }
 catch (e) {
@@ -21,7 +18,8 @@ text = args.join(' ')
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙰 𝙽𝙾𝚃𝙰 𝙳𝙴 𝚅𝙾𝚉, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} es Hola Mundo*`
 res = await tts(text, defaultLang)
 } finally {
-if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+if (res) conn.sendMessage(m.chat, { audio: { url: res }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })    
+//conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
 }}
 handler.help = ['tts <lang> <teks>']
 handler.tags = ['tools']
