7661e3f6477f35e250cdabcdf7b29df797272db4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-17 11:09:12 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,7 @@
-let handler = async (m, { conn, args, command }) => {
-await m.reply('*Adios a todos, el Bot se despide! (≧ω≦)ゞ*') 
-await  conn.groupLeave(m.chat)}
+let handler = async (m, { conn, text, command }) => {
+let id = text ? text : m.chat  
+await conn.reply(id, '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*') 
+await conn.groupLeave(id)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
 handler.rowner = true
