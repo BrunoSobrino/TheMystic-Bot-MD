766d3307f56c99655cff22d7d2c24f555df6480c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-17 10:22:57 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -7,9 +7,12 @@ let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
 let json = await res.json()
 m.reply(json.success)
 } catch {
-let res2 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${text}`)  
+let res2 = await fetch(`https://api.phamvandien.xyz/sim?type=ask&ask=${text}`)  
 let json2 = await res2.json()
-let result = json2.result
+let result = json2.answer  
+/*let res2 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${text}`)  
+let json2 = await res2.json()
+let result = json2.result*/
 let lol = await translate(`${result}`, { to: 'es', autoCorrect: true })
 m.reply(lol.text)
 }}
