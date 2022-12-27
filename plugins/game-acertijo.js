e448e9af121e22a9cbc1230d3b69b7c2e2a48206
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-27 17:15:59 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/game-acertijo.js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+conn.tekateki = conn.tekateki ? conn.tekateki : {}
+let id = m.chat
+if (id in conn.tekateki) {
+conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
+throw false
+}
+let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
+let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+let _clue = json.response
+let clue = _clue.replace(/[A-Za-z]/g, '_')
+let caption = `
+ⷮ *${json.question}*
+*• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*• Bono:* +${poin} Exp
+`.trim()
+conn.tekateki[id] = [
+await conn.reply(m.chat, caption, m), json, 
+poin,
+setTimeout(async () => {
+if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
+delete conn.tekateki[id]
+}, timeout)]}
+handler.help = ['acertijo']
+handler.tags = ['game']
+handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i
+export default handler
