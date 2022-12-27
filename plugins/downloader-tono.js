2456eeb43f68bb30c15e8ce403986c21d3676437
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-27 17:48:25 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-tono.js
+++ plugins/downloader-tono.js
@@ -2,15 +2,16 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 if (!text) throw `Contoh:
 ${usedPrefix + command} 10`
-let json = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
-let jsons = await json.json()
-let caption = '*⎔┉━「 Search 」━┉⎔*'
+let aa = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
+let jsons = await aa.json()
+console.log(jsons)
+//conn.sendMessage(m.chat, { document: { url: n2 }, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m})
+/*let caption = '*⎔┉━「 Search 」━┉⎔*'
 for (let x of jsons.results) {
 caption += `
 *Name :* ${x.name}
 *Sound :* ${x.sound}
-`}
-return m.reply(caption)
+`}*/
 }
 handler.command = ['sfx', 'ringtone2', 'tono']
 export default handler
