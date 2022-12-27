ce4085eea6e2a5f0f21d32e361f815dd04f5715f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-27 17:51:03 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-tono.js
+++ plugins/downloader-tono.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 if (!text) throw `Contoh:
 ${usedPrefix + command} 10`
-let aa = await fetch(`http://www.myinstants.com/api/v1/instants/?format=json&page=${text}`)
+let aa = await fetch(`https://www.myinstants.com/api/v1/instants/${text}/?format=json`)
 let jsons = await aa.json()
 console.log(jsons)
 //conn.sendMessage(m.chat, { document: { url: n2 }, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m})
