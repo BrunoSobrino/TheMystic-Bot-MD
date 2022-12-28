25e53c729a6b79ab253b5b9f0ac9c195d63f78c0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-28 11:31:57 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -4,6 +4,25 @@ import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`
 await m.reply(global.wait)
+const res2 = await igstory(args[0])
+let res3 = await axios.head(url)
+let mime = res3.headers['content-type']
+if (/image/.test(mime)) await conn.sendFile(m.chat, res2.url, 'error.jpg', null, m)
+if (/video/.test(mime)) await conn.sendFile(m.chat, res2.url, 'error.mp4', null, m)
+//} catch {
+//throw `*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾 𝚂𝙸𝙽 𝙷𝙸𝚂𝚃𝙾𝚁𝙸𝙰𝚂*`
+}//}}
+handler.help = ['instagramstory'].map(v => v + ' <username>')
+handler.tags = ['downloader']
+handler.command = /^((igs|instagrams)(tory)?(dl)?(downloa?d(er)?)?)$/i
+export default handler
+
+/*import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`
+await m.reply(global.wait)
 try {
 const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`)
 var anu = await res.json()
@@ -18,7 +37,6 @@ try {
 const res2 = await igstory(args[0])
 let res3 = await axios.head(url)
 let mime = res3.headers['content-type']
-console.log(res2)
 //for ( const { downloadUrl, url, preview, type, fileType } of res2 )
 if (/image/.test(mime)) await conn.sendFile(m.chat, res2.url, 'error.jpg', null, m)
 if (/video/.test(mime)) await conn.sendFile(m.chat, res2.url, 'error.mp4', null, m)
@@ -29,7 +47,7 @@ throw `*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾
 handler.help = ['instagramstory'].map(v => v + ' <username>')
 handler.tags = ['downloader']
 handler.command = /^((igs|instagrams)(tory)?(dl)?(downloa?d(er)?)?)$/i
-export default handler
+export default handler*/
 
 async function igstory(username) {
 return new Promise(async (resolve, reject) => {
