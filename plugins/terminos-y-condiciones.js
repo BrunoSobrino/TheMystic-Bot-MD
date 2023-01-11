1f3f749116287a21ee0a7bcebd51bc9950b16aa8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-11 01:39:15 -0400

    Update terminos-y-condiciones.js

--- plugins/terminos-y-condiciones.js
+++ plugins/terminos-y-condiciones.js
@@ -1,6 +1,9 @@
 let handler = async (m, { conn }) => {
 m.reply(global.terminos)
 }
+handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i
+handler.command = new RegExp
+
 export default handler
 
 global.terminos = `
