45557a6d127e1d1481dd0a60b2a117660a9737f4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-01-15 13:22:49 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-pista.js
+++ plugins/game-pista.js
@@ -5,7 +5,7 @@ conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {}
 let id = m.chat
 if (!(id in conn.tebaklagu)) throw false
 let json = conn.tebaklagu[id][1]
-let nya = json.judul
+let nya = json.jawaban
 let nyanya = nya.replace(/[bcdfghjklmnñpqrstvwxyzBCDEFGHJKLMNÑPQRSTVWXYZ]/g, '_')
 m.reply('' + nyanya + '')
 }
