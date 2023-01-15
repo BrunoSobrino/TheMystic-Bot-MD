b776cabb290f9e38d068371d79da2d5bdf8de701
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-01-15 13:23:58 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-cancion.js
+++ plugins/game-cancion.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
 let timeout = 60000
 let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
@@ -8,10 +9,12 @@ if (id in conn.tebaklagu) {
 conn.reply(m.chat, 'Todavía hay canciones sin respuesta en este chat.', conn.tebaklagu[id][0])
 throw false
 } //5LTV57azwaid7dXfz5fzJu
-let res = await fetch(global.API('xteam', '/game/tebaklagu/', { id: '0ISD8mk5kiv1YC5884lISM' }, 'APIKEY'))
-if (res.status !== 200) throw await res.text()
-let result = await res.json()
-let json = result.result
+let res = await fetchJson(`https://fatiharridho.github.io/tebaklagu.json`)
+let json = res[Math.floor(Math.random() * res.length)]    
+//let res = await fetch(global.API('xteam', '/game/tebaklagu/', { id: '0ISD8mk5kiv1YC5884lISM' }, 'APIKEY'))
+//if (res !== 200) throw 'Error'
+//let result = await res.json()
+//let json = result.result
 let caption = `
 ADIVINA EL TITULO DE LA CANCION
 Tiempo ${(timeout / 1000).toFixed(2)} segundos
@@ -22,14 +25,22 @@ conn.tebaklagu[id] = [
 await m.reply(caption),
 json, poin,
 setTimeout(() => {
-if (conn.tebaklagu[id]) conn.reply(m.chat, `Se acabó el tiempo!\nLa respuesta es ${json.judul}`, conn.tebaklagu[id][0])
+if (conn.tebaklagu[id]) conn.reply(m.chat, `Se acabó el tiempo!\nLa respuesta es ${json.jawaban}`, conn.tebaklagu[id][0])
 delete conn.tebaklagu[id]
 }, timeout)
 ]
-let aa = await conn.sendMessage(m.chat, { audio: { url: json.preview }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
-if (!aa) return conn.sendFile(m.chat, json.preview, 'coba-lagi.mp3', '', m)
+let aa = await conn.sendMessage(m.chat, { audio: { url: json.link_song }, fileName: `error.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
+if (!aa) return conn.sendFile(m.chat, json.link_song, 'coba-lagi.mp3', '', m)
 }
 handler.help = ['tebaklagu']
 handler.tags = ['game']
 handler.command = /^cancion|canción$/i
 export default handler
+async function fetchJson(url, options) {
+try {
+options ? options : {}
+const res = await axios({ method: 'GET', url: url, headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}, ...options })
+return res.data
+} catch (err) {
+return err
+}}
