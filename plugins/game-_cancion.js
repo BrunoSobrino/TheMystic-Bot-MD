4b2a55ebe617b361c58960bab2ffd5f8e02b9c93
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-01-15 13:25:32 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -1,6 +1,4 @@
-/* Created by https://github.com/unptoadrih15 */
-
-import similarity from'similarity'
+import similarity from 'similarity'
 const threshold = 0.72
 let handler = {
 async before(m) {
@@ -10,12 +8,12 @@ this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
 if (!(id in this.tebaklagu)) return m.reply('El juego ha terminado')
 if (m.quoted.id == this.tebaklagu[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
-if (m.text.toLowerCase() == json.judul.toLowerCase().trim()) {
+if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
 m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} XP`)
 clearTimeout(this.tebaklagu[id][3])
 delete this.tebaklagu[id]
-} else if (similarity(m.text.toLowerCase(), json.judul.toLowerCase().trim()) >= threshold) m.reply(`Casii!`)
+} else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`)
 else m.reply(`❌Incorrecto!`)
 }
 return !0
