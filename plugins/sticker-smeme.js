6765cdefb42bb38c04c0346efbc4c5063b76b3d6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-01-15 11:45:14 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/sticker-smeme.js
@@ -0,0 +1,21 @@
+import uploadImage from '../lib/uploadImage.js'
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let [atas, bawah] = text.split`|`
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!mime) throw `balas gambar dengan perintah\n\n${usedPrefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    let img = await q.download()
+    let url = await uploadImage(img)
+    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
+    let stiker = await sticker(false, meme, global.packname, global.author)
+    if (stiker) await conn.sendFile(m.chat, stiker, '', author, m, '', { asSticker: 1 })
+}
+handler.help = ['smeme <teks atas>|<teks bawah>']
+handler.tags = ['tools']
+handler.command = /^(smeme)$/i
+
+handler.limit = true
+
+export default handler
