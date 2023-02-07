3c5ff017fcbb881cb33a92a6ca4783cb6e2174c6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-06 22:24:30 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch'
 let handler = m => m
 handler.before = async (m) => {
+try {   
 let chat = global.db.data.chats[m.chat]
-if (chat.simi) {
-try {  
+if (chat.simi) { 
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
-let data = await ressimi.json() 
-let resultf = `${data.success}`  
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.' || !ressimi) return
-await m.reply(resultf) 
-} catch {
-throw '*Error en la IA / API de simsimi*'  
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
+let data = await ressimi.json();
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return                                                                                                 
+await m.reply(data.success)
 return !0 }
-return true }
+return true 
+} catch {
+throw '*Error en la IA / API de simsimi*'}}
 export default handler
