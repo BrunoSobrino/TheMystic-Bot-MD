43bc16d93e1a2730cc094ba8948ee40217fd904f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-06 21:50:00 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -8,7 +8,7 @@ if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
 let data = await ressimi.json();
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return => {
-let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
+let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${m.text}&user=user-unique-id`)
 let hasil = await tiores.json()
 await m.reply(`${hasil.result}`.trim())  
 }
