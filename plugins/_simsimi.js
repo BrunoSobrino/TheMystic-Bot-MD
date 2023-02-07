e19d39cf517b8f72d9d31c252c05af2946c455bd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-06 22:20:24 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -3,23 +3,15 @@ let handler = m => m
 handler.before = async (m) => {
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
+try {  
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
 let data = await ressimi.json() 
-let resultf = data.success  
-let aa1 = await fetchJson(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${m.text}&user=user-unique-id`)
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.' || !ressimi) resultf = aa1.result
-await m.reply(resultf)      
-return !0
-}
-return true
-}
+let resultf = `${data.success}`  
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.' || !ressimi) return
+await m.reply(resultf) 
+} catch {
+throw '*Error en la IA / API de simsimi*'  
+return !0 }
+return true }
 export default handler
-async function fetchJson(url, options) {
-try {
-options ? options : {}
-const res = await axios({ method: 'GET', url: url, headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}, ...options })
-return res.data
-} catch (err) {
-return err
-}}
