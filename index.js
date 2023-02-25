8c016b23539e2f821379200bc628ec135d2b2666
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-02-25 02:19:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('AsakuraMao\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Colapsus | Benja`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
