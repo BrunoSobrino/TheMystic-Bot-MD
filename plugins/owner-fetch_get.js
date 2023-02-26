a16def5239373f59b22f08425e7f3ed854a60b0e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-26 16:54:00 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/owner-fetch_get.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+import { format } from 'util'
+let handler = async (m, { text }) => {
+    if (!/^https?:\/\//.test(text)) throw 'Awali *URL* dengan http:// atau https://'
+    let _url = new URL(text)
+    let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
+    let res = await fetch(url)
+    if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+        // delete res
+        throw `Content-Length: ${res.headers.get('content-length')}`
+    }
+    if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+    let txt = await res.buffer()
+    try {
+        txt = format(JSON.parse(txt + ''))
+    } catch (e) {
+        txt = txt + ''
+    } finally {
+        m.reply(txt.slice(0, 65536) + '')
+    }
+}
+handler.help = ['fetch', 'get'].map(v => v + ' <url>')
+handler.tags = ['internet']
+handler.command = /^(fetch|get)$/i
+handler.rowner = true
+export default handler
