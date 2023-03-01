09fd5a7db0ef5fb36bf3370729f994adbd2e7341
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-28 21:42:25 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -8,12 +8,13 @@ if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
 let textodem = m.text  
 try {
 let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`)
-let data = await ressimi.json();
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`)
+let data = await ressimi.json();                                                             
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
 await m.reply(data.success)
 } catch {
+/* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
 if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello')
-if (textodem.includes('hola')) textodem = textodem.replace('hola', 'Hello')
+if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello')
 if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO')    
 let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + textodem)
 let resu = await reis.json()  
@@ -28,20 +29,3 @@ return !0
 return true
 }
 export default handler
-
-/*import fetch from 'node-fetch'
-let handler = m => m
-
-handler.before = async (m) => {
-let chat = global.db.data.chats[m.chat]
-if (chat.simi) {
-if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
-let data = await ressimi.json();
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return 0                                                                                                
-await m.reply(data.success)
-return !0
-}
-return true
-}
-export default handler*/
