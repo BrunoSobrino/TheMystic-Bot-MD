10edd4c5c2726bc1b8f5a44a4baa87331c2cb022
Leo-cyber-sys <125781107+Leo-cyber-sys@users.noreply.github.com>
2023-03-02 12:35:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
+  ['51912340597', '👑 Mystic - Creador 👑', true],
+  ['51979687092', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
