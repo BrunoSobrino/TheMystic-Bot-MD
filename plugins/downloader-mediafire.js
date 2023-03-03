f47ed8829d6d1ad21e084a1b2b356729effa5be2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-03 08:46:03 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import fetch from 'node-fetch'
+import cheerio from 'cheerio'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
    if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/pbabuzyc7i8ord5/TheMystic-Bot-MD-master_%25285%2529.zip/file*`
