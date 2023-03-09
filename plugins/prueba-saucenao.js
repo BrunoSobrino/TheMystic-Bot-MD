6060e1a56894699c281495bc96916b3a2daafb67
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-09 05:23:26 -0300

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -1,30 +1,62 @@
 //Código creado por https://github.com/GataNina-Li || @gata_dios
 
+import fs from 'fs'
 import axios from 'axios'
 import fetch from "node-fetch"
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+import formData from 'form-data'
+
 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-const api_key = '45e67c4cbc3d784261ffc83806b5a1d7e3bd09ae'
 
-try {
+try {   
 let url
 let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+const urlRegex = /\.(jpg|jpeg|png)$/i;
+const pageUrlRegex = /^https?:\/\/[^\s/$.?#].[^\s]*$/i
 
-if (text) {
+if (pageUrlRegex.test(text) && urlRegex.test(text)) {
 url = text
     
 } else if (m.quoted && /image\/(png|jpe?g)/.test(mime)) {
 let media = await q.download()
 url = await uploadImage(media)
     
+} else if (m.quoted && /image\/webp/.test(mime)) {
+let media = await q.download()
+url = await webp2png(media)
+    
 } else {
-return m.reply('Ingrese un enlace o responda al mensaje con una imagen en formato PNG o JPG o JPEG.');
+return m.reply('Ingrese un enlace o responda al mensaje con una imagen en formato PNG o JPG o JPEG.')
 }
 
-const response = await axios.get(`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=6&api_key=${api_key}&url=${encodeURIComponent(url)}`)
+const apiKeys = ["45e67c4cbc3d784261ffc83806b5a1d7e3bd09ae"
+//"d3a88baf236200c2ae23f31039e599c252034be8",
+//"a74012c56b54b8d36d2675e12b1a216809c353fe",
+//"9812eb9464efa1201c69e5592ba0c74e7edd95e8",
+//"2e7da9f5e70c65f2885b07d48595ba03c4be2ba7"
+]
+
+let response;
+let success = false;
+
+for (let i = 0; i < apiKeys.length; i++) {
+const apiKey = apiKeys[i]
+try {
+response = await axios.get(`https://saucenao.com/search.php?db=999&output_type=2&testmode=1&numres=6&api_key=${apiKey}&url=${encodeURIComponent(url)}`);
+success = true;
+break;
+} catch (error) {
+//console.error(error);
+}}
+if (!success) {
+m.reply("Todas las solicitudes fallaron. No se pudo encontrar una respuesta exitosa.")
+return 
+}
+
 const results = response.data.results;
 const primerResultado = results[0]
     
@@ -124,11 +156,13 @@ await conn.sendButton(m.chat, `*Número de resultados:* ${results.length}
 • ${primerResultado.data.ext_urls}
 
 *Autor*
-• ${primerResultado.data.member_name === undefined ? 'No encontrado' : primerResultado.data.member_name}\n`,  `*◎ R E S U L T A D O  E N  B R U T O*
+• ${primerResultado.data.member_name === undefined ? 'No encontrado' : primerResultado.data.member_name}\n`,  `*◎ I N F O  A D I C C I O N A L*
 
 ${resultadoEnBruto}`.trim(), url, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, frep)
-} catch (error) {
-console.log(error)}
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
 }
 
 handler.command = /^sauce$/i
