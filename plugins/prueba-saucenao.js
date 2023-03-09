1f51268596173d5b61e54a1963f9220c670c3a35
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-09 05:31:21 -0300

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -33,12 +33,8 @@ url = await webp2png(media)
 return m.reply('Ingrese un enlace o responda al mensaje con una imagen en formato PNG o JPG o JPEG.')
 }
 
-const apiKeys = ["45e67c4cbc3d784261ffc83806b5a1d7e3bd09ae"
-//"d3a88baf236200c2ae23f31039e599c252034be8",
-//"a74012c56b54b8d36d2675e12b1a216809c353fe",
-//"9812eb9464efa1201c69e5592ba0c74e7edd95e8",
-//"2e7da9f5e70c65f2885b07d48595ba03c4be2ba7"
-]
+const apiKeys = ["45e67c4cbc3d784261ffc83806b5a1d7e3bd09ae", "d3a88baf236200c2ae23f31039e599c252034be8", "a74012c56b54b8d36d2675e12b1a216809c353fe",
+"9812eb9464efa1201c69e5592ba0c74e7edd95e8", "2e7da9f5e70c65f2885b07d48595ba03c4be2ba7"]
 
 let response;
 let success = false;
