a494ff3fd5ce95255670f0f8e002aa525a3face5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-12 01:17:18 -0600

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,18 +1,9 @@
-import cheerio from 'cheerio'
-import axios from 'axios'
 let handler  = async (m, { conn, usedPrefix: prefix, command, text }) => {
-//try {
+try {
 switch(command) {
 case 'modapk': case 'apkmod':
 if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`        
-//const data2 = await fetchJson('https://api.akuari.my.id/search/searchmod2?query=' + text)
-const daaaaa = await searchApk(text)
-//console.log(daaaaa)
-const daaaaa2 = await getApk(daaaaa.result[0].url)
-console.log(daaaaa.result[0].url_download)
-console.log(daaaaa2)
-
-const data2 = await fetchJson('https://api.akuari.my.id/search/searchmod?query=' + text)
+const data2 = await fetchJson('https://api.akuari.my.id/search/searchmod2?query=' + text)
 global.fetchJson = fetchJson
 const data = data2.respon
 if (data.length < 1) return await  conn.sendMessage(m.chat, { text: '*[❗] 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝚁 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺*' }, { quoted: m } )
@@ -26,16 +17,15 @@ break
 case 'dapk2': 
 if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙻𝙸𝙽𝙺 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙰𝙿𝙺, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${prefix + command}* https://rexdl.com/android/minecraft-pocket-edition-apk-download1.html/`     
 await conn.reply(m.chat, global.wait, m)
-//let data5 = await fetchJson('https://api.akuari.my.id/downloader/dlmod?link=' + text)
-let data5 = await fetchJson('https://api.akuari.my.id/downloader/dlmod?link=' + text)    
+let data5 = await fetchJson('https://api.akuari.my.id/downloader/dlmod2?link=' + text)
 if ( data5.respon.size.replace(' MB' , '') > 200) return await conn.sendMessage(m.chat, { text: '*[ ⛔ ] ᴇʟ ᴀʀᴄʜɪᴠᴏ ᴇs ᴅᴇᴍᴀsɪᴀᴅᴏ ᴘᴇsᴀᴅᴏ*' }, { quoted: m } )
 if ( data5.respon.size.includes('GB')) return await conn.sendMessage(m.chat, { text: '*[ ⛔ ] ᴇʟ ᴀʀᴄʜɪᴠᴏ ᴇs ᴅᴇᴍᴀsɪᴀᴅᴏ ᴘᴇsᴀᴅᴏ*' }, { quoted: m } )
-const apk5 = await conn.sendMessage(m.chat, { document: { url: data5.respon.linkdl /*respon.download[0].url*/ }, mimetype: 'application/vnd.android.package-archive', fileName: 'APK' + '.apk', caption: null }, { quoted: m })   
+const apk5 = await conn.sendMessage(m.chat, { document: { url: data5.respon.download[0].url }, mimetype: 'application/vnd.android.package-archive', fileName: data5.respon.title + '.apk', caption: null }, { quoted: m })   
 break        
 }
-//} catch {
-//throw `*[❗] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽 𝙼𝙾𝙳 𝙰𝙿𝙺 𝙲𝙾𝙽 𝙴𝚂𝙴 𝙽𝙾𝙼𝙱𝚁𝙴 𝙾 𝙻𝙰 𝙰𝙿𝙸 (𝙿𝙰𝙶𝙸𝙽𝙰) 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰*`
-}//}    
+} catch {
+throw `*[❗] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽 𝙼𝙾𝙳 𝙰𝙿𝙺 𝙲𝙾𝙽 𝙴𝚂𝙴 𝙽𝙾𝙼𝙱𝚁𝙴 𝙾 𝙻𝙰 𝙰𝙿𝙸 (𝙿𝙰𝙶𝙸𝙽𝙰) 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰*`
+}}    
 handler.command = /^(apkmod|modapk|dapk2)$/i
 export default handler
 async function fetchJson(url, options) {
@@ -46,88 +36,3 @@ return res.data
 } catch (err) {
 return err
 }}
-
-async function getApk(link) {
-	return new Promise((resolve) => {
-		axios.get(link)
-			.then(({
-				data
-			}) => {
-				const $ = cheerio.load(data)
-				const link = [];
-				const url = [];
-				const link_name = [];
-				const judul = $('#page > div > div > div > section > div:nth-child(2) > article > div > h1.post-title').text();
-				const plink = $('#page > div > div > div > section > div:nth-child(2) > center:nth-child(3) > h2 > span > a').attr('href')
-				axios.get(plink)
-					.then(({
-						data
-					}) => {
-						const $$ = cheerio.load(data)
-						$$('#dlbox > ul.dl > a > li > span').each(function(a, b) {
-							deta = $$(b).text();
-							link_name.push(deta)
-						})
-						$$('#dlbox > ul.dl > a').each(function(a, b) {
-							url.push($$(b).attr('href'))
-						})
-						for (let i = 0; i < link_name.length; i++) {
-							link.push({
-								link_name: link_name[i],
-								url: url[i]
-							})
-						}
-						resolve({
-							creator: 'Fajar Ihsana',
-							judul: judul,
-							update_date: $$('#dlbox > ul.dl-list > li.dl-update > span:nth-child(2)').text(),
-							version: $$('#dlbox > ul.dl-list > li.dl-version > span:nth-child(2)').text(),
-							size: $$('#dlbox > ul.dl-list > li.dl-size > span:nth-child(2)').text(),
-							download: link
-						})
-					})
-			})
-	})
-}
-
-async function searchApk(apkname) {
-     return new Promise((resolve, reject) => {
-          axios.get(`https://rexdl.com/?s=${apkname}`)
-               .then(({ data }) => {
-                    const $ = cheerio.load(data)
-                    let name = []
-                    let url = []
-                    let url_download = []
-                    let thumb = []
-                    let desc = []
-                    $('h2.post-title > a').get().map((rest) => {
-                         name.push($(rest).text())
-                    })
-                    $('div > div.post-thumbnail > a').get().map((rest) => {
-                         url.push($(rest).attr('href'))
-                    })
-                    $('div > div.post-thumbnail > a').get().map((rest) => {
-                         url_download.push('https://rexdlfile.com/index.php?id=' + $(rest).attr('href').split('/')[4].replace('.html', ''))
-                    })
-                    $('div > div.post-thumbnail > a > img').get().map((rest) => {
-                         thumb.push($(rest).attr('data-src'))
-                    })
-                    $('div.entry.excerpt > p').get().map((rest) => {
-                         desc.push($(rest).text())
-                    })
-                    let result = []
-                    for (let i = 0; i < name.length; i++) {
-                         result.push({
-                              title: name[i],
-                              thumb: thumb[i],
-                              url: url[i],
-                              url_download: url_download[i],
-                              desc: desc[i]
-                         })
-                    }
-                    resolve({
-                        result: result
-                    })
-               }).catch(reject)
-     })
-}
