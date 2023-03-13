1dff36d9cad9263f3bc262d686f9ad1004472121
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-13 05:25:49 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -20,46 +20,46 @@ astro = 'papel'
 }
 if (text == astro) {
 global.db.data.users[m.sender].exp += 500
-m.reply(`🔰 Empate!\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +500 XP*`)
+m.reply(`*🔰 Empate!*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +500 XP*`)
 } else if (text == 'papel') {
 if (astro == 'piedra') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`🥳 Tú ganas! 🎉\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +1000 XP*`)
+m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`☠️ Tú pierdes! ❌\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n❌ Premio -300 XP*`)
+m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`)
 }
 } else if (text == 'tijera') {
 if (astro == 'papel') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`🥳 Tú ganas! 🎉\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +1000 XP*`)
+m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`☠️ Tú pierdes! ❌\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n❌ Premio -300 XP*`)
+m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`)
 }
 } else if (text == 'tijera') {
 if (astro == 'papel') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`🥳 Tú ganas! 🎉\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +1000 XP*`)
+m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`☠️ Tú pierdes! ❌\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n❌ Premio -300 XP*`)
+m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`)
 }
 } else if (text == 'papel') {
 if (astro == 'piedra') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`🥳 Tú ganas! 🎉\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +1000 XP*`)
+m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`☠️ Tú pierdes! ❌\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n❌ Premio -300 XP*`)
+m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`)
 }
 } else if (text == 'piedra') {
 if (astro == 'tijera') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`🥳 Tú ganas! 🎉\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n🎁 Premio +1000 XP*`)
+m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Premio +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`☠️ Tú pierdes! ❌\n\n*👉🏻 Tu: ${text}\n👉🏻 El Bot: ${astro}\n❌ Premio -300 XP*`)
+m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Tu: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Premio -300 XP*`)
 }}
 global.db.data.users[m.sender].wait = new Date * 1
 }
