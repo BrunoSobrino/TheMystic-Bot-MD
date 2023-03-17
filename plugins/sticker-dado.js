f4cf37eb1bb97b8dbd33b772629da747fffcd016
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-17 16:29:46 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-dado.js
+++ plugins/sticker-dado.js
@@ -1,18 +1,17 @@
-import { sticker } from '../lib/sticker.js'
+/*import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn }) => {
 const dir = ['https://www.random.org/dice/dice1.png', 'https://www.random.org/dice/dice2.png', 'https://www.random.org/dice/dice3.png', 'https://www.random.org/dice/dice4.png', 'https://www.random.org/dice/dice5.png', 'https://www.random.org/dice/dice6.png'];  
 let img = dir[Math.floor(Math.random() * dir.length)]
-//let stiker = await sticker(img, false, global.packname, global.author) 
-let stiker = sticker(false, img, global.packname, global.author)
+let stiker = await sticker(img, false, global.packname, global.author) 
 await conn.sendFile(m.chat, stiker, 'error.webp', '', m, { asSticker: true })
 }
 handler.command = ['dado', 'dados', 'dadu'] 
-export default handler
+export default handler*/
 
 
-/*let handler = async (m, { conn }) => {
+let handler = async (m, { conn }) => {
 conn.sendFile(m.chat, `https://violetics.pw/api/random/dadu?apikey=beta`, 'error.webp', '', m, { asSticker: true })
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/sticker/dadu?apikey=${lolkeysapi}`, 'error.webp', '', m, { asSticker: true })  
+//conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/sticker/dadu?apikey=${lolkeysapi}`, 'error.webp', '', m, { asSticker: true })  
 }
 handler.command = ['dado', 'dados', 'dadu'] 
-export default handler*/
+export default handler
