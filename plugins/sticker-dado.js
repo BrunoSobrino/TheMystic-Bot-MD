3daf113e4903294e7d35833f2268d2c91e704fa9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-17 16:27:14 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-dado.js
+++ plugins/sticker-dado.js
@@ -2,7 +2,8 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn }) => {
 const dir = ['https://www.random.org/dice/dice1.png', 'https://www.random.org/dice/dice2.png', 'https://www.random.org/dice/dice3.png', 'https://www.random.org/dice/dice4.png', 'https://www.random.org/dice/dice5.png', 'https://www.random.org/dice/dice6.png'];  
 let img = dir[Math.floor(Math.random() * dir.length)]
-let stiker = await sticker(img, false, global.packname, global.author) 
+//let stiker = await sticker(img, false, global.packname, global.author) 
+let stiker = sticker(false, img, global.packname, global.author)
 await conn.sendFile(m.chat, stiker, 'error.webp', '', m, { asSticker: true })
 }
 handler.command = ['dado', 'dados', 'dadu'] 
