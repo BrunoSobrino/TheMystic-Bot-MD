6a8eeec29441c3ce44dcd2efbb7c1e6f335a3165
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-18 14:33:14 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -10,7 +10,7 @@ pp = await conn.profilePictureUrl(who)
 } catch (e) {
 
 } finally {
-let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
+let { name, limit, lastclaim, registered, regTime, age, premiumTime } = global.db.data.users[who]
 let username = conn.getName(who)
 let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
@@ -19,7 +19,7 @@ let str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + '
 *𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
 *𝙻𝙸𝙼𝙸𝚃𝙴:* ${limit} 𝚄𝚂𝙾𝚂
 *𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾:* ${registered ? 'Si': 'No'}
-*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${prem ? 'Si' : 'No'}
+*𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : 'No' || ''}
 *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
 ${sn}`
 conn.sendButton(m.chat, str, author, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}}
