4b6dec42319e790b712e253a406b44b140e3e3f6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-18 12:29:57 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-spamwa.js
+++ plugins/herramientas-spamwa.js
@@ -16,7 +16,7 @@ handler.help = ['spamwa <number>|<mesage>|<no of messages>']
 handler.tags = ['General']
 handler.command = /^spam(wa)?$/i
 handler.group = false
-handler.premium = false
-handler.private = true
-handler.limit = true
+handler.premium = true
+//handler.private = true
+//handler.limit = true
 export default handler
