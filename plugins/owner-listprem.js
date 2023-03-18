f844a73e46dd7152b3b0c6887770d5bfad9e155e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-17 23:27:26 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -1,34 +1,33 @@
 let handler = async (m, { conn, args }) => {
-  let usuario = global.db.data.users[m.sender].premiumTime
-  let user = Object.entries(global.db.data.users).filter(user => user[1].premiumTime).map(([key, value]) => {
-    return { ...value, jid: key }
-  })
-  let name = '🎟️ 𝗣𝗥𝗘𝗠𝗜𝗨𝗠'
-  let premTime = global.db.data.users[m.sender].premiumTime
-  let prem = global.db.data.users[m.sender].premium
-  let waktu = clockString(`${premTime - new Date() * 1} `)
-  let sortedP = user.map(toNumber('premiumTime')).sort(sort('premiumTime'))
-  let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 10)) : Math.min(10, sortedP.length)
-  await conn.sendButton(m.chat, `${htki} *🎟️ PREMIUM 🎟️* ${htka}
-  
-*╭ ༻✦༺ 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙄𝙉𝙁𝙊 ༻✦༺*
-*┃✢ 𝙉𝙊𝙈𝘽𝙍𝙀 : 𝙐𝙎𝙀𝙍*\n*┃✢* ${conn.getName(m.sender)}
-${prem ? `${clockString (usuario - new Date() * 1)}` : '┃✢ *𝙏𝙄𝙀𝙈𝙋𝙊 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 | 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙏𝙄𝙈𝙀*\n┃🚫 𝘾𝘼𝘿𝙐𝘾𝘼𝘿𝙊 : 𝙏𝙄𝙈𝙀𝘿 𝙊𝙐𝙏 '}
-*╰•·–––––––––––––––·•*
+let usuario = global.db.data.users[m.sender].premiumTime
+let user = Object.entries(global.db.data.users).filter(user => user[1].premiumTime).map(([key, value]) => { return { ...value, jid: key } })
+let name = '🎟️ 𝗣𝗥𝗘𝗠𝗜𝗨𝗠'
+let premTime = global.db.data.users[m.sender].premiumTime
+let prem = global.db.data.users[m.sender].premium
+let waktu = clockString(`${premTime - new Date() * 1} `)
+let sortedP = user.map(toNumber('premiumTime')).sort(sort('premiumTime'))
+let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 10)) : Math.min(10, sortedP.length)
+let usuarioo = await '@' + m.sender.split`@`[0]
+await conn.sendButton(m.chat, `
 
-╭•·–––––––––––––––·•
-🌟 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈
-╰•·–––––––––––––––·•${sortedP.slice(0, len).map(({ jid, name, premiumTime, prem, registered }, i) => `\n\n╭–✦ ${registered ? name : conn.getName(jid)}\n┃• wa.me/${jid.split`@`[0]}\n${premiumTime > 0 ? `${clockString (premiumTime - new Date() * 1)}` : '┃🚫 𝘾𝘼𝘿𝙐𝘾𝘼𝘿𝙊 : 𝙏𝙄𝙈𝙀𝘿 𝙊𝙐𝙏'}`).join`\n╰–––––––––––·•`}
-╰–––––––––––·•`.trim(), `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${prem ? '✅' : '❌'}\n${wm}`, null, [[`${prem ? '✦ 𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦': '✦ 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 𝙋𝘼𝙎𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦'}`, `${prem ? '.allmenu': '.pase premium'}`]]) //${premiumTime > 0 ?
-setTimeout(() => {
-    if (global.db.data.chats[m.chat].deletemedia) conn.deleteMessage(m.chat, key)
-  }, global.db.data.chats[m.chat].deletemediaTime)
+*「 INFO USER 」*
+Usuario: ${usuarioo}
+Timpo premium: ${prem ? `${clockString (usuario - new Date() * 1) || ''}`
+
+
+*「 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 」*
+${sortedP.slice(0, len).map(({ jid, name, premiumTime, prem, registered }, i) => 
+                            
+`User: ${usuarioo}
+${premiumTime > 0 ? `${clockString (premiumTime - new Date() * 1)}` : '┃🚫 𝘾𝘼𝘿𝙐𝘾𝘼𝘿𝙊 : 𝙏𝙄𝙈𝙀𝘿 𝙊𝙐𝙏'}`).join`
+`}
+`.trim(), `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${prem ? '✅' : '❌'}\n${wm}`, null, [[`${prem ? '✦ 𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦': '✦ 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 𝙋𝘼𝙎𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦'}`, `${prem ? '.allmenu': '.pase premium'}`]]) //${premiumTime > 0 ?
+
+  
 }
 handler.help = ['premlist [angka]']
 handler.tags = ['info']
 handler.command = /^(listprem|premlist|listavip|viplista)$/i
-//handler.command = /^(vip|prem|premium|lista|list)vip|prem|premium|lista|list$/i
-
 export default handler
 
 function clockString(ms) {
