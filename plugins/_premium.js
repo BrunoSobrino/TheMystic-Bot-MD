7f780f2268b2947e022be52993054ed93f2a002b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-18 13:36:25 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -6,9 +6,9 @@ export async function all(m) {
             if (new Date() * 1 >= user.premiumTime) {
                 user.premiumTime = 0
                 user.premium = false
-                await this.sendMessage(Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user), {text: `*[❗] @${Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user).split`@`[0]} 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙲𝙾𝙼𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙷𝙰 𝙴𝚇𝙿𝙸𝚁𝙰𝙳𝙾, 𝚈𝙰 𝙽𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`}, {contextInfo: {"mentionedJid": [Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)]}, quoted: ''})
+                await this.sendMessage(Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user), {text: `*[❗] @${Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user).split`@`[0]} 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙲𝙾𝙼𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙷𝙰 𝙴𝚇𝙿𝙸𝚁𝙰𝙳𝙾, 𝚈𝙰 𝙽𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`}, {contextInfo: {"mentions": [Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)]}, quoted: ''})
                 //await m.reply(, `${Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)}`, { mentions: [Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)] })
-                console.log(`El premium del usuario con el número ${Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)} ha expirado.`)
+                //console.log(`El premium del usuario con el número ${Object.keys(global.db.data.users).find(key => global.db.data.users[key] === user)} ha expirado.`)
             }
         }
     }
