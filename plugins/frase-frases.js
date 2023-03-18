9cd9b5db026507e739d38fb9eaea94378a4e46f8
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-18 15:05:09 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/frase-frases.js
+++ plugins/frase-frases.js
@@ -17,14 +17,21 @@ let frase = json.result.message
 let frase1 = await translate(frase, { to: 'es', autoCorrect: true }).catch(_ => null)
 m.reply(`*╭─◆────◈⚘◈─────◆─╮*\n*❥  ${frase1.text}*\n*╰─◆────◈⚘◈─────◆─╯*`)}
 
-if (command == 'historiaromantica') {    
+if (command == 'historiaromantica') {  
+try {    
 let cerpe = await cerpen(`cinta romantis`)
 let storytime = await translate(cerpe.cerita, { to: 'es', autoCorrect: true }).catch(_ => null)
 let titletime = await translate(cerpe.title, { to: 'es', autoCorrect: true }).catch(_ => null)
 conn.reply(m.chat, `᭥🫐᭢ Título: ${titletime.text}
 ᭥🍃᭢ Autor: ${cerpe.author}
 ────────────────
-${storytime.text}`, m)}}
+${storytime.text}`, m)
+} catch {
+let err = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=Elabora%20una%20historia%20romantica%20que%20use%20el%20siguiente%20formato:%20%E1%AD%A5%F0%9F%AB%90%E1%AD%A2%20T%C3%ADtulo:%20%E1%AD%A5%F0%9F%8D%83%E1%AD%A2%20Autor:%20%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%E2%94%80%20hsitoria...%20adalah&user=user-unique-id`)
+let json = await err.json()
+let fraseChat = json.result
+conn.reply(m.chat, fraseChat, m)
+}}
 handler.tags = ['frases']
 handler.command = handler.help = ['consejo', 'fraseromantica', 'historiaromantica']
 export default handler
