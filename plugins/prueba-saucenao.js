5cda3d452c210a59892c528eed7bd59254e99d26
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-19 05:30:08 -0300

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -122,7 +122,7 @@ propName = prop
 }
 resultadoEnBruto += `*${propName}*\n${primerResultado.data[prop]}\n\n`}
     
-let frep = { contextInfo: { externalAdReply: {md}}}
+let frep = { contextInfo: { externalAdReply: {null}}}
 await m.reply('*ESPERE UN MOMENTO...*')
 await conn.sendButton(m.chat, `*Número de resultados:* ${results.length}
 *Resultados encontrados:* ${Boolean(results) === true ? 'Si' : 'No'}
