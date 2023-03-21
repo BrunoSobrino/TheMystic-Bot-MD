0289bd0e546e69821b6f214943d85e3fb08f45e3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 14:19:40 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,4 +1,5 @@
 import { googleIt } from '@bochilteam/scraper'
+import axios from 'axios' 
 let handler = async (m, { conn, command, args }) => {
 const fetch = (await import('node-fetch')).default
 let full = /f$/i.test(command)
@@ -9,6 +10,8 @@ let search = await googleIt(text)
 let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
 let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${lolkeysapi}&url=${url}`)).arrayBuffer()
+let ss2 = await ssweb(url, 'desktop')
+if (!ss || ss == '' || ss == null || ss === '') ss = ss2.result
 await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
 } catch {
 m.reply(msg)
@@ -17,3 +20,40 @@ handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
 handler.command = /^googlef?$/i
 export default handler
+
+async function ssweb(url, device = 'desktop'){
+     return new Promise((resolve, reject) => {
+          const base = 'https://www.screenshotmachine.com'
+          const param = {
+            url: url,
+            device: device,
+            cacheLimit: 0
+          }
+          axios({url: base + '/capture.php',
+               method: 'POST',
+               data: new URLSearchParams(Object.entries(param)),
+               headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+               }
+          }).then((data) => {
+               const cookies = data.headers['set-cookie']
+               if (data.data.status == 'success') {
+                    axios.get(base + '/' + data.data.link, {
+                         headers: {
+                              'cookie': cookies.join('')
+                         },
+                         responseType: 'arraybuffer'
+                    }).then(({ data }) => {
+                       let result = {
+                            status: 200,
+                            author: 'Ryzn',
+                            result: data
+                        }
+                         resolve(result)
+                    })
+               } else {
+                    reject({ status: 404, author: 'Ryzn', message: data.data })
+               }
+          }).catch(reject)
+     })
+}
