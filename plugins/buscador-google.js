e4fa2f5f31d290756c6c92ff516361a3d9e02fad
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-21 14:22:53 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -21,38 +21,14 @@ handler.command = /^googlef?$/i
 export default handler
 
 async function ssweb(url, device = 'desktop'){
-     return new Promise((resolve, reject) => {
-          const base = 'https://www.screenshotmachine.com'
-          const param = {
-            url: url,
-            device: device,
-            cacheLimit: 0
-          }
-          axios({url: base + '/capture.php',
-               method: 'POST',
-               data: new URLSearchParams(Object.entries(param)),
-               headers: {
-                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-               }
-          }).then((data) => {
-               const cookies = data.headers['set-cookie']
-               if (data.data.status == 'success') {
-                    axios.get(base + '/' + data.data.link, {
-                         headers: {
-                              'cookie': cookies.join('')
-                         },
-                         responseType: 'arraybuffer'
-                    }).then(({ data }) => {
-                       let result = {
-                            status: 200,
-                            author: 'Ryzn',
-                            result: data
-                        }
-                         resolve(result)
-                    })
-               } else {
-                    reject({ status: 404, author: 'Ryzn', message: data.data })
-               }
-          }).catch(reject)
-     })
-}
+return new Promise((resolve, reject) => {
+const base = 'https://www.screenshotmachine.com'
+const param = { url: url, device: device, cacheLimit: 0 }
+axios({url: base + '/capture.php', method: 'POST', data: new URLSearchParams(Object.entries(param)), headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }}).then((data) => {
+const cookies = data.headers['set-cookie']
+if (data.data.status == 'success') {
+axios.get(base + '/' + data.data.link, { headers: { 'cookie': cookies.join('') }, responseType: 'arraybuffer' }).then(({ data }) => {
+let result = { status: 200, author: '@BrunoSobrino', result: data } 
+resolve(result)})
+} else {
+reject({ status: 404, author: 'Ryzn', message: data.data })}}).catch(reject)})}
