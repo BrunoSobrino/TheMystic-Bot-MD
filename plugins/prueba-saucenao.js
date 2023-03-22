f451f922012ee0d37bfc870c951edb32fa55c857
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-22 01:56:20 -0300

    Update prueba-saucenao.js

--- plugins/prueba-saucenao.js
+++ plugins/prueba-saucenao.js
@@ -51,6 +51,7 @@ const primerResultado = results[0]
 let resultadoEnBruto = ''
 for (let prop in primerResultado.header) {
 let propName = '';
+switch (prop) {
 case 'similarity': propName = 'Puntuación de similitud' 
 break                              
 case 'author_name': propName = 'Nombre del autor' 
@@ -63,6 +64,7 @@ resultadoEnBruto += `*${propName}*\n${primerResultado.header[prop]}\n\n`}
     
 for (let prop in primerResultado.data) {
 let propName = ''
+switch (prop) {
 case 'title': propName = 'Título' 
 break        
 case 'ext_urls': propName = 'URLs' 
