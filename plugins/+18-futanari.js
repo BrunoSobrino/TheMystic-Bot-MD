0371bb054ad08f1840207c7781bcca081edf8b10
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-26 20:57:11 -0300

    Update +18-futanari.js

--- plugins/+18-futanari.js
+++ plugins/+18-futanari.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 let url = futanari[Math.floor(Math.random() * futanari.length)]
-conn.sendMessage(m.chat, `_*ACA TIENES UNA RICA FUTANARI 🔥*_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, `_*ACA TIENES UNA RICA FUTANARI 🔥*_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['futanari']
 handler.tags = ['nsfw']
