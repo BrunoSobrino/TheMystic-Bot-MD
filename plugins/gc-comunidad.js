5e027a341388d38146c8b00b3ce7e32ae5b44356
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-03-26 21:06:22 -0300

    Update gc-comunidad.js

--- plugins/gc-comunidad.js
+++ plugins/gc-comunidad.js
@@ -2,14 +2,12 @@
 let handler = async (m, { conn, command }) => {
 let str = `
 *GRUPO DE LA COMUNIDAD*
-
 _⚠️| Este grupo será solamente de avisos o noticias relacionada con los grupos de la comunidad Futabu, solo podran hablar los del Equipo de Staff y los miembros solo podran ver, usaremos este grupo ya que facilitara el ver mas rapido el mensaje ya que en varios grupos se habla demasiado y se pierden los mensajes._
-
 Link: https://chat.whatsapp.com/KAVBUhcHZvqK510kn7wofQ
 `.trim()
-  
-conn.sendHydrated(m.chat, str, wm, null, null, null, 
-m,)}
+
+conn.sendHydrated(m.chat, str, wm, null, 'https://github.com/ColapsusHD/AsakuraMaoBotV2-MD', 'AsakuraMao Bot', null, null, [
+], m,)}
 
 handler.command = /^comunidad|grupocomunidad|grupoavisos$/i
 handler.exp = 35
