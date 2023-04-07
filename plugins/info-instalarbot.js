37ea2f12002af5c9a63f35d64fd92c2e8ac2b330
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-04-06 22:27:38 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -17,10 +17,6 @@ let texto = `
 - apt-get update -y && apt-get upgrade -y
 - pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
 - git clone https://github.com/BrunoSobrino/Mystic-termux.git && cd Mystic-termux 
-- yarn install 
-- npm install
-- npm update
-- npm install 
 - npm start` 
 let aa = { quoted: m, userJid: conn.user.jid }
 let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: `${texto}`.trim(), contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD' }, mentionedJid: [m.sender] }}}, aa)
