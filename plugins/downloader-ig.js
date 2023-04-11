73d6924179f9690417fe076749000c402ac809e1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-04-11 00:32:09 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -3,10 +3,20 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 import instagramGetUrl from 'instagram-url-direct'
 import vm from 'node:vm'
+import { instagram } from "@xct007/frieren-scraper";
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, command, usedPrefix }) => {
 if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link` 
 m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)
+try {
+const datTa = await instagram.v1(args[0])
+if (datTa.error) return m.reply(`${datTa.message}`);
+for (let urRRl of datTa) {
+let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${urRRl.url}`)).text()    
+let tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim()    
+conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m)    
+await new Promise(resolve => setTimeout(resolve, 10000))}
+} catch {  
 try {  
 const AA = await savefrom(args[0])    
 let shortUrl222 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
@@ -38,7 +48,7 @@ await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
 } catch { 
 console.log('Error en el intento 4, sin resultados')  
 throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`
-}}}}}
+}}}}}}
 handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 export default handler
 async function savefrom(urlL) {
