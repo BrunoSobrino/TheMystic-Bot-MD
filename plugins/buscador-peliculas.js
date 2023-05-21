0e795875f82859bf14ac06d84155d5c9ceb693b4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-05-20 20:42:50 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-peliculas.js
+++ plugins/buscador-peliculas.js
@@ -1,7 +1,63 @@
+/* Creado/adaptado por Bruno Sobrino (https://github.com/BrunoSobrino) */
+
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { load } from 'cheerio'
+let handler = async (m, {text, usedPrefix, command, conn}) => {
+if (!text) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙿𝙴𝙻𝙸𝙲𝚄𝙻𝙰 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*'   
+let aaaa
+let img
+try {   
+aaaa = await searchC(text)
+img = 'https://cinefilosoficial.com/wp-content/uploads/2021/07/cuevana.jpg'    
+} catch {
+aaaa = await searchP(text)    
+img = 'https://elcomercio.pe/resizer/RJM30xnujgfmaODGytH1rRVOrAA=/400x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BJ2L67XNRRGHTFPKPDOEQ2AH5Y.jpg'    
+}    
+if (aaaa == '') throw '*[❗] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙿𝙴𝙻𝙸𝙲𝚄𝙻𝙰*' 
+let res = await aaaa.map((v) => `*🎬 • Nombre:* ${v.title}\n*🍿 • Url:* ${v.link}`).join`\n\n───────────────\n\n`
+let ads = '*💫 • Bloqueador de anuncios recomendado:* Block This\n*⛨ • Link:* https://block-this.com/block-this-latest.apk\n\n≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣\n\n'
+conn.sendMessage(m.chat, { image: { url: img }, caption: ads + res }, {quoted: m})
+}
+handler.command = ['cuevana', 'pelisplus']
+export default handler
+
+const safeLoad = async(url, options = {}) => {
+try {
+const { data: pageData } = await axios.get(url, options)
+const $ = load(pageData) 
+return $
+} catch (err) {
+if (err.response)
+throw new Error(err.response.statusText)
+throw err }}
+
+async function searchC(query, numberPage = 1) {
+const $ = await safeLoad(`https://cuevana3.mu/page/${numberPage}/`, {
+params: { s: query }})
+const resultSearch = []
+$(".results-post > article").each((_, e) => {
+const element = $(e)
+const title = element.find("header > h2").text()
+const link = element.find(".lnk-blk").attr("href")
+resultSearch.push({ title: title, link: link })})
+return resultSearch }
+
+async function searchP(query, numberPage = 1) { 
+const $ = await safeLoad(`https://pelisplushd.cx/search/`, {
+params: { s: query, page: numberPage }})
+const resultSearch = []
+$("a[class^='Posters']").each((_, e) => {
+const element = $(e)
+const title = element.find(".listing-content > p").text()
+const link = element.attr("href")
+resultSearch.push({ title: title,  link: link })})
+return resultSearch }
+
 /* creado adaptado por bruno sobrino*/
 /* arreglado por skid */
 
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 import axios from 'axios'
 import { load } from 'cheerio'
 
@@ -71,4 +127,4 @@ async function searchPelisPlus(query, numberPage = 1) {
   })
   
   return resultSearch
-}
+}*/
