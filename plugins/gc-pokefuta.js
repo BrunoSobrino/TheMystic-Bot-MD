1fe0c7e729ade65565a6bea54e359037b3c1e761
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-05-22 00:58:59 -0300

    Update gc-pokefuta.js

--- plugins/gc-pokefuta.js
+++ plugins/gc-pokefuta.js
@@ -1,16 +1,11 @@
-let handler = async (m, { conn, command }) => {
-let str = `
-Nuestro grupo de una sola tematica!
+let handler = async (m, { conn }) => {
+m.reply(global.telegram)}
+handler.customPrefix = /pokefuta|linkpokefuta/i
+handler.command = new RegExp
+export default handler
+
+global.telegram = `Nuestro grupo de una sola tematica!
 
 *⚠️| RECUERDA ESTE GRUPO ES DE UNA SOLA TEMATICA QUE ES FUTANARI RELACIONADO CON LA SERIE POKEMON, PUEDES MANDAR CONTENIDO VARIADO PERO SE RECOMIENDA MANDAR MAS DE LA TEMATICA!*
 
-Link: https://chat.whatsapp.com/Hu4JbtFdWWDItj2hoe4X1D
-`.trim()
-  
-conn.sendHydrated(m.chat, str, wm, null, 'https://chat.whatsapp.com/Hu4JbtFdWWDItj2hoe4X1D', 'ENTRAR | JOIN 🤠', null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)}
-
-handler.command = /^pokefuta|linkpokefuta$/i
-handler.exp = 35
-export default handler
+Link: https://chat.whatsapp.com/Hu4JbtFdWWDItj2hoe4X1D`
