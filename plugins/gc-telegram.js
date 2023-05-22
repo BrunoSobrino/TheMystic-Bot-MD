f0da54a933137569523ed872498716037db77533
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-05-22 00:50:18 -0300

    Update gc-telegram.js

--- plugins/gc-telegram.js
+++ plugins/gc-telegram.js
@@ -1,13 +1,10 @@
-let handler = async (m, { conn, command }) => {
-let str = `
+let handler = async (m, { conn }) => {
+m.reply(global.telegram, 'https://t.me/FutabuClub', 'ENTRAR | JOIN 🤠')}
+handler.customPrefix = /telegram|grupodetelegram|linktelegram/i
+handler.command = new RegExp
+export default handler
+
+global.telegram = `
 Nuestro grupo de Telegram!
 Link: https://t.me/FutabuClub
-`.trim()
-  
-conn.sendHydrated(m.chat, str, wm, null, 'https://t.me/FutabuClub', 'ENTRAR | JOIN 🤠', null, null, [
-['null']
-], m,)}
-
-handler.command = /^telegram|grupodetelegram|linktelegram$/i
-handler.exp = 35
-export default handler
+`
