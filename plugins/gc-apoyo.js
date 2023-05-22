6e14a09ed833264fe83041874a3a05bffd02a718
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-05-22 01:10:05 -0300

    Update gc-apoyo.js

--- plugins/gc-apoyo.js
+++ plugins/gc-apoyo.js
@@ -1,18 +1,13 @@
-//let media = './media/menus/telefutaclub.jpeg'
-let handler = async (m, { conn, command }) => {
-let str = `
-*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
+let handler = async (m, { conn }) => {
+m.reply(global.apoyo)}
+handler.customPrefix = /apoyo|mejorar|apoyobot|mejorarbot/i
+handler.command = new RegExp
+export default handler
+
+global.apoyo = `*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
 *Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
+
 *Banco Virtual (Mercado Pago, Uala, Etc)*
 Alias: COLAPSUSHD2020.UALA
 CBU/CVU: 0000007900204654633937
-Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo
-`.trim()
-  
-conn.sendHydrated(m.chat, str, wm, null, null, null, null, null, [
-['✊ APOYO A LA MEJORA!', 'null']
-], m,)}
-
-handler.command = /^apoyo|mejorar|apoyobot|mejorarbot$/i
-handler.exp = 35
-export default handler
+Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`
