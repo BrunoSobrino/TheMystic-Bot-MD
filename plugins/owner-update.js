c8ae34af0de030cd333c9c641e1974f10e07b609
skid <112231743+Skidy89@users.noreply.github.com>
2023-05-27 14:35:37 -0600

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,15 +1,23 @@
-import { execSync } from 'child_process'
+import { execSync } from 'child_process';
+
 let handler = async (m, { conn, text }) => {
-try {  
-if (global.conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)}
-} catch {
-var update = execSync('git remote set-url origin https://github.com/BrunoSobrino/TheMystic-Bot-MD.git && git pull')
-await m.reply(update.toString())
-}}
-handler.help = ['update']
-handler.tags = ['owner']
-handler.command = /^update|actualizar$/i
-handler.rowner = true
-export default handler
+  try {
+    if (global.conn.user.jid == conn.user.jid) {
+      let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+      conn.reply(m.chat, stdout.toString(), m);
+    } 
+  } catch (error) {
+    console.error(error);
+    let errorMessage = "An error occurred while executing the command.";
+    if (error.message) {
+      errorMessage += "\nError message: " + error.message;
+    }
+    await conn.reply(m.chat, errorMessage, m);
+  }
+};
+
+handler.help = ['update']; 
+handler.tags = ['owner']; 
+handler.command = /^update|actualizar$/i;
+handler.rowner = true; 
+export default handler; 
