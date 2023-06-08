153ce3597b61a6a2a7e1bf1bcc4a52d97064c160
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-06-08 10:26:05 -0500

    Update random-ppcp.js

--- plugins/random-ppcp.js
+++ plugins/random-ppcp.js
@@ -1,11 +1,16 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
-let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkeysapi}`)
-if (res.status != 200) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-conn.sendButton(m.chat, '𝙲𝙷𝙸𝙲𝙰 𝙲𝚄𝚃𝙴', wm, json.result.female, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
-conn.sendButton(m.chat, '𝙲𝙷𝙸𝙲𝙾 𝙲𝚄𝚃𝙴', wm, json.result.male, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+let handler = async(m, {
+	conn,
+	command
+}) => {
+	let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkeysapi}`)
+	if (res.status != 200) throw await res.text()
+	let json = await res.json()
+	if (!json.status) throw json
+	conn.sendFile(m.chat, json.result.female, 'error.jpg', `𝙲𝙷𝙸𝙲𝙰 𝙲𝚄𝚃𝙴`, m)
+	conn.sendFile(m.chat, json.result.male, 'error.jpg', `𝙲𝙷𝙸𝙲𝙾 𝙲𝚄𝚃𝙴`, m)
+		//conn.sendButton(m.chat, '𝙲𝙷𝙸𝙲𝙰 𝙲𝚄𝚃𝙴', wm, json.result.female, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+		//conn.sendButton(m.chat, '𝙲𝙷𝙸𝙲𝙾 𝙲𝚄𝚃𝙴', wm, json.result.male, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['ppcouple']
 handler.tags = ['internet']
