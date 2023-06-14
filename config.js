115e09234cdefefc616c33201f235c3f67420ff1
Gamotto <112080813+Gamotto@users.noreply.github.com>
2023-06-14 13:21:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,27 +8,27 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['593968585283'],
-  ['5219993404349'],
-  ['584125778026'],
-  ['5219991402134'],	
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5492266613038'],
-  ['5218442114446'],
-  ['59894808483']
+  ['17123153364', '👑 Mystic - Creador 👑', true],
+  ['573227212791', '💫 Mystic - Collaborator 1 💫', true],
+  ['17123153364', '💫 Mystic - Collaborator 2 💫', true],
+  ['17123153364', '💫 Mystic - Collaborator 3 💫', true],
+  ['17123153364', '💫 Mystic - Collaborator 4 💫', true],
+  ['17123153364', '💫 Mystic - Collaborator 5 💫', true],  
+  ['17123153364', '💫 Mystic - Collaborator 6 💫', true],
+  ['17123153364', '💫 Mystic - Collaborator 7 💫', false],
+  ['17123153364'],
+  ['17123153364'],
+  ['17123153364'],
+  ['17123153364'],	
+  ['17123153364'],
+  ['17123153364'],
+  ['17123153364'],
+  ['17123153364'],
+  ['17123153364']
 ] 
 
-global.suittag = ['5219993404349'] 
-global.prems = ['51995386439'] 
+global.suittag = ['17123153364'] 
+global.prems = ['17123153364'] 
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
