7d37aebecc4cde3cf773d2431491fcdfa070be3e
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-07-04 14:01:06 -0300

    Update gc-apoyo.js

--- plugins/gc-apoyo.js
+++ plugins/gc-apoyo.js
@@ -1,19 +1,9 @@
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
-import fs from 'fs'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let name = await conn.getName(m.sender)
-let apoyo =`*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
-*Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
-
-*Banco Virtual (Mercado Pago, Uala, Etc)*
-Alias: COLAPSUSHD2020.UALA
-CBU/CVU: 0000007900204654633937
-Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`.trim()
-let aa = { quoted: m, userJid: conn.user.jid }
-let res = generateWAMessageFromContent (m.chat, {null, null, null, caption: apoyo, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
-conn.relayMessage(m.chat, res.message, {})
-}
+let handler = async (m, { conn }) => {
+m.reply(global.telegram)}
 handler.help = ['apoyo']
 handler.tags = ['grupo']
-handler.command = /^apoyo|mejorar|apoyobot|mejorarbot$/i
+handler.command = /^(apoyo|mejorar|apoyobot|mejorarbot)$/i
 export default handler
+
+global.telegram = `Nuestro grupo de Telegram!
+Link: https://t.me/FutabuClub`
