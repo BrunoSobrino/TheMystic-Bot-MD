f21678a452df1a473f8b37cc2a30a3e365835560
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-07-04 14:02:46 -0300

    Update gc-apoyo.js

--- plugins/gc-apoyo.js
+++ plugins/gc-apoyo.js
@@ -1,9 +1,14 @@
 let handler = async (m, { conn }) => {
-m.reply(global.telegram)}
+m.reply(global.apoyo)}
 handler.help = ['apoyo']
 handler.tags = ['grupo']
 handler.command = /^(apoyo|mejorar|apoyobot|mejorarbot)$/i
 export default handler
 
-global.telegram = `Nuestro grupo de Telegram!
-Link: https://t.me/FutabuClub`
+global.apoyo = `*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
+*Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
+
+*Banco Virtual (Mercado Pago, Uala, Etc)*
+Alias: COLAPSUSHD2020.UALA
+CBU/CVU: 0000007900204654633937
+Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`
