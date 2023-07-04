90b013e853f71d50370ef77081980c7536746f04
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-07-04 13:48:09 -0300

    Update gc-apoyo.js

--- plugins/gc-apoyo.js
+++ plugins/gc-apoyo.js
@@ -1,13 +1,19 @@
-let handler = async (m, { conn }) => {
-m.reply(global.apoyo)}
-handler.command = /^apoyo|mejorar|apoyobot|mejorarbot$/i
-handler.command = new RegExp
-export default handler
-
-global.apoyo = `*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import fs from 'fs'
+let handler = async (m, { conn, usedPrefix, command }) => {
+let name = await conn.getName(m.sender)
+let apoyo =`*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
 *Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
 
 *Banco Virtual (Mercado Pago, Uala, Etc)*
 Alias: COLAPSUSHD2020.UALA
 CBU/CVU: 0000007900204654633937
-Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`
+Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`.trim()
+let aa = { quoted: m, userJid: conn.user.jid }
+let res = generateWAMessageFromContent (m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: apoyo, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
+conn.relayMessage(m.chat, res.message, {})
+}
+handler.help = ['apoyo']
+handler.tags = ['grupo']
+handler.command = /^apoyo|mejorar|apoyobot|mejorarbot$/i
+export default handler
