5caed6885245af94ad8336828f4edef03bcea6ed
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-07-04 13:41:40 -0300

    Update gc-concurso.js

--- plugins/gc-concurso.js
+++ plugins/gc-concurso.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 m.reply(global.concurso)}
-handler.command = /^(concurso|concursofutabuclub)$/i
+handler.command = /^concurso|concursofutabuclub$/i
 handler.command = new RegExp
 export default handler
 
