f3c6ffce67e6b174942adf9e1dce930c9da3ab46
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-07-05 16:44:48 -0300

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-fakeReply.js
+++ plugins/herramientas-fakeReply.js
@@ -1,12 +1,12 @@
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return m.reply(`[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, { mentions: [m.sender] })
+  if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, { mentions: [m.sender] })
   let cm = copy(m)
   let who
   if (text.includes('@0')) who = '0@s.whatsapp.net'
   else if (m.isGroup) who = cm.participant = m.mentionedJid[0]
   else who = m.chat
-  if (!who) return m.reply(`[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, { mentions: [m.sender] })
+  if (!who) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, { mentions: [m.sender] })
   cm.key.fromMe = false 
   cm.message[m.mtype] = copy(m.msg)
   let sp = '@' + who.split`@`[0]
