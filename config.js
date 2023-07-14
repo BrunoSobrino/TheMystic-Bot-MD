cb7c5d01dfacfaf8b55fade6b0fd464e0625aa2d
zainnawab97 <115409891+zainnawab97@users.noreply.github.com>
2023-07-14 08:25:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,34 +8,34 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['593968585283'],
-  ['5219993404349'],
-  ['584125778026'],
-  ['5219991402134'],	
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5492266613038'],
-  ['5218442114446'],
-  ['59894808483']
+  ['923074060856', '👑 Mystic - Creador 👑', true],
+  ['923074060856', '💫 Mystic - Collaborator 1 💫', true],
+  ['923074060856', '💫 Mystic - Collaborator 2 💫', true],
+  ['923074060856', '💫 Mystic - Collaborator 3 💫', true],
+  ['923074060856', '💫 Mystic - Collaborator 4 💫', true],
+  ['923074060856', '💫 Mystic - Collaborator 5 💫', true],  
+  ['923074060856', '💫 Mystic - Collaborator 6 💫', true],
+  ['923074060856', '💫 Mystic - Collaborator 7 💫', false],
+  ['923074060856'],
+  ['923074060856'],
+  ['923074060856'],
+  ['923074060856'],	
+  ['923074060856'],
+  ['923074060856'],
+  ['923074060856'],
+  ['923074060856'],
+  ['923074060856']
 ] 
 
-global.suittag = ['5219993404349'] 
-global.prems = ['51995386439'] 
-global.reportes_solicitudes = ['5219992095479']
+global.suittag = ['923074060856'] 
+global.prems = ['923074060856'] 
+global.reportes_solicitudes = ['923074060856']
 
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.packname = '(☞ﾟZain🥰NawabZaaDaa ²⁰²³)☞'
+global.author = '★𝐙𝐀𝐈𝐍 - 𝙱𝚘𝚝★'
+global.wm = '★𝐙𝐀𝐈𝐍 - 𝙱𝚘𝚝★'
+global.igfg = '★𝐙𝐀𝐈𝐍 - 𝙱𝚘𝚝★'
+global.wait = '*[❗] Loading, Wait For a  Moment...*'
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
@@ -55,14 +55,14 @@ global.año = d.toLocaleDateString('es', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 //*****************************
 global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`
-global.gt = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.mysticbot = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.gt = '★𝐙𝐀𝐈𝐍 - 𝙱𝚘𝚝★'
+global.mysticbot = '𝐙𝐀𝐈𝐍 - 𝙱𝚘𝚝★'
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
-global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waittt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waitttt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.nomorown = '5219993404349'
+global.waitt = '*[❗] Loading, Wait For a  Moment...*'
+global.waittt = '*[❗] Loading, Wait For a  Moment...*'
+global.waitttt = '*[❗] Loading, Wait For a  Moment...*'
+global.nomorown = '923074060856'
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 global.cmenut = '❖––––––『'
 global.cmenub = '┊✦ '
