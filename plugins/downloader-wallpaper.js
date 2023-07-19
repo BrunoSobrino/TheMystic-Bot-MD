1032f31e28594d6fb3bf90ef506e5e0aa151caae
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-07-18 19:56:10 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-wallpaper.js
+++ plugins/downloader-wallpaper.js
@@ -1,7 +1,7 @@
-import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
+import { wallpaper } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} Minecraft*`
-const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
+const res = await wallpaper(text)
 const img = res[Math.floor(Math.random() * res.length)]
 conn.sendFile(m.chat, img, 'error.jpg', `*𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴 ${text}*`, m)
 }
