9a326ad84cda44cd56839ec56f824b4d8134846d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-19 12:46:58 -0400

    Update adult-xnxxsearch.js

--- plugins/adult-xnxxsearch.js
+++ plugins/adult-xnxxsearch.js
@@ -1,91 +1,4 @@
-
-import fs from 'fs';
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
-  
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} Con mi prima*`
-    try {
-let vids_ = { 
-from: m.sender, 
-urls: [] 
-}
-if (!global.videoList) {
-global.videoList = [];
-}
-if (global.videoList[0]?.from == m.sender) {
-delete global.videoList;
-}
-let results = await xnxxsearch(text)
-let textoInfo = `*[❗] Puedes descargar el video que quieras de la siguiente forma:*
-◉ ${usedPrefix}audio <numero>
-◉ ${usedPrefix}video <numero> 
-
-*—◉ Ejemplos:*
-*◉ ${usedPrefix}audio 5*
-*◉ ${usedPrefix}video 8*`.trim()  
-let teks = results.all.map((v, i) => {
-let link = v.link;
-vids_.urls.push(link);
-return `[${i + 1}] ${v.title}
-↳ 🫐 *_Link :_* ${v.link}
-↳📋 *_info:_* ${v.info} `}).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
-conn.sendFile(m.chat, results.all[0].thumbnail, 'xnxxsearch.jpeg', textoInfo + '\n\n' + teks, m)
-global.videoList.push(vids_);
-} catch {    
-await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*')  
-}}
-
-handler.help = ['xnxxsearch *<texto>*'];
-handler.tags = ["downloader", "premium"];
-handler.command = /^xnxxsearch|xnxxs$/i;
-
-handler.register = false;
-handler.premium = true;
-
-export default handler;
-
-async function xnxxsearch(query) {
-  return new Promise((resolve, reject) => {
-    const baseurl = "https://www.xnxx.com";
-    fetch(`${baseurl}/search/${query}/${Math.floor(Math.random() * 3) + 1}`, {method: "get"})
-      .then((res) => res.text())
-      .then((res) => {
-        let $ = cheerio.load(res, {xmlMode: false});
-        let title = [];
-        let url = [];
-        let desc = [];
-        let results = [];
-        $("div.mozaique").each(function (a, b) {
-          $(b)
-            .find("div.thumb")
-            .each(function (c, d) {
-              url.push(baseurl + $(d).find("a").attr("href").replace("/THUMBNUM/", "/"));
-            });
-        });
-        $("div.mozaique").each(function (a, b) {
-          $(b)
-            .find("div.thumb-under")
-            .each(function (c, d) {
-              desc.push($(d).find("p.metadata").text());
-              $(d)
-                .find("a")
-                .each(function (e, f) {
-                  title.push($(f).attr("title"));
-                });
-            });
-        });
-        for (let i = 0; i < title.length; i++) {
-          results.push({title: title[i], info: desc[i], link: url[i]});
-        }
-        resolve({code: 200, status: true, result: results});
-      })
-      .catch((err) => reject({code: 503, status: false, result: err}));
-  });
-                                                                     }
-
-
-
-/*import fetch from "node-fetch";
+import fetch from "node-fetch";
 let handler = async (m, {text, usedPrefix, command}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
   
@@ -152,7 +65,7 @@ async function xnxxsearch(query) {
         resolve({code: 200, status: true, result: results});
       })
       .catch((err) => reject({code: 503, status: false, result: err}));
-  });*/
+  });
                                                                      }
 
 
