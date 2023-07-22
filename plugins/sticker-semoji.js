a3b01a5c8f804b92ab2e072e9fe4bb22f432a4c0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-07-22 09:07:26 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-semoji.js
+++ plugins/sticker-semoji.js
@@ -46,6 +46,7 @@ if (tipe == 'tw') tipe = 'twitter'
 emoji = emoji.trim()
 tipe = tipe.trim().toLowerCase()
 let json = await semoji(emoji)
+console.log(json)  
 let stiker = await createSticker(false, json.find(v => v.nama == tipe).url, global.packname, global.author, 20)
 //let stikerPI = await sticker(false, json.find(v => v.nama == tipe).url, global.packname, global.author)
 //if (tipe == 'pi' && json || tipe == 'pixel' && json ) stiker = await conn.sendFile(m.chat, stikerPI, null, { asSticker: true }, m)
