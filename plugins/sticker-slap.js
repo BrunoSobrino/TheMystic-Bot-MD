e09edd3532459f33254007a201d901d576aec06b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-07-31 05:54:42 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -2,20 +2,22 @@ import {sticker} from '../lib/sticker.js'
 import fetch from 'node-fetch'
 const handler = async (m, {conn, args, usedPrefix, command}) => {  
  try {
-	 let who
+   let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
        else who = m.chat
     if (!who) throw `*[❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙾 𝙼𝙴𝙽𝙲𝙸𝙾𝙽𝙰 𝙰 𝙰𝙻𝙶𝚄𝙸𝙴𝙽*\n\n*📌 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:* ${usedPrefix + command} @tag`  
-       let user = global.db.data.users[who]
-       let name = conn.getName(who) 
+       let name 
+    if (who === m.chat) { 
+       name = '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝'
+      } else {    
+       name = conn.getName(who) 
+      }
        let name2 = conn.getName(m.sender) 
-       let rki = await fetch(`https://api.waifu.pics/sfw/slap`)
-    if (!rki.ok) throw await rki.text()
-       let jkis = await rki.json()
+       let apislap = await fetch(`https://api.waifu.pics/sfw/slap`)
+       let jkis = await apislap.json()
        let { url } = jkis
-       let stiker = await sticker(null, url, `${name2} le dio una bofetada a`, `${name}`)
-   // conn.sendFile(m.chat, stiker, null, {asSticker: true});
-    conn.sendFile(m.chat, stiker, null, {asSticker: true}, m, true, {contextInfo: {'forwardingScore': 200, 'isForwarded': false}}, {quoted: m})   
+       let stiker = await sticker(null, url, `${name2} le dio una bofetada a ${name}`, null)
+    conn.sendFile(m.chat, stiker, null, {asSticker: true});
 } catch {
 throw `*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*`   
 }}
