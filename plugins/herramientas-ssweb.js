3bf50732823388c6ad0bef59100a30571184e723
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 18:14:38 -0400

    ssweb mejorado

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,10 +1,17 @@
-import fetch from 'node-fetch';
-const handler = async (m, {conn, command, args}) => {
-  if (!args[0]) return conn.reply(m.chat, '*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*', m);
-  const ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer();
-  conn.sendFile(m.chat, ss, 'error.png', args[0], m);
-};
-handler.help = ['ss', 'ssf'].map((v) => v + ' <url>');
-handler.tags = ['internet'];
-handler.command = /^ss(web)?f?$/i;
-export default handler;
+import fetch from 'node-fetch' 
+ var handler = async (m, {conn,text, args}) => {   
+  
+     if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);  
+  try {  
+       const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=${lolkeysapi}&url=${text}`; 
+     conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); } catch {  
+ try { 
+  const link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;  
+  conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m}); } catch { 
+ const ssweb2 = `https://api.lolhuman.xyz/api/SSWeb2?apikey=${lolkeysapi}&url=${text}` 
+ conn.sendMessage(m.chat, {image: {url: ssweb2}}, {quoted: m});  
+ }}}; 
+   handler.help = ["ss", "ssf"].map((v) => v + " <url>");   
+   handler.tags = ["internet"];   
+   handler.command = /^ss(web)?f?$/i;   
+   export default handler
