c0584b8763dbf35d7fda6c6e6e4497faf0927dff
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-01 23:44:40 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch' 
-const handler = async (m, {conn,text, args}) => {   
+const handler = async (m, {conn, text, args}) => {   
 if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);  
    try {
      const ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer();
