b523be0ac9f6c609e626596a75fbed16289ce7b0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-02 13:18:45 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -29,35 +29,16 @@ async function remini(imageData, operation) {
     }
     const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
     const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {
-      filename: "enhance_image_body.jpg",
-      contentType: "image/jpeg",
-    });
-    formData.append("model_version", 1, {
-      "Content-Transfer-Encoding": "binary",
-      contentType: "multipart/form-data; charset=utf-8",
-    });
-
-    formData.submit(
-      {
-        url: baseUrl,
-        host: "inferenceengine.vyro.ai",
-        path: "/" + operation,
-        protocol: "https:",
-        headers: {"User-Agent": "okhttp/4.9.3",
-        Connection: "Keep-Alive", "Accept-Encoding": "gzip"},
-      },
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
       function (err, res) {
         if (err) reject(err);
         const chunks = [];
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-        res.on("end", function () {
-          resolve(Buffer.concat(chunks));
-        });
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
         res.on("error", function (err) {
-          reject(err);
+        reject(err);
         });
       },
     );
