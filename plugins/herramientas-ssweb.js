f43e5e672c97bfa0566a43c8f20797c44b3061f9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-01 23:37:36 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,16 +1,24 @@
 import fetch from 'node-fetch' 
- const handler = async (m, {conn,text, args}) => {   
-  
-     if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);  
-  try {  
-       const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=${lolkeysapi}&url=${text}`; 
-     conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); } catch {  
- try { 
-  const link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;  
-  conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m}); } catch { 
- const ssweb2 = `https://api.lolhuman.xyz/api/SSWeb2?apikey=${lolkeysapi}&url=${text}`;
- conn.sendMessage(m.chat, {image: {url: ssweb2}}, {quoted: m});  
- }}}; 
+const handler = async (m, {conn,text, args}) => {   
+if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);  
+   try {
+     const ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer();
+        conn.sendFile(m.chat, ss, 'error.png', '', m);
+   } catch { 
+   try {  
+     const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=${lolkeysapi}&url=${text}`; 
+        conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); 
+   } catch {  
+   try { 
+     const link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;  
+        conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m}); 
+   } catch { 
+     const ssweb2 = `https://api.lolhuman.xyz/api/SSWeb2?apikey=${lolkeysapi}&url=${text}`;
+        conn.sendMessage(m.chat, {image: {url: ssweb2}}, {quoted: m});  
+   }
+  }
+ }
+}; 
    handler.help = ["ss", "ssf"].map((v) => v + " <url>");   
    handler.tags = ["internet"];   
    handler.command = /^ss(web)?f?$/i;   
