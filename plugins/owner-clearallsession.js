e85c0afc7a92ff520a663c55a668dac55f5f187a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-04 21:01:16 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-clearallsession.js
+++ plugins/owner-clearallsession.js
@@ -63,5 +63,6 @@ const handler = async (m, { conn, usedPrefix }) => {
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
-handler.command = /^(del_reg_in_session_owner|dsowner)$/i;
+handler.command = /^(del_reg_in_session_owner|dsowner|clearallsession)$/i;
+handler.rowner = true
 export default handler;
