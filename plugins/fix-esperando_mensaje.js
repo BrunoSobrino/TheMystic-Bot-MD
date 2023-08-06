6809f84a90629097e63fba31589401bfe8571e72
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-05 19:44:36 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -5,11 +5,7 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(
-      m.chat,
-      { text: '*[❗] Utiliza este comando directamente en el número principal del Bot*' },
-      { quoted: m }
-    );
+    return conn.sendMessage(m.chat, {text: '*[❗] Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
   const sessionPath = './MysticSession/';
