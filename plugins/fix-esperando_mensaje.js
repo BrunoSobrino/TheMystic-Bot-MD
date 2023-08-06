8d625f8c259672d5ced543a2c1ab4b1742a6db50
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-05 19:44:00 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -1,4 +1,5 @@
 /* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
+
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
 
@@ -10,11 +11,6 @@ const handler = async (m, { conn, usedPrefix }) => {
       { quoted: m }
     );
   }
-  await conn.sendMessage(
-    m.chat,
-    { text: '*[❗] Iniciando proceso de eliminación de todos los archivos de sesión que coincidan con su ID...*' },
-    { quoted: m }
-  );
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
   const sessionPath = './MysticSession/';
   try {
@@ -30,33 +26,15 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(
-        m.chat,
-        { text: '*[❗] No se encontró ningún archivo que incluya la ID del chat*' },
-        { quoted: m }
-      );
+      await conn.sendMessage(m.chat, {text: '*[❗] No se encontró ningún archivo que incluya la ID del chat*'}, {quoted: m});
     } else {
-      await conn.sendMessage(
-        m.chat,
-        { text: `*[❗] Se eliminaron ${filesDeleted} archivos de sesión*` },
-        { quoted: m }
-      );
+      await conn.sendMessage(m.chat, {text: `*[❗] Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
-    await conn.sendMessage(
-      m.chat,
-      { text: '*[❗] Ocurrió un error al eliminar los archivos de sesión*' },
-      { quoted: m }
-    );
+    await conn.sendMessage(m.chat, {text: '*[❗] Ocurrió un error al eliminar los archivos de sesión*'}, {quoted: m);
   }
-  await conn.sendMessage(
-    m.chat,
-    {
-      text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`,
-    },
-    { quoted: m }
-  );
+  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['fixmsgespera'];
 handler.tags = ['fix'];
