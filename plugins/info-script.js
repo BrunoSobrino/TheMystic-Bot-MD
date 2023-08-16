0f35b3bde77adf7d0f66958c029c4a0b75dadf4e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 10:06:08 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,12 +4,12 @@ const handler = async (m, { conn, args }) => {
    const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
    const json = await res.json();
    let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || ''}\n`;
-      txt += `◦  *Visitantes:* ${json?.watchers_count || ''}\n`;
-      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || ''} MB\n`;
-      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || ''}\n`;
-      txt += `◦  *Url:* ${json?.html_url || ''}\n\n`;
-      txt += `	 ${json?.forks_count || ''} Forks · ${json?.stargazers_count || ''} Stars · ${json?.open_issues_count || ''} Issues`;
+      txt += `◦  *Nombre:* ${json?.name || 'TheMystic-Bot-MD'}\n`;
+      txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
+      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
+      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
+      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}\n\n`;
+      txt += `	 ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
 };
 handler.help = ['scbot'];
