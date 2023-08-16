87438eeeae1be525964b5ca936da861f32828b17
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 11:46:27 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/maker-gay.js
+++ plugins/maker-gay.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn}) => {
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'error.png', '*🏳️‍🌈 𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙶𝙰𝚈 🏳️‍🌈*', m);
-  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mp4', ptt: true}, {quoted: m});
+  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
 handler.tags = ['maker'];
