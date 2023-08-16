4cbb587e0e5f2df278cfdc39ba426e6fc6809658
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 10:00:31 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -9,7 +9,7 @@ const handler = async (m, { conn, args }) => {
       txt += `◦  *Tamaño:* ${(json.size / 1024).toFixed(2)} MB\n`;
       txt += `◦  *Actualización:* ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
       txt += `◦  *Url:* ${json.html_url}\n\n`;
-      txt += `	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues`;
+      txt += `	 ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
 };
 handler.help = ['scbot'];
