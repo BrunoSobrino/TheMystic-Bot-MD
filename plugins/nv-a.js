79a9800c87842a31d26ba7b44a4a47baf4e8fbaa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 11:32:51 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -1,9 +1,10 @@
 const handler = async (m, {conn}) => {
   if (!db.data.chats[m.chat].audios && m.isGroup) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
+  //const s = seconds: '1934.4'
   const vn = './media/a.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
-  conn.sendMessage(m.chat, {audio: {url: vn}, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: m});
+  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: m});
 };
 handler.customPrefix = /ª|a|A/
 handler.command = /^(a|ª|A?$)/
