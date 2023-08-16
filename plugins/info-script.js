73633990661c66bdda41a2effe52da498819abcc
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 09:06:21 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,8 +1,8 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   let res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
-   let json = await res.json();
+   const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
+   const json = await res.json();
    let txt = `           *乂  B O T  -  S C R I P T  乂 *\n\n`;
       txt += `◦  *Nombre:* ${json.name}\n`;
       txt += `◦  *Visitantes: ${json.watchers_count}\n`;
@@ -11,8 +11,8 @@ const handler = async (m, { conn, args }) => {
       txt += `◦  *Url:* ${json.html_url}\n\n`;
       txt += `	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n`;
       txt += global.titulowm;
-   await conn.reply(m.chat,txt)
-}
+   await conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
+};
 handler.help = ['scbot'];
 handler.tags = ['info'];
 handler.command = /^(sc|scbot|scrip|script)$/i;
