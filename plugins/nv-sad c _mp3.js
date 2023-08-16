025de6235e315f149fc24c817ed4a74d48d6ab8a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 11:20:32 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/nv-sad c _mp3.js
+++ plugins/nv-sad c _mp3.js
@@ -1,5 +1,6 @@
 const handler = async (m, {conn}) => {
-  if (!db.data.chats[m.chat].audios && m.isGroup) throw 0;
+  if (!db.data.chats[m.chat].audios && m.isGroup) return;
+  if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
   const vn = './media/toma.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3`}, {quoted: m});
