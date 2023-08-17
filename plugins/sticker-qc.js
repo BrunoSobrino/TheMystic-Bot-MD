1f6727ec1ac74a1597f62690e7d0d2030439df51
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 18:17:13 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -10,12 +10,12 @@ let text
         text = m.quoted.text;
     } else throw "*[❗️] Uso incorrecto del comando, agregue un texto*";
    if (!text) return m.reply('*[❗️] Uso incorrecto del comando, agregue un texto*');
-   if (text.length > 30) return m.reply('*[❗️] El texto no puede tener mas de 30 caracteres*');
+   if (text.replace(who, '').join.length > 30) return m.reply('*[❗️] El texto no puede tener mas de 30 caracteres*');
 
 
     const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg');
-    const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": m.name, "photo": {"url": pp}}, "text": text, "replyMessage": {}}]};
+    const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": who.name, "photo": {"url": pp}}, "text": text.replace(who, '').join, "replyMessage": {}}]};
     const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
     const buffer = Buffer.from(json.data.result.image, 'base64');
   
