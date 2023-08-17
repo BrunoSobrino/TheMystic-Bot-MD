53dbec387ba76829622320d2c5a762581e8a61e3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 22:05:26 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,7 +1,7 @@
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
   //try {
-    const link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
+    const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
     if (!link || !link.match(linkRegex)) throw '*[❗] Link erroneo o faltante, ingrese el enlace de un grupo de WhatsApp*\n\n*—◉ Ejemplo:*\n*◉ #join https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC*';
     const [_, code] = link.match(linkRegex) || [];
     if ( isPrems || isMods || isOwner || m.fromMe) {
@@ -10,10 +10,7 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
     } else {
       const data = global.owner.filter(([id]) => id)[0];
       const dataArray = Array.isArray(data) ? data : [data];
-      for (const entry of dataArray) {
-      const id = entry;
-        await conn.sendMessage(id + '@s.whatsapp.net', {text: '*[❗] NUEVA SOLICITUD DE UN BOT PARA UN GRUPO [❗]*\n\n*—◉ Numero del solicitante:* ' + 'wa.me/' + m.sender.split('@')[0] + '\n*—◉ Link del grupo:* ' + link});
-      }
+      for (const entry of dataArray) await conn.sendMessage(entry + '@s.whatsapp.net', {text: '*[❗] NUEVA SOLICITUD DE UN BOT PARA UN GRUPO [❗]*\n\n*—◉ Numero del solicitante:* ' + 'wa.me/' + m.sender.split('@')[0] + '\n*—◉ Link del grupo:* ' + link});
       await conn.sendMessage(m.chat, {text: '*[❗] El link de su grupo fue enviado a mi propietario/a.*\n\n*—◉ Su grupo estará en evaluación y el propietario/a del Bot decidirá si agrega o no al Bot.*\n\n*—◉ Algunas de las razones por la cual su solicitud puede ser rechazada son:*\n*1.- El Bot está saturado.*\n*2.- El Bot fue eliminado del grupo recientemente.*\n*3.- El link del grupo ha sido restablecido.*\n*4.-El Bot no se agrega a grupos por decisión del propietario/a.*\n\n*—◉ El proceso de evaluación puede tomar algo de tiempo, incluso dias, tenga paciencia.*'}, {quoted: m});
     }
   //} catch {
