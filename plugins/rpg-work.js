1af64cbb748b01781c8c2410de93e8df6566e257
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-17 13:20:43 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/rpg-work.js
+++ plugins/rpg-work.js
@@ -1,17 +1,15 @@
-// creditos a https://github.com/FG98F
 const handler = async (m, {conn, isPrems}) => {
   const hasil = Math.floor(Math.random() * 5000);
   const time = global.db.data.users[m.sender].lastwork + 600000;
-  if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*𝙴𝚜𝚝𝚊𝚜 𝚌𝚊𝚗𝚜𝚊𝚍𝚘, 𝚍𝚎𝚋𝚎𝚜 𝚍𝚎𝚜𝚌𝚊𝚗𝚜𝚊𝚛 𝚌𝚘𝚖𝚘 𝚖𝚒𝚗𝚒𝚖𝚘 ${msToTime(time - new Date())} 𝚙𝚊𝚛𝚊 𝚟𝚘𝚕𝚟𝚎𝚛 𝚊 𝚝𝚛𝚊𝚋𝚊𝚓𝚊𝚛!*`;
-
-  m.reply(`${pickRandom(global.work)} *${hasil} XP*`);
-  global.db.data.users[m.sender].lastwork = new Date * 1;
+  if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `⚔️ *¡Espera un momento aventurero!* ⚔️\n\n*Regresa a la travesía en ${msToTime(time - new Date())}* ⏳`;
+  conn.sendMessage(m.chat, {text: `🏞️ *Te embarcas en una emocionante aventura:*\n\n🛠️ ${pickRandom(global.work)}\n\n💰 *¡Ganaste ${hasil} XP por tu valentía!* 💰`}, {quoted: m});
+  global.db.data.users[m.sender].exp += hasil;
+  global.db.data.users[m.sender].lastwork = new Date() * 1;
 };
 handler.help = ['work'];
 handler.tags = ['xp'];
 handler.command = ['work', 'trabajar'];
 handler.fail = null;
-handler.exp = 0;
 export default handler;
 
 function msToTime(duration) {
@@ -22,33 +20,57 @@ function msToTime(duration) {
   hours = (hours < 10) ? '0' + hours : hours;
   minutes = (minutes < 10) ? '0' + minutes : minutes;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
-
   return minutes + ' m ' + seconds + ' s ';
 }
-
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
 }
 
-global.work = ['Trabajas como cortador de galletas y ganas', 'Trabaja para una empresa militar privada, ganando', 'Organiza un evento de cata de vinos y obtiene',
-  'Te secuestran y te llevan a un coliseo subterráneo donde luchaste contra monstruos con personas que nunca antes habías conocido. Ganas', 'Limpias la chimenea y encuentras',
-  'Desarrollas juegos para ganarte la vida y ganas',
-  '¿Por qué este comando se llama trabajo? Ni siquiera estás haciendo nada relacionado con el trabajo. Sin embargo, ganas', 'Trabajaste en la oficina horas extras por',
-  'Trabajas como secuestrador de novias y ganas',
-  'Alguien vino y representó una obra de teatro. Por mirar te dieron', 'Compraste y vendiste artículos y Ganaste', 'Trabajas en el restaurante de la abuela como cocinera y ganas',
-  'Trabajas 10 minutos en un Pizza Hut local. Ganaste',
-  'Trabajas como escritor(a) de galletas de la fortuna y ganas', 'Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía',
-  'Ves a alguien luchando por subir una caja a su auto, te apresuras a ayudarlo antes de que se lastime. Después de ayudarlos, amablemente te dan',
-  'Desarrollas juegos para ganarte la vida y ganas',
-  'Ganas un concurso de comer chili picante. ¡El premio es',
-  'Trabajas todo el día en la empresa por',
-  'Ayudas a moderar el grupo de DyLux por', 'Diseñaste un logo para *FG* por',
-  'Moderaste el grupo cuando *FG* no estaba, el pago fue',
-  '¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!',
-  'Trabajas como podador de arbustos para *FG98* y ganas', 'La demanda de juegos para dispositivos móviles ha aumentado, por lo que creas un nuevo juego lleno de micro-transacciones. Con tu nuevo juego ganas un total de',
-  'Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar',
-  'Estabas cultivando y Ganaste', 'Trabajas como constructor de castillos de arena y ganas', 'Trabajaste y Ganaste',
-  'Trabajas como artista callejera y ganas', '¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste',
-  'Llevas mujeres a la tienda por',
+global.work = [
+  "Eres un maestro alquimista y ganas",
+  "Te conviertes en un intrépido cazador de tesoros y obtienes",
+  "Diriges un negocio de transmutación de metales y ganas",
+  "Exploras antiguas ruinas y encuentras una reliquia valiosa que te otorga",
+  "Trabajas como mercenario en una guerra épica y ganas",
+  "Eres un investigador de lo paranormal y recibes",
+  "Entrenas dragones para carreras y ganas",
+  "Te conviertes en el mejor herrero de la ciudad y obtienes",
+  "Descubres un bosque encantado lleno de criaturas mágicas y recibes",
+  "Eres un domador de bestias feroces y ganas",
+  "Viajas en el tiempo y resuelves problemas históricos para ganar",
+  "Eres un asesor real y obtienes",
+  "Desarrollas tecnología futurista y ganas",
+  "Eres un maestro en el arte de la persuasión y recibes",
+  "Piloteas un mecha gigante en batallas épicas y ganas",
+  "Diriges una granja de dragones y obtienes",
+  "Eres un espía internacional y ganas",
+  "Exploras el espacio y haces descubrimientos asombrosos que te otorgan",
+  "Eres un mago de renombre y realizas trucos impresionantes para ganar",
+  "Eres un científico loco y recibes",
+  "Defiendes el reino contra un ejército invasor y ganas",
+  "Eres un navegante audaz y encuentras una isla llena de tesoros, ganando",
+  "Eres un maestro en el arte del sigilo y obtienes",
+  "Eres un chef renombrado y ganas",
+  "Investigas crímenes complejos como un detective hábil y recibes",
+  "Eres un diplomático hábil y ganas",
+  "Eres un chamán poderoso y recibes",
+  "Desarrollas aplicaciones mágicas para dispositivos mágicos y ganas",
+  "Eres un campeón en torneos de lucha y obtienes",
+  "Eres un arquitecto visionario y ganas",
+  "Eres un psíquico con habilidades sobrenaturales y recibes",
+  "Eres un famoso director de cine y ganas",
+  "Eres un astrónomo y descubres un nuevo planeta, obteniendo",
+  "Eres un experto en supervivencia y ganas",
+  "Eres un músico talentoso que toca en conciertos masivos y recibes",
+  "Eres un explorador submarino y descubres tesoros hundidos, obteniendo",
+  "Eres un diseñador de moda reconocido y ganas",
+  "Eres un líder revolucionario y obtienes",
+  "Eres un médico que descubre una cura para una enfermedad mortal, ganando",
+  "Eres un hacker informático y recibes",
+  "Eres un jardinero botánico que encuentra una planta rara, obteniendo",
+  "Eres un cazador de mitos y ganas",
+  "Eres un arqueólogo que desentierra una ciudad antigua y obtienes",
+  "Eres un líder espiritual respetado y ganas",
+  "Eres un jugador profesional y obtienes",
 ];
