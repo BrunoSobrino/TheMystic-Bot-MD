12252145631ea76ecac424b8d114a2905b6cf568
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-16 19:01:21 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -14,7 +14,8 @@ let text
     const mishi = text.replace(who, '') 
    if (mishi.length > 30) return m.reply('*[❗️] El texto no puede tener mas de 30 caracteres*');
     const pp = await conn.profilePictureUrl(who, 'image')
-    const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || '-'}`, "photo": {url: `${pp || 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg'}`}}, "text": mishi, "replyMessage": {}}]};
+    const nombre = await conn.getName(who)
+    const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp || 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg'}`}}, "text": mishi, "replyMessage": {}}]};
     const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
     const buffer = Buffer.from(json.data.result.image, 'base64');
    let stiker = await sticker(buffer, false, global.packname, global.author);
