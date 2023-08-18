a266e6b90665a583591333e6fd9453a2544b5c5a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-17 19:15:08 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/anonymous_chat_.js.js
+++ plugins/anonymous_chat_.js.js
@@ -1,18 +1,19 @@
 export async function before(m, {match}) {
   if (!m.chat.endsWith('@s.whatsapp.net')) {
-    return;
+    return !0;
   }
   this.anonymous = this.anonymous ? this.anonymous : {};
   const room = Object.values(this.anonymous).find((room) => [room?.a, room?.b].includes(m.sender) && room?.state === 'CHATTING');
   if (room) {
-    if (/^.*(next|leave|start)/.test(m.text)) {
+    if (/^(next|leave|start)/.test(m.text)) {
+      const other = [room?.a, room?.b].find((user) => user !== m.sender);
+      await m.copyNForward(other, true);
+    }
+  } else {
+    if (/^(next|leave|start)/.test(m.text)) {
       return;
     }
-    const other = [room?.a, room?.b].find((user) => user !== m.sender);
-    await m.copyNForward(other, true);
-  } else if (!room) {
-    if (/^.*(next|leave|start)/.test(m.text)) return;
-   conn.sendMessage(m.chat, {text: `*[❗] No estás en un chat, por favor espera a estar en uno.*`}, {quoted: m});
+    conn.sendMessage(m.chat, {text: `*[❗] No estás en un chat, por favor espera a estar en uno.*`}, {quoted: m});
   }
   return !0;
 }
