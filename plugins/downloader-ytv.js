bee76acb1b9d2c89a1ad67dae3905274dbb18990
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-19 11:02:01 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -45,14 +45,14 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
     const fileSizeInMB = fileSizeInKB / 1024;
     const roundedFileSizeInMB = fileSizeInMB.toFixed(2);
    if (fileSizeInMB > 100) {
-    await conn.sendMessage(m.chat, {document: buff, fileName: ttl_1 + '.mp4', mimetype: 'video/mp4'}, {quoted: m});
-    await conn.sendMessage(m.chat, {text: `*[ ✔ ] Video descargado exitosamente, se envio en formato de docuemnto por que el video pesa ${fileSizeInMB} MB.*`, edit: key}, {quoted: m});
+    await conn.sendMessage(m.chat, {document: buff, caption: `*▢ Titulo:* ${ttl_1}\n*▢ Peso Del Video:* ${size} MB`, fileName: ttl_1 + '.mp4', mimetype: 'video/mp4'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: `*[ ✔ ] Video descargado exitosamente, se envio en formato de docuemnto por que el video pesa ${roundedFileSizeInMB} MB.*`, edit: key}, {quoted: m});
     enviando = false
    } else if (fileSizeInMB > 1000) { 
-    await conn.sendMessage(m.chat, {text: `*[ ❌ ] El video no pudo ser enviado debido a que pesa mas de 1GB, el video pesa ${fileSizeInMB} MB, puedes descargar el video en el siguiente link: ${formats.url}.*`, edit: key}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: `*[ ❌ ] El video no pudo ser enviado debido a que pesa mas de 1GB, el video pesa ${roundedFileSizeInMB} MB, puedes descargar el video en el siguiente link: ${formats.url}.*`, edit: key}, {quoted: m});
     enviando = false
    } else {
-    await conn.sendMessage(m.chat, {video: buff, caption: `_𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭_`, fileName: ttl_1 + '.mp4', mimetype: 'video/mp4'}, {quoted: m});
+    await conn.sendMessage(m.chat, {video: buff, caption: `*▢ Titulo:* ${ttl_1}\n*▢ Peso Del Video:* ${size} MB`, fileName: ttl_1 + '.mp4', mimetype: 'video/mp4'}, {quoted: m});
     await conn.sendMessage(m.chat, {text: `*[ ✔ ] Video descargado exitosamente.*`, edit: key}, {quoted: m});
     enviando = false   
    }
