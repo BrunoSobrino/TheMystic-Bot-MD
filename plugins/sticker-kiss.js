7a53677c6767fd537a69c7103f03dd30728d2386
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-21 22:59:23 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-kiss.js
+++ plugins/sticker-kiss.js
@@ -4,6 +4,7 @@ import MessageType from '@whiskeysockets/baileys';
 const handler = async (m, {conn, text}) => {
   try {
     const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender;
+    if (!who) throw "*[❗] Por favor, etiqueta o menciona a un participante del grupo.*";
     const res = await fetch('https://nekos.life/api/kiss');
     const json = await res.json();
     const {url} = json;
@@ -14,4 +15,5 @@ const handler = async (m, {conn, text}) => {
   } catch { }
 };
 handler.command = /^(kiss|skiss|kis|besos|beso)$/i;
+handler.group = true;
 export default handler;
