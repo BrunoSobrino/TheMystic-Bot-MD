29437d88c824c5d8b0c21ba39028b57e48b5b68a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-08-21 22:54:05 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/sticker-kiss.js
+++ plugins/sticker-kiss.js
@@ -3,14 +3,13 @@ import fetch from 'node-fetch';
 import MessageType from '@whiskeysockets/baileys';
 const handler = async (m, {conn, text}) => {
   try {
-    if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender);
-    if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
+    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender;
     const res = await fetch('https://nekos.life/api/kiss');
     const json = await res.json();
     const {url} = json;
     const name_1 = conn.getName(m.sender)
-    const name_2 = conn.getName(m.mentionedJid[0])
-    const stiker = await sticker(null, url, `${name_1} le dio besos a ${name_2 ? name_2 : 'alguien'}`);
+    const name_2 = conn.getName(who)
+    const stiker = await sticker(null, url, `${name_1} le dio besos a ${name_2}`);
     conn.sendFile(m.chat, stiker, null, null, m, false, {asSticker: true});
   } catch { }
 };
