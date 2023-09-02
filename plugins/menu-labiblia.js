21dbbbe59c4a117993c2cac7397529d73202c564
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-09-02 01:56:11 -0300

    Update menu-labiblia.js

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -32,7 +32,7 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `[❗] Los comandos +18
 ┣ ඬ⃟ 🔞 ${usedPrefix}hentaisearch <texto> / Este comando sirve para buscar un hentai con alguna palabra.
 ┗━━━━━━━━━━━━━━━━┛`.trim();
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-   await conn.reply(m.chat, str.trim(), {mentions: [taguser]})
+   await conn.reply(m.chat, str.trim(), m, {mentions: [m.sender]})
     }
   } catch {
     conn.reply(m.chat, '[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃', m);
