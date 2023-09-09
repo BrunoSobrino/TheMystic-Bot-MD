b4f7d157f7dce998daa32a9baf6dc04d415da4bb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-09 16:46:33 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
-let previousCommitUser = ''; // user commits?
+let previousCommitUser = ''; 
 const owner = 'BrunoSobrino';
 const repo = 'TheMystic-Bot-MD';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
