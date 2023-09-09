9ae13bf40745bacc9b3fefe6ad8f5c7081ea11cd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-09 11:29:07 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-kprocess.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import './config.js';
 import './api.js';
 import {createRequire} from 'module';
