9e96ae438a3e71f676253dc48ea9919ca4c28443
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-18 20:50:04 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -3,8 +3,14 @@ import { execSync } from 'child_process';
 const handler = async (m, { conn, text }) => {
   try {
     if (global.conn.user.jid == conn.user.jid) {
+          const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+          let messager = stdout.toString()
+          if (messager.includes('Already up to date.')) messager = '*[❗] Tienes todo actualiado.*'
+          conn.reply(m.chat, messager, m);
+    }
+  } catch {      
+ try {    
       const status = execSync('git status --porcelain');
-      const errorMessage = `*[❗] Se han hecho cambios en los archivos del Bot en local y causa conflictos al actualizar ya que igual se han modificado en el repositorio oficial.*\n\n*—◉ Archivos con conflicto:*\n`;
       if (status.length > 0) {
         const conflictedFiles = status
           .toString()
@@ -17,40 +23,23 @@ const handler = async (m, { conn, text }) => {
             return '*◉ ' + line.slice(3) + '*';
           })
           .filter(Boolean);
+        if (conflictedFiles.length > 0) {
+          const errorMessage = `*[❗] Se han hecho cambios en los archivos del Bot en local y causa conflictos al actualizar ya que igual se han modificado en el repositorio oficial.*\n\n*—◉ Archivos con conflicto:*\n${conflictedFiles.join('\n')}\n\n*—◉ Si deseas actualizar el Bot, deberás reinstalar el Bot o hacer las actualizaciones manualmente.*`;
+          await conn.reply(m.chat, errorMessage, m);  
+        }
       }
-      const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-      conn.reply(m.chat, stdout.toString(), m);
-    }
   } catch (error) {
     console.error(error);
-    await conn.reply(m.chat, errorMessage + conflictedFiles.join('\n'), m);  }
+    let errorMessage2 = 'An error occurred while executing the command.';
+    if (error.message) {
+      errorMessage2 += '\nError message: ' + error.message;
+    }
+    await conn.reply(m.chat, errorMessage2, m);
+  }
+ }
 };
 handler.help = ['update'];
 handler.tags = ['owner'];
-handler.command = /^update|actualizar$/i;
+handler.command = /^(update2|actualizar)$/i;
 handler.rowner = true;
 export default handler;
-
-/*import {execSync} from 'child_process';
-
-const handler = async (m, {conn, text}) => {
-  try {
-    if (global.conn.user.jid == conn.user.jid) {
-      const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-      conn.reply(m.chat, stdout.toString(), m);
-    }
-  } catch (error) {
-    console.error(error);
-    let errorMessage = 'An error occurred while executing the command.';
-    if (error.message) {
-      errorMessage += '\nError message: ' + error.message;
-    }
-    await conn.reply(m.chat, errorMessage, m);
-  }
-};
-
-handler.help = ['update'];
-handler.tags = ['owner'];
-handler.command = /^update|actualizar$/i;
-handler.rowner = true;
-export default handler;*/
