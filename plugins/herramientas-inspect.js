1b49c5ab27dd97fb1aa79d7f65db92c55b48c83f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-18 23:46:03 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-inspect.js
+++ plugins/herramientas-inspect.js
@@ -2,18 +2,16 @@ import * as baileys from '@whiskeysockets/baileys';
 
 const handler = async (m, {conn, text}) => {
   const [, code] = text.match(/chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i) || [];
-  if (!code) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*';
+  if (!code) throw '*[❗] Ingrese el link de un grupo de WhatsApp.*';
   const res = await conn.query({tag: 'iq', attrs: {type: 'get', xmlns: 'w:g2', to: '@g.us'}, content: [{tag: 'invite', attrs: {code}}]});
   const data = extractGroupMetadata(res);
   const txt = Object.keys(data).map((v) => `*${v.capitalize()}:* ${data[v]}`).join('\n');
   const pp = await conn.profilePictureUrl(data.id, 'image').catch(console.error);
   if (pp) return conn.sendMessage(m.chat, {image: {url: pp}, caption: txt}, {quoted: m});
-  const groupinfo = `⫹⫺ 𝙸𝙳: ${data.id}\n⫹⫺ 𝙽𝙾𝙼𝙱𝚁𝙴: ${data.subject}\n⫹⫺ 𝙲𝚁𝙴𝙰𝙳𝙾: ${data.creation}\n⫹⫺ 𝙾𝚆𝙽𝙴𝚁: ${data.owner}\n⫹⫺ 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:\n${data.desc}`;
+  const groupinfo = `*⫹⫺ ID:* ${data.id}\n*⫹⫺ Nombre:* ${data.subject}\n*⫹⫺ Fecha de creación:* ${data.creation}\n*⫹⫺ Creador:* ${data.owner}\n*⫹⫺ Descripción:*\n${data.desc}`;
   await conn.reply(m.chat, groupinfo, m);
-  // m.reply(txt)
 };
 handler.command = /^(inspect)$/i;
-
 export default handler;
 
 const extractGroupMetadata = (result) => {
@@ -25,9 +23,7 @@ const extractGroupMetadata = (result) => {
     id: group.attrs.id.includes('@') ? group.attrs.id : baileys.jidEncode(group.attrs.id, 'g.us'),
     subject: group.attrs.subject,
     creation: new Date(+group.attrs.creation * 1000).toLocaleString('id', {timeZone: 'Asia/Jakarta'}),
-    owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] :
-			// group.attrs.s_o ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.s_o).split('@')[0] :
-			group.attrs.id.includes('-') ? 'wa.me/' + group.attrs.id.split('-')[0] : '',
+    owner: group.attrs.creator ? 'wa.me/' + baileys.jidNormalizedUser(group.attrs.creator).split('@')[0] : group.attrs.id.includes('-') ? 'wa.me/' + group.attrs.id.split('-')[0] : '',
     desc,
   };
   return metadata;
