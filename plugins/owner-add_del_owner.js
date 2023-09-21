1e9fbc049200da852a9ae62f825fd6880aedd04d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-20 19:14:20 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,7 +1,7 @@
-const handler = async (m, { conn, args, usedPrefix, command }) => {
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
   const why = `*[❗] Uso incorrecto del comando.*\n\n*—◉ Ejemplo de uso válido:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} <mensaje citado>*`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
