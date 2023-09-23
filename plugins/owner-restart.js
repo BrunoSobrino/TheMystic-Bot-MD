8f42c1cba56a3e5f9d57e45baa12d296e3c775b7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-23 11:48:08 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,11 +1,10 @@
-import {spawn} from 'child_process';
 const handler = async (m, {conn, isROwner, text}) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
-  if (conn.user.jid == conn.user.jid) {
+  //if (conn.user.jid == conn.user.jid) {
     // conn.readMessages([m.key])
-    await m.reply('🔄 Reiniciando Bot...\n Espere un momento');
+    await m.reply('*[ ⚠ ] Reiniciando el Bot...*\n\n*—◉ Espere un momento, puede tomar unos minutos.*');
     process.send('reset');
-  } else throw 'eh';
+  //} else throw 'eh';
 };
 handler.help = ['restart'];
 handler.tags = ['owner'];
