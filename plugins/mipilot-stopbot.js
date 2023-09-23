5ec4c080bdccbfff06c30a0b2fa9071bc10bba56
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-09-23 00:32:40 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/mipilot-stopbot.js
+++ plugins/mipilot-stopbot.js
@@ -1,6 +1,6 @@
 import fs from "fs"
 async function handler(m, {conn, usedPrefix}) {
-   if (conn.user.jid == global.conn.user.jid) return m.reply(`*No puedes apagar el bot principal.*`)
+   if (conn.user.jid == global.conn.user.jid) return m.reply(`*[❗] No puedes apagar el Bot principal.*`)
    m.reply(`*[❗] Dejarás de ser un Sub Bot en 5 segundos...*`)
    conn.fstop = true
    conn.ws.close()
