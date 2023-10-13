c8dac03f69eab4cd828fbd8438567226f4573700
Hisiskanam <125500626+Hisiskanam@users.noreply.github.com>
2023-10-12 20:48:40 -0500

    config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['51950615082', '👑 Mystic - Creador 👑', true],
   ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
