ea0cc4e90ff3310207c311694d769a028e779ec7
Jencruz <89224467+jenfast@users.noreply.github.com>
2023-10-15 00:15:14 -0400

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,36 +1,69 @@
-const handler = async (m, {conn}) => {
+const handler = async (m, { conn }) => {
   try {
-    const pp = imagen6;
-    const img = await(await fetch('https://github.com/BrunoSobrino.png')).buffer();
+    const pp = 'https://github.com/jenfast.png'; // URL de la imagen
+    const img = await (await fetch(pp)).buffer(); // Se descarga la imagen
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `╭────[ *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* ]
+    const str = `╭────[ *FAST OBB - BOT* ]
 │
 │ *➤ ʜᴏʟᴀ ${taguser}*
 │
 │ *=> 🤖 ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ:* ${uptime}
-│ *=> ✅ ʙᴏᴛ ᴅᴇ ᴜsᴏ ᴘᴜʙʟɪᴄᴏ*
-│ *=> 👑 ᴄʀᴇᴀᴅᴏʀ: ʙʀᴜɴᴏ sᴏʙʀɪɴᴏ*
-│ *=> 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ:* https://www.atom.bio/theshadowbrokers-team
+│ *=> ✅ 𝖇𝖔𝖙 𝖉𝖊 𝖚𝖘𝖔 𝖕𝖗𝖎𝖛𝖆𝖉𝖔*
+│ *=> 👑 𝕮𝖗𝖊𝖆𝖉𝖔𝖗:  𝕵𝖊𝖓 𝖋𝖆$✝️*
 ╰────────────────`.trim();
+
     if (m.isGroup) {
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
+      // En un grupo
+      conn.sendMessage(m.chat, {
+        text: str.trim(),
+        mentions: [m.sender], // Mencionamos al remitente
+        contextInfo: {
+          forwardingScore: 9999999,
+          isForwarded: true,
+          mentionedJid: [m.sender],
+          externalAdReply: {
+            "showAdAttribution": true,
+            "containsAutoReply": true,
+            "renderLargerThumbnail": true,
+            "title": global.titulowm,
+            "containsAutoReply": true,
+            "mediaType": 2, // Cambiamos el tipo de medio a imagen
+            "thumbnail": img, // La imagen se establece como miniatura
+            "mediaUrl": pp, // URL de la imagen
+            "sourceUrl": pp // URL de la imagen
+          }
+        }
+      }, { quoted: m });
     } else {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: fkontak2});
+      // En un chat individual
+      conn.sendMessage(m.chat, {
+        text: str.trim(),
+        mentions: [m.sender],
+        contextInfo: {
+          forwardingScore: 9999999,
+          isForwarded: true,
+          mentionedJid: [m.sender],
+          externalAdReply: {
+            "showAdAttribution": true,
+            "containsAutoReply": true,
+            "renderLargerThumbnail": true,
+            "title": global.titulowm,
+            "containsAutoReply": true,
+            "mediaType": 2,
+            "thumbnail": img,
+            "mediaUrl": pp,
+            "sourceUrl": pp
+          }
+        }
+      }, { quoted: m });
     }
-  } catch {
+  } catch (error) {
+    console.error(error);
   }
 };
-handler.help = ['estado'];
-handler.tags = ['main'];
-handler.command = /^(estado|status|estate|state|stado|stats|runtime|uptime)$/i;
+
+// Resto del código (clockString, handler.help, handler.tags, handler.command)
+
 export default handler;
-function clockString(ms) {
-  const d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
-  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
-  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n│ *=> 💥 ` + d, ' Día(s)* ', `\n│ *=> 💫 ` + h, ' Hora(s)* ', `\n│ *=> 💠 ` + m, ' Minuto(s)* ', `\n│ *=> ♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
-}
