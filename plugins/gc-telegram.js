baa7eca1c661af04f9fdb1272a7889ddc9bd8ff6
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-19 23:46:54 -0300

    Update gc-telegram.js

--- plugins/gc-telegram.js
+++ plugins/gc-telegram.js
@@ -3,6 +3,7 @@ m.reply(global.telegram)}
 handler.help = ['telegram']
 handler.tags = ['grupo']
 handler.command = /^(telegram|grupodetelegram|linktelegram)$/i
+handler.group = true;
 export default handler
 
 global.telegram = `Nuestro grupo de Telegram!
