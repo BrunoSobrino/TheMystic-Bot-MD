a692bef5d7a7d395cde7ef575fc23c4dd5540ec4
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-19 23:45:47 -0300

    Update gc-concurso.js

--- plugins/gc-concurso.js
+++ plugins/gc-concurso.js
@@ -3,6 +3,7 @@ m.reply(global.concurso)}
 handler.help = ['concurso']
 handler.tags = ['grupo']
 handler.command = /^(concurso|concursofutabuclub)$/i
+handler.group = true;
 export default handler
 
 global.concurso = `Nada aún!`
