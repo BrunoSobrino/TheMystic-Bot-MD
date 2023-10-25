71f3b75f6136e10081ceea43485ad73ac36beac9
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-25 00:24:29 -0300

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('*[❗] ' + `${user.warn == 1 ? `Hola @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, decir la palabra "${isToxic}" está prohibido en este grupo. Advertencia: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 15)) await m.reply('*[❗] ' + `${user.warn == 1 ? `Hola @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, decir la palabra "${isToxic}" está prohibido en este grupo. Advertencia: ${user.warn}/15.` + '*', false, {mentions: [m.sender]});
   }
 
-  if (user.warn >= 5) {
+  if (user.warn >= 15) {
     user.warn = 0;
-    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, superaste las 5 advertencias por lo que serás eliminado de este grupo por tu comportamiento.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, superaste las 15 advertencias por lo que serás eliminado de este grupo por tu comportamiento.*`, false, {mentions: [m.sender]});
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
