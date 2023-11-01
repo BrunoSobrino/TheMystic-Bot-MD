8a7dc91659d876148f11792a956df3356f337fb7
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-11-01 05:05:22 -0300

    Update gc-eventos.js

--- plugins/gc-eventos.js
+++ plugins/gc-eventos.js
@@ -6,12 +6,4 @@ handler.command = /^(evento|eventos|eventofutabuclub)$/i
 handler.group = true;
 export default handler
 
-global.eventos = `ℹ| Hola miembro!, Bienvenido al evento de Halloween de Futabu Club, Este evento consiste en que el miembro que mas aporte sobre la tematica de Futanaris de Halloween, ganara una cuenta de Crunchyroll Premium (1 Mes) [Pagina para ver animes, exclusivos, nuevos y doblados!], Los aportes pueden ser de Imagenes, Videos e Gifs!, Buena suerte!
-
-🏆| Cuenta Crunchyroll Premium (1 Mes)
-
-⏰| 31 de Octubre de 2023 (Finaliza el 1 de Noviembre de 2023)
-
-🤔| Como se dara los resultados?, Los resultados se dara contando cada aporte del miembro con la tematica, Las imagenes son 1 punto, los gifs 3 puntos y los videos 5 puntos! (estos se iran acumlando en total)
-
-🔗| Lugar del evento: https://chat.whatsapp.com/KT4Ct4CyUbGHxcRxzNu7hX`
+global.eventos = `Nada aun!`
