df83d5fca46c151ea76e8896b573a1ce5ac8b1e9
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-11-01 02:25:36 -0300

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,23 +1,11 @@
-const handler = async (m, { text, conn }) => {
+const handler = async (m, {text}) => {
   const user = global.db.data.users[m.sender];
-  user.afk = +new Date;
+  user.afk = + new Date;
   user.afkReason = text;
-  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 ${conn.getName(m.sender)} 𝙴𝚂𝚃𝙰𝚁𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺), 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙽𝙾 𝙻𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴𝙽*\n\n*—◉ 𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙻𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺)${text ? ': ' + text : ''}*`);
+  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 ${conn.getName(m.sender)} 𝙴𝚂𝚃𝙰𝚁𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺), 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙽𝙾 𝙻𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴𝙽*\n\n*—◉ 𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙻𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
+`);
 };
-
 handler.help = ['afk [alasan]'];
 handler.tags = ['main'];
 handler.command = /^afk$/i;
-
-handler.before = async function (m, { command }) {
-  if (command === 'banuser' || command === 'kick' || command === 'warn') {
-    const user = global.db.data.users[m.sender];
-    if (user.afk) {
-      user.afk = -1;
-      user.afkReason = '';
-    }
-  }
-  return true;
-};
-
 export default handler;
