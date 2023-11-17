8143e9cde3009151caaf860b1401fdad2d6b7206
Diego Turcios <dftzippo@zipponodes.com>
2023-11-17 17:30:53 -0600

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -98,10 +98,10 @@ function secondString(seconds) {
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d == 1 ? ' d ' : ' d ') : '';
-  const hDisplay = h > 0 ? h + (h == 1 ? ' h ' : ' h ') : '';
-  const mDisplay = m > 0 ? m + (m == 1 ? ' m ' : ' m ') : '';
-  const sDisplay = s > 0 ? s + (s == 1 ? ' s' : ' s') : '';
+  const dDisplay = d > 0 ? d + (d == 1 ? 'd ' : 'd ') : '';
+  const hDisplay = h > 0 ? h + (h == 1 ? 'h ' : 'h ') : '';
+  const mDisplay = m > 0 ? m + (m == 1 ? 'm ' : 'm ') : '';
+  const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : '';
   return dDisplay + hDisplay + mDisplay + sDisplay;
 }
 
