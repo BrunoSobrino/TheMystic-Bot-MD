c64a1b7a37576b942086d944f8e4481542cbfdd3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-11-19 18:37:50 -0600

    Update game-math_answer.js

--- plugins/game-math_answer.js
+++ plugins/game-math_answer.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn}) => {
   const id = m.chat;
   if (!m.quoted) return;
   if (m.quoted.sender != conn.user.jid) return;
-  if (!/^𝙲𝚄𝙰𝙽𝚃𝙾 𝙴𝚂 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴/i.test(m.quoted.text)) return;
+  if (!/^_*< JUEGOS - MATEMÁTICAS />*_/i.test(m.quoted.text)) return;
   if (!(m.chat in global.math)) return conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚂𝙴 𝙰𝙷 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙸𝙳𝙾 𝙰 𝙴𝚂𝙰 𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰*`, m);
   // conn.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚂𝙴 𝙰𝙷 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙸𝙳𝙾 𝙰 𝙴𝚂𝙰 𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰*', author, null, [['𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝙹𝚄𝙶𝙰𝚁', '/mates']], m)
   if (m.quoted.id == global.math[id][0].id) {
