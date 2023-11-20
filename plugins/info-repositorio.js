bea12f6402f43213080ed9a712b9bffd2535f297
Diego Turcios <dftzippo@zipponodes.com>
2023-11-20 11:50:26 -0600

    Update info-repositorio.js

--- plugins/info-repositorio.js
+++ plugins/info-repositorio.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
-const handler = async (m, { conn, args }) => {
+const handler = async (m, { conn, args, usedPrefix }) => {
    const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
    const json = await res.json();
    let txt = `*[ ℹ️ ] Info - Repositorio*\n\n`;
@@ -10,7 +10,7 @@ const handler = async (m, { conn, args }) => {
       txt += `▢ *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n\n`;
       txt += `▢ *URL:* ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}\n\n`;
       txt += `${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues\n\n`;
-      txt += `*[ ℹ️ ] Puedes descargar este repositorio enviando el comando:*\n_#gitclone ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}_`;
+      txt += `*[ ℹ️ ] Puedes descargar este repositorio enviando el comando:*\n_${usedPrefix}gitclone ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}_`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
 };
 handler.command = ['script', 'repositorio']
