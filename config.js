9e11a39c54a60f3b119b4b97f28939ac51e6d8bb
XxShadowXXY2 <151991823+XxShadowXXY2@users.noreply.github.com>
2023-11-25 03:21:22 -0400

    Update config.js
    Ñ

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
+  ['12709060288', '👑 Mystic - Creador 👑', true],
+  ['584124172474', '👑 Mystic - Creador 👑', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
