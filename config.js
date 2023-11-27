3d55503ee519a2140dbc65d9fdb3ca7d32e055bc
LxShadow <152157350+LxShadow@users.noreply.github.com>
2023-11-27 15:36:16 -0600

    config.js

--- config.js
+++ config.js
@@ -10,47 +10,47 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50431953376'],
-  ['595992611272']
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow ', true],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250', ' LxShadow 🥀', false],
+  ['5215541081250', ' LxShadow 🥀', true],
+  ['5215541081250',' LxShadow 🥀', true],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250'], 
+  ['5215541081250'],  
+  ['5215541081250'],
+  ['5215541081250'],
+  ['5215541081250']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['5215541081250'];
+global.prems = ['5215541081250'];
 
-global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
-global.wait = '*[ ⏳ ] Cargando...*';
+global.packname = 'LxShadow ~ Bot 🥀';
+global.author = 'LxShadow ~ Bot 🥀';
+global.wm = 'LxShadow ~ Bot 🥀';
+global.titulowm = 'LxShadow ~ Bot 🥀';
+global.titulowm2 = `LxShadow ~ Bot 🥀`
+global.igfg = 'LxShadow ~ Bot 🥀';
+global.wait = '*[ ❗️ ] Cargando...*';
 
-global.imagen1 = fs.readFileSync('./Menu2.jpg');
-global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
-global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
-global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./src/+18.jpg');
-global.imagen6 = fs.readFileSync('./Menu3.png');
+global.imagen1 = fs.readFileSync('./shadow.png');
+global.imagen2 = fs.readFileSync('./src/shadow.png');
+global.imagen3 = fs.readFileSync('./src/shadow.png');
+global.imagen4 = fs.readFileSync('./shadow.png');
+global.imagen5 = fs.readFileSync('./src/shadow.png');
+global.imagen6 = fs.readFileSync('./shadow.png');
 
 global.mods = [];
 
@@ -63,15 +63,15 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] Cargando...*';
-global.waittt = '*[ ⏳ ] Cargando...*';
-global.waitttt = '*[ ⏳ ] Cargando...*';
-global.nomorown = '5219993404349';
+global.wm2 = `${dia} ${fecha}\n @LxShadow ~ Bot 🥀`;
+global.gt = '@LxShadow ~ Bot 🥀';
+global.mysticbot = ' @LxShadow ~ Bot 🥀';
+global.md = 'https://github.com/LxShadow/LxShadow';
+global.mysticbot = 'https://github.com/LxShadow/LxShadow';
+global.waitt = '*[ ❗️ ] Cargando...*';
+global.waittt = '*[ ❗️ ] Cargando...*';
+global.waitttt = '*[ ❗️ ] Cargando...*';
+global.nomorown = '5215541081250';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
