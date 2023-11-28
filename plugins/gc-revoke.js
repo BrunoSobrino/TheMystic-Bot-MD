b52943dccb7dd017c535d3a5b00ceefb3e997210
LxShadow <152157350+LxShadow@users.noreply.github.com>
2023-11-28 15:30:24 -0600

    gc-revoke.js

--- plugins/gc-revoke.js
+++ plugins/gc-revoke.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, {conn}) => {
   const revoke = await conn.groupRevokeInvite(m.chat);
-  await conn.reply(m.chat, `🔹️ *_Se restableció con éxito el link del grupo._*\n♾ • Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m);
+  await conn.reply(m.chat, `🔹️ *_S𝐒𝐞 𝐇𝐚 𝐑𝐞𝐬𝐭𝐚𝐛𝐥𝐞𝐜𝐢𝐝𝐨 𝐂𝐨𝐧 𝐄́𝐱𝐢𝐭𝐨 𝐄𝐥 𝐋𝐢𝐧𝐤 𝐃𝐞𝐥 𝐆𝐫𝐮𝐩𝐨._*\n♾ • Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m);
 };
 handler.command = ['resetlink', 'revoke'];
 handler.botAdmin = true;
