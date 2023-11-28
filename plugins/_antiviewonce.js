9f52fce0fa4af06957783400c0f3989c1aa0f155
LxShadow <152157350+LxShadow@users.noreply.github.com>
2023-11-28 00:56:47 -0600

    _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -12,7 +12,7 @@ export async function before(m, {isAdmin, isBotAdmin}) {
     for await (const chunk of media) {
       buffer = Buffer.concat([buffer, chunk]);
     }
-    const cap = '*- En este grupo, no se permite ocultar nada.*'
+    const cap = '*- ❌️ Aqui no ocultaras nada.*'
     if (/video/.test(type)) {
       return mconn.conn.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
     } else if (/image/.test(type)) {
