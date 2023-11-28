d58dbadb0f34eaaf8f2c7a02f048b8c3ba12d5a0
LxShadow <152157350+LxShadow@users.noreply.github.com>
2023-11-28 15:36:02 -0600

    gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*');
+  m.reply('*✅ 𝐒𝐞 𝐇𝐚 𝐌𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐝𝐨 𝐂𝐨𝐫𝐫𝐞𝐜𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐋𝐚 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨́𝐧 𝐃𝐞𝐥 𝐆𝐫𝐮𝐩𝐨 );
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
