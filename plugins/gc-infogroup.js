6bd9f81736f5bb311efaacd10f2f147891e4660e
LxShadow <152157350+LxShadow@users.noreply.github.com>
2023-11-27 19:24:58 -0600

    gc-infogroup.js

--- plugins/gc-infogroup.js
+++ plugins/gc-infogroup.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/avatar_contact.png';
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/shadow.png';
   const {antiToxic, antiTraba, antidelete, antiviewonce, isBanned, welcome, detect, detect2, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, modoadmin, audios, delete: del} = global.db.data.chats[m.chat];
   const groupAdmins = participants.filter((p) => p.admin);
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
