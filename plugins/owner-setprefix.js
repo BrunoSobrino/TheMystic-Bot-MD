c3e6b993a72ff5aff685dcebad7b505fbd174dc1
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-12-04 19:06:58 -0300

    Update owner-setprefix.js

--- plugins/owner-setprefix.js
+++ plugins/owner-setprefix.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*[❗] No se encontro ningun prefijo, por favor ingrese el prefijo que quiera establecer. Ejemplo:* ${usedPrefix + command} /`;
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
   await m.reply(`*[❗] El prefijo actual del Bot se establecio a ${text}*`);
 };
 handler.help = ['setprefix'].map((v) => v + ' [prefix]');
