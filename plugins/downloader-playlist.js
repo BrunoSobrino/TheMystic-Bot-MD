1ae6b9d20d4f7cdaf7a2bf74b199574b9dcc1e5d
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2023-12-17 15:50:21 +0100

    Update downloader-playlist.js

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -1,8 +1,7 @@
 import yts from 'yt-search';
 import fs from 'fs';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `**إسم الأغنية مفقود عزيري المستخدم ⌛⚠️*
-*${usedPrefix + command} how to like*`;
+  if (!text) throw `*هذا الامر لتحميل قوائم الموسيقى 💿*\n\n*مثال:*\n*${usedPrefix + command} like you*`;
   try {
     const vids_ = {
       from: m.sender,
@@ -19,23 +18,22 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 ◉ ${usedPrefix}audio <numero>
 ◉ ${usedPrefix}video <numero> 
 
-*—◉ مثال:*
+*—◉ Ejemplos:*
 *◉ ${usedPrefix}audio 5*
 *◉ ${usedPrefix}video 8*`.trim();
     const teks = results.all.map((v, i) => {
       const link = v.url;
       vids_.urls.push(link);
       return `[${i + 1}] ${v.title}
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_المدة  :_* ${v.timestamp}
+↳ 📥 *_التحميلات :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`;
     }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
     conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
     global.videoList.push(vids_);
   } catch {
-    await m.reply('*إسم الأغنية مفقود عزيري المستخدم ⌛⚠️*
-*.playlist blackpink  how you like*');
+    await m.reply('*أعتذر القائمة كبيرة الحجم 😞⚠️*');
   }
 };
 handler.help = ['playlist *<texto>*'];
