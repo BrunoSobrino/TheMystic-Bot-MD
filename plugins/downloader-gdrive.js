f3e5961a2ee390a3084a10d3884b86e8f10f52ae
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2023-12-22 19:12:01 +0100

    Update downloader-gdrive.js

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -1,41 +1,46 @@
-import fetch from 'node-fetch';
-import {sizeFormatter} from 'human-readable';
-const formatSize = sizeFormatter({
-  std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
-const handler = async (m, {conn, args}) => {
-  if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*';
-  try {
-    GDriveDl(args[0]).then(async (res) => {
-      conn.reply(m.chat, الأمر التالي يسمح لك بتحميل الملفات الأصغر من 100 بايت', m);
-      if (!res) throw res;
-      conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, {mimetype: res.mimetype, asDocument: true});
-    });
-  } catch (e) {
-    m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*');
-    console.log(e);
-  }
-};
-handler.command = /^(gdrive)$/i;
-export default handler;
+import fetch from 'node-fetch'
+import { sizeFormatter } from 'human-readable'
+let formatSize = sizeFormatter({
+	std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`
+})
+
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw '\nهذا الامر خاص بتحميل الملفات من غوغل درايف مثال : \n\n*.gdrive*  https://drive.google.com/u/0/uc?id=1-XxkNNYHEvKlbPMEMNbgEBg6tgKEC8bO&export=download' 
+	GDriveDl(args[0]).then(async (res) => {
+		if (!res) throw res
+		await m.reply(JSON.stringify(res, null, 2))
+		conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
+	})
+}
+
+handler.help = ['gdrive']
+handler.tags = ['downloader']
+handler.command = /^(gdrive)$/i
+handler.disabled = false
+
+export default handler
+
 async function GDriveDl(url) {
-  let id;
-  if (!(url && url.match(/drive\.google/i))) throw 'Invalid URL';
-  id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1];
-  if (!id) throw 'ID Not Found';
-  const res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
-    method: 'post',
-    headers: {
-      'accept-encoding': 'gzip, deflate, br',
-      'content-length': 0,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      'origin': 'https://drive.google.com',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-      'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
-      'x-drive-first-party': 'DriveWebUi',
-      'x-json-requested': 'true'}});
-  const {fileName, sizeBytes, downloadUrl} = JSON.parse((await res.text()).slice(4));
-  if (!downloadUrl) throw 'Link Download Limit!';
-  const data = await fetch(downloadUrl);
-  if (data.status !== 200) throw data.statusText;
-  return {downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type')};
+	let id
+	if (!(url && url.match(/drive\.google/i))) throw 'هذا الرابط غير موجود'
+	id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1]
+	if (!id) throw 'لم يتم العثور على المعرف'
+	let res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
+		method: 'post',
+		headers: {
+			'accept-encoding': 'gzip, deflate, br',
+			'content-length': 0,
+			'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+			'origin': 'https://drive.google.com',
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+			'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
+			'x-drive-first-party': 'DriveWebUi',
+			'x-json-requested': 'true' 
+		}
+	})
+	let { fileName, sizeBytes, downloadUrl } =  JSON.parse((await res.text()).slice(4))
+	if (!downloadUrl) throw 'رابط تحميل محدود!'
+	let data = await fetch(downloadUrl)
+	if (data.status !== 200) throw data.statusText
+	return { downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type') }
 }
