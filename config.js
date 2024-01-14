a9d2be0a81285af107f66c57e5123ecc28ab79fc
Werkopp7 <155495339+Werkopp@users.noreply.github.com>
2024-01-13 22:35:28 -0600

    Update config.js wrko

--- config.js
+++ config.js
@@ -7,43 +7,20 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "+994 40 300 38 76"
 
-global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50431953376'],
-  ['51906662557'],
-  ['595992611272']
-];
+global.owner = [' 𝙒𝙚𝙧𝙠𝙞𝙩𝙤𝙥 ']
+  ['+994 40 580 80 01', '👑 Wrko - Creador 👑', true],
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['+994405808001'];
+global.prems = ['+994405808001'];
 
-global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.packname = ' 𝐖𝐑𝐊𝐎 𝐁𝐎𝐓 ';
+global.author = '';
+global.wm = '  𝐖𝐑𝐊𝐎 𝐁𝐎𝐓 ';
+global.titulowm = ' 𝐖𝐑𝐊𝐎 𝐁𝐎𝐓 ';
+global.titulowm2 = ` 𝐖𝐑𝐊𝐎 𝐁𝐎𝐓 `
+global.igfg = ' 𝐖𝐑𝐊𝐎 𝐁𝐎𝐓';
 global.wait = '*[ ⏳ ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
