664b0dd30bd092785877b958513a27bfdc6adf31
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-01-23 10:32:27 -0300

    🔮 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🧿

--- handler.js
+++ handler.js
@@ -953,6 +953,7 @@ export async function handler(chatUpdate) {
         if (!('antiArab' in chat)) chat.antiArab = false;
         if (!('antiArab2' in chat)) chat.antiArab2 = false;
         if (!('antiporno' in chat)) chat.antiporno = false;
+	if (!('game' in chat)) chat.game = true;
         if (!('modoadmin' in chat)) chat.modoadmin = false;
         if (!('simi' in chat)) chat.simi = false;
         if (!isNumber(chat.expired)) chat.expired = 0;
@@ -980,6 +981,7 @@ export async function handler(chatUpdate) {
 	  antiporno: false,
 	  modoadmin: false,
 	  simi: false,
+	  game: true, 
           expired: 0,
         };
       }
