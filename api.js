fc351932666d77a100a781e980bbd239dbbacbb1
Diego Flores <dftzippo@gmail.com>
2024-02-03 21:25:34 -0600

    Se eliminó "ApiEmpire" de api.js

--- api.js
+++ api.js
@@ -26,7 +26,6 @@ global.lolkeysapi = ['GataDios']; // ['BrunoSobrino_2']
 global.itsrose = ['4b146102c4d500809da9d1ff'];
 
 global.APIs = {
-  // ApiEmpire: 'https://',
   CFROSAPI: 'https://api.cafirexos.com',
   xteam: 'https://api.xteam.xyz',
   dzx: 'https://api.dhamzxploit.my.id',
