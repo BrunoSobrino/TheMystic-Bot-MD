0b3c99de1e3893e0c06b42e14d55bdc8c3b596ca
Diego Flores <dftzippo@gmail.com>
2024-02-04 18:14:56 -0600

    Se hicieron cambios en las URL utilizadas para las descargas : descargas-play_v2.js

--- plugins/descargas-play_v2.js
+++ plugins/descargas-play_v2.js
@@ -28,30 +28,30 @@ if (enviando) return;
       enviando = false;
       throw `_*< DESCARGAS - PLAY V2 />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
     } else {
-      try {
-        if (command === 'play.1') {
+      try {      
+        if (command === 'play.1') { // play.1 con CFROS API v1 ytmp3
               apiUrl = `https://api.cafirexos.com/api/v1/ytmp3?url=${data.resultado.url}`;
               mimeType = 'audio/mpeg';
               fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);
-            } else if (command === 'play.2') {
+              buff = await conn.getFile(apiUrl);          
+            } else if (command === 'play.2') { // play.2 con CFROS API v1 ytmp4
               apiUrl = `https://api.cafirexos.com/api/v1/ytmp4?url=${data.resultado.url}`;
               mimeType = 'video/mp4';
               fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);
+              buff = await conn.getFile(apiUrl);        
         }
-      } catch {
+      } catch {        
           try {
-            if (command === 'play.1') {
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp3?url=${data.resultado.url}`;
+            if (command === 'play.1') { // play.1 con CFROS API v2 ytmp3
+              apiUrl = `https://api.cafirexos.com/api/v2/ytmp3?url=${data.resultado.url}`;
               mimeType = 'audio/mpeg';
               fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);
-            } else if (command === 'play.2') {
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}`;
+              buff = await conn.getFile(apiUrl);              
+            } else if (command === 'play.2') { // play.2 con CFROS API v2 ytmp4
+              apiUrl = `https://api.cafirexos.com/api/v2/ytmp4?url=${data.resultado.url}`;
               mimeType = 'video/mp4';
               fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);
+              buff = await conn.getFile(apiUrl);              
             }
           } catch {
             enviando = false;
