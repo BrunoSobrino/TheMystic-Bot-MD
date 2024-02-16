3157dda55d83cfafc5a605169917e8a968cd77a3
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-17 00:09:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -543,7 +543,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `Essaouidi Yassine[ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
