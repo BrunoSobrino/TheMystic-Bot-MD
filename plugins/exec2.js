0fe9bd1c3a6b5ebd6e50cd113cc68045ef8bcf3f
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-17 00:31:38 +0100

    Create exec2.js

---
+++ plugins/exec2.js
@@ -0,0 +1,35 @@
+import cp, {
+  exec as _exec
+} from 'child_process'
+import {
+  promisify
+} from 'util'
+let exec = promisify(_exec).bind(cp)
+
+let handler = async (m, {
+  conn,
+  isOwner,
+  command,
+  text
+}) => {
+  if (global.conn.user.jid != conn.user.jid) return
+  m.reply('Executing...')
+  let o
+  try {
+      o = await exec(command.trimStart() + ' ' + text.trimEnd())
+  } catch (e) {
+      o = e
+  } finally {
+      let {
+          stdout,
+          stderr
+      } = o
+      if (stdout.trim()) m.reply(stdout)
+      if (stderr.trim()) m.reply(stderr)
+  }
+}
+handler.customPrefix = /^[$] /
+handler.command = new RegExp
+handler.rowner = true
+
+export default handler
