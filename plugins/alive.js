33e3158026c4e752b9add39e7823104fa269a691
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-17 00:17:12 +0100

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+╭────────────────────
+│👋 مرحبا يا , ${conn.getName(m.sender)}!
+│🤖 أتمنى أنك بخير ♥ البوت أون لاين الآن 
+يمكنك إستخدامه عبر كتابة menu.
+╰────────────────────
+*─[ BY ESSAOUIDI_YASSINE ]*🌟✨
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['alive']
+handler.tags = ['infobot']
+handler.command = /^(alive)$/i
+
+
+export default handler
