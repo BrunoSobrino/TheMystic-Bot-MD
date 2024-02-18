e8ec2e49719a777ef174aa5e546afd621efe0cf9
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-18 02:09:00 +0100

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://atom.bio/essaouidi_yassine';
+  const githubRepoURL = 'https://github.com/Essaouidi25';
 
   try {
   
