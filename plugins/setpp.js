c3f4595692e2072b38161068c97738c0d80a9537
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-18 01:29:46 +0100

    Create setpp.js

---
+++ plugins/setpp.js
@@ -0,0 +1,20 @@
+import { webp2png } from '../lib/webp2mp4.js'
+import { URL_REGEX } from '@adiwajshing/baileys'
+
+let handler = async (m, { conn, args }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ''
+  if (/image/.test(mime)) {
+    let url = await webp2png(await q.download())
+    await conn.updateProfilePicture(m.chat, { url }).then(_ => m.reply('Success update profile picture'))
+  } else if (args[0] && args[0].match(URL_REGEX)) {
+    await conn.updateProfilePicture(m.chat, { url: args[0] }).then(_ => m.reply('Success update profile picture'))
+  } else throw 'Where\'s the media?'
+}
+handler.help = ['setppgroup *profile picture full*']
+handler.tags = ['group']
+handler.alias = ['setppgc', 'setppgrup', 'setppgroup']
+handler.command = /^setpp(gc|grup|group)$/i
+handler.group = handler.admin = handler.botAdmin = true
+
+export default handler
