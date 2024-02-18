4d7fe016f8277e14a6229dcec8d319e504fee96d
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-18 20:37:05 +0100

    Create Essaouidi.js

---
+++ plugins/Essaouidi.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `بوت السـويدي يـاسـيـن متصل الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
+await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
+      isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
+      await conn.sendMessage(m.chat, {
+        audio: {
+            url: "toma.mp3"
+        },
+        seconds: 256,
+        ptt: true,
+        mimetype: "audio/mpeg",
+        fileName: "vn.mp3",
+        waveform: [100, 0, 100, 0, 100, 0, 100]
+    }, {
+        quoted: m
+    })
+}
+handler.customPrefix = /^(tes|سلام|menu|Menu|apk|bobizaty|♥|salam|hy|Hello|.|شكرا|مرحبا|hi|slm)$/i
+handler.command = new RegExp
+
+export default handler
