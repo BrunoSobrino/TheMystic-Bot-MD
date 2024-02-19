a86bba8f1da667173cd540e065fbf3391c2e7989
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-19 13:33:29 +0100

    Create ayati♥.js

---
+++ plugins/ayati♥.js
@@ -0,0 +1,75 @@
+import fetch from 'node-fetch';
+
+const fetchQuranData = async (surahNumber) => {
+  try {
+    const response = await fetch(`https://quran-wudy.vercel.app/surah/${surahNumber}`);
+    const data = await response.json();
+    return data.data.verses;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const handler = async (m, { conn }) => {
+conn.qurannData = conn.qurannData ? conn.qurannData : {};
+
+  const surahNumber = parseInt(m.text.split(' ')[1]);
+  if (isNaN(surahNumber) || surahNumber < 1 || surahNumber > 114) {
+    m.reply("❌ رقم السورة غير صحيح. يرجى تقديم رقم سورة صالح بين 1 و 114.\n مثال : \n .ayati 114");
+    return;
+  }
+
+  const ayahs = await fetchQuranData(surahNumber);
+  if (!ayahs) {
+    m.reply("Failed to fetch Quran data.");
+    return;
+  }
+
+  const formattedList = Object.values(ayahs).map(v => (
+    `*${v.number.inSurah}.* ${v.text.arab}`
+  )).join('\n');
+
+  const instructions = "قم بالرد على هذه الرسالة برقم الآية المطلوب لاستقبال الصوت. \n يمكنك زيارة أنستغرام صاحب البوت لمعرفة المزيد عن هذه الميزة \n instagram.com/noureddine_ouafy";
+
+  let { key } = await m.reply(`📖 List of Ayahs in Surah ${surahNumber}:\n${formattedList}\n\n${instructions}`);
+  // Store the Quran data in conn.qurannData variable for later use
+  conn.qurannData[m.chat] = { list: Object.values(ayahs), key };
+};
+
+handler.before = async (m, { conn }) => {
+conn.qurannData = conn.qurannData ? conn.qurannData : {};
+
+if (m.isBaileys || !(m.chat in conn.qurannData)) return;
+  const input = m.text.trim();
+  if (!/^\d+$/.test(input)) return; // If the input is not a number, do nothing
+
+  const { list, key } = conn.qurannData[m.chat];
+  if (!m.quoted || m.quoted.id !== key.id || !m.text) return;
+  const index = parseInt(input);
+
+  if (isNaN(index) || index < 1 || index > list.length) {
+    m.reply("❌ رقم الآية غير صحيح. يرجى تقديم رقم آية صالح من القائمة.\nمثال :\n .ayati 1");
+  } else {
+  const selectedObj = list[index - 1];
+
+  // Check if the message is a reply and the quoted message id matches the key.id from the list
+    await conn.sendMessage(m.chat, {
+      audio: {
+        url: selectedObj.audio.primary,
+      },
+      mimetype: "audio/mpeg",
+      filename: "quran_audio.mp3",
+      ptt: true,
+    }, { quoted: m });
+
+    clearTimeout(conn.qurannData[m.chat].timeout);
+    //delete conn.qurannData[m.chat];
+  }
+};
+
+handler.help = ["ayati"];
+handler.tags = ["islam"];
+handler.command = /^ayati$/i;
+
+export default handler;
