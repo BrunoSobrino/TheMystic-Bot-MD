0c114ac3591782574f378f68f0292f3c13a8fdfc
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-20 00:19:04 +0100

    Create animepic.js

---
+++ plugins/animepic.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+let handler = async (m, {
+    conn,
+    usedPrefix
+}) => {
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '😍',
+            key: m.key,
+        }
+    })
+    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    await conn.sendFile(m.chat, json.url, null, "- B O B I Z A -", m)
+}
+handler.help = ['animepic']
+handler.tags = ['anime']
+handler.command = /^(animepic)$/i
+export default handler
