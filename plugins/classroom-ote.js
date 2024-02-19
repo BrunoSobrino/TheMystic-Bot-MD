afd5e63d273a75e6b2cc6919c61c8596174c0c99
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-19 12:40:01 +0100

    Create classroom-ote.js

---
+++ plugins/classroom-ote.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import { pinterest } from '../lib/scrape.js'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+
+  try {
+    const hasil = await pinterest('classroom of the eitil 4K');
+    let gambarUrls = hasil.slice(0, 20); // Ambil 20 gambar pertama
+
+    // Mengacak array gambarUrls
+    for (let i = gambarUrls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [gambarUrls[i], gambarUrls[j]] = [gambarUrls[j], gambarUrls[i]];
+    }
+
+    // Mengirim 10 gambar secara acak
+    for (let i = 0; i < 5; i++) {
+      let imageUrl = gambarUrls[i];
+      let imageRes = await fetch(imageUrl);
+      let imageBuffer = await imageRes.buffer();
+
+      // Menggunakan fungsi sendImage untuk mengirim gambar ke WhatsApp
+      await conn.sendFile(m.chat, imageBuffer, 'Dragon-ball-z.jpg', '');
+
+      // Tambahkan jeda agar tidak mengirim gambar terlalu cepat
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+  } catch (e) {
+    console.log(e)
+    conn.reply(m.chat, 'Error', m)
+  }
+}
+
+handler.help = ['classroom-ote']
+handler.tags = ['anime']
+handler.command = /^classroom-ote$/i
+export default handler
