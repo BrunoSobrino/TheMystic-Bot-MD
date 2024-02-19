af361b300a3926c81245ef6e0190b9460e8d46a2
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-19 12:06:38 +0100

    Create autodl-facebook.js

---
+++ plugins/autodl-facebook.js
@@ -0,0 +1,29 @@
+import { fbdl } from '../../lib/download.js'
+export let event = {
+	on: async (m, {
+		conn,
+		budy,
+		Limit,
+		autodl,
+		mess,
+		checkLimitUser
+	}) => {
+		if (autodl && /(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.gg)\/[^\s/]+(?:\/videos\/\d+\/?)?/.test(budy)) {
+			let exf = budy.includes('User') || budy.includes('Premium')
+			if (exf) return
+			if (checkLimitUser(m.sender) <= 0) {
+				return m.reply(mess.limit);
+			}
+			m.react('🕒', m.chat)
+			let {
+				video
+			} = await fbdl(budy);
+			m.react('🕗', m.chat)
+			conn.sendFile(m.chat, video, {
+				caption: ` Facebook\ninstagram.com/f.b.i_ys._ess._ui_.di_man_6000`,
+				quoted: m
+			});
+			Limit(m.sender, 3);
+		}
+	}
+};
