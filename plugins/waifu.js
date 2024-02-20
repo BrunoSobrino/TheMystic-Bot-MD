40fee7535d321d632517f3b8a4d99e13d724263d
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-20 13:41:26 +0100

    Create waifu.js

---
+++ plugins/waifu.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+let handler = async (m, {
+    conn,
+    usedPrefix
+}) => {
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '⏳',
+            key: m.key,
+        }
+    })
+    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    await conn.sendFile(m.chat, json.url, null, "- E S S A O U I D I -", m)
+}
+handler.help = ['waifu']
+handler.tags = ['anime']
+handler.command = /^(waifu)$/i
+export default handler
