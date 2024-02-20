3d310534db07cb7528985e82fb04c48d3efe58e5
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-20 11:54:53 +0100

    Create luffy.js

---
+++ plugins/luffy.js
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch'
+import { pinterest } from '../lib/scrape.js'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+
+  try {
+    const hasil = await pinterest('Luffy one piece');
+    let gambarUrls = hasil.slice(0, 20); // Ambil 20 gambar pertama
+
+    // Mengacak array gambarUrls
+    for (let i = gambarUrls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [gambarUrls[i], gambarUrls[j]] = [gambarUrls[j], gambarUrls[i]];
+    }
+
+    // Mengirim 10 gambar secara acak
+    for (let i = 0; i < 10; i++) {
+      let imageUrl = gambarUrls[i];
+      let imageRes = await fetch(imageUrl);
+      let imageBuffer = await imageRes.buffer();
+
+      // Menggunakan fungsi sendImage untuk mengirim gambar ke WhatsApp
+      await conn.sendFile(m.chat, imageBuffer, 'luffy.jpg', '');
+
+      // Tambahkan jeda agar tidak mengirim gambar terlalu cepat
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+  } catch (e) {
+    console.log(e)
+    conn.reply(m.chat, 'Error', m)
+  }
+}
+
+handler.help = ['luffy']
+handler.tags = ['anime']
+handler.command = /^luffy$/i
+
+export default handler
