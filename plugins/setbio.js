55c54fc0788409050cbf24fa74976e9fbf384b43
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-20 13:26:11 +0100

    Update setbio.js

--- plugins/setbio.js
+++ plugins/setbio.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) throw `هذا الامر يمكنك من خلاله ان تغير البيو الخاص ببوتك ♥ هذا الامر يشتغل فقط عند صاحب البوت اكتب هكذا \n*.setbio hello im nour bot*`
+  if (!text) throw `هذا الامر يمكنك من خلاله ان تغير البيو الخاص ببوتك ♥ هذا الامر يشتغل فقط عند صاحب البوت اكتب هكذا \n*.setbio hello im Essaouidi bot*`
     try {
    await conn.updateProfileStatus(text).catch(_ => _)
    conn.reply(m.chat, 'تم تغيير Bio  البوت  بنجاح', m)
