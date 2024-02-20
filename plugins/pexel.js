9118f13d0927f53100226e6b6a671f00f9f5e341
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-20 11:36:00 +0100

    Create pexel.js

---
+++ plugins/pexel.js
@@ -0,0 +1,12 @@
+const handler = async (m, {conn, usedprefix, text}) => {
+  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/pixelate', {
+    avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    comment: text,
+    username: conn.getName(m.sender),
+  }), 'error.png', '*صورة منقطة مع الخروج*', m);
+};
+handler.help = ['pixel', 'difuminar'];
+handler.tags = ['maker'];
+handler.command = /^(pixel|pixelar|difuminar)$/i;
+export default handler;
