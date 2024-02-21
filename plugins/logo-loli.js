f8a89be29730e82f448f60344c447f098e99cf59
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-21 14:59:09 +0100

    Create logo-loli.js

---
+++ plugins/logo-loli.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Enter Parameters'
+  m.reply('Proses...')
+  let res = `https://ziy.herokuapp.com/api/maker/lolimaker?nama=${response[0]}&apikey=xZiyy`
+  conn.sendFile(m.chat, res, 'loli.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['logololi'].map(v => v + ' <text>')
+handler.tags = ['maker']
+handler.command = /^(logololi)$/i
+
+export default handler
