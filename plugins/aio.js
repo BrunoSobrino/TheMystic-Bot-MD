0d1cf472e1f37db80575df07bb6d0e1cad4834f5
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-21 20:39:13 +0100

    Create aio.js

---
+++ plugins/aio.js
@@ -0,0 +1,20 @@
+import {
+    aiovideodl
+} from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+try {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://vt.tiktok.com/ZSdDyUHcR/https://www.tiktok.com/@kata__kasar/video/7088823247373946138'`
+const { title, medias } = await aiovideodl(args[0])
+for (const { url, quality, formattedSize} of medias) await conn.sendFile(m.chat, url, 'save.mp4', `*AIOVIDEO DOWNLOADER*\n\n${title ? `*Title:* ${title}` : "NotFound"}\n*💽Format:* ${quality ? `${quality}` : "Unknown"}\n*📨Size:* ${formattedSize ? `${formattedSize}` : "countless" }\n`, m)
+} catch (e) {
+m.reply("Eror")
+}
+}
+handler.help = ['aiovideo'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(aio(video)?(dl)?)$/i
+
+export default handler
