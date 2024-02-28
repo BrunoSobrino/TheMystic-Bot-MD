88e2742c6332dfbf337cc954f61f48d4bea9eb69
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-28 17:44:20 +0100

    Update hidetag.js

--- plugins/hidetag.js
+++ plugins/hidetag.js
@@ -1,28 +1,18 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text, participants }) => {
-const fkontak = {
-  "key": {
-    "participants":"0@s.whatsapp.net",
-    "remoteJid": "status@broadcast",
-    "fromMe": false,
-    "id": "Halo"
-  },
-  "message": {
-    "contactMessage": {
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-    }
-  },
-  "participant": "0@s.whatsapp.net"
-}
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 
-    conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, {quoted:fkontak})
-    }
-handler.help = ['hidetag']
-handler.tags = ['owner']
+let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+  if (!(isOwner || isAdmin)) throw dfail('admin', m, conn)
+  let users = participants.map(u => conn.decodeJid(u.id))
+  let q = m.quoted ? m.quoted : m
+  let c = m.quoted ? await m.getQuotedObj() : m.msg
+  let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
+  // console.log(msg)
+  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = handler.alias = ['hidetag']
+handler.tags = ['group']
 handler.command = /^(hidetag)$/i
 
 handler.group = true
-handler.admin = true
 
 export default handler
