c3404f1cdc8e92e748294b2dafaec68fade6e274
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-02-28 17:46:51 +0100

    Update hidetag.js

--- plugins/hidetag.js
+++ plugins/hidetag.js
@@ -1,18 +1,27 @@
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import MessageType from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
-  if (!(isOwner || isAdmin)) throw dfail('admin', m, conn)
-  let users = participants.map(u => conn.decodeJid(u.id))
-  let q = m.quoted ? m.quoted : m
-  let c = m.quoted ? await m.getQuotedObj() : m.msg
-  let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
-  // console.log(msg)
-  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+let handler = async (m, { conn, text, participants }) => {
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
 }
-handler.help = handler.alias = ['hidetag']
+}, {
+quoted: m,
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = ['hidetag']
 handler.tags = ['group']
-handler.command = /^(hidetag)$/i
-
+handler.command = ['hidetag', 'notify'] 
 handler.group = true
+handler.admin = true
 
 export default handler
