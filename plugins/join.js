45c1ca07f4c6b5e7515633109a7fe92fede26f52
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-03 12:05:10 +0100

    Update and rename owner-join.js to join.js

---
+++ plugins/join.js
@@ -0,0 +1,21 @@
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+
+let handler = async (m, { conn, text, isOwner }) => {
+    let [_, code, expired] = text.match(linkRegex) || []
+    if (!code) throw 'الرابط غير صالح'
+    let res = await conn.groupAcceptInvite(code)
+    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
+    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في حسابه\ninstagram.com/f.b.i_ys._ess._ui_.di_man_6000`)
+    let chats = global.db.data.chats[res]
+    if (!chats) chats = global.db.data.chats[res] = {}
+    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
+}
+handler.help = ['join']
+handler.tags = ['owner']
+
+handler.command = /^join$/i
+handler.rowner = true
+
+export default handler
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
