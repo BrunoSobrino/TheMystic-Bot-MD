e71fd029a7b79f9f3beadc72623d8f9d84a6464b
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-03 11:53:49 +0100

    Delete plugins/support.js

--- plugins/support.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
-instagram.com/f.b.i_ys._ess._ui_.di_man_6000
-`.trim())
-handler.help = ['support']
-handler.tags = ['main']
-handler.command = ['list', 'menua'] 
-
-export default handler
