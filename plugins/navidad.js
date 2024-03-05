aff2bbfce4a18a8d0517060e539ea3968f01dd65
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-05 15:26:16 +0100

    Create navidad.js

---
+++ plugins/navidad.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const handler = async (m, {
+  conn,
+  args,
+  usedPrefix,
+  command,
+}) => {
+  const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/navidad.json`)).data;
+  const mystic = await res[Math.floor(res.length * Math.random())];
+  conn.sendMessage(m.chat, {
+    image: {
+      url: mystic,
+    },
+    caption: `_عيد الميلاد 🧑‍🎄_`,
+  }, {
+    quoted: m,
+  });
+  // conn.sendButton(m.chat, `_Navidad 🧑‍🎄_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
+};
+handler.help = ['navidad'];
+handler.tags = ['internet'];
+handler.command = /^(عيد)$/i;
+export default handler;
