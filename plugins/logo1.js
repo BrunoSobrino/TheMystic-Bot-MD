b04a34c3386dceaf3d6ff1b037065ac5c850738c
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-05 16:01:54 +0100

    Create logo1.js

---
+++ plugins/logo1.js
@@ -0,0 +1,12 @@
+import  fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+  let response = args.join(' ').split('|')
+  if (!args[0]) throw 'نسيت النص '
+  m.reply('_المرجو الانتظار..._')
+  let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&script=fluffy-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
+  conn.sendFile(m.chat, res, 'essaouidi.jpg', `مكين شكرا  ولا حتى حاجة 🙂 ❤`, m, false)
+}
+handler.help = ['flaming1'].map(v => v + ' <text>')
+handler.tags = ['maker','logo']
+handler.command = /^(تصميم)$/i
+export default handler
