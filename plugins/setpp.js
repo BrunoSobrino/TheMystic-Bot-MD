14697d4cf480ab565be89b057f2c822831b7affa
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-05 19:40:25 +0100

    Update setpp.js

--- plugins/setpp.js
+++ plugins/setpp.js
@@ -1,20 +1,59 @@
-import { webp2png } from '../lib/webp2mp4.js'
-import { URL_REGEX } from '@adiwajshing/baileys'
+import fs from 'fs'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-let handler = async (m, { conn, args }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ''
-  if (/image/.test(mime)) {
-    let url = await webp2png(await q.download())
-    await conn.updateProfilePicture(m.chat, { url }).then(_ => m.reply('Success update profile picture'))
-  } else if (args[0] && args[0].match(URL_REGEX)) {
-    await conn.updateProfilePicture(m.chat, { url: args[0] }).then(_ => m.reply('Success update profile picture'))
-  } else throw 'Where\'s the media?'
+const jimp_1 = require('jimp')
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+ let q = m.quoted ? m.quoted : m
+ let mime = (q.msg ? q.msg : q).mimetype ? q.mimetype : q.mediaType || ''
+ if (/image/g.test(mime) && !/webp/g.test(mime)) {
+  try {
+   let media = await q.download()
+   let botNumber = await conn.user.jid
+   let { img } = await pepe(media)
+   await conn.query({
+    tag: 'iq',
+    attrs: {
+     to: botNumber,
+     type:'set',
+     xmlns: 'w:profile:picture'
+    },
+    content: [
+     {
+      tag: 'picture',
+      attrs: { type: 'image' },
+      content: img
+     }
+    ]
+   })
+   m.reply('تم بنجاح تغيير صورة بروفيل البوت')
+  } catch (e) {
+   console.log(e)
+   m.reply('حدث خطأ، حاول مرة أخرى لاحقًا')
+  }
+ } else {
+  m.reply(`أرسل الصورة مع التسمية التوضيحية ${usedPrefix + command} \nأو قم  بالاشارة  للصورة التي تريد وضعها كبروفايل البوت`)
+ }
 }
-handler.help = ['setppgroup *profile picture full*']
-handler.tags = ['group']
-handler.alias = ['setppgc', 'setppgrup', 'setppgroup']
-handler.command = /^setpp(gc|grup|group)$/i
-handler.group = handler.admin = handler.botAdmin = true
+
+handler.help = ['setpp']
+handler.tags = ['owner']
+handler.command = /^(setpp)$/i
+
+handler.owner = true
 
 export default handler
+
+async function pepe(media) {
+    const jimp = require('jimp');
+    const image = await jimp.read(media)
+    const min = image.getWidth()
+    const max = image.getHeight()
+    const cropped = image.crop(0, 0, min, max)
+    return {
+     img: await cropped.scaleToFit(720, 720).getBufferAsync(jimp.MIME_JPEG),
+     preview: await cropped.normalize().getBufferAsync(jimp.MIME_JPEG)
+    }
+   }
+   
