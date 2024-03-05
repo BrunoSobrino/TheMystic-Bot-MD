8ad00e90819e380a41dcbabd85d5a3e79f639c17
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-05 15:50:30 +0100

    Create love.js

---
+++ plugins/love.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, command, text }) => {
+let love = `*✨💜 نسبة الحب 💜✨*
+*نسبة الحب ${text} ليك ✨💜* *${Math.floor(Math.random() * 100)}%* *من 100%*
+*اطلب منها ان تكون صديقة لك ؟*
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(الحب)$/i
+export default handler
