73412ecc610adef005c05cf4dc403cb5f7b6a4f8
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-05 16:27:47 +0100

    Create youtube-video-2.js

---
+++ plugins/youtube-video-2.js
@@ -0,0 +1,29 @@
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw '*هذا الامر خاص بتحميل الفيديو من يوتوب*'
+await m.reply(`جاري التحميل المرجو الانتظار قليلا \ninstagram.com/f.b.i_ys._ess._ui_.di_man_6000`)
+try {
+let qu = args[1] || '360'
+let q = qu + 'p'
+let v = args[0]
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+const dl_url = await yt.video[q].download()
+const ttl = await yt.title
+const size = await yt.video[q].fileSizeH
+let cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄  📥⌋—◉*\n❏*:* ${ttl}\n❏*:* ${size}`.trim()
+await await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m})
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+let n2 = lolh.result.link
+let n3 = lolh.result.size
+let cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 video 📥⌋—◉*\n❏*:* ${n}\n❏*:* ${n3}`.trim()
+await conn.sendMessage(m.chat, { document: { url: n2 }, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m})
+} catch {
+await conn.reply(m.chat, 'تعذر تحميل الفيديو المرجو مراسلة نورالدين لتعرف السبب\ninstagram.com/f.b.i_ys._ess._ui_.di_man_6000' m)}
+}}
+handler.command = /^video2$/i
+export default handler
