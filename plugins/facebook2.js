9e5ba86741b71327f1e3f01b3fdc49c19cdb9f02
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-06 21:31:10 +0100

    Update facebook2.js

--- plugins/facebook2.js
+++ plugins/facebook2.js
@@ -5,16 +5,16 @@ import fbDownloader from 'fb-downloader-scrapper';
 import {facebook} from '@xct007/frieren-scraper';
 import axios from 'axios';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `_*< DESCARGAS - FACEBOOK />*_\n\n*[ ℹ️ ] Ingrese un enlace de Facebook.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://fb.watch/fOTpgn6UFQ/_`;
-  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `_*< DESCARGAS - FACEBOOK />*_\n\n*[ ℹ️ ] Ingrese un enlace de Facebook.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://fb.watch/fOTpgn6UFQ/_`;
+  if (!args[0]) throw `_*instagram.com/essaouidi_yassine*_\n\n*[ 🎥 ] المرجو ارسل رابط فيسبوك خاص بالفيديو.*\n\n*[ 💡 ] على سبيل المثال:* _${usedPrefix + command} https://fb.watch/fOTpgn6UFQ/_`;
+  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `_*instagram.com/essaouidi_yassine*_\n\n*[ ℹ️ ] ارسل رابط فيسبوك خاص بالفيديو*\n\n*[ 💡 ] على سبيل المثال:* _${usedPrefix + command} https://fb.watch/fOTpgn6UFQ/_`;
   try {
-    await m.reply(`_*< DESCARGAS - FACEBOOK />*_\n\n*[ ℹ️ ] Se está enviando el video. espere...*`);
+    await m.reply(`_*instagram.com/essaouidi_yassine*_\n\n*[ ⏳ ] المرجو الإنتظار قليلا ...*`);
     const d2ata = await facebook.v1(args[0]);
     let r2es = '';
     if (d2ata.urls && d2ata.urls.length > 0) {
       r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
     }
-    conn.sendFile(m.chat, r2es, 'error.mp4', `_*< DESCARGAS - FACEBOOK />*_\n\n`, m);
+    conn.sendFile(m.chat, r2es, 'error.mp4', `_*instagram.com/essaouidi_yassine*_\n\n`, m);
   } catch (err1) {
     try {
       const req = await igeh(args[0]);
@@ -25,12 +25,12 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
         const Jjson = await Rres.json();
         let VIDEO = Jjson.result[0];
         if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
-        conn.sendFile(m.chat, VIDEO, 'error.mp4', `_*< DESCARGAS - FACEBOOK />*_\n\n`, m);
+        conn.sendFile(m.chat, VIDEO, 'error.mp4', `_*instagram.com/essaouidi_yassine*_\n\n`, m);
       } catch (err2) {
         try {
           const ress = await fg.fbdl(args[0]);
           const urll = await ress.data[0].url;
-          await conn.sendFile(m.chat, urll, 'error.mp4', '_*< DESCARGAS - FACEBOOK />*_\n\n', m);
+          await conn.sendFile(m.chat, urll, 'error.mp4', '_*instagram.com/essaouidi_yassine*_\n\n', m);
         } catch (err3) {
           try {
             const res = await fbDownloader(args[0]);
@@ -43,13 +43,13 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
               const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
               const json = await res3.json();
               const url3 = await json.video;
-              await conn.sendFile(m.chat, url3, 'error.mp4', '_*< DESCARGAS - FACEBOOK />*_\n\n', m);
+              await conn.sendFile(m.chat, url3, 'error.mp4', '_*instagram.com/essaouidi_yassine*_\n\n', m);
             } catch (err5) {
               try {
                 const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
-                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*[ 📥 ] Descargas - Facebook*\n_---> The Mystic Bot._', m);
+                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*[ 📥 ] التحميلات - الفيسبوك*\n_---> Essaouidi Bot._', m);
               } catch (err6) {
-                throw `_*< DESCARGAS - FACEBOOK />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
+                throw `_*instagram.com/essaouidi_yassine*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
               }
             }
           }
