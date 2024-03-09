4a860cb53e13113af19e61f416a5de7b94f36ad1
ffr4qdfd <158234787+ffr4qdfd@users.noreply.github.com>
2024-03-09 14:39:51 +0200

    Delete plugins directory

--- plugins/owner-leavegc.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, {conn, text, command}) => {
-  const id = text ? text : m.chat;
-  await conn.reply(id, '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*');
-  await conn.groupLeave(id);
-};
-handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i;
-handler.group = true;
-handler.rowner = true;
-export default handler;
