f1a23daad922f2cac37d1d8a45522ce921a81b4b
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-15 17:20:23 -0300

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- main.js
+++ main.js
@@ -138,6 +138,7 @@ console.log('[ ❗ ] Por favor, seleccione solo 1 o 2.\n')
 }} while (opcion !== '1' && opcion !== '2' || fs.existsSync(`./${authFile}/creds.json`))
 }
 
+console.info = () => {} //pre-key
 const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
