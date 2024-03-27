15829e38b1c72e3fb622991211c83186c4922f5a
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-03-27 23:17:43 +0200

    Update gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,14 +1,27 @@
+import MessageType from '@adiwajshing/baileys'
 import { generateWAMessageFromContent } from '@adiwajshing/baileys'
-let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+
+let handler = async (m, { conn, text, participants }) => {
 let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m || m.text || m.sender
-let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: m, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || 'يرجى كتابه الرساله'
+}
+}, {
+quoted: m,
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
 handler.help = ['hidetag']
 handler.tags = ['group']
-handler.command = /^مخفي$/i
+handler.command = ['مخفي'] 
 handler.group = true
 handler.admin = true
+
 export default handler
