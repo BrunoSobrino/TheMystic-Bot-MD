0aca83973f05b53c5b9a0de4e9a4bd6986d9558f
Botmego <150967600+Botmego@users.noreply.github.com>
2024-03-29 11:40:02 +0200

    Create تفعيلات.js

---
+++ plugins/تفعيلات.js
@@ -0,0 +1,49 @@
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+const sections = [
+{
+title: `◥ ツقائمة الخياراتツ ◤`,
+rows: [
+{title: "✨ | ◥ ツالترحيبツ ◤", description: "تنشيط الترحيب او ايقاف تشغيله", rowId: `${usedPrefix + command} welcome`},
+{title: "🌎 | ◥ ツالوضع العامツ ◤", description: "تفعيل البوت ف العام والخاص (الشات - الجروبات) )", rowId: `${usedPrefix + command} public`},
+{title: "🥵 | ◥ ツالاباحيةツ ◤", description:" تم حذفها من قبل المطور", rowId: `${usedPrefix + command} modohorny`},
+{title: "🔗 | ◥ ツمضاد الروابطツ ◤", description: "مضادات روابط جروبات الواتس", rowId: `${usedPrefix + command} antilink`},   
+{title: "🔗 | ◥ ツ2 مضاد الروابطツ ◤", description: "مضادات جميع الروابط", rowId: `${usedPrefix + command} antilink2`},    
+{title: "🔎 | ◥ ツتغيراتツ ◤", description: "اشعارات لتغيرات المجموعة", rowId: `${usedPrefix + command} detect`},      
+{title: "❗ | ◥ ツتقيدツ ◤", description: "تنشيط او تعطيل الطرد", rowId: `${usedPrefix + command} restrict`},    
+{title: "☑️ | ◥ ツالقراءة التلقائيةツ ◤", description: "قراءة النصوص تلقائيا", rowId: `${usedPrefix + command} autoread`},
+{title: "🔊 | ◥ ツالصوتツ ◤", description: "اوامر تغير الصوت", rowId: `${usedPrefix + command} audios`},
+{title: "👾 | ◥ ツاستيكر تلقائيツ ◤", description: "هتبقي اي حاجه البوت هيبعتها بعد كده استيكر", rowId: `${usedPrefix + command} autosticker`},
+{title: "💬 | ◥ ツفقطツ ◤", description: "تنفيذ الاوامر ف الشات الخاص بس", rowId: `${usedPrefix + command} pconly`},
+{title: "🏢 | ◥ ツجروبات فقطツ ◤", description: "تنفيذ الاوامر للجروبات بس", rowId: `${usedPrefix + command} gconly`},
+{title: "❌ | ◥ ツضد المشاهدةツ ◤", description: "مضاد الرؤيه مره واحده فقط", rowId: `${usedPrefix + command} antiviewonce`},
+{title: "📵 | ◥ ツانتي المكالماتツ ◤", description: "مضاد المكالمات", rowId: `${usedPrefix + command} anticall`},
+{title: "💬 | ◥ ツمكافحة خاصةツ ◤", description: "البوت هيحظر اي حد هيتكلم معاه ف الخاص", rowId: `${usedPrefix + command} antiprivado`},
+{title: "🤬 | ◥ ツانتي توكسيكツ ◤", description: "مضاد التوكسيك", rowId: `${usedPrefix + command} antitoxic`},
+{title: "🕸️ | ◥ ツمضاد فيرس تراباツ ◤", description: "تنشيط او تعطيل مضادات الفيروسات", rowId: `${usedPrefix + command} antitraba`},
+{title: "👎🏻 | ◥ ツمعاداة الغربツ ◤", description: "طرد الارقام الغربية من الجروب - معطل من قبل المطور", rowId: `${usedPrefix + command} antiarabes`},  
+]}, ]
+//let name = await conn.getName(m.sender)
+const listMessage = {
+text: ' ',
+footer: `┏━━━━━━━━━━━━━┓
+
+
+
+
+
+
+
+
+
+𝑩𝒚 : 𝙢,𝙚,𝙜,𝙤, ❦︎
+┗━━━━━━━━━━━━━┛`,
+title: null,
+buttonText: "◥ ツاختر من هناツ ◤",
+sections }
+conn.sendButton(m.chat, `🗂️ خيار: ${type} 
+🎚️ الحالة: ${isEnable ? 'مفعل' : 'معطل'}
+📣 ل: ${isAll ? 'البوت ده' : isUser ? '' : 'الشات ده'}`, author, null,`${isEnable ? `#disable ${type}` : `#enable ${type}`}`]], m)}
+handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.tags = ['group', 'owner']
+handler.command = /^((en|dis)able|(تفعي)ل|(turn)?[01])$/i
+export default handler
