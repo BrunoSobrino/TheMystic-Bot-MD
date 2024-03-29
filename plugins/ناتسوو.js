db6018b67260c913bd0edc9e8b6330554f1dcacd
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-03-29 16:51:38 +0200

    Update ناتسوو.js

--- plugins/ناتسوو.js
+++ plugins/ناتسوو.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = *مرحبا ماذا تريد*;
+    let message = 🙄🙄مرحبا ماذا تريد مني;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/9456fe895b3315ea993e4.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(natsu|ناتسوو|ناتسو|ناتسووو|NATSU)$/i;
+handler.customPrefix = /^(NATSU|ناتسو|ناتسوو|natsu)$/i;
 handler.command = new RegExp;
 
 export default handler;
