9cd049eb02e08df028175cac0644b11a3e2f17bd
Botmego <150967600+Botmego@users.noreply.github.com>
2024-03-29 03:40:46 +0200

    Add files via upload

---
+++ plugins/ابحث.js
@@ -0,0 +1,24 @@
+import yts from 'yt-search'
+import fs from 'fs'
+
+let handler = async (m, {conn, text }) => {
+  if (!text) throw '⚠️ *_يرجى كتابة الامر وجنبه ما تريد البحث عنه باليوتيوب_*'
+  await conn.reply(m.chat, global.wait, m)
+  let results = await yts(text)
+  let tes = results.all
+  let teks = results.all.map(v => {
+    switch (v.type) {
+      case 'video': return `
+° *_${v.title}_*
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_الوقت :_* ${v.timestamp}
+↳ 📥 *_اصدر في :_* ${v.ago}
+↳ 👁 *_المشاهدات :_* ${v.views}`}}).filter(v => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+}
+handler.help = ['ytsearch *<texto>*'] 
+handler.tags = ['search']
+handler.command = ['بحث', 'البحث'] 
+
+
+export default handler
