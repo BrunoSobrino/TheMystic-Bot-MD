67b3796e8a3db64bc0f4105e880106d55510dea3
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-03-29 16:38:43 +0200

    Create ناتسوو.js

---
+++ plugins/ناتسوو.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = *مرحبا ماذا تريد*;
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/9456fe895b3315ea993e4.jpg', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(bot|بوت|بووت|بوت)$/i;
+handler.command = new RegExp;
+
+export default handler;
