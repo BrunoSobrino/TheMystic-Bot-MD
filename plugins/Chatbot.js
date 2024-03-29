a86150e106026ca4c8abf9cb1f8c28ea58e13a5f
Botmego <150967600+Botmego@users.noreply.github.com>
2024-03-29 12:37:58 +0200

    Create Chatbot.js

---
+++ plugins/Chatbot.js
@@ -0,0 +1,26 @@
+let handler = m => m; 
+
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat]; 
+ let responses; 
+ if (/^ميجو$/i.test(m.text)) { 
+ responses = [ 
+ 'تحت امرك حبي'
+ ]; 
+ } else if (/^|بوووت|بموووووووووت|بوت$/i.test(m.text)) { 
+     responses = [ 
+'تحت امرك يا حبي'
+     ]; 
+   } else if (/^اوامر$/i.test(m.text)) { 
+     responses = [ 
+       '*لا تنسى ال .*',  
+     ];
+ }
+ if (responses) { 
+ let randomIndex = Math.floor(Math.random() * responses.length); 
+ conn.reply(m.chat, responses[randomIndex], m); 
+ } 
+ return !0 
+ }; 
+
+ export default handler;
