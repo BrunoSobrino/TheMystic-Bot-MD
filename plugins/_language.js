3acdbf277d9cb362ce4a83b8dd996b41380dd291
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-03-30 08:34:46 -0600

    Update _language.js

--- plugins/_language.js
+++ plugins/_language.js
@@ -1,99 +1,55 @@
 /*************************************************/
+/*
 /* Créditos al creador de este módulo.
 /* Jefferson: https://github.com/jeffersonalionco
 /* 
 /*************************************************/
 
-const handler = async (m, { args, usedPrefix, command, isAdmin }) => {
-    try {
-        const data = global
-        const idioma = data.db.data.users[m.sender].language
-        const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-        const tradutor = _translate.plugins._language
-
-
-        data.db.data.users[m.sender].language
-        let sigla // Args user
-
-        //---- Transformar "cadena" en letras minúsculas
-        if (args[0] != undefined) {
-            sigla = args[0].toLowerCase()
+const handler = async (m, { args, usedPrefix, command, isAdmin}) => {
+  try {  
+    const idioma = global.db.data.users[m.sender].language || 'es';
+    const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+    const tradutor = _translate.plugins._language
+    let sigla 
+    if (args[0] != undefined) sigla = args[0].toLowerCase()
+    
+    if (command === 'lang') {
+      delete global.db.data.users[m.sender].language;
+        if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
+            //global.imagen4 = global.imagen7
+            global.db.data.users[m.sender].language = 'pt-br'
+            m.reply(`*[ ✅ ] The Mystic - Bot*\n\n*_Idioma definido em Português 🇧🇷_*`)
+        } else if (sigla === 'es') {
+            global.db.data.users[m.sender].language = 'es';
+            m.reply(`*[ ✅ ] The Mystic - Bot*\n\n*_Idioma definido a Español 🇪🇸_*`)
+        } else {
+            m.reply(`${tradutor.texto1[0]}\n*${usedPrefix}lang* es\n\n${tradutor.texto1[1]}`)
         }
-
-        if (command === 'lang') {
-
-            // ----- Opciones de lenguaje
-            if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
-                data.db.data.users[m.sender].language = 'pt-br'
-
-                global.imagen4 = global.imagen7 // Imagen (MENU) - Para cambiar esta imagen, vaya a src/menu_pt.png
-                m.reply(`*✅The Mistic - Bot*\n\n_Definido para_ *🇧🇷 Português*`)
-
-            } else if (sigla === 'es') {
-                data.db.data.users[m.sender].language = 'es' // Imagen(MENU) - Para cambiar esta imagen, vaya a (RAIZ) menu.png
-
-                m.reply(`*✅The Mistic - Bot*\n\n_Definido para_ *🇪🇸 Español*`)
-
-            } else {
-
-                m.reply(`
-${tradutor.texto1[0]}
-*${usedPrefix}lang* es
-
-${tradutor.texto1[1]}
-
-`)
-            }
-
-        }
-
-        // - DEFINIDO TRADUÇÕES PARA GRUPOS NO BOT THE MYSTIC 
         
-        if (command === 'langgroup') {
-
-            // ----- Condiciones para grupos
-            if (m.isGroup === false) {
-                return m.reply(tradutor.texto3)
-            }
-            if (m.isGroup === true && isAdmin === false) {
-                return m.reply(tradutor.texto4)
-            }
-
-            // ----- Opciones de lenguaje
-            if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
-                global.db.data.chats[m.chat].language = 'pt-br';
-
-                m.reply(`*✅Este grupo foi:*\n_Definido para_ *🇧🇷 Português*`)
-            } else if (sigla === 'es') {
-                global.db.data.chats[m.chat].language = 'es';
-
-                m.reply(`*✅Este grupo foi:*\n_Definido para_ *🇪🇸 Español*`)
-            } else {
-                m.reply(`
-${tradutor.texto2[0]}
-*${usedPrefix}langgroup* es
-
-${tradutor.texto2[1]}
-
-`)
-            }
-
+    } else if (command === 'langgroup') {
+        if (m.isGroup === false) {
+            return m.reply(tradutor.texto3)
         }
-        // Fim 
-    } catch (error) {
-        data.db.data.users[m.sender].language = 'es'
-        global.db.data.chats[m.chat].language = 'es'
-
-        m.reply(`*[ERROR]* -  _Por defecto el idioma estaba configurado en español._
-
-        \`\`\`contacta a los creadores del bot\`\`\` `)
+        if (m.isGroup === true && isAdmin === false) {
+            return m.reply(tradutor.texto4)
+        }
+       delete global.db.data.chats[m.chat].language;
+        if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
+            global.db.data.chats[m.chat].language = 'pt-br';
+            m.reply(`*[ ✅ ] Configuración del grupo*\n\n*_Idioma definido em Português 🇧🇷_*`)
+        } else if (sigla === 'es') {
+            global.db.data.chats[m.chat].language = 'es';
+             m.reply(`*[ ✅ ] Configuración del grupo*\n\n*_Idioma definido a Español 🇪🇸_*`)
+        } else {
+            m.reply(`${tradutor.texto2[0]}\n*${usedPrefix}langgroup* es\n\n${tradutor.texto2[1]}`)
+        }
     }
-
-
+  } catch (error) {
+      data.db.data.users[m.sender].language = 'es'
+          global.db.data.chats[m.chat].language = 'es'
+              m.reply(`*[ERROR]* -  _Por defecto el idioma estaba configurado en español._
+              \`\`\`contacta a los creadores del bot\`\`\` `)
+  }
 }
-
-
-
-handler.command = /^(lang||langgroup)$/i;
-
-export default handler
+handler.command = /^(lang|langgroup)$/i;
+export default handler;
