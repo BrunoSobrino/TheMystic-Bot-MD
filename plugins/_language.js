cdd9b54019403ef4f91557bb65670c6c887a6710
Jefferson L. Alionco <jeffersonalionco@gmail.com>
2024-03-29 21:35:56 -0300

    Update _language

--- plugins/_language.js
+++ plugins/_language.js
@@ -1,4 +1,8 @@
-
+ /*************************************************/
+ /* Créditos al creador de este módulo.
+ /* Jefferson: https://github.com/jeffersonalionco
+ /* 
+ /*************************************************/
 
 const handler = async (m, { args, usedPrefix, command, isAdmin}) => {
     const data = global
@@ -8,11 +12,15 @@ const handler = async (m, { args, usedPrefix, command, isAdmin}) => {
 
     data.db.data.users[m.sender].language
     let sigla // Args user
+
+//---- Transformar "cadena" en letras minúsculas
     if (args[0] != undefined) {
         sigla = args[0].toLowerCase()
     }
 
     if (command === 'lang') {
+
+// ----- Opciones de lenguaje
         if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
             data.db.data.users[m.sender].language = 'pt-br'
 
@@ -39,14 +47,16 @@ ${tradutor.texto1[1]}
     
     // - Traduções Grupos
     if (command === 'langgroup') {
+
+// ----- Condiciones para grupos
         if(m.isGroup === false){
            return  m.reply(tradutor.texto3)
         }
-        
         if(m.isGroup === true && isAdmin === false){
             return  m.reply(tradutor.texto4)
         }
 
+// ----- Opciones de lenguaje
         if (sigla === 'pt-br' || sigla === 'pt' || sigla === 'br') {
             global.db.data.chats[m.chat].language = 'pt-br';
 
