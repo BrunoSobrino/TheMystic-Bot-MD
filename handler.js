d52e8a9e63a0baae92b42414a8e22da32d2659af
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-03-29 21:47:07 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -1436,7 +1436,7 @@ export async function participantsUpdate({ id, participants, action }) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m.conn.groupMetadata(id) || (conn.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = './src/avatar_contact.png';
+          let pp = 'https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/avatar_contact.png';
           try {
             pp = await m.conn.profilePictureUrl(user, 'image');
           } catch (e) {
