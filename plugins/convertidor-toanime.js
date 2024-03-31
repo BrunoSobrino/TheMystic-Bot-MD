2e8b8a0b4ba19c78da6ebc171a2680eef9fc0dc0
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-03-31 09:46:25 -0600

    Update convertidor-toanime.js

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -1,18 +1,21 @@
 import uploadImage from '../lib/uploadImage.js';
 
-
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.convertidor_toanime
-
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || q.mediaType || '';
   if (!/image/g.test(mime)) throw `*${tradutor.texto1}*`;
   m.reply(`*${tradutor.texto2}*`);
   const data = await q.download?.();
   const image = await uploadImage(data);
+  try {
+    const img = await conn.getFile(image);  
+    const tuanime = await jadianime(img.data);
+    await conn.sendFile(m.chat, 'https://www.drawever.com' + tuanime.urls[1] ? tuanime.urls[1] : tuanime.urls[0], 'error.jpg', null, m);
+  } catch {
   try {
     const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
     await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
@@ -27,6 +30,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
       } catch (e) {
         throw `*${tradutor.texto3}*`;
       }
+     }
     }
   }
 };
@@ -34,3 +38,48 @@ handler.help = ['toanime'];
 handler.tags = ['tools'];
 handler.command = /^(jadianime|toanime)$/i;
 export default handler;
+
+async function jadianime(image) {
+	return new Promise(async(resolve, reject) => {
+        axios("https://www.drawever.com/api/photo-to-anime", {
+            headers: {
+                "content-type": "application/json"
+            },
+            "data": { "data": "data:image/jpeg;base64," + image.toString('base64') },
+            "method": "POST"
+        }).then(res => { 
+            let yanz = res.data
+            resolve(yanz)
+        })})
+}
+
+/*async function tozombie(input) {
+  const image = await Jimp.read(input);
+  const buffer = await new Promise((resolve, reject) => {
+    image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
+      if (err) {
+        reject('Terjadi Error Saat Mengambil Data......');
+      } else {
+        resolve(buf);
+      }
+    });
+  });
+  const form = new FormData();
+  form.append('image', buffer, { filename: 'toanime.jpg' });
+  try {
+    const { data } = await axios.post(`https://tools.betabotz.eu.org/ai/tozombie`, form, {
+      headers: {
+        ...form.getHeaders(),
+        'accept': 'application/json',
+      },
+    });
+    var res = {
+      image_data: data.result,
+      image_size: data.size
+    };
+    return res;
+  } catch (error) {
+    console.error('Identifikasi Gagal:', error);
+    return 'Identifikasi Gagal';
+  }
+}*/
