fd1a0af1bea965e71ea1801c9384f74a0026714e
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-04-05 02:58:06 +0200

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
-if (!who) throw `*[❗] اسف سيدي لكنك نسيت المنشن @منشن ارجو من اختيار احد الاعضاء لجعلهم مميزين*`
+if (!who) throw `*[❗] اسف سيدي لكنك نسيت المنشن @منشن ارجو منك اختيار احد الاعضاء لجعلهم مميزين*`
 if (global.prems.includes(who.split`@`[0])) throw '*[❗] المستخدم الذي تم إدخاله هو بالفعل مستخدم متميز (بريميوم)*'
 global.prems.push(`${who.split`@`[0]}`)
 let textprem = `*[❗] @${who.split`@`[0]} أنت الآن مستخدم متميز ولن يكون لديك حدود عند استخدام البوت (تستطيع استخدام اي امر مثل المطور)*`
