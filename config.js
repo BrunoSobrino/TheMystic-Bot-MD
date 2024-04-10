35c768804860b025c0680f172e19195de85837cf
NamelessE95 <166423188+NamelessE95@users.noreply.github.com>
2024-04-10 15:44:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 global.confirmCode = ""
 
-global.owner = [
+global.owner = [529211307496]
   ['5219992095479', '👑 Mystic - Creador 👑', true],
   ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
