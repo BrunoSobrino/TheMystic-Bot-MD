d350571aa0aa27331e4c0ad528e70c83f1fe1681
Jefferson L. Alionco <jeffersonalionco@gmail.com>
2024-05-07 07:17:40 -0300

    Commit das alterações locais

--- plugins/game-glx.js
+++ plugins/game-glx.js
@@ -2,9 +2,7 @@
 // By https://github.com/jeffersonalionco
 
 import fs from 'fs-extra'
-import { createCanvas, loadImage } from 'canvas'
 import simpleGit from 'simple-git'
-const { Baileys } = (await import('@whiskeysockets/baileys'));
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
     createDataBase() // Criar arquivo DataBase se caso não existir
@@ -619,30 +617,8 @@ Use: ${usedPrefix}glx
                                 break;
                         }
                         break;
-                    case 'mapa':
-                        mapa()
-                        setTimeout(() => {
-                            enviar(`*>>>>>>>>>>> MAPA <<<<<<<<<<<*
-                            
-_- SEU PLANETA: *${data.perfil.casa.planeta}*_ 
-
-Para saber dados das colonias 
-> Use: ${usedPrefix}glx planeta act
-
-
-
-╔════════════════════╗
-
- *_⚙️ TODOS OS COMANDOS_*
-Use: ${usedPrefix}glx
-
-╚════════════════════╝
-
-*_🛸  GAME GALAXIA 🛸_*
-
-`, `./src/glx/temp/${data.perfil.username}.png`)
-                        }, 2000)
-
+                    case 'mapa': 
+                        enviar(`Mapa foi desativado fo jogo, Devido um erro no Debiam`)
                         break;
                     case 'perfil':
                         let nave = data.perfil.nave.nome ? data.perfil.nave.nome : 'Não tem Nave'
@@ -675,9 +651,9 @@ Use: ${usedPrefix}glx
 `
 
 
-                        imagemPerfil() // Função para gerar a imgem do perfil após 3s apaga automaticamente
+                        // Função para gerar a imgem do perfil após 3s apaga automaticamente
                         setTimeout(() => {
-                            enviar(strr, `./src/glx/temp/${data.perfil.username}.png`)
+                            enviar(strr, `./src/glx/perfil.png`)
                         }, 1000)
 
                         break;
@@ -1072,195 +1048,6 @@ Use: ${usedPrefix}glx
 
             }
         }
-
-        async function imagemPerfil() {
-            const largura = 1000;
-            const altura = 600;
-
-            const canvas = createCanvas(largura, altura);
-            const context = canvas.getContext('2d');
-
-            loadImage('./src/glx/perfil.png').then((imagemMapa) => {
-
-                context.drawImage(imagemMapa, 0, 0, largura, altura);
-
-                const novoPlaneta = data.perfil.casa.planeta;
-                const novoX = 70;
-                const novoY = 520;
-
-                context.font = 'bold 50px Arial';
-                context.fillStyle = 'yellow'; // Definindo a cor do texto como amarelo
-                context.fillText(novoPlaneta, novoX, novoY);
-
-                //-----INICIO NOME
-                const novoNome = data.perfil.nome;
-                const novoX1 = 330;
-                const novoY1 = 350;
-
-                context.font = 'bold 30px Arial';
-                context.fillStyle = '#ffffff'; // Definindo a cor do texto como branco
-                context.fillText(novoNome, novoX1, novoY1);
-                //-----FIM NOME
-
-                //-----INICIO INFORMAÇÕES
-                const novoX2 = 70;
-                const novoY2 = 75;
-                let corLetra = 'yellow'; // Corrigindo o nome da variável
-                let xp = `🆙 XP: ${data.perfil.xp} XP`
-                let nivel = `📈 Nivel: ${data.perfil.nivel.nome}`
-                let Poder = `💪 Poder [Força]: ${data.perfil.poder}`
-                let username = `🌀 Username: ${data.perfil.username}`
-                let idioma = `🗣️ Idioma: ${data.perfil.idioma}`
-                let moeda = `💰 Moeda: ${data.perfil.carteira.currency}`
-                let str = `         
-🏠 Colonia: _${data.perfil.casa.colonia.nome}      
-🛸 Sua nave Atual: ${data.perfil.nave.nome}
-
-`;
-                // Definindo os nomes na imagem 
-                context.font = 'bold 20px Arial';
-                context.fillStyle = 'yellow'; // Definindo a cor do texto como branco
-                context.fillText(nivel, 250, 160); // Nivel
-                context.fillText(idioma, 530, 160);
-                context.fillText(Poder, 250, 190);
-                context.fillText(moeda, 530, 190);
-                context.fillText(username, 250, 220);
-                context.fillText(xp, 250, 245);
-                //----FIM INFORMAÇÕES
-
-                // Salvar o mapa como uma imagem
-                const buffer = canvas.toBuffer('image/png');
-                return fs.writeFileSync(`./src/glx/temp/${data.perfil.username}.png`, buffer), setTimeout(() => { fs.unlinkSync(`./src/glx/temp/${data.perfil.username}.png`); }, 3000);
-
-
-            }).catch((error) => {
-                console.error('Erro ao carregar imagem do mapa:', error);
-            });
-
-
-        }
-
-        async function mapa() {
-            /*Esta função cria uma imagem de um mapa com os dados especificado de cada planeta
-            1) A marcação no mapa de cada planeta ficara no X E Y de cada planeta no database
-            2) 
-            */
-            let planeta = db.planetas[data.perfil.casa.idpelonome]
-            let colonias = Object.keys(planeta.colonias)
-
-
-            // Configurações do mapa
-            const largura = 1000;
-            const altura = 600;
-
-            // Criar um canvas com as dimensões do mapa
-            const canvas = createCanvas(largura, altura);
-            const context = canvas.getContext('2d');
-
-            // Carregar a imagem de fundo do mapa
-            loadImage('./src/glx/fundomapa.jpg').then((imagemMapa) => {
-                // Desenhar a imagem de fundo do mapa
-                context.drawImage(imagemMapa, 0, 0, largura, altura);
-
-                /* COLONIA 1
-               const xInicio =  300; // Inicio da linha Horizontal da esquerda para direita
-               const xFim = 400; // Inicio da linha Horizontal da esquerda para direita
-               const yInicio = 160; // inicio da linha vertical de cima para baixo 
-               const yFim = 260; // Fim da linha vertical de cima para baixo 
-               const larguraBorda = 3;
-               context.strokeStyle = 'red'; // Cor da borda
-               context.lineWidth = larguraBorda;
-               context.strokeRect(xInicio, yInicio, xFim - xInicio, yFim - yInicio);*/
-
-
-                // Função para desenhar uma caixa de texto com cantos arredondados
-                async function drawRoundRect(x, y, largura, altura, raio, corFundo, corBorda, opacidade) {
-                    context.beginPath();
-                    context.moveTo(x + raio, y);
-                    context.arcTo(x + largura, y, x + largura, y + altura, raio);
-                    context.arcTo(x + largura, y + altura, x, y + altura, raio);
-                    context.arcTo(x, y + altura, x, y, raio);
-                    context.arcTo(x, y, x + largura, y, raio);
-                    context.closePath();
-                    context.fillStyle = `rgba(255, 255, 255, ${opacidade})`; // Fundo branco quase transparente
-                    context.strokeStyle = corBorda;
-                    context.lineWidth = 1;
-                    context.fill();
-                    context.stroke();
-                }
-
-                let titulos2 = []
-                for (let i = 1; i <= Object.keys(planeta.colonias).length; i++) {
-                    let template = { nome: 'teste', x: 0, y: 0 }
-                    template.nome = planeta.colonias[`colonia${i}`].nome
-                    template.x = planeta.colonias[`colonia${i}`].localizacao.x
-                    template.y = planeta.colonias[`colonia${i}`].localizacao.y
-                    titulos2.push(template)
-                }
-                // Títulos das cidades
-                const titulosCidades = titulos2
-
-                // Desenhar os títulos das cidades
-                context.fillStyle = 'white'; // Cor das letras
-                context.font = 'bold 20px Arial'; // Estilo da fonte
-                titulosCidades.forEach(titulo => {
-                    // Determinar a largura do texto para centralizá-lo na caixa
-                    const larguraTexto = context.measureText(titulo.nome).width;
-                    // Desenhar a caixa de texto com cantos arredondados
-                    drawRoundRect(titulo.x - larguraTexto / 2 - 5, titulo.y - 20, larguraTexto + 10, 30, 5, 'white', 'white', 0.3); // Opacidade de 70%
-                    // Definir a cor do texto como marrom
-                    context.fillStyle = 'white';
-                    context.arc(titulosCidades.x, titulosCidades.y, 5, 0, Math.PI * 3);
-                    // Escrever o texto dentro da caixa
-                    context.fillText(titulo.nome, titulo.x - larguraTexto / 2, titulo.y);
-                });
-
-                // -------------- Faz uma busca no db e busca as posições ocupadas no planeta do usuario que mandou a mensagem
-
-                let planetaa = data.perfil.casa.idpelonome
-                let coloniaa = data.perfil.casa.colonia.id //
-                for (let i = 0; i < db.planetas[planetaa].colonias[`colonia${coloniaa}`].posicaoOcupadas.length; i++) {
-
-                    // Posição dos usuarios no planeta TODOS
-                    let posicoes = db.planetas[planetaa].colonias[`colonia${coloniaa}`].posicaoOcupadas
-                    // Pega o data de apenas um usuario por vez e utiliza para marcar no mapa
-                    let dataa = global.db.data.users[posicoes[i].id].gameglx
-
-
-                    //Posição e nome do usuario que sera marcado no mapa
-                    const novoNome = dataa.perfil.nome;
-                    const novoX = posicoes[i].x;
-                    const novoY = posicoes[i].y
-
-                    const corOriginal = context.fillStyle;
-
-                    // Desenhar o novo nome sem borda ou fundo formatados, apenas a cor do texto
-                    context.fillStyle = 'yellow'; // Definindo a cor do texto como amarelo
-                    context.fillText(novoNome, novoX - 50, novoY - 10);
-
-                    // Restaurar a cor original do contexto
-                    context.fillStyle = corOriginal;
-
-                    // Desenhar uma marcação
-                    context.beginPath();
-                    context.arc(novoX, novoY, 10, 0, Math.PI * 2); // Desenha um círculo de raio 5 nas coordenadas do novo nome
-                    context.fillStyle = 'blue'; // Cor da marcação
-                    context.fill();
-                }
-
-
-                // Salvar o mapa como uma imagem
-                const buffer = canvas.toBuffer('image/png');
-                return fs.writeFileSync(`./src/glx/temp/${data.perfil.username}.png`, buffer), setTimeout(() => { fs.unlinkSync(`./src/glx/temp/${data.perfil.username}.png`) }, 5000);
-
-
-            }).catch((error) => {
-                console.error('Erro ao carregar imagem do mapa:', error);
-            });
-
-        }
-
-
 
         async function criarGrupo() {
             /*Esta Função Cria um grupo para cada planeta cadastrado no database do glx. Para realizar esta opeção tem algumas condições para ser seguidas
