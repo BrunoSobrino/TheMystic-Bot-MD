7f4d16e8fb8754b24f3b861926541b7686295c44
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-05-16 19:38:19 -0600

    Update api.js

--- api.js
+++ api.js
@@ -18,6 +18,10 @@ global.openai_org_id = 'org-3';
 global.MyApiRestBaseUrl = 'https://api-brunosobrino.onrender.com';
 global.MyApiRestApikey = 'BrunoSobrino';
 
+global.MyApiRestBaseUrl2 = 'https://api-for-canvas-brunosobrino.koyeb.app';
+
+global.MyApiRestBaseUrl3 = 'https://api.cafirexos.com'; // Bloqueo de IP
+
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
