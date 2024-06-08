f5e998c124cac2399dde6c2cebe543bb98f7d7ff
maroupp02 <3mkjackyakosomk@gmail.com>
2024-06-08 22:40:13 +0300

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -1,17 +1,5 @@
-// TheMystic-Bot-MD@BrunoSobrino - _antiarab.js
-
-
 const handler = (m) => m;
-handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-
-  const tradutor = _translate.plugins._antiarab
-  // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
+handler.before = async function(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
   /* if (m.message) {
     console.log(m.message)
   }*/
@@ -19,20 +7,20 @@ handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwn
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[conn.user.jid] || {};
   if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
-    if (m.sender.startsWith('212' || '212')) {
-      m.reply(tradutor.texto1);
+    if (m.sender.startsWith('90' || '90')) {
+      m.reply(`*[❗] تم طرد الوهمي عايز تعمل راجل تعالي بالاساسي😉*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
 
-    if (m.sender.startsWith('265' || '265')) {
-      m.reply(tradutor.texto2);
+    if (m.sender.startsWith('7' || '7')) {
+      m.reply(`*[❗] تم طرد الوهمي عايز تعمل راجل تعالي بالاساسي😉*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
 
     if (m.sender.startsWith('92' || '92')) {
-      m.reply(tradutor.texto3);
+      m.reply(`*[❗] تم طرد الوهمي عايز تعمل راجل تعالي بالاساسي😉*`);
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     }
