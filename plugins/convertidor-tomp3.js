f056a24c3a3b53f773eeb9fe3ca4370b02117112
Imadkinge <imadchat47@gmail.com>
2024-06-29 19:53:08 +0100

    Update convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -3,7 +3,7 @@ import {toAudio} from '../lib/converter.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.convertidor_tomp3
 
 
