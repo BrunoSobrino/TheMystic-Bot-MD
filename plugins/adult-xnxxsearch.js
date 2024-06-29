2e39eaaa3a7b8ef2f2c506d7c30839637f7bf903
Imadkinge <imadchat47@gmail.com>
2024-06-29 20:21:11 +0100

    Update adult-xnxxsearch.js

--- plugins/adult-xnxxsearch.js
+++ plugins/adult-xnxxsearch.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.adult_xnxxsearch
 
 
