1dae571c9f22bac7f5ebca1c35dcdf79b6bd4ae8
Imadkinge <imadchat47@gmail.com>
2024-06-30 00:59:25 +0100

    Update herramientas-chatgpt.js

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -23,7 +23,7 @@ const openaiii = new OpenAIApi(configuration);
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.herramientas_chatgpt
 
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
