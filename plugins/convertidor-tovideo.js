f91f450a75b7584fb6b3493e91d928705802abbf
Imadkinge <imadchat47@gmail.com>
2024-06-30 10:48:41 +0100

    Update convertidor-tovideo.js

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -7,7 +7,7 @@ import {ffmpeg} from '../lib/converter.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.convertidor_tovideo
 
   if (!m.quoted) throw `*${tradutor.texto1} ${usedPrefix + command}*`;
