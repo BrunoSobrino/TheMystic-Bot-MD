bf0e38611933aa8cc8e2c4e24bae6710124f65d7
Imadkinge <imadchat47@gmail.com>
2024-06-30 12:08:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('bobiza♥️\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
