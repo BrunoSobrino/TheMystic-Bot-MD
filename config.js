bd3f40f2e6f494ccdd4d920c8d44728ce0d1709c
𝗖𝗮𝘀𝗽𝗲𝗿  ᥫ᭡ <167849226+Casper0273@users.noreply.github.com>
2024-07-04 20:40:18 -0600

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['523121674494', '👑 Mystic - Creador 👑', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
