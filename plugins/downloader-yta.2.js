9429defa2e2297c41df2de4fdc215d9d8234bb7b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-07-07 10:23:09 -0600

    Update downloader-yta.2.js

--- plugins/downloader-yta.2.js
+++ plugins/downloader-yta.2.js
@@ -1,36 +1,75 @@
-import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import fetch from 'node-fetch';
+import yts from 'yt-search';
+import axios from 'axios';
 
-const handler = async (m, {conn, args}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.downloader_yta_2
+const handler = async (m, { conn, args }) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
+  const tradutor = _translate.plugins.downloader_yta_2;
+  const tradutorrr = _translate.plugins.downloader_yta;  
 
-  if (!args[0]) throw tradutor.texto1;
-  await m.reply(tradutor.texto2);
+  if (!args[0]) return await conn.sendMessage(m.chat, { text: tradutor.texto1 }, { quoted: m });
+
+  const { key } = await conn.sendMessage(m.chat, { text: tradutor.texto2 }, { quoted: m });
+
+  const youtubeLink = args[0];
+
   try {
-    const q = '128kbps';
-    const v = args[0];
-    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
-    const dl_url = await yt.audio[q].download();
-    const ttl = await yt.title;
-    const size = await yt.audio[q].fileSizeH;
-    const cap = `${tradutor.texto3[0]} ${ttl}\n${tradutor.texto3[1]}  ${size}`.trim();
-    await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
-  } catch {
+    const yt_search = await yts(youtubeLink);
+    const audioUrl = `${global.MyApiRestBaseUrl}/api/v1/ytmp3?url=${yt_search.all[0].url}&apikey=${global.MyApiRestApikey}`;
+    const buff_aud = await getBuffer(audioUrl);
+    const fileSizeInBytes = buff_aud.byteLength;
+    const fileSizeInKB = fileSizeInBytes / 1024;
+    const fileSizeInMB = fileSizeInKB / 1024;
+    const size = fileSizeInMB.toFixed(2);
+    const title = yt_search.all[0].title;
+    const cap = `${tradutor.texto3[0]} ${title}\n${tradutor.texto3[1]}  ${size} MB`.trim();
+
+    await conn.sendMessage(m.chat, { document: buff_aud, caption: cap, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: m });
+    await conn.sendMessage(m.chat, { text: tradutorrr.texto5[4], edit: key }, { quoted: m });
+  } catch (error) {
+    console.log('Primera API falló, intentando con la segunda...', error);
     try {
-      const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${args[0]}`);
-      const lolh = await lolhuman.json();
-      const n = lolh.result.title || 'error';
-      const n2 = lolh.result.link;
-      const n3 = lolh.result.size;
-      const cap2 = `${tradutor.texto3[0]} ${n}\n${tradutor.texto3[1]}${n3}`.trim();
-      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m});
-    } catch {
-      await conn.reply(m.chat, tradutor.texto4, m);
+      const yt_search = await yts(youtubeLink);
+      const audioUrl = `${global.MyApiRestBaseUrl}/api/v2/ytmp3?url=${yt_search.all[0].url}&apikey=${global.MyApiRestApikey}`;
+      const buff_aud = await getBuffer(audioUrl);
+      const fileSizeInBytes = buff_aud.byteLength;
+      const fileSizeInKB = fileSizeInBytes / 1024;
+      const fileSizeInMB = fileSizeInKB / 1024;
+      const size = fileSizeInMB.toFixed(2);
+      const title = yt_search.all[0].title;
+      const cap = `${tradutor.texto3[0]} ${title}\n${tradutor.texto3[1]}  ${size} MB`.trim();
+
+      await conn.sendMessage(m.chat, { document: buff_aud, caption: cap, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: m });
+      await conn.sendMessage(m.chat, { text: tradutorrr.texto5[4], edit: key }, { quoted: m });
+    } catch (error) {
+      console.log('Segunda API también falló', error);
+      await conn.sendMessage(m.chat, { text: tradutor.texto4, edit: key }, { quoted: m });
     }
   }
 };
-handler.command = /^ytmp3doc|ytadoc|ytmp3.2|yta.2$/i;
+
+handler.command = /^(ytmp3doc|ytadoc|ytmp3.2|yta.2)$/i;
 export default handler;
+
+const getBuffer = async (url, options) => {
+  try {
+    options ? options : {};
+    const res = await axios({
+      method: 'get',
+      url,
+      headers: {
+        'DNT': 1,
+        'Upgrade-Insecure-Request': 1,
+      },
+      ...options,
+      responseType: 'arraybuffer',
+    });
+
+    return res.data;
+  } catch (e) {
+    console.log(`Error : ${e}`);
+    throw e;  
+  }
+};
