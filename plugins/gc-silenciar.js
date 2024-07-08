14195da48e0d6cb226e20477be22cf56603785e6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-07-07 22:56:37 -0600

    Update gc-silenciar.js

--- plugins/gc-silenciar.js
+++ plugins/gc-silenciar.js
@@ -2,79 +2,79 @@
 // Recreado del Código por: MoonContentCreator
 import fetch from 'node-fetch';
 
-// Función para obtener cadenas codificadas
-const getStringFromCode = (index) => {
-    const strings = [
-        '✨ *Sólo otro administrador puede desmutarte*', '👑 *El creador del bot no puede ser mutado*',
-        '129PEBBUv', '51289OMfQvC', 'data', '9070eQsjRl', 'users', 'owner', 'user',
-        'Tus mensajes serán eliminados', 'BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD',
-        'mute', 'muto', 'Halo', '𝗨𝘀𝘂𝗮𝗿𝗶𝗼 𝗺𝘂𝘁𝗮𝗱𝗼', '3136705kmgDtk', 'mentionedJid', '💥 *Menciona a la persona que deseas demutar*',
-        '@s.whatsapp.net', 'reply', 'sender', '𝗨𝘀𝘂𝗮𝗿𝗶𝗼 𝗱𝗲𝗺𝘂𝘁𝗮𝗱𝗼', '🚩 *No puedes mutar el bot*',
-        'group', '0@s.whatsapp.net', 'unmute', '14340DPskXA', '☁ *Este usuario no ha sido mutado*',
-        '💥 *Menciona a la persona que deseas mutar*', '472398wcTpbo', '848XQksSy', '🚩 *Este usuario ya ha sido mutado*',
-        'groupMetadata', '👑 *No puedes mutar el creador del grupo*', '6547128nviniQ', '3051obNcjV', 'chat', 'jid', 'quoted',
-        '💭 *Solo un administrador puede ejecutar este comando', '955169UoGRsU'
-    ];
-    return strings[index - 310];
+const messages = {
+    onlyAdmin: '💌 *Solo un administrador puede ejecutar este comando*',
+    muteBotOwner: '👑 *El creador del bot no puede ser mutado*',
+    mentionTarget: '💥 *Menciona a la persona que deseas mutar*',
+    alreadyMuted: '🚩 *Este usuario ya ha sido mutado*',
+    cannotMuteBot: '🚩 *No puedes mutar el bot*',
+    muteSuccess: 'Tus mensajes serán eliminados',
+    unmuteSuccess: 'Tus mensajes no serán eliminados',
+    notMuted: '☁ *Este usuario no ha sido mutado*',
+    creatorCannotBeMuted: '👑 *No puedes mutar el creador del grupo*',
+    userMuted: '𝗨𝘀𝘂𝗮𝗿𝗶𝗼 𝗺𝘂𝘁𝗮𝗱𝗼',
+    userUnmuted: '𝗨𝘀𝘂𝗮𝗿𝗶𝗼 𝗱𝗲𝗺𝘂𝘁𝗮𝗱𝗼',
+    vcard: 'BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD',
+    botImageUrl: 'https://telegra.ph/file/f8324d9798fa2ed2317bc.png',
+    userImageUrl: 'https://telegra.ph/file/aea704d0b242b8c41bf15.png'
 };
 
-// Handler para procesar comandos de mensajes
 const handler = async (message, { conn, command, text, isAdmin }) => {
     const muteCommand = 'mute';
     const unmuteCommand = 'unmute';
 
     if (command === muteCommand) {
-        if (!isAdmin) throw getStringFromCode(367);
+        if (!isAdmin) throw messages.onlyAdmin;
 
-        const ownerJid = global.owner[0][0] + getStringFromCode(320);
-        if (message.mentionedJid[0] === ownerJid) throw getStringFromCode(311);
+        const ownerJid = global.owner[0][0] + '@s.whatsapp.net';
+        if (message.mentionedJid[0] === ownerJid) throw messages.muteBotOwner;
 
         let targetJid = message.mentionedJid[0] ? message.mentionedJid[0] : message.quoted ? message.quoted.sender : text;
-        if (targetJid === conn.user.jid) throw getStringFromCode(323);
+        if (targetJid === conn.user.jid) throw messages.cannotMuteBot;
 
         const groupMetadata = await conn.groupMetadata(message.chat);
-        const groupOwnerJid = groupMetadata.owner || message.chat.split('-')[0] + getStringFromCode(320);
-        if (message.mentionedJid[0] === groupOwnerJid) throw getStringFromCode(350);
+        const groupOwnerJid = groupMetadata.owner || message.chat.split('-')[0] + '@s.whatsapp.net';
+        if (message.mentionedJid[0] === groupOwnerJid) throw messages.creatorCannotBeMuted;
 
         let userData = global.db.data.users[targetJid];
 
-        if (userData.mute === true) throw getStringFromCode(345);
+        if (userData.mute === true) throw messages.alreadyMuted;
 
         let responseMessage = {
-            key: { participants: getStringFromCode(329), fromMe: false, id: getStringFromCode(315) },
+            key: { participants: '0@s.whatsapp.net', fromMe: false, id: 'Halo' },
             message: {
                 locationMessage: {
-                    name: getStringFromCode(318),
-                    jpegThumbnail: await (await fetch('https://telegra.ph/file/f8324d9798fa2ed2317bc.png')).buffer(),
-                    vcard: getStringFromCode(309)
+                    name: messages.userMuted,
+                    jpegThumbnail: await (await fetch(messages.botImageUrl)).buffer(),
+                    vcard: messages.vcard
                 }
             },
-            participant: getStringFromCode(329)
+            participant: '0@s.whatsapp.net'
         };
 
-        conn.reply(message.chat, getStringFromCode(317), responseMessage, null, { mentions: [targetJid] });
+        conn.reply(message.chat, messages.muteSuccess, responseMessage, null, { mentions: [targetJid] });
         global.db.data.users[targetJid].mute = true;
     } else if (command === unmuteCommand) {
-        if (!isAdmin) throw getStringFromCode(367);
+        if (!isAdmin) throw messages.onlyAdmin;
 
         let targetJid = message.mentionedJid[0] ? message.mentionedJid[0] : message.quoted ? message.quoted.sender : text;
         let userData = global.db.data.users[targetJid];
 
-        if (userData.mute === false) throw getStringFromCode(328);
+        if (userData.mute === false) throw messages.notMuted;
 
         let responseMessage = {
-            key: { participants: getStringFromCode(329), fromMe: false, id: getStringFromCode(315) },
+            key: { participants: '0@s.whatsapp.net', fromMe: false, id: 'Halo' },
             message: {
                 locationMessage: {
-                    name: getStringFromCode(325),
-                    jpegThumbnail: await (await fetch('https://telegra.ph/file/aea704d0b242b8c41bf15.png')).buffer(),
-                    vcard: getStringFromCode(309)
+                    name: messages.userUnmuted,
+                    jpegThumbnail: await (await fetch(messages.userImageUrl)).buffer(),
+                    vcard: messages.vcard
                 }
             },
-            participant: getStringFromCode(329)
+            participant: '0@s.whatsapp.net'
         };
 
-        conn.reply(message.chat, 'Tus mensajes no serán eliminados', responseMessage, null, { mentions: [targetJid] });
+        conn.reply(message.chat, messages.unmuteSuccess, responseMessage, null, { mentions: [targetJid] });
         global.db.data.users[targetJid].mute = false;
     }
 };
