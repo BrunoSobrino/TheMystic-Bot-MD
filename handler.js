b0f9b8876fa4806799c064102e091b92b4aa912a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-07-07 22:26:58 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -73,7 +73,7 @@ export async function handler(chatUpdate) {
         if (!isNumber(user.money)) user.money = 15;
         if (!('language' in user)) user.language = 'es';
         if (!('registered' in user)) user.registered = false;
-        if (!('muto' in user)) user.muto = false
+        if (!('mute' in user)) user.mute = false
         if (!user.registered) {
           if (!('name' in user)) user.name = m.name;
           if (!isNumber(user.age)) user.age = -1;
@@ -611,7 +611,7 @@ export async function handler(chatUpdate) {
           gadodado: 0,
           gajah: 0,
           gamemines: false,
-          muto: false,
+          mute: false,
           ganja: 0,
           gardenboxs: 0,
           gems: 0,
