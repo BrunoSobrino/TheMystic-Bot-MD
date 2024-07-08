ea235b74dcf6096701962b939f9ee9aa1f5f83f2
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-07-07 22:06:01 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys';d
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
 import { smsg } from './lib/simple.js';
 import { format } from 'util';
 import { fileURLToPath } from 'url';
