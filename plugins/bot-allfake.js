78a0a029688ec4ab2409a0f1f53cb5d6ef10937d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-14 12:05:35 -0500

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -27,12 +27,21 @@ return res.data
 console.log(`Error : ${e}`)
 }}
 
+//Enlaces Mystic - Random!
+var canalmystic = 'https://whatsapp.com/channel/0029Vaein6eInlqIsCXpDs3y'  
+var gituser = 'https://github.com/BrunoSobrino' 
+var mysticyt = 'https://www.youtube.com/@theshadowbrokers-team' 
+var githubmystic = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
+
+global.mysticredes = [canalmystic, gituser, mysticyt, githubmystic].getRandom()
+
+
 //Fakes
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 global.mysticicon = 'https://qu.ax/GOer.jpg'
 
-global.fakechannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363316342878334@newsletter", serverMessageId: 100, newsletterName: 'The Shadow Brokers - Channel ℹ️', }, externalAdReply: { showAdAttribution: true, title: wm, body: 'The Shadow Brokers - TEAM', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: mysticicon, sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
+global.fakechannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363316342878334@newsletter", serverMessageId: 100, newsletterName: 'The Shadow Brokers - Channel ℹ️', }, externalAdReply: { showAdAttribution: true, title: wm, body: 'The Shadow Brokers - TEAM', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: mysticicon, sourceUrl: mysticredes, mediaType: 1, renderLargerThumbnail: false
 }, }, }}
 
 export default handler
