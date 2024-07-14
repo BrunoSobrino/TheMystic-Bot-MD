e4654f6172b082b761ad30d61090161f7c2bec05
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-14 11:58:56 -0500

    mystic - fakechannel

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -30,33 +30,9 @@ console.log(`Error : ${e}`)
 //Fakes
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-global.mysticicon = [ 
-'', 
-'',  
-'', 
-''
-].getRandom()
+global.mysticicon = 'https://qu.ax/GOer.jpg'
 
-global.fake = {
-contextInfo: {
-isForwarded: true,
-forwardedNewsletterMessageInfo: {
-newsletterJid: "120363316342878334@newsletter",
-serverMessageId: 100,
-newsletterName: 'The Shadow Brokers - Channel ℹ️',
-},
-externalAdReply: { 
-showAdAttribution: true,                                       title: wm,
-body: 'The Shadow Brokers - TEAM',
-mediaUrl: null,
-description: null,
-previewType: "PHOTO",
-thumbnailUrl: mysticicon,
-sourceUrl: md,
-mediaType: 1,
-renderLargerThumbnail: false
-},
-},
-}}
+global.fakechannel = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363316342878334@newsletter", serverMessageId: 100, newsletterName: 'The Shadow Brokers - Channel ℹ️', }, externalAdReply: { showAdAttribution: true, title: wm, body: 'The Shadow Brokers - TEAM', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: mysticicon, sourceUrl: md, mediaType: 1, renderLargerThumbnail: false
+}, }, }}
 
 export default handler
