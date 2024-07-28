683c2f0b6800c6ebef66eed24f767b901b93c412
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-07-28 16:34:10 -0600

    Update owner-bcgc2.js

--- plugins/owner-bcgc2.js
+++ plugins/owner-bcgc2.js
@@ -10,7 +10,7 @@ const handler = async (m, {conn, text, participants, isAdmin, isOwner, usedPrefi
   const users = participants.map((u) => u.id).filter((v) => v !== conn.user.jid);
   const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map((v) => v[0]);
   const fproducto = {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? {remoteJid: '17608914335@s.whatsapp.net'} : {})}, message: {'productMessage': {'product': {'productImage': {'mimetype': 'image/jpeg', 'jpegThumbnail': imagen1}, 'title': `ᴄᴏᴍᴜɴɪᴄᴀᴅᴏ ᴏғɪᴄɪᴀʟ ᴀ ɢʀᴜᴘᴏs`, 'description': 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ', 'currencyCode': 'USD', 'priceAmount1000': '1000000000', 'retailerId': 'Ghost', 'productImageCount': 1}, 'businessOwnerJid': `0@s.whatsapp.net`}}};
-  if (!m.quoted) throw `${tradutor.texto1[0]} *${usedPrefix + command}* ${traduto.texto1[1]}`;
+  if (!m.quoted) throw `${tradutor.texto1[0]} *${usedPrefix + command}* ${tradutor.texto1[1]}`;
   for (const id of groups) {
     await conn.sendMessage(id, {forward: m.quoted.fakeObj, mentions: (await conn.groupMetadata(`${id}`)).participants.map((v) => v.id)}, {quoted: fproducto});
   }
