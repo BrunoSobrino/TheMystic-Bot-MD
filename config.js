78a0437ca9c4258895b90030cb6f98f6eb8ae85b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-14 22:55:25 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import {watchFile, unwatchFile} from 'fs';
 import chalk from 'chalk';
 import {fileURLToPath} from 'url';
 import fs from 'fs'; 
-import * as cheerio from 'cheerio';
+import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
