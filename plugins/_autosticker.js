af0b37dc331fd2e1a1a83769b6bdfdc2a104fd54
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-16 16:22:50 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -1,15 +1,9 @@
 import {sticker} from '../lib/sticker.js';
 
-
- // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
 const handler = (m) => m;
 
 handler.all = async function(m) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
+  const idioma = global.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins._autosticker
 
