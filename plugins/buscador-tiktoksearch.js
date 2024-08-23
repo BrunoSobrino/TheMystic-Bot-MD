07c5ae183e94b86f60286d776be959d1fbf143b4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-23 16:26:39 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -49,5 +49,5 @@ await conn.reply(message.chat, error.toString(), message)
 
 handler.help = ['tiktoksearch <txt>']
 handler.tags = ['buscador']
-handler.command = ['tiktoksearch', 'tts', 'tiktoks']
+handler.command = ['tiktoksearch', 'tiktoks']
 export default handler
