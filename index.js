bad7c658f299b19674dfa4128f513f42048179c9
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-08-24 18:36:50 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
+console.log('[ ℹ️ ] Iniciando...');
+import {join, dirname} from 'path';
+import {createRequire} from 'module';
+import {fileURLToPath} from 'url';
+import {setupMaster, fork} from 'cluster';
 import cfonts from 'cfonts';
-import { createInterface } from 'readline';
+import {createInterface} from 'readline';
 import yargs from 'yargs';
-import chalk from 'chalk';
-console.log('\n[ ℹ️ ] Iniciando....');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
+const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
+
 say('The Mystic\nBot', {
   font: 'chrome',
   align: 'center',
@@ -21,53 +19,61 @@ say(`Bot creado por Bruno Sobrino`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
-   var isRunning = false;
-     function start(file) {
-    if (isRunning) return;
-   isRunning = true;
-   let args = [join(__dirname, file), ...process.argv.slice(2)];
-   say([process.argv[0], ...args].join(' '), {
-   font: 'console',
-     align: 'center',
-    colors: ['green']
-   });
-    setupMaster({
-  exec: args[0],
-   args: args.slice(1),
-   });
-   let p = fork();
-   p.on('message', data => {
-      switch (data) {
-    case 'reset':
-   p.process.kill();
-  isRunning = false;
-      start.apply(this, arguments);
-  break;
-   case 'uptime':
-    p.send(process.uptime());
- break;
+
+let isRunning = false;
+/**
+* Start a js file
+* @param {String} file `path/to/file`
+*/
+function start(file) {
+  if (isRunning) return;
+  isRunning = true;
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+
+  /** say('[ ℹ️ ] Escanea el código QR o introduce el código de emparejamiento en WhatsApp.', {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']}); **/
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1)});
+  const p = fork();
+  p.on('message', (data) => {
+
+    console.log('[RECIBIDO]', data);
+    switch (data) {
+      case 'reset':
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
     }
- });
+  });
   p.on('exit', (_, code) => {
- isRunning = false;
-  console.error('[ ℹ️ ] Error:\n', code);
-  process.exit();
-  if (code === 0) return;
-  watchFile(args[0], () => {
-  unwatchFile(args[0]);
-  start(file);
+    isRunning = false;
+    console.error('[ ℹ️ ] Ocurrio un error inesperado:', code);
+
+    p.process.kill();
+    isRunning = false;
+    start.apply(this, arguments);
+
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
   });
-    });
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-  if (!opts['test'])
-  if (!rl.listenerCount()) rl.on('line', line => {
-    p.emit('message', line.trim());
- });
- }
-   process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-  console.warn('[ ℹ️ ] Se excedió el límite de Listeners en:');
-  console.warn(warning.stack);
+  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', (line) => {
+        p.emit('message', line.trim());
+      });
+    }
   }
-  });
-  start('main.js');
+}
+start('main.js');
