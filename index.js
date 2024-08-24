37b6afb59b8ecffef17b8d725843867d4b4ca93a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-23 23:23:52 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('[ ℹ️ ] Iniciando...'); g
+console.log('[ ℹ️ ] Iniciando...');
 import {join, dirname} from 'path';
 import {createRequire} from 'module';
 import {fileURLToPath} from 'url';
