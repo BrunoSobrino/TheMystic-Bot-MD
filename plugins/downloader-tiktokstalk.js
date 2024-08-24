1c12128d87f4682df337c8eaa9453ac7c829216e
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-08-24 15:28:08 -0300

    ☆ TheMystic-Bot-MD ☆

--- plugins/downloader-tiktokstalk.js
+++ plugins/downloader-tiktokstalk.js
@@ -1,19 +1,26 @@
-import fg from 'api-dylux'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args }) => {
 
   if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*`
-  let res = await fg.ttStalk(args[0])
-  let gabrieltxt = `
+  const response = await axios.get("https://deliriusapi-official.vercel.app/tools/tiktokstalk", { params: { q: text } });
+    const data = response.data;
+
+    if (data.status && data.result) {
+
+      const user = data.result.users;
+      const stats = data.result.stats;
+      
+      let msg = `
 ┌────「 TIKTOKSTALK 」
-│✰ *Nombre:* ${res.name}
-│✰ *Usuario:* ${res.username}
-│✰ *Seguidores:* ${res.followers}
-│✰ *Siguiendo:* ${res.following}
-│✰ *Data:* ${res.desc}
+│✰ *Nombre:* ${user.nickname}
+│✰ *Usuario:* ${user.username}
+│✰ *Seguidores:* ${(stats.followerCount) || stats.followerCount}
+│✰ *Siguiendo:* ${(stats.followingCount) || stats.followingCount}
+│✰ *Data:* ${user.signature}
 │✰ *Enlace:*
-│✰ https://tiktok.com/${res.username}
+│✰ https://tiktok.com/${user.nickname}
 └───────────────┈`
-  await conn.sendFile(m.chat, res.profile, 'tt.png', gabrieltxt, m)
+  await conn.sendFile(m.chat, user.avatarLarger, 'tt.png', msg, m)
 }
 handler.help = ['tiktokstalk']
 handler.tags = ['dl']
