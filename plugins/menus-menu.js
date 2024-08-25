c765353f69b7dbbaf61889c74f5a86485ef10892
TheH2SO4 <theh2so4@protonmail.com>
2024-08-25 21:45:26 +0200

    Update

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -15,7 +15,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     // const pp = imagen7;
 
     // let vn = './media/menu.mp3'
-    const img = './Menu2.jpg';
+    const img = './src/media/images/menu-2.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';
     const week = d.toLocaleDateString(locale, { weekday: 'long' });
