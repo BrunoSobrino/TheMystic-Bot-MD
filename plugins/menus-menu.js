c5144b551d34254e9941c6ad6c59c1ddf40972bf
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-08-25 15:08:03 -0500

    Actualizar menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -36,7 +36,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
 
 ${tradutor.texto1[1]} ${taguser}
 
-> ⚡︎ Sunlight - Team ⚡︎
+> Moonlight - Team
 
 ${tradutor.texto1[2]}
 
