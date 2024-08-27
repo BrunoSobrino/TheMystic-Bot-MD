d3b55c34143a65203e67fc670d48caf8162fdad8
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-26 20:28:45 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/nv-a.js
+++ plugins/nv-a.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  const vn = './src/assets/audio/./src/assets/audio/01J672JMF3RCG7BPJW4X2P94N2.mp3';
+  const vn = './src/assets/audio/01J672JMF3RCG7BPJW4X2P94N2.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `./src/assets/audio/01J672JMF3RCG7BPJW4X2P94N2.mp3`}, {quoted: m});
 };
