3574d34de23e4a95f82a544fdc91008277458e36
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-08-31 12:36:05 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- src/libraries/store.js
+++ src/libraries/store.js
@@ -11,32 +11,165 @@
  * Fix:
  * chats.set, contacts.set does exist in newer versions of Baileys
  * see more https://github.com/Skidy89/baileys?tab=readme-ov-
- */
-
-
-import {readFileSync, writeFileSync, existsSync} from 'fs';
-
-/**
- * Importing necessary functions and objects from Baileys library
- * @type {import("baileys")}
- */
-const {initAuthCreds, BufferJSON, proto, jidNormalizedUser} = (await import('baileys')).default;
-
-/**
- * This function binds event listeners to the WhatsApp socket connection (`conn`)
- * for handling events such as chats, groups, participants, contacts, and presence updates.
  *
- * @param {import("baileys").WASocket | import("baileys").WALegacySocket} conn
+ * Contribution:
+ * Ciphertext error fix and additional improvements by @BrunoSobrino
+ * See: https://github.com/BrunoSobrino
  */
-function bind(conn) {
-  // Ensure that conn.chats exists
-  if (!conn.chats) conn.chats = {};
+const { BufferJSON, proto, isJidBroadcast, WAMessageStubType, updateMessageWithReceipt, updateMessageWithReaction, jidNormalizedUser } = (await import('baileys')).default;
+const TIME_TO_DATA_STALE = 5 * 60 * 1000;
 
-  /**
-   * Updates the contacts and stores them in `conn.chats`.
-   * 
-   * @param {import("baileys").Contact[]|{contacts:import("baileys").Contact[]}} contacts
-   */
+function makeInMemoryStore() {
+    let chats = {};
+    let messages = {};
+    let state = { connection: 'close' };
+
+    function loadMessage(jid, id = null) {
+        let message = null;
+        if (jid && !id) {
+            id = jid;
+            const filter = m => m.key?.id == id;
+            const messageFind = Object.entries(messages).find(([, msgs]) => msgs.find(filter));
+            message = messageFind?.[1]?.find(filter);
+        } else {
+            jid = jid?.decodeJid?.();
+            if (!(jid in messages)) return null;
+            message = messages[jid]?.find(m => m.key.id == id);
+        }
+        return message || null;
+    }
+
+    async function fetchGroupMetadata(jid, groupMetadata) {
+        jid = jid?.decodeJid?.();
+        if (!isJidGroup(jid)) return;
+        if (!(jid in chats)) return (chats[jid] = { id: jid });
+        const isRequiredToUpdate = !chats[jid].metadata || Date.now() - (chats[jid].lastfetch || 0) > TIME_TO_DATA_STALE;
+        if (isRequiredToUpdate) {
+            const metadata = await groupMetadata?.(jid);
+            if (metadata) Object.assign(chats[jid], { subject: metadata.subject, lastfetch: Date.now(), metadata });
+        }
+        return chats[jid].metadata;
+    }
+
+    function upsertMessage(jid, message, type = 'append') {
+        jid = jid?.decodeJid?.();
+        if (!(jid in messages)) messages[jid] = [];
+        delete message.message?.messageContextInfo;
+        delete message.message?.senderKeyDistributionMessage;
+        const msg = loadMessage(jid, message.key.id);
+        if (msg) Object.assign(msg, message);
+        else type === 'append' ? messages[jid].push(message) : messages[jid].unshift(message);
+    }
+
+    function bind(conn) {
+        if (!conn.chats) conn.chats = {};
+
+        conn.ev.on('messages.upsert', ({ messages: newMessages, type }) => {
+            if (['append', 'notify'].includes(type)) {
+                for (const msg of newMessages) {
+                    const jid = msg.key.remoteJid?.decodeJid?.();
+                    if (!jid || isJidBroadcast(jid)) continue;
+                    upsertMessage(jid, proto.WebMessageInfo.fromObject(msg), type);
+                }
+            }
+        });
+
+        conn.ev.on('messages.update', updates => {
+            for (const { key, update } of updates) {
+                const jid = key.remoteJid?.decodeJid?.();
+                const message = loadMessage(jid, key.id);
+                if (message) Object.assign(message, update);
+            }
+        });
+
+        conn.ev.on('message-receipt.update', updates => {
+            for (const { key, receipt } of updates) {
+                const jid = key.remoteJid?.decodeJid?.();
+                const message = loadMessage(jid, key.id);
+                if (message) updateMessageWithReceipt(message, receipt);
+            }
+        });
+
+        conn.ev.on('messages.reaction', updates => {
+            for (const { key, reaction } of updates) {
+                const jid = key.remoteJid?.decodeJid?.();
+                const message = loadMessage(jid, key.id);
+                if (message) updateMessageWithReaction(message, reaction);
+            }
+        });
+
+        conn.ev.on('chats.set', ({ chats: newChats }) => {
+            for (const chat of newChats) {
+                const jid = chat.id.decodeJid();
+                if (!(jid in chats)) chats[jid] = { id: jid };
+                Object.assign(chats[jid], chat);
+            }
+        });
+
+        conn.ev.on('contacts.set', ({ contacts: newContacts }) => {
+            for (const contact of newContacts) {
+                const jid = contact.id.decodeJid();
+                if (!(jid in chats)) chats[jid] = { id: jid };
+                Object.assign(chats[jid], contact);
+            }
+        });
+
+        conn.ev.on('chats.upsert', newChats => {
+            for (const chat of newChats) {
+                const jid = chat.id.decodeJid();
+                if (!(jid in chats)) chats[jid] = { id: jid };
+                Object.assign(chats[jid], chat);
+            }
+        });
+
+        conn.ev.on('chats.update', updates => {
+            for (const update of updates) {
+                const jid = update.id.decodeJid();
+                if (!(jid in chats)) chats[jid] = { id: jid };
+                Object.assign(chats[jid], update);
+            }
+        });
+
+        conn.ev.on('presence.update', ({ id, presences: updates }) => {
+            const jid = id.decodeJid();
+            if (!(jid in chats)) chats[jid] = { id: jid };
+            Object.assign(chats[jid], { presences: { ...chats[jid].presences, ...updates } });
+        });
+
+        conn.ev.on('message-reaction.update', updates => {
+            for (const update of updates) {
+                const message = loadMessage(update.key.remoteJid, update.key.id);
+                if (message) {
+                    message.reactions = message.reactions || [];
+                    message.reactions.push(update.reaction);
+                }
+            }
+        });
+    }
+
+    function toJSON() {
+        return { chats, messages };
+    }
+
+    function fromJSON(json) {
+        Object.assign(chats, json.chats);
+        for (const jid in json.messages) {
+            messages[jid] = json.messages[jid].map(m => m && proto.WebMessageInfo.fromObject(m));
+        }
+    }
+
+    return { bind, loadMessage, toJSON, fromJSON, upsertMessage };
+}
+
+export default makeInMemoryStore();
+
+/* ---------------------------------------------------------- */
+
+// Version Skid, backup
+/*import {readFileSync, writeFileSync, existsSync} from 'fs';
+const {initAuthCreds, BufferJSON, proto, jidNormalizedUser} = (await import('baileys')).default;
+function bind(conn) {
+  if (!conn.chats) conn.chats = {};
   function updateNameToDb(contacts) {
     if (!contacts) return;
     try {
@@ -106,33 +239,19 @@ const KEY_MAP = {
   'app-state-sync-version': 'appStateVersions',
   'sender-key-memory': 'senderKeyMemory',
 };
-
-/**
- * Use a single file to persist authentication state (credentials and keys).
- *  - This function has been deprecated in favor of `useMultiFileAuthState`
- *  - This function will be removed in the next major version
- *  - You can use `useMultiFileAuthState` instead
- *  - deprecated by being so slow. has memory issues/leakages 
-  * @deprecated use `useMultiFileAuthState` instead
-  * 
- */
 function useSingleFileAuthState(filename, logger) {
   let creds; let keys = {}; let saveCount = 0;
-  // Save the authentication state to a file
   const saveState = (forceSave) => {
     logger?.trace('saving auth state');
     saveCount++;
     if (forceSave || saveCount > 5) {
       writeFileSync(
           filename,
-          // BufferJSON replacer utility saves buffers nicely
           JSON.stringify({creds, keys}, BufferJSON.replacer, 2),
       );
       saveCount = 0;
     }
   };
-
-  // Load authentication state from the file if it exists
   if (existsSync(filename)) {
     const result = JSON.parse(
         readFileSync(filename, {encoding: 'utf-8'}),
@@ -177,22 +296,10 @@ function useSingleFileAuthState(filename, logger) {
     saveState,
   };
 }
-
-/**
- * Load a message by its ID and optionally by JID (if both are provided).
- *
- * @param {string} jid - The JID of the chat.
- * @param {string} id - The ID of the message.
- * @returns {Object|null} The found message or null if not found.
- */
 function loadMessage(jid, id = null) {
-  // TODO: Implement a better way to load messages
-  // TODO: Implement a way to load messages by JID
-  // TODO: Implement Map to store messages
   let message = null;
   if (jid && !id) {
     id = jid;
-    /** @type {(m: import('@whiskeysockets/baileys').proto.WebMessageInfo) => Boolean} */
     const filter = (m) => m.key?.id == id;
     const messages = {};
     const messageFind = Object.entries(messages)
@@ -208,8 +315,7 @@ function loadMessage(jid, id = null) {
   }
   return message ? message : null;
 }
-
 export default {
   bind,
   loadMessage,
-};
+};*/
