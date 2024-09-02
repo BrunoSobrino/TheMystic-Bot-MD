2bf05326af2e2c64adfa1041e2e9764faa111996
Saad - DxD <131401635+Hyodu@users.noreply.github.com>
2024-09-02 01:42:10 +0100

    🍟 TheMystic-Bot-MD 🍟

--- config.js
+++ config.js
@@ -67,6 +67,7 @@ global.imagen2 = fs.readFileSync('./src/assets/images/menu/languages/pt/menu.png
 global.imagen3 = fs.readFileSync('./src/assets/images/menu/languages/fr/menu.png')
 global.imagen4 = fs.readFileSync('./src/assets/images/menu/languages/en/menu.png')
 global.imagen5 = fs.readFileSync('./src/assets/images/menu/languages/ru/menu.png')
+global.imagen6 = fs.readFileSync('./src/assets/images/menu/languages/es/menu.png');
 
 global.mods = [];
 
