c71ffb3027364d57a42b4a3a6193dcbdc354dd6a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-09-03 09:44:51 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/herramientas-whatmusic.js
+++ plugins/herramientas-whatmusic.js
@@ -1,51 +1,58 @@
 import { Shazam } from 'node-shazam';
 import fetch from 'node-fetch';
+import fs from 'fs';
+import ytSearch from 'yt-search'; 
+import path from 'path';
 const shazam = new Shazam();
 
-const handler = async (m) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
-  const traductor = _translate.plugins.herramientas_whatmusic;
+const handler = async (m, { conn }) => {
+  const idioma = global.db.data.users[m.sender].language;
+  const traductor = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`)).plugins.herramientas_whatmusic;
 
-  const q = m.quoted ? m.quoted : m;
+  const q = m.quoted || m;
   const mime = (q.msg || q).mimetype || '';
   if (/audio|video/.test(mime)) {
-    //if ((q.msg || q).seconds > 20) return m.reply(traductor.texto1);
     const media = await q.download();
     const ext = mime.split('/')[1];
-    fs.writeFileSync(`./src/tmp/${m.sender}.${ext}`, media);
+    const baseFilePath = `./src/tmp/${m.sender}`; 
+    const tempPath = await getUniqueFileName(baseFilePath, ext); 
+    fs.writeFileSync(tempPath, media);
 
-    let recognise;
+    let recognisePromise;
     if (/audio/.test(mime)) {
-      recognise = await shazam.fromFilePath(`./src/tmp/${m.sender}.${ext}`, false, 'en');
+      recognisePromise = shazam.fromFilePath(tempPath, false, 'en');
     } else if (/video/.test(mime)) {
-      recognise = await shazam.fromVideoFile(`./src/tmp/${m.sender}.${ext}`, false, 'en');
+      recognisePromise = shazam.fromVideoFile(tempPath, false, 'en');
     }
-      
+
+    const recognise = await recognisePromise;
     const { title, subtitle, artists, genres, images } = recognise.track;
-    const imagen = await (await fetch(`${images.coverart}`)).buffer();    
-    const texto = `${traductor.texto3[0]}\n\n${traductor.texto3[1]} ${title || traductor.texto2}\n${traductor.texto3[2]} ${subtitle || traductor.texto2}\n${traductor.texto3[4]} ${genres.primary || traductor.texto2}`;
+    const apiTitle = `${title} - ${subtitle || ''}`.trim();
 
-    const apiTitle = `${title} - ${subtitle || ''}`;
-
-    let url = 'https://github.com/BrunoSobrino'; 
+    let ytUrl = 'https://github.com/BrunoSobrino';
     try {
-      const response = await fetch(`${global.MyApiRestBaseUrl}/api/ytplay?text=${apiTitle}&apikey=${global.MyApiRestApikey}`);
-      const data = await response.json();
-      url = data.resultado.url;
+      const searchResult = await ytSearch(apiTitle);
+      if (searchResult && searchResult.videos.length > 0) {
+        ytUrl = searchResult.videos[0].url;
+      }
     } catch (error) {
-      console.error('Error al obtener la URL del video:', error);
+      console.error(error);
     }
+
+    const texto = `${traductor.texto3[0]}\n\n${traductor.texto3[1]} ${title || traductor.texto2}\n${traductor.texto3[2]} ${subtitle || traductor.texto2}\n${traductor.texto3[4]} ${genres.primary || traductor.texto2}`;
+    const imagen = await (await fetch(images.coverart)).buffer();
     
-    const audiolink = `${global.MyApiRestBaseUrl}/api/v1/ytmp3?url=${url}&apikey=${global.MyApiRestApikey}`;  
-    const audiobuff = await conn.getFile(audiolink)  
+    conn.sendMessage(m.chat, { text: texto.trim(), contextInfo: { forwardingScore: 9999999, isForwarded: true, externalAdReply: { showAdAttribution: true, containsAutoReply: true, renderLargerThumbnail: true, title: apiTitle, mediaType: 1, thumbnail: imagen, thumbnailUrl: imagen, mediaUrl: ytUrl, sourceUrl: ytUrl }}}, { quoted: m });
 
-    await conn.sendMessage(m.chat, { text: texto.trim(), contextInfo: { forwardingScore: 9999999, isForwarded: true, "externalAdReply": { "showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": apiTitle, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "thumbnailUrl": imagen, "mediaUrl": url, "sourceUrl": url } } }, { quoted: m });
+    try {
+      const audiolink = `${global.MyApiRestBaseUrl}/api/v1/ytmp3?url=${encodeURIComponent(ytUrl)}&apikey=${global.MyApiRestApikey}`;
+      const audiobuff = await conn.getFile(audiolink);
+      await conn.sendMessage(m.chat, { audio: audiobuff.data, fileName: `${title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
+    } catch (error) {
+      console.error(error);
+    }
 
-    await conn.sendMessage(m.chat, { audio: audiobuff.data, fileName: `${title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
-
-    fs.unlinkSync(`./src/tmp/${m.sender}.${ext}`);
+    fs.unlinkSync(tempPath);
   } else {
     throw traductor.texto4;
   }
@@ -53,3 +60,13 @@ const handler = async (m) => {
 
 handler.command = /^(quemusica|quemusicaes|whatmusic|shazam)$/i;
 export default handler;
+
+async function getUniqueFileName(basePath, extension) {
+  let fileName = `${basePath}.${extension}`;
+  let counter = 1;
+  while (fs.existsSync(fileName)) {
+    fileName = `${basePath}_${counter}.${extension}`;
+    counter++;
+  }
+  return fileName;
+};
