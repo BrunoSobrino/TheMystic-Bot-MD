fe306f5475151c78e0111550c7772bbd6020a8aa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-09-03 06:39:33 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/fun-verdad.js
+++ plugins/fun-verdad.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn}) => {
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.fun_verdad
 
-  global.verdad = tradutor.texto1;
+  global.verdad = tradutor.texto3;
 
   conn.reply(m.chat, `*┌────「 𝚅𝙴𝚁𝙳𝙰𝙳 」─*\n*“${pickRandom(global.verdad)}”*\n*└────「 𝙼𝚈𝚂𝚃𝙸𝙲 」─*`, m);
 };
