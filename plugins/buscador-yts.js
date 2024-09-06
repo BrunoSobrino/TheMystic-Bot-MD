7821b1f77eed6dc289535cccfd190f2545603a4b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-09-06 00:22:35 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -52,13 +52,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP3',
-                  id: `${prefijo}play.1 ${video.url}`
+                  id: `${prefijo}ytmp3 ${video.url}`
                 },
                 {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP4',
-                  id: `${prefijo}play.2 ${video.url}`
+                  id: `${prefijo}ytmp4 ${video.url}`
                 }
               ]
             }))
