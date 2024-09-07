be786e276d9922ef2bb2743f6d52223745a9c4ef
skid <112231743+Skidy89@users.noreply.github.com>
2024-09-06 18:39:49 -0600

    Update sticker.js

--- src/libraries/sticker.js
+++ src/libraries/sticker.js
@@ -153,7 +153,7 @@ function sticker6(img, url) {
  */
 async function addExif(webpSticker, packname, author, categories = [''], metadata) {
   const img = new webp.Image();
-  const stickerPackId = 'Mystic' + randomBytes(12).toString('hex').toUpperCase()
+  const stickerPackId = 'Mystic' + crypto.randomBytes(12).toString('hex').toUpperCase()
   const json = {
       "sticker-pack-id": metadata.packId ? metadata.packId : `${stickerPackId}`,
       "sticker-pack-name": packname ? packname : undefined,
