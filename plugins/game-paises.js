4ef499102e8aa180ef476ea63b10e5e2daa58993
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-09-07 09:48:52 -0300

    🧿 The Mystic Bot - MD 🔮

--- plugins/game-paises.js
+++ plugins/game-paises.js
@@ -3,20 +3,17 @@ import db from '../src/libraries/database.js'
 let handler = async (m, { conn, args, text, isOwner, usedPrefix, command }) => {
     conn.advpais = conn.advpais ? conn.advpais : {}
     if (conn.advpais[m.chat]) return m.reply(`*_< ADIVINANZA - MYSTIC >_*\n\n*TODAVIA QUEDA UNA ADIVINANZA QUE RESPONDER*`)
-    conn.advpais[m.chat] = {
-        number: (9).getRandom(),
-        time: 60000,
-        bonus: 350,
-        opp: 4
-    }
+    const json = tekateki[Math.floor(Math.random() * tekateki.length)];
+    const _clue = json.response;
+    const clue = _clue.replace(/[A-Za-z]/g, '_');
     let teks = `*🌎 \`ADIVINA EL PAIS\` 🌎*
    QUE PAIS ES: *${json.question}*
-「○」 *TIEMPO:* 60.00 SEGUNDO/S
-「○」 *PREMIO:* *+10* MYSTIC-COINS\t• TIEMPO : ${(conn.advpais[m.chat].time / 1000).toFixed(2)} segundos
-\t• BONO : +${conn.advpais[m.chat].bonus} Exp`
+
+\t• TIEMPO : ${(conn.advpais[m.chat].time / 1000).toFixed(2)} SEGUNDO/S
+\t• BONO : +${conn.advpais[m.chat].bonus} EXP`
     let idmsg = await m.reply(teks)
     setTimeout(() => {
-      if (conn.advpais[m.chat]) conn.advpais(m.chat, `*¡ ✨ SE ACABO TU TIEMPO !*\n\t  • RESPUESTA : ${conn.advpais[m.chat].number}`, m, { quoted: idmsg })
+      if (conn.advpais[m.chat]) conn.advpais(m.chat, `*¡¡SE ACABO TU TIEMPO!!*\n\t  • RESPUESTA: ${conn.advpais[m.chat].number}`, m, { quoted: idmsg })
       delete conn.advpais[m.chat]
     }, conn.advpais[m.chat].time)
 }
