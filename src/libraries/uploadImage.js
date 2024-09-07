06ab2998e3e1ad2ea9a9ccaba3f648d57da53e5c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-09-06 19:20:22 -0600

    🧿 The Mystic Bot - MD 🔮

--- src/libraries/uploadImage.js
+++ src/libraries/uploadImage.js
@@ -1,16 +1,37 @@
 import fetch from 'node-fetch';
-import {FormData, Blob} from 'formdata-node';
-import {fileTypeFromBuffer} from 'file-type';
+import { FormData, Blob } from 'formdata-node';
+import { fileTypeFromBuffer } from 'file-type';
 
 /**
- * Upload image to telegra.ph
- * Supported mimetype:
+ * Upload file to qu.ax
+ * Supported mimetypes:
  * - `image/jpeg`
  * - `image/jpg`
- * - `image/png`s
- * @param {Buffer} buffer Image Buffer
+ * - `image/png`
+ * - `video/mp4`
+ * - `video/webm`
+ * - `audio/mpeg`
+ * - `audio/wav`
+ * @param {Buffer} buffer File Buffer
  * @return {Promise<string>}
  */
+export default async (buffer) => {
+  const { ext, mime } = await fileTypeFromBuffer(buffer);
+  const form = new FormData();
+  const blob = new Blob([buffer.toArrayBuffer()], { type: mime });
+  form.append('files[]', blob, 'tmp.' + ext);
+  const res = await fetch('https://qu.ax/upload.php', { method: 'POST', body: form });
+  const result = await res.json();
+  if (result && result.success) {
+    return result.files[0].url;
+  } else {
+    throw new Error('Failed to upload the file to qu.ax');
+  }
+};
+
+/*import fetch from 'node-fetch';
+import {FormData, Blob} from 'formdata-node';
+import {fileTypeFromBuffer} from 'file-type';
 export default async (buffer) => {
   const {ext, mime} = await fileTypeFromBuffer(buffer);
   const form = new FormData();
@@ -23,5 +44,5 @@ export default async (buffer) => {
   const img = await res.json();
   if (img.error) throw img.error;
   return 'https://telegra.ph' + img[0].src;
-};
+};*/
 
