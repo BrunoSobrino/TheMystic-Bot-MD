a4260ae90ccb1e4bdf1c82670de87dee3bcce774
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-09-07 00:09:55 -0300

    🧿 The Mystic Bot - MD 🔮

--- plugins/game-paises.js
+++ plugins/game-paises.js
@@ -26,6 +26,20 @@ QUE PAIS ES: *${json.question}*
       delete conn.tekateki[id];
     }, timeout)];
 };
+const id = m.chat;
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
+  this.tekateki = this.tekateki ? this.tekateki : {};
+  if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!');
+  if (m.quoted.id == this.tekateki[id][0].id) {
+    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
+    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+      global.db.data.users[m.sender].exp += this.tekateki[id][2];
+      m.reply(`\`\`\`¡¡RESPUESTA CORRECTA!!\`\`\`\n+${this.tekateki[id][2]} Exp`);
+      clearTimeout(this.tekateki[id][3]);
+      delete this.tekateki[id];
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`\`\`\`¡¡ESTAS CERCA!!\`\`\``);
+    else m.reply(`\`\`\`¡¡RESPUESTA INCORRECTA!!\`\`\``);
+};
 handler.help = ['paises'];
 handler.tags = ['game'];
 handler.command = /^(advpais|paisadv|adivinarpais|advpaises|countryadv|advcountry)$/i;
