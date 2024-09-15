dbbd3a927e5d2db9caf70d96218ad70c650fc64f
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2024-09-14 21:18:48 -0600

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- index.js
+++ index.js
@@ -6,41 +6,96 @@ import {setupMaster, fork} from 'cluster';
 import cfonts from 'cfonts';
 import {createInterface} from 'readline';
 import yargs from 'yargs';
+import readline from 'readline';
+import chalk from 'chalk'; 
+const {PHONENUMBER_MCC} = await import("baileys");
+import fs from 'fs'; 
+import './config.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const {say} = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-
-say('The Mystic\nBot', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']});
-
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 let isRunning = false;
-/**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
-function start(file) {
+const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
+
+function verificarOCrearCarpetaAuth() {
+  const authPath = join(__dirname, global.authFile);
+  if (!fs.existsSync(authPath)) {
+    fs.mkdirSync(authPath, { recursive: true }); 
+  }
+}
+
+function verificarCredsJson() {
+  const credsPath = join(__dirname, global.authFile, 'creds.json');
+  return fs.existsSync(credsPath);
+}
+
+function formatearNumeroTelefono(numero) {
+  let formattedNumber = numero.replace(/[^\d+]/g, '');
+  if (formattedNumber.startsWith('+52') && !formattedNumber.startsWith('+521')) {
+    formattedNumber = formattedNumber.replace('+52', '+521');
+  } else if (formattedNumber.startsWith('52') && !formattedNumber.startsWith('521')) {
+    formattedNumber = `+521${formattedNumber.slice(2)}`;
+  } else if (formattedNumber.startsWith('52') && formattedNumber.length >= 12) {
+    formattedNumber = `+${formattedNumber}`;
+  } else if (!formattedNumber.startsWith('+')) {
+    formattedNumber = `+${formattedNumber}`;
+  }
+  return formattedNumber;
+}
+
+function esNumeroValido(numeroTelefono) {
+  const numeroSinSigno = numeroTelefono.replace('+', '');
+  return Object.keys(PHONENUMBER_MCC).some(codigo => numeroSinSigno.startsWith(codigo));
+}
+
+async function start(file) {
   if (isRunning) return;
   isRunning = true;
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
 
-  /** say('[ ℹ️ ] Escanea el código QR o introduce el código de emparejamiento en WhatsApp.', {
+  say('The Mystic\nBot', {
+    font: 'chrome',
+    align: 'center',
+    gradient: ['red', 'magenta'],
+  });
+  say(`Bot creado por Bruno Sobrino`, {
     font: 'console',
     align: 'center',
-    gradient: ['red', 'magenta']}); **/
+    gradient: ['red', 'magenta'],
+  });
 
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1)});
+   verificarOCrearCarpetaAuth();
+  if (verificarCredsJson()) {
+    const args = [join(__dirname, file), ...process.argv.slice(2)];
+    setupMaster({ exec: args[0], args: args.slice(1) });
+    const p = fork();
+    return;  
+  }
+
+  const opcion = await question('[ ℹ️ ] Seleccione una opción:\n1. Con código QR\n2. Con código de texto de 8 dígitos\n---> ');
+  let numeroTelefono = '';
+  if (opcion === '2') {
+    const phoneNumber = await question(chalk.bgBlack(chalk.bold.yellowBright('Por favor, escriba su número de WhatsApp.\nEjemplo: +5219992095479\n')));
+    numeroTelefono = formatearNumeroTelefono(phoneNumber);
+    if (!esNumeroValido(numeroTelefono)) {
+      console.log(chalk.bgBlack(chalk.bold.redBright("Número inválido. Comience con el código de país de su número de WhatsApp.\nEjemplo: +5219992095479\n")));
+      process.exit(0);
+    }
+    process.argv.push(numeroTelefono);
+  }
+
+  if (opcion === '1') {
+    process.argv.push('qr');
+  } else if (opcion === '2') {
+    process.argv.push('code');
+  }
+
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+  setupMaster({ exec: args[0], args: args.slice(1) });
+
   const p = fork();
-  p.on('message', (data) => {
 
+  p.on('message', (data) => {
     console.log('[RECIBIDO]', data);
     switch (data) {
       case 'reset':
@@ -53,20 +108,20 @@ function start(file) {
         break;
     }
   });
+
   p.on('exit', (_, code) => {
     isRunning = false;
-    console.error('[ ℹ️ ] Ocurrio un error inesperado:', code);
-
+    console.error('[ ℹ️ ] Ocurrió un error inesperado:', code);
     p.process.kill();
     isRunning = false;
     start.apply(this, arguments);
-
     if (process.env.pm_id) {
       process.exit(1);
     } else {
       process.exit();
     }
   });
+
   const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
   if (!opts['test']) {
     if (!rl.listenerCount()) {
@@ -76,4 +131,5 @@ function start(file) {
     }
   }
 }
+
 start('main.js');
