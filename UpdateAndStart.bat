15e7ba0254f9cf129283aaf884f3cb77e8913d5f
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-16 19:08:58 -0400

    Delete UpdateAndStart.bat

--- UpdateAndStart.bat
+++
@@ -1,9 +0,0 @@
-echo La ventana de Conversion FFMPEG se vera cada vez que se utilice un comando Multimedia, para Solucionar esto Debes crear una Tarea de Inicio Automatico. Mas info en la Repo
-
-echo The FFMPEG Conversion window will appear every time a Multimedia command is used, to solve this you must create an Autostart Task. More info in the Repo
-
-git stash
-git pull https://github.com/BrunoSobrino/TheMystic-Bot-MD.git
-git stash pop
-npm install @whiskeysockets/baileys@latest @adiwajshing/baileys@latest --force
-npm start .
