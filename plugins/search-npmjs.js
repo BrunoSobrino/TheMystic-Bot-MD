193de078ec252bf125e2965d8b232de05452920b
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-09-17 20:17:47 -0300

    🧿 TheMystic-Bot-MD 🔮

--- plugins/search-npmjs.js
+++ plugins/search-npmjs.js
@@ -11,10 +11,10 @@ let handler = async (m, { text }) => {
   if (!objects.length) throw `「 ❗️ 」 LA BUSQUEDA "${text}" NO FUE ENCONTRADA`
   let txt = objects.map(({ package: pkg }) => {
     return `
-     ✰ 𝐍𝐨𝐦𝐛𝐫𝐞: ${pkg.name}
-    ✰ 𝐕𝐞𝐫𝐬𝐢𝐨𝐧: V${pkg.version}
-    ✰ 𝐄𝐧𝐥𝐚𝐜𝐞: ${pkg.links.npm}
-    ✰ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧: ${pkg.description}\n\n\`\`\`----------\`\`\``
+  ✰ 𝐍𝐨𝐦𝐛𝐫𝐞: ${pkg.name}
+  ✰ 𝐕𝐞𝐫𝐬𝐢𝐨𝐧: V${pkg.version}
+  ✰ 𝐄𝐧𝐥𝐚𝐜𝐞: ${pkg.links.npm}
+  ✰ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧: ${pkg.description}\n\n\`\`\`----------\`\`\``
   }).join`\n\n`
   m.reply(txt)
 }
