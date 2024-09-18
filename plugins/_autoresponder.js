98787fd5e6234b9c1c2e597283bb038066e8e882
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-09-17 21:05:37 -0300

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -8,7 +8,7 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat];
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-//await this.sendPresenceUpdate('composing', m.chat);
+await this.sendPresenceUpdate('composing', m.chat);
 
 async function luminsesi(q, username, logic) {
 try {
@@ -29,7 +29,7 @@ let jailbreak = await fetch('https://raw.githubusercontent.com/Skidy89/chat-gpt-
 var syms = `Actuaras como un Bot de WhatsApp el cual fue creado por BrunoSobrino tu seras The-MysticBot-MD.\n${jailbreak}`
 
 let result = await luminsesi(query, username, syms1)
-await m.reply(result)
+await m.conn.sendMessage(m.chat, { text: result }, { quoted: m });
 }
 return true
 }
