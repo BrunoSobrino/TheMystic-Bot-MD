91ce0ccab8c1447be42d5f8a021c19ebe3c90f17
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-18 21:15:45 -0400

    Update Termux.sh

--- web/Guias/Utilidades/Termux.sh
+++ web/Guias/Utilidades/Termux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/weskerty/test/main/Termux/.bashrc -O ~/.bashrc
+wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/.bashrc -O ~/.bashrc
 proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs nano npm git ffmpeg python imagemagick --noconfirm && \
 wget https://raw.githubusercontent.com/weskerty/test/main/Termux/update.sh -O ~/update.sh && \
 chmod 777 ~/update.sh && \
