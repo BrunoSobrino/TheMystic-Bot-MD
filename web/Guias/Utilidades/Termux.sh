738eb0604458404d21d13a59cb273a57ea15fc14
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-10-02 17:07:13 -0400

    Update Termux.sh

--- web/Guias/Utilidades/Termux.sh
+++ web/Guias/Utilidades/Termux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/.bashrc -O ~/.bashrc
-proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs nano npm git ffmpeg python imagemagick --noconfirm && \
+proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs npm git ffmpeg imagemagick --noconfirm && \
 wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/update.sh -O ~/update.sh && \
 chmod 777 ~/update.sh && \
 git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic && \
