96db53533ba6b729dcec866cccfb46c6fe00a3e5
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-10-03 02:32:16 -0400

    Update update.sh

--- web/Guias/Utilidades/update.sh
+++ web/Guias/Utilidades/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-pacman -Syu --noconfirm
+apt update -y && apt upgrade -y
 cd $HOME/mystic/ || echo Falla. Mystic no Existe.
-npm install @whiskeysockets/baileys@latest --force || echo "#########Error al instalar en mystic"
-#npm install --force
+npm install @whiskeysockets/baileys@latest --force || echo "#########Error al Actualizar Baileys. Continuando Igualmente"
 npm start .
