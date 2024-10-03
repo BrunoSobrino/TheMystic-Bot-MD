14d3f38a26d886b35e80ce337206ed60b049fc73
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-10-03 02:31:35 -0400

    Update Termux.sh

--- web/Guias/Utilidades/Termux.sh
+++ web/Guias/Utilidades/Termux.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/.bashrc -O ~/.bashrc
-proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs npm git ffmpeg imagemagick --noconfirm && \
-wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/update.sh -O ~/update.sh && \
-chmod 777 ~/update.sh && \
-git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic && \
-cd mystic && \
-npm install && \
-npm start code "
-
+proot-distro login debian -- bash -c "
+  apt-get update && \
+  apt-get install wget curl git ffmpeg imagemagick -y && \
+  curl -fsSL https://deb.nodesource.com/setup_lts.x -o nodesource_setup.sh && \
+  bash nodesource_setup.sh && \
+  apt-get install -y nodejs && \
+  wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/update.sh -O ~/update.sh && \
+  chmod +x ~/update.sh && \
+  git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic && \
+  cd mystic && \
+  npm install && \
+  npm start code
+"
