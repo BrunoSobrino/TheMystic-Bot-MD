bd4dc55420b2b777338305c4b99d8aae50ca5dcc
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-10-03 03:22:57 -0400

    Update Termux.sh

--- web/Guias/Utilidades/Termux.sh
+++ web/Guias/Utilidades/Termux.sh
@@ -2,10 +2,9 @@
 wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/.bashrc -O ~/.bashrc
 proot-distro login debian -- bash -c "
   apt-get update && \
-  apt-get install wget curl git ffmpeg imagemagick -y && \
   curl -fsSL https://deb.nodesource.com/setup_lts.x -o nodesource_setup.sh && \
   bash nodesource_setup.sh && \
-  apt-get install -y nodejs && \
+  apt-get install nodejs wget curl git ffmpeg imagemagick -y && \
   wget https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/refs/heads/master/web/Guias/Utilidades/update.sh -O ~/update.sh && \
   chmod +x ~/update.sh && \
   git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic && \
