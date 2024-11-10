feef9bda5f48bad093944c02d761a07258d17e79
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2024-11-09 21:42:46 -0300

    Update gc-eventos.js

--- plugins/gc-eventos.js
+++ plugins/gc-eventos.js
@@ -6,23 +6,4 @@ handler.command = /^(evento|eventos|eventofutabuclub)$/i
 handler.group = true;
 export default handler
 
-global.eventos = `*_CONCURSO DE LOGO DE HALLOWEEN 🎃_*
-
-Los miembros tendran tiempo de hacer un logo/edit al estilo Halloween con Futanaris referido a esa temática!.
-
-*Requisitos Importantes:*
-* Usar imagenes de *futanaris referido a la temática*, también puedes hacer uso de Collages. 
-
-* Puedes usar cualquier tipo de fuente de letras y que en el texto diga *"Futabu Club!" O "Futabu Club"*.
-
-* El tamaño del lienzo/dimensiones de la imagen tiene que ser *1000x1000* o *500x500*, cualquier otro tipo de tamaño sera rechazado ya que tiene que verse en las dimensiones que usa WhatsApp.
-
-* Trata de exportar la imagen en los siguientes formatos: *PNG y JPG*.
-
-*Tiempo⌛* 
-* Tienes tiempo de mandar esto hasta el *5 de octubre.*
-
-*Premio🏆*
-* El premio que se llevara el ganador sera *5 DOLARES*! (si no sabes cuantos es 5 dolares en tu pais puedes buscar en internet 5 dolares en "pon tu moneda de pais aqui", ejemplo: 5 Dolares en Pesos chilenos/argentinos), Tambien puedes elegir una membresía o algo que este a ese precio o un poco menos de ese valor, Por ejemplo: Netflix, Spotify, Crunchyroll, Youtube Premium, Telegram Premium, etc.
-
-_Buena suerte a todos los que participan, Recuerden darle al boton *"Me interesa"* si te importa el evento desde ya muchas gracias a todos los que lleguen a participar ❤️_`
+global.eventos = `Nada aún!`
