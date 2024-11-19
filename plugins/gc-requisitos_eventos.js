38accd0a9cea94aa769997b8d4bad226af7ddc2c
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2024-11-19 16:37:53 -0300

    Update gc-requisitos_eventos.js

--- plugins/gc-requisitos_eventos.js
+++ plugins/gc-requisitos_eventos.js
@@ -8,9 +8,9 @@ export default handler
 
 global.requisitos_eventos = `*_SECCION REQUISITOS (EVENTO NAVIDAD)_*
 
-⚠️REQUISITOS:
-* NO usar IA/AI (Inteligencia Artificial).
-* NO hacer uso de collages.
-* La imagen debe tener un lienzo/dimension/tamaño de 500x500px o 1000x1000px (Esto es para que la imagen sea compatible con Whatsapp y otras redes, tambien para que encaje correctamente).
-* La fuente de letra/texto tiene que decir la palabra "Futabu Club" o "Futabu Club!".
-* Una vez envies tu diseño/edicion, tienes que brindar una captura/video del proceso que hiciste de tu imagen en el programa de edicion.`
+⚠️| REQUISITOS:
+• *NO* usar IA/AI (Inteligencia Artificial).
+• *NO* hacer uso de collages.
+• La imagen debe tener un lienzo/dimensión/tamaño de 500x500px o 1000x1000px (Esto es para que la imagen sea compatible con Whatsapp y otras redes, también para que encaje correctamente).
+• La fuente de letra/texto tiene que decir la palabra "Futabu Club" o "Futabu Club!".
+• Una vez envíes tu diseño/edición, tienes que brindar una captura/video del proceso que hiciste de tu imagen en el programa de edición.`
