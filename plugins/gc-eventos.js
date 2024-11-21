ea448eb38c2f5ac9dd80b5a6b11d12a670fdedc2
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2024-11-20 21:30:49 -0300

    Update gc-eventos.js

--- plugins/gc-eventos.js
+++ plugins/gc-eventos.js
@@ -6,4 +6,12 @@ handler.command = /^(evento|eventos|eventofutabuclub)$/i
 handler.group = true;
 export default handler
 
-global.eventos = `Nada aún!`
+global.eventos = `*_CONCURSO DE LOGO DE NAVIDAD 🎄_*
+
+Los miembros tendrán que hacer un logo (imagen) para el grupo *Futabu Club* con temática de *futanaris navideñas o relacionado con las futas y navidad!*, este evento no hay premio debido a problemas técnicos, tendrán tiempo de terminar su imagen/edición hasta el *7 de Diciembre del 2024 (Sábado).* [En caso de que allá problemas con la fecha se atrasara un poco mas]
+
+⚠️| Usa el comando *.requisitos_evento* para ver los requisitos necesario a seguir para aprobar tu edición.
+
+🤝| Usa el comando *.ayuda_evento* por si necesitas ayuda con las imágenes.
+
+❤| Buena suerte a todos los participantes!.`
