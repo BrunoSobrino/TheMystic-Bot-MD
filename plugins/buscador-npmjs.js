d1215d4b661620129b59b30aedc2d4654b6e546e
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2024-12-14 09:53:14 -0300

    TheMystic-Bot-MD | Updates

--- plugins/buscador-npmjs.js
+++ plugins/buscador-npmjs.js
@@ -1,14 +1,15 @@
 /*
-By GabrielVz (@glytglobal)
+# Codigo creado por: GabrielVz (@glytglobal)
+# https://github.com/glytglobal
 */
 
 import fetch from 'node-fetch'
 
 let handler = async (m, { text }) => {
-  if (!text) throw '*「 ❗️ 」INGRESA EL TITULO O NOMBRE DEL SCRAPER DE NMPJS A BUSCAR*\n\n*• EJEMPLO:*\n> */npmjs yt-search*'
+  if (!text) throw `*[ ❗️ ] INGRESA EL TITULO O NOMBRE DEL SCRAPER DE NPMJS (NPM.ORG) A BUSCAR*`
   let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
   let { objects } = await res.json()
-  if (!objects.length) throw `「 ❗️ 」 LA BUSQUEDA "${text}" NO FUE ENCONTRADA`
+  if (!objects.length) throw `[ ❗️ ] LA BUSQUEDA "${text}" NO FUE ENCONTRADA\n\nINTENTE CON OTRO TIPO DE RESULTADOS`
   let txt = objects.map(({ package: pkg }) => {
     return `
   ✰ 𝐍𝐨𝐦𝐛𝐫𝐞: ${pkg.name}
