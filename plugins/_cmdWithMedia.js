94556f28a96e279bfd7830634a069393a0c08334
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-01-04 15:00:52 -0300

    𝙏𝙃𝙀 𝙈𝙔𝙎𝙏𝙄𝘾 - 𝘽𝙊𝙏

--- plugins/_cmdWithMedia.js
+++ plugins/_cmdWithMedia.js
@@ -2,7 +2,7 @@ const {
   proto,
   generateWAMessage,
   areJidsSameUser,
-} = (await import('@whiskeysockets/baileys')).default;
+} = (await import('baileys')).default;
 
 export async function all(m, chatUpdate) {
   if (m.isBaileys) return;
