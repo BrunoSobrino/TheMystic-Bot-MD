fbf59eeec94287840db492e1817bb5728de99f21
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2025-03-14 11:09:46 -0300

    Add files via upload

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -6,7 +6,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { ytmp3, ytmp4 } = require("@hiudyy/ytdl");
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {    
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {    
 const datas = global;
 const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
 const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
