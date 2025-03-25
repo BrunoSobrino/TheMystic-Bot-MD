9d33a08e81aa80dcdd8885958e3272e7ccd8c284
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-03-25 14:37:24 -0300

    ```TheMystic-Bot-MD | BrunoSobrino```

--- plugins/herramientas-chatgpt2.js
+++ plugins/herramientas-chatgpt2.js
@@ -7,7 +7,7 @@ if (usedPrefix == 'a' || usedPrefix == 'A') return;
 if (!text) return m.reply(`⚠️ Hablar con exploit de esta forma\n${usedPrefix + command} hola negro`);
 
 const exploit = await fetch('https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt').then(v => v.text());
-const prompt = `Actuaras como un Bot de WhatsApp el cual fue creado por BrunoSobrina tu seras The-MysticBot-MD.\n${jailbreak}`
+const prompt = `actuaras como un Bot de WhatsApp el cual fue creado por BrunoSobrina, tu seras The-MysticBot-MD.\n${exploit}`
 const api = await axios.get(`https://skynex.boxmine.xyz/docs/ai/myprompt?text=${text}&prompt=${prompt}&apikey=BrunoSobrino`);
 const data = api.data;
 await m.reply(data.answer)
