8e9719742c08b08ce221ca4989712cd0e181f25c
Alex-X <argumedodie@gmail.com>
2025-06-05 09:59:02 -0500

    Actualizar owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -3,13 +3,12 @@ import {promisify} from 'util';
 
 const exec = promisify(_exec).bind(cp);
 const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
+  if (!isROwner) return;
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.owner_exec2
 
-  if (!isROwner) return;
-  if (global.conn.user.jid != conn.user.jid) return;
   m.reply(tradutor.texto1);
   let o;
   try {
