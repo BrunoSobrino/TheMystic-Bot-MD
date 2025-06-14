f192e1a81256d002d48f7327da981600a562590c
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-14 08:09:42 -0300

    `Made With | GabrielZks`

--- plugins/rpg-profile.js
+++ plugins/rpg-profile.js
@@ -2,20 +2,15 @@ import {createHash} from 'crypto';
 import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
 
-
 const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.rpg_perfil
-
-  let pp = 'https://telegra.ph/file/06cc652844ea19e8aed1c.jpg';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   if (!(who in global.db.data.users)) throw tradutor.texto1;
   try {
-    pp = await conn.profilePictureUrl(who);
-  } catch (e) {
-  } finally {
+  const pp = await conn.profilePictureUrl(who)
     const {name, limit, lastclaim, registered, regTime, age, premiumTime} = global.db.data.users[who];
     const username = conn.getName(who);
     const prem = global.prems.includes(who.split `@` [0]);
@@ -29,9 +24,13 @@ ${tradutor.texto2[5]}  ${premiumTime > 0 ? 'Si' : (isPrems ? tradutor.texto2[10]
 ${tradutor.texto2[6]}  
 ${sn}`;
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
-  }
+  } catch (error) {
+  console.log(error);
+ }
 };
-handler.help = ['profile [@user]'];
+
+handler.help = ['profile'];
 handler.tags = ['xp'];
 handler.command = /^perfil|profile?$/i;
+
 export default handler;
