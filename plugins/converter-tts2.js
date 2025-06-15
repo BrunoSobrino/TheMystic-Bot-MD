2863df36b10b04c2cb2a5424624901fd16c70c51
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-15 09:31:56 -0300

    `Made With | GabrielZks`

--- plugins/converter-tts2.js
+++ plugins/converter-tts2.js
@@ -15,48 +15,46 @@ import axios from 'axios';
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.convertidor_tts2
+ const datas = global
+ const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+ const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+ const tradutor = _translate.plugins.convertidor_tts2
 
+ const [efecto, ...textoArray] = text.split(" ");
+ const texto = textoArray.join("");
 
-  const [efecto, ...textoArray] = text.split(" ");
-  const texto = textoArray.join("");
-
-  if (!efecto) {
-    let voiceList = await getVoiceList();
-    let responseText = `*${tradutor.texto1}*\n`;
-
-    for (let i = 0, count = 0; count < 100 && i < voiceList.resultado.length; i++) {
-      const entry = voiceList.resultado[i];
-      if (entry.ID.length <= 20) {
-        responseText += `*◉ ${usedPrefix + command} ${entry.ID} ${tradutor.texto2}*\n`;
-        count++;
-      }
-    }
-
-    return conn.sendMessage(m.chat, { text: responseText.trim() }, { quoted: m });
+ if (!efecto) {
+ let voiceList = await getVoiceList();
+ let responseText = `*${tradutor.texto1}*\n`;
+ for (let i = 0, count = 0; count < 100 && i < voiceList.resultado.length; i++) {
+ const entry = voiceList.resultado[i];
+ if (entry.ID.length <= 20) {
+   responseText += `*◉ ${usedPrefix + command} ${entry.ID} ${tradutor.texto2}*\n`;
+   count++;
   }
+}
+ return conn.sendMessage(m.chat, { text: responseText.trim() }, { quoted: m });
+}
 
-  let efectoValido = false;
-  let voiceList = await getVoiceList();
-  for (const entry of voiceList.resultado) {
-    if (entry.ID === efecto) {
-      efectoValido = true;
-      break;
-    }
+ let efectoValido = false;
+ let voiceList = await getVoiceList();
+ for (const entry of voiceList.resultado) {
+ if (entry.ID === efecto) {
+   efectoValido = true;
+   break;
   }
+}
 
-  if (!efectoValido) return conn.sendMessage(m.chat, { text: `*${tradutor.texto3[0]} ${usedPrefix + command} ${tradutor.texto3[1]}*` }, { quoted: m });
-
-  if (!texto) return conn.sendMessage(m.chat, {text: `*${tradutor.texto4[0]}*\n*◉ ${usedPrefix + command} ${efecto} ${tradutor.texto4[1]}*`}, {quoted: m});
-
-  let masivo = await makeTTSRequest(texto, efecto);
-  conn.sendMessage(m.chat, {audio: {url: masivo.resultado}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+ if (!efectoValido) return conn.sendMessage(m.chat, { text: `*${tradutor.texto3[0]} ${usedPrefix + command} ${tradutor.texto3[1]}*` }, { quoted: m });
+ if (!texto) return conn.sendMessage(m.chat, { text: `*${tradutor.texto4[0]}*\n*◉ ${usedPrefix + command} ${efecto} ${tradutor.texto4[1]}*` }, { quoted: m });
+ let masivo = await makeTTSRequest(texto, efecto);
+ conn.sendMessage(m.chat, { audio: { url: masivo.resultado }, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
 };
 
-handler.command = /^(g?tts2)$/i;
+handler.help = ['tts2'];
+handler.tags = ['converter'];
+handler.command = ['tts2'];
+
 export default handler;
 
 const secretKey = 'fe2ee40099494579af0ecf871b5af266';
