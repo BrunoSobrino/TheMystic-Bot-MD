f51b4d1dfb568e8162642554fcb3731f9b92c283
Alex-X <argumedodie@gmail.com>
2025-06-15 12:04:46 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,103 +1,97 @@
+import axios from 'axios';
 const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import("baileys")).default;
-import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
+    if (!text) return conn.sendMessage(m.chat, { text: '[❗] Ingresa un texto para la búsqueda en Pinterest.' }, { quoted: m });
 
-  if (!text) return reply(`[ ❗ ] Ingresa un texto para la busqueda.`);
- // await reply("「 ✰ 」ESTOY ENVIANDO SUS RESULTADOS...");
+    try {
+        let response = await pinterestSearch(text);
+        if (!response.status) throw new Error(response.resultado);
+        let searchResults = response.resultado;
 
-  try {
-  async function createImage(url) {
-    const { imageMessage } = await generateWAMessageContent({
-      image: {
-        url
-      }
-    }, {
-      upload: Rifky.waUploadToServer
-    });
-    return imageMessage;
-  }
+        shuffleArray(searchResults);
+        let selectedResults = searchResults.slice(0, 5);
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+        let imageCards = await Promise.all(
+            selectedResults.map(async (result, index) => ({
+                body: proto.Message.InteractiveMessage.Body.fromObject({ text: `\nRESULTADO: ${index + 1}\n` }),
+                footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: global.pickbot }),
+                header: proto.Message.InteractiveMessage.Header.fromObject({
+                    title: '「 ✰ 」RESULTADOS ENCONTRADOS',
+                    hasMediaAttachment: true,
+                    imageMessage: await createImage(result.imageUrl)
+                }),
+                nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+                    buttons: [
+                        {
+                            "name": "cta_url",
+                            "buttonParamsJson": `{"display_text":"Enlace","url":"https://www.pinterest.com/search/pins/?rs=typed&q=${text}"}`
+                        },
+                        {
+                            "name": "cta_url",
+                            "buttonParamsJson": `{"display_text":"Página Web","url":"https://www.pinterest.com/"}`
+                        }
+                    ]
+                })
+            }))
+        );
+
+        const responseMessage = generateWAMessageFromContent(m.chat, {
+            viewOnceMessage: {
+                message: {
+                    messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
+                    interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+                        body: proto.Message.InteractiveMessage.Body.create({
+                            text: "「 ✰ 」INFORMACION"
+                        }),
+                        footer: proto.Message.InteractiveMessage.Footer.create({
+                            text: `✰ *BUSQUEDA:*\n> ${text}\n\n✰ *USUARIO:*\n> ${m.pushName}`
+                        }),
+                        header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
+                        carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: imageCards })
+                    })
+                }
+            }
+        });
+
+        await conn.relayMessage(m.chat, responseMessage.message, { messageId: responseMessage.key.id });
+
+    } catch (error) {
+        await conn.sendMessage(m.chat, { text: `[❗] Error: ${error.message}` }, { quoted: m });
     }
-  }
-
-  let push = [];
-  const link = `https://id.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${encodeURIComponent(text)}%26rs%3Dtyped&data=%7B%22options%22%3A%7B%22applied_unified_filters%22%3Anull%2C%22appliedProductFilters%22%3A%22---%22%2C%22article%22%3Anull%2C%22auto_correction_disabled%22%3Afalse%2C%22corpus%22%3Anull%2C%22customized_rerank_type%22%3Anull%2C%22domains%22%3Anull%2C%22dynamicPageSizeExpGroup%22%3A%22control%22%2C%22filters%22%3Anull%2C%22journey_depth%22%3Anull%2C%22page_size%22%3Anull%2C%22price_max%22%3Anull%2C%22price_min%22%3Anull%2C%22query_pin_sigs%22%3Anull%2C%22query%22%3A%22${encodeURIComponent(text)}%22%2C%22redux_normalize_feed%22%3Atrue%2C%22request_params%22%3Anull%2C%22rs%22%3A%22typed%22%2C%22scope%22%3A%22pins%22%2C%22selected_one_bar_modules%22%3Anull%2C%22seoDrawerEnabled%22%3Afalse%2C%22source_id%22%3Anull%2C%22source_module_id%22%3Anull%2C%22source_url%22%3A%22%2Fsearch%2Fpins%2F%3Fq%3D${encodeURIComponent(text)}%26rs%3Dtyped%22%2C%22top_pin_id%22%3Anull%2C%22top_pin_ids%22%3Anull%7D%2C%22context%22%3A%7B%7D%7D`;
-  let { data } = await axios.get(`${link}`);
-  let res = data.resource_response.data.results.map(v => v.images.orig.url);
-
-  shuffleArray(res);
-  let ult = res.splice(0, 5); 
-  let i = 1;
-
-  for (let lucuy of ult) {
-    push.push({
-      body: proto.Message.InteractiveMessage.Body.fromObject({
-        text: `\nRESULTADO: ${i++}\n`
-      }),
-      footer: proto.Message.InteractiveMessage.Footer.fromObject({
-        text: global.pickbot
-      }),
-      header: proto.Message.InteractiveMessage.Header.fromObject({
-        title: '「 ✰ 」RESULTADOS ENCONTRADOS',
-        hasMediaAttachment: true,
-        imageMessage: await createImage(lucuy)
-      }),
-      nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-        buttons: [
-          {
-            "name": "cta_url",
-            "buttonParamsJson": `{"display_text":"Enlace","url":"https://www.pinterest.com/search/pins/?rs=typed&q=${text}","merchant_url":"https://www.pinterest.com/search/pins/?rs=typed&q=${text}"}`
-          },
-          {
-            "name": "cta_url",
-            "buttonParamsJson": `{"display_text":"Pagina Web","url":"https://www.pinterest.com/","merchant_url":"https://www.pinterest.com/"}`
-          }
-        ]
-      })
-    });
-  }
-
-  let bot = generateWAMessageFromContent(m.chat, {
-    viewOnceMessage: {
-      message: {
-        messageContextInfo: {
-          deviceListMetadata: {},
-          deviceListMetadataVersion: 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: "「 ✰ 」INFORMACION"
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: `✰ *BUSQUEDA:*\n> ${text}\n\n✰ *USUARIO:*\n> ${pushname}`
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            hasMediaAttachment: false
-          }),
-          carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-            cards: [
-              ...push
-            ]
-          })
-        })
-      }
-    }
-  }, {});
-
-  await Rifky.relayMessage(m.chat, bot.message, {
-    messageId: bot.key.id
-  });
-   } catch (e) {
-    m.reply(`[ ❗ ] Error: ${e}`)
-}}
+};
 
 handler.help = ['pinterest', 'pin'];
 handler.tags = ['download'];
 handler.command = ['pinterest', 'pin'];
-
 export default handler;
+
+async function pinterestSearch(query) {
+    try {
+        const response = await axios.get(`https://api.stellarwa.xyz/search/pinterest?query=${query}`);
+        const images = response.data.data;
+        if (!images || images.length === 0) return { status: false, resultado: "No se encontraron imágenes." };
+
+        return {
+            status: true,
+            resultado: images.map(img => ({
+                description: img.title || "Sin descripción",
+                imageUrl: img.hd
+            }))
+        };
+    } catch (error) {
+        return { status: false, resultado: error.message };
+    }
+}
+
+async function createImage(url) {
+    const { imageMessage } = await generateWAMessageContent({ image: { url } }, { upload: conn.waUploadToServer });
+    return imageMessage;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
