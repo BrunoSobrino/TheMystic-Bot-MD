c2f02a07724efaf6a9234f6e95bc77f11aef5fd8
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-14 23:06:57 -0300

    `Made With | GabrielZks`

--- plugins/converter-toptt.js
+++ plugins/converter-toptt.js
@@ -5,7 +5,6 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
  const tradutor = _translate.plugins.convertidor_toptt
-
   
  const q = m.quoted ? m.quoted : m;
  const mime = (m.quoted ? m.quoted : m.msg).mimetype || '';
@@ -16,8 +15,8 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  const audio = await toPTT(media, 'mp4');
  if (!audio.data && !/audio/.test(mime)) throw `*${tradutor.texto4}*`;
  if (!audio.data && !/video/.test(mime)) throw `*${tradutor.texto5}*`;
- const aa = conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, {mimetype: 'audio/mpeg'});
- if (!aa) return conn.sendMessage(m.chat, {audio: {url: media}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+ const aa = conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, { mimetype: 'audio/mpeg' });
+ if (!aa) return conn.sendMessage(m.chat, { audio: { url: media }, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
 };
 
 handler.help = ['tovn'];
