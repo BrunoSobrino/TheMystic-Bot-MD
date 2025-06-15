77608a8733c54f901d5cc98fed0594a6be78c904
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-15 10:49:42 -0300

    `Made With | GabrielZks`

--- plugins/converter-tts3.js
+++ plugins/converter-tts3.js
@@ -1,27 +1,29 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, usedPrefix, command, text }) => {
-  const match = text.match(/^(\w+)\s*\|\s*(.+)/i);
-  if (!match) {
-    const voices = await getVoices();
-    const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
-    return m.reply(`*[❗] Formato de uso erroneo, voz o texto faltante.*\n\n*—◉ Ejemplo:*\n◉ ${usedPrefix + command} nombre_voz | texto\n\n*—◉ Ejemplo de uso:*\n◉ ${usedPrefix + command} ${voices.voices[0].name} | este es un texto de ejemplo\n\n*—◉ Lista de voces disponibles:*\n◉ ${voiceNames}`
-    );
-  }
-  const [, voiceName, inputText] = match;
-  const voices = await getVoices();
-  const voice = voices.voices.find(voice => voice.name.toLowerCase() === voiceName.toLowerCase());
-  if (!voice) {
-    const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
-    return m.reply(`[❗] No se encontró ninguna voz con el nombre "${voiceName}".\n\n—◉ Lista de voces disponibles:\n◉ ${voiceNames}`);
-  }
-  const audio = await convertTextToSpeech(inputText, voice.voice_id);
-  if (audio) {
-    conn.sendMessage(m.chat, { audio: audio.audio, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
-  }
+ const match = text.match(/^(\w+)\s*\|\s*(.+)/i);
+ if (!match) {
+ const voices = await getVoices();
+ const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
+ return m.reply(`*[❗] Formato de uso erroneo, voz o texto faltante.*\n\n*—◉ Ejemplo:*\n◉ ${usedPrefix + command} nombre_voz | texto\n\n*—◉ Ejemplo de uso:*\n◉ ${usedPrefix + command} ${voices.voices[0].name} | este es un texto de ejemplo\n\n*—◉ Lista de voces disponibles:*\n◉ ${voiceNames}`);
+ }
+ const [, voiceName, inputText] = match;
+ const voices = await getVoices();
+ const voice = voices.voices.find(voice => voice.name.toLowerCase() === voiceName.toLowerCase());
+ if (!voice) {
+ const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
+ return m.reply(`[❗] No se encontró ninguna voz con el nombre "${voiceName}".\n\n—◉ Lista de voces disponibles:\n◉ ${voiceNames}`);
+ }
+ const audio = await convertTextToSpeech(inputText, voice.voice_id);
+ if (audio) {
+ conn.sendMessage(m.chat, { audio: audio.audio, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
+ }
 };
 
-handler.command = /^(tts3)$/i;
+handler.help = ['tts3'];
+handler.tags = ['converter'];
+handler.command = ['tts3'];
+
 export default handler;
 
 const apiKey = 'a0e2c6022f1aeb28b5020b1dd0faf6ee';
