735dd4473795036ba18273cc52901ff2650e3340
Alex-X <argumedodie@gmail.com>
2025-06-15 09:22:34 -0500

    Update

--- plugins/group-kick2.js
+++ plugins/group-kick2.js
@@ -14,6 +14,8 @@ const handler = async (m, {conn, participants, usedPrefix, command}) => {
   const owr = m.chat.split`-`[0];
   await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 };
+handler.tags = ['group'];
+handler.help = ['kick2'];
 handler.command = /^(kick2|echar2|hechar2|sacar2)$/i;
 handler.admin = true;
 handler.group = true;
