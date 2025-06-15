b9f3ecb5fe825d7fe5d5e1b1a9e0ec345ad0e094
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-15 09:17:21 -0300

    `Made With | GabrielZks`

---
+++ plugins/rpg-afk.js
@@ -0,0 +1,17 @@
+const handler = async (m, {text}) => {
+ const datas = global
+ const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+ const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+ const tradutor = _translate.plugins.afk_afk
+
+ const user = global.db.data.users[m.sender];
+ user.afk = + new Date;
+ user.afkReason = text;
+ m.reply(`${tradutor.texto1[0]} ${conn.getName(m.sender)} ${tradutor.texto1[1]} ${text ? ': ' + text : ''}*
+`);
+};
+handler.help = ['afk'];
+handler.tags = ['xp'];
+handler.command = ['afk']
+
+export default handler;
