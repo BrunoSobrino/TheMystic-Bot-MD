629813755da0a88c936c620d18094b4be11d8c5b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-06-23 05:46:07 -0600

    Update marker-ephoto360.js

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -1,4 +1,5 @@
-import {Maker} from 'imagemaker.js';
+import { ephoto } from 'nayan-api-servers';
+
 
 
 const handler = async (m, {conn, args, command, usedPrefix}) => {
@@ -12,7 +13,7 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
   if (command == 'logocorazon') {
     try {
       await conn.reply(m.chat, tradutor.texto2, m);
-      const res = await new Maker().Ephoto360('https://en.ephoto360.com/text-heart-flashlight-188.html', [response[0]]);
+      const res = await ephoto('https://en.ephoto360.com/text-heart-flashlight-188.html', [response[0]]);
       await conn.sendFile(m.chat, res.imageUrl, 'error.jpg', null, m);
     } catch {
       await conn.reply(m.chat, tradutor.texto3, m);
@@ -21,7 +22,7 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
   if (command == 'logochristmas') {
     try {
       await conn.reply(m.chat, tradutor.texto4, m);
-      const res2 = await new Maker().Ephoto360('https://en.ephoto360.com/christmas-effect-by-name-376.html', [response[0]]);
+      const res2 = await ephoto('https://en.ephoto360.com/christmas-effect-by-name-376.html', [response[0]]);
       await conn.sendFile(m.chat, res2.imageUrl, 'error.jpg', null, m);
     } catch {
       await conn.reply(m.chat, tradutor.texto5, m);
