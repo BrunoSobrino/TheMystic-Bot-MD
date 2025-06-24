9f0504c0d9f139a9c10c27bef4794bce71ac1396
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-06-24 08:50:26 -0600

    Update simple.js

--- src/libraries/simple.js
+++ src/libraries/simple.js
@@ -2295,8 +2295,8 @@ export function serialize() {
                     }
                     const parsedJid = safeDecodeJid(rawParticipant, self.conn);
                     return (
-                      parsedJid?.resolveLidToRealJid(this.chat, self.conn) ||
-                      parsedJid
+                      parsedJid?.resolveLidToRealJid(this.chat, self.conn).toString() ||
+                      parsedJid.toString()
                     );
                   } catch (e) {
                     console.error("Error en quoted sender getter:", e);
