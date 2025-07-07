84ee5c8fb82d74a19e8b1bdb894a8856d7f8c08f
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-07-07 04:19:35 -0300

    Update gc-eventos.js

--- plugins/gc-eventos.js
+++ plugins/gc-eventos.js
@@ -6,4 +6,8 @@ handler.command = /^(evento|eventos|eventofutabuclub)$/i
 handler.group = true;
 export default handler
 
-global.eventos = `Nada aún!.`
+global.eventos = `*_Limpieza de Miembros Inactivos (6 de Septiembre 2025)_*
+
+Esto se hace como un aviso para empezar a hacer activo en los meses que faltan antes que llegue el dia, traten de ser activos constantemente y aportar una que otra vez contenido, si son personas ocupadas traten de pasarse aqui para hablar de vez en cuando.
+
+Buena suerte si sobrevives a la purga ese dia ;)`
