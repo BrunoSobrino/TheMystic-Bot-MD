b664c87860f695145f2e32b3860a3b05442f3558
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-07-07 04:13:45 -0300

    Update gc-apoyo.js

--- plugins/gc-apoyo.js
+++ plugins/gc-apoyo.js
@@ -7,9 +7,9 @@ handler.group = true;
 export default handler
 
 global.apoyo = `*_Aca tienes los 2 bancos para transferir dinero para la mejora del bot!_*
-*Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
 
-*Banco Virtual (Mercado Pago, Uala, Etc)*
-Alias: COLAPSUSHD2020.UALA
-CBU/CVU: 0000007900204654633937
-Si estas deacuerdo con apoyar porfavor presiona el boton que esta debajo`
+*💸 Paypal:* colapsuspaypal2005@gmail.com (Benjamin Chacon)
+
+*🏦 Banco Virtual (Mercado Pago, Uala, Etc)*
+• Alias: COLAPSUSHD2020.UALA
+• CBU/CVU: 0000007900204654633937`
