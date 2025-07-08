52cf46c4176c5610927cf55c219c990ba1565ba8
Alex-X <argumedodie@gmail.com>
2025-07-08 11:26:58 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,50 +1,27 @@
 import axios from 'axios'
 import fs from 'fs'
 
-const handler = async (m, { conn, args }) => {
+import {search, download} from 'aptoide-scraper';
+ 
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.downloader_modapk
 
-  const query = args[0]
+ if (!text) throw `${tradutor.texto1}`;
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastup}\n${tradutor.texto2[3]} ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
 
-  if (!query) {
-    throw `${tradutor.texto1}`
-  }
-
-  try {
-    const response = await axios.get(`https://api.stellarwa.xyz/search/apk?query=${query}`)
-    const data5 = response.data.data
-
-    if (data5.name && data5.dl) {
-      const formattedMessage = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastUpdated}\n${tradutor.texto2[3]} ${data5.size}`.trim()
-
-      try {
-        await conn.sendMessage(m.chat, {
-          image: { url: data5.banner },
-          caption: formattedMessage
-        }, { quoted: m })
-      } catch (uploadError) {
-        console.error(`Failed to upload image: ${uploadError.message}`)
-        await m.reply(`Image upload failed, here is the link instead: ${data5.banner}`)
-      }
-
-      await conn.sendMessage(m.chat, {
-        document: { url: data5.dl },
-        fileName: `${data5.name}.apk`,
-        mimetype: 'application/vnd.android.package-archive',
-        caotion: null
-      }, { quoted: m })
-
-    } else {
-      throw `${tradutor.texto4}`
-    }
-  } catch (error) {
-    console.error(`Error details: ${error.message}`)
-    await m.reply(`[ ❗ ] *Fail:* ${error.response ? error.response.data : error.message}`)
-  }
-}
+await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+  } catch {
+    throw `${tradutor.texto4}`;
+  }}
 
 handler.help = ['apk']
 handler.tags = ['search']
