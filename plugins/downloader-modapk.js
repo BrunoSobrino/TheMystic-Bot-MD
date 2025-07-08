f5d5bd519e3d7b849add816d766bd40b9c69c54d
Alex-X <argumedodie@gmail.com>
2025-07-08 11:37:20 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 const handler = async (m, { conn, text }) => {
 
-  if (!text) {
-    return conn.reply(m.chat, `✧ Ingresa el nombre de la aplicación que quieras descargar.`, m);
-  }
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins.downloader_modapk
+
+ if (!text) throw `${tradutor.texto1}`;
 
   const query = text.trim();
 
@@ -13,30 +16,24 @@ const handler = async (m, { conn, text }) => {
     const data = response.data.data;
 
     if (data.name && data.dl) {
-      const formattedMessage = `
-❀ *Descargando ›* ${data.name}
-
-➭ *Package ›* ${data.package}
-> ✩ *Última actualización ›* ${data.lastUpdated}
-> ✩ *Peso ›* ${data.size}`.trim();
-
+      let response = `${tradutor.texto2[0]} ${data.name}\n${tradutor.texto2[1]}* ${data.lastUpdated}\n${tradutor.texto2[2]} ${data.lastup}\n${tradutor.texto2[3]} ${data.size}`
             await conn.sendMessage(m.chat, {
         image: { url: data.banner },
-        caption: formattedMessage
+        caption: response
       }, { quoted: m });
 
       await conn.sendMessage(m.chat, {
         document: { url: data.dl },
         fileName: `${data.name}.apk`,
         mimetype: 'application/vnd.android.package-archive',
-        caption: ""
+        caption: "The Mystic • BrunoSobrino"
       }, { quoted: m });
 
     } else {
-      await conn.reply(m.chat, `✎ No se encontró la aplicación solicitada.`, m);
+      throw `${tradutor.texto4}`;
     }
   } catch (error) {
-    await m.reply(`${error}`);
+    await m.reply(`[ ❗ ] *Fail:* ${error}`);
   }
 };
 
