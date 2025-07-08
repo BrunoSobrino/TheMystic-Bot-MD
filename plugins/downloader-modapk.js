0057b7e187c06171694a86069bdacb18b297eb03
Alex-X <argumedodie@gmail.com>
2025-07-08 11:37:31 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,44 +1,28 @@
-import axios from 'axios';
-
-const handler = async (m, { conn, text }) => {
-
+import {search, download} from 'aptoide-scraper';
+ 
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.downloader_modapk
 
  if (!text) throw `${tradutor.texto1}`;
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastup}\n${tradutor.texto2[3]} ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
 
-  const query = text.trim();
+await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+   }
+  } catch {
+    throw `${tradutor.texto4}`;
+  }}
 
-  try {
-    const response = await axios.get(`https://api.stellarwa.xyz/search/apk?query=${query}`);
-    const data = response.data.data;
+handler.help = ['apk']
+handler.tags = ['search']
+handler.command = /^(apk|apkmod|modapk|dapk2|aptoide|aptoidedl)$/i
 
-    if (data.name && data.dl) {
-      let response = `${tradutor.texto2[0]} ${data.name}\n${tradutor.texto2[1]}* ${data.lastUpdated}\n${tradutor.texto2[2]} ${data.lastup}\n${tradutor.texto2[3]} ${data.size}`
-            await conn.sendMessage(m.chat, {
-        image: { url: data.banner },
-        caption: response
-      }, { quoted: m });
-
-      await conn.sendMessage(m.chat, {
-        document: { url: data.dl },
-        fileName: `${data.name}.apk`,
-        mimetype: 'application/vnd.android.package-archive',
-        caption: "The Mystic • BrunoSobrino"
-      }, { quoted: m });
-
-    } else {
-      throw `${tradutor.texto4}`;
-    }
-  } catch (error) {
-    await m.reply(`[ ❗ ] *Fail:* ${error}`);
-  }
-};
-
-handler.help = ['aptoide', 'apk', 'apkdl'];
-handler.tags = ['search'];
-handler.command = ['aptoide', 'apk', 'apkdl'];
-
-export default handler;
+export default handler
