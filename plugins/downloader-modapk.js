0c57b1971e02e3f073aea0a986c7a0366f8e479c
Alex-X <argumedodie@gmail.com>
2025-07-08 11:34:25 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,39 +1,45 @@
+import fs from 'fs';
 import axios from 'axios';
 
 const handler = async (m, { conn, text }) => {
+  const idioma = global.db.data.users[m.sender]?.language || global.defaultLenguaje;
 
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.downloader_modapk
+  let tradutor;
+  try {
+    const rawData = fs.readFileSync(`./src/languages/${idioma}.json`, 'utf-8');
+    const traducciones = JSON.parse(rawData);
+    tradutor = traducciones.plugins.downloader_modapk;
+  } catch (e) {
+    return m.reply('[ ❗ ] Error al cargar las traducciones.');
+  }
 
- if (!text) throw `${tradutor.texto1}`;
+  if (!text) throw tradutor.texto1;
 
   const query = text.trim();
 
   try {
-    const response = await axios.get(`https://api.stellarwa.xyz/search/apk?query=${query}`);
-    const data = response.data.data;
+    const res = await axios.get(`https://api.stellarwa.xyz/search/apk?query=${query}`);
+    const data = res.json.data;
 
     if (data.name && data.dl) {
-      let response = `${tradutor.texto2[0]} ${data.name}\n${tradutor.texto2[1]}* ${data.lastUpdated}\n${tradutor.texto2[2]} ${data.lastup}\n${tradutor.texto2[3]} ${data.size}`
-            await conn.sendMessage(m.chat, {
+      const mensaje = `${tradutor.texto2[0]} ${data.name}\n${tradutor.texto2[1]} ${data.lastUpdated}\n${tradutor.texto2[2]} ${data.lastup}\n${tradutor.texto2[3]} ${data.size}`;
+
+      await conn.sendMessage(m.chat, {
         image: { url: data.banner },
-        caption: response
+        caption: mensaje
       }, { quoted: m });
 
       await conn.sendMessage(m.chat, {
         document: { url: data.dl },
         fileName: `${data.name}.apk`,
         mimetype: 'application/vnd.android.package-archive',
-        caption: "The Mystic • BrunoSobrino"
+        caption: 'The Mystic • BrunoSobrino'
       }, { quoted: m });
-
     } else {
-      throw `${tradutor.texto4}`;
+      throw tradutor.texto4;
     }
   } catch (error) {
-    await m.reply(`[ ❗ ] *Fail:* ${error}`);
+    await m.reply(`[ ❗ ] *Fail:* ${error.message || error}`);
   }
 };
 
