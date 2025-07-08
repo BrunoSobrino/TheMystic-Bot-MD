34e6a377b6ce920b896a4afa0b94c6a2f6ac1bb2
Alex-X <argumedodie@gmail.com>
2025-07-08 11:36:44 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,48 +1,42 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 const handler = async (m, { conn, text }) => {
-  const idioma = global.db.data.users[m.sender]?.language || global.defaultLenguaje;
 
-  let tradutor;
-  try {
-    const rawData = fs.readFileSync(`./src/languages/${idioma}.json`, 'utf-8');
-    const traducciones = JSON.parse(rawData);
-    tradutor = traducciones.plugins.downloader_modapk;
-  } catch (e) {
-    return m.reply('[ ❗ ] Error al cargar las traducciones.');
+  if (!text) {
+    return conn.reply(m.chat, `✧ Ingresa el nombre de la aplicación que quieras descargar.`, m);
   }
-
-  if (!text) throw tradutor.texto1;
 
   const query = text.trim();
 
   try {
-    const response = await fetch(`https://api.stellarwa.xyz/search/apk?query=${encodeURIComponent(query)}`);
-    if (!response.ok) throw new Error(`HTTP error ${response.status}`);
-    
-    const json = await response.json();
-    const data = json.data;
+    const response = await axios.get(`https://api.stellarwa.xyz/search/apk?query=${query}`);
+    const data = response.data.data;
 
-    if (data?.name && data?.dl) {
-      const mensaje = `${tradutor.texto2[0]} ${data.name}\n${tradutor.texto2[1]} ${data.lastUpdated}\n${tradutor.texto2[2]} ${data.lastup}\n${tradutor.texto2[3]} ${data.size}`;
+    if (data.name && data.dl) {
+      const formattedMessage = `
+❀ *Descargando ›* ${data.name}
 
-      await conn.sendMessage(m.chat, {
+➭ *Package ›* ${data.package}
+> ✩ *Última actualización ›* ${data.lastUpdated}
+> ✩ *Peso ›* ${data.size}`.trim();
+
+            await conn.sendMessage(m.chat, {
         image: { url: data.banner },
-        caption: mensaje
+        caption: formattedMessage
       }, { quoted: m });
 
       await conn.sendMessage(m.chat, {
         document: { url: data.dl },
         fileName: `${data.name}.apk`,
         mimetype: 'application/vnd.android.package-archive',
-        caption: 'The Mystic • BrunoSobrino'
+        caption: ""
       }, { quoted: m });
+
     } else {
-      throw tradutor.texto4;
+      await conn.reply(m.chat, `✎ No se encontró la aplicación solicitada.`, m);
     }
   } catch (error) {
-    await m.reply(`[ ❗ ] *Fail:* ${error.message || error}`);
+    await m.reply(`${error}`);
   }
 };
 
