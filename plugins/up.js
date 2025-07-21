fe7aac39866a0802c0ce6073678c3ceadd7d2804
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2025-07-21 00:48:00 -0300

    Update up.js

--- plugins/up.js
+++ plugins/up.js
@@ -2,12 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 let handler = async (m, { text }) => {
-  if (!text) throw 'Nombre Archivo?';
-  if (!m.quoted || !m.quoted.fileSha256) throw 'Citar Sr';
+  if (!m.quoted || !m.quoted.fileSha256) throw 'Citar el Archivo a Subir';
   
   try {
     let media = await m.quoted.download();
-    const filePath = `src/tmp/YTDLP/${text}`;
+    const filePath = `src/tmp/YTDLP/cookies.txt`;
     
     const dir = path.dirname(filePath);
     if (!fs.existsSync(dir)) {
@@ -18,12 +17,12 @@ let handler = async (m, { text }) => {
     m.reply(`Ok ${filePath}`);
     
   } catch (error) {
-    console.error('UPPLUGIN Error Sub Archivo:', error);
+    console.error('UPKOOKIES PLUGIN Error Sub Archivo:', error);
     m.reply(`Error: ${error.message}`);
   }
 };
 
-handler.help = ['up <nombre>'];
+handler.help = ['upcookie'];
 handler.tags = ['tools'];
 handler.command = /^(up)$/i;
 handler.owner = true;
