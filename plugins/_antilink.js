cd00c6c0e7ef8e74450e318f2ff8b49b1b390877
BrunoSobrino <sobrinoflotab@gmail.com>
2025-07-24 16:54:26 -0600

    Revert "Update _antilink.js"
    This reverts commit a1c7f586898b03e53bf3bccbe2931bd706375c75.

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -15,7 +15,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin}) {
   const delet = m.key.participant;
   const bang = m.key.id;
   const bot = global.db.data.settings[this.user.jid] || {};
-  const user = `@${await m.sender.split`@`[0]}`;
+  const user = `@${m.sender.split`@`[0]}`;
   const isGroupLink = linkRegex.exec(m.text);
   const grupo = `https://chat.whatsapp.com`;
   if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.texto1.replace('@user', '@' + user.split('@')[0]));
@@ -24,12 +24,11 @@ export async function before(m, {conn, isAdmin, isBotAdmin}) {
       const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
       if (m.text.includes(linkThisGroup)) return !0;
     }
-    const testi = await m.sender 
-    await this.sendMessage(m.chat, {text: tradutor.texto2.replace('@user', '@' + user.split('@')[0]), mentions: [testi]}, {quoted: m});
+    await this.sendMessage(m.chat, {text: tradutor.texto2.replace('@user', '@' + user.split('@')[0]), mentions: [m.sender]}, {quoted: m});
     if (!isBotAdmin) return m.reply(tradutor.texto3);
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [testi], 'remove');
+      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
     } else if (!bot.restrict) return m.reply(tradutor.texto4);
   }
