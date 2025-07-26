5e85d3027bd9f4627eb1144d97374e3293ccb170
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-07-26 11:41:28 -0600

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn}) => {
 };
 
 handler.help = ['tyc'];
-handler.tags = ['tyc', 'info'];
+handler.tags = ['info'];
 handler.command = /^(términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones)$/i;
 export default handler;
 
