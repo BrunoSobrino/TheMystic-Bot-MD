bd36d10340be570f26cd214233faddac49295b12
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-07-26 12:37:25 -0600

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -13,7 +13,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `${tradutor.texto1[0]} ${pesan}`;
   let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
   for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
+    teks += `┣➥ @${mem.jid.split('@')[0]}\n`;
   }
   teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
