e5e5923963e9083d9193d06feee10c74d4c90df4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-07-29 07:46:30 -0600

    Update group-infogroup.js

--- plugins/group-infogroup.js
+++ plugins/group-infogroup.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.gc_infogroup
 
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/avatar_contact.png';
+  const pp = await conn.profilePictureUrl(m.chat, 'image') || imagen1 ||'./src/avatar_contact.png';
   const {antiToxic, antiTraba, antidelete, antiviewonce, isBanned, welcome, detect, detect2, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, modoadmin, audios, delete: del} = global.db.data.chats[m.chat];
   const groupAdmins = participants.filter((p) => p.admin);
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
