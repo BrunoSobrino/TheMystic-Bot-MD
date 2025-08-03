070f27b5ad8577046466ff9234f860f24ef4f233
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-03 12:07:00 -0600

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,8 +1,4 @@
 const {downloadContentFromMessage} = (await import("baileys"));
- // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
 
 export async function before(m, {isAdmin, isBotAdmin}) {
   const datas = global
@@ -13,19 +9,21 @@ export async function before(m, {isAdmin, isBotAdmin}) {
   const chat = db.data.chats[m.chat];
   if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return;
   if (!chat?.antiviewonce || chat?.isBanned) return;
-  if (m.mtype == 'viewOnceMessageV2') {
-    const msg = m.message.viewOnceMessageV2.message;
-    const type = Object.keys(msg)[0];
-    const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+  if (m.viewOnce) {
+    const msg = m;
+    const type = msg.mtype;
+    const media = await downloadContentFromMessage(msg, type == 'imageMessage' ? 'image' : type == 'videoMessage' ? 'video' : 'audio');
     let buffer = Buffer.from([]);
     for await (const chunk of media) {
       buffer = Buffer.concat([buffer, chunk]);
     }
     const cap = tradutor.texto1
     if (/video/.test(type)) {
-      return mconn.conn.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+      return mconn.conn.sendFile(m.chat, buffer, 'error.mp4', `${msg?.caption ? msg?.caption + '\n\n' + cap : cap}`, m);
     } else if (/image/.test(type)) {
-      return mconn.conn.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+      return mconn.conn.sendFile(m.chat, buffer, 'error.jpg', `${msg?.caption ? msg?.caption + '\n\n' + cap : cap}`, m);
+    } else if (/audio/.test(type)) {
+      return mconn.conn.sendFile(m.chat, buffer, 'error.mp3', `${msg?.caption ? msg?.caption + '\n\n' + cap : cap}`, m);
     }
   }
 }
