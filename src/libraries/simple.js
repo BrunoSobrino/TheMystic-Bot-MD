81ca690eebb8349263746d7b5e5cc7796608f358
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-03 14:02:25 -0600

    Update simple.js

--- src/libraries/simple.js
+++ src/libraries/simple.js
@@ -2173,7 +2173,7 @@ export function serialize() {
                     const baileysStarts = ['NJX-', 'Lyru-', 'META-', 'EvoGlobalBot-', 'FizzxyTheGreat-', 'BAE5', '3EB0', 'B24E', '8SCO', 'SUKI', 'MYSTIC-'];
                     const hasKnownPrefix = baileysStarts.some(prefix => messageId.startsWith(prefix));
 	            const isHexPattern = /^[A-F0-9]{16,32}$/.test(messageId)
-		    const isSukiPattern = /^SUKI[A-F0-9]{16}$/.test(messageId)
+		    const isSukiPattern = /^SUKI[A-F0-9]+$/.test(messageId);
                     return isSukiPattern || hasKnownPrefix || isHexPattern || false;
                 } catch (e) {
                     console.error("Error en isBaileys getter:", e);
@@ -2375,7 +2375,7 @@ export function serialize() {
                                     const baileysStarts = ['NJX-', 'Lyru-', 'META-', 'EvoGlobalBot-', 'FizzxyTheGreat-', 'BAE5', '3EB0', 'B24E', '8SCO', 'SUKI', 'MYSTIC-'];
                                     const hasKnownPrefix = baileysStarts.some(prefix => messageId.startsWith(prefix));
 				    const isHexPattern = /^[A-F0-9]{16,32}$/.test(messageId)
-				    const isSukiPattern = /^SUKI[A-F0-9]{16}$/.test(messageId)
+				    const isSukiPattern = /^SUKI[A-F0-9]+$/.test(messageId);
                                     return isSukiPattern || hasKnownPrefix || isHexPattern || false;
                                 },
                                 enumerable: true,
