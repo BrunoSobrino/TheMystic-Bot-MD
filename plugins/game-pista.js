b939190181d446422b86c6f8c128e063c41bb27d
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-03 00:19:23 -0600

    Update game-pista.js

--- plugins/game-pista.js
+++ plugins/game-pista.js
@@ -1,13 +1,28 @@
-/* Created by https://github.com/unptoadrih15 */
-
 const handler = async (m, {conn}) => {
   conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {};
   const id = m.chat;
   if (!(id in conn.tebaklagu)) throw false;
   const json = conn.tebaklagu[id][1];
-  const nya = json.jawaban;
-  const nyanya = nya.replace(/[bcdfghjklmnñpqrstvwxyzBCDEFGHJKLMNÑPQRSTVWXYZ]/g, '_');
-  m.reply('' + nyanya + '');
+  const answer = json.jawaban;
+  const artist = json.artist;
+  const hint = createHint(answer);
+  const wordCount = answer.split(' ').length;
+  const letterCount = answer.replace(/\s/g, '').length;
+  const hintMessage = `🎵 *PISTA:*\n\n\`${hint}\`\n\n🎤 *Artista:* ${artist}\n📝 *Palabras:* ${wordCount}\n📏 *Letras:* ${letterCount}\n\n💡 _Las vocales están visibles, adivina las consonantes_`;
+  m.reply(hintMessage);
 };
+
 handler.command = /^hint|pista$/i;
 export default handler;
+
+function createHint(text) {
+  return text.split('').map(char => {
+    if (/[aeiouAEIOU\s0-9]/.test(char)) {
+      return char;
+    }
+    else if (/[a-zA-ZñÑ]/.test(char)) {
+      return '_';
+    } else {
+      return char;
+    }}).join('');
+}
