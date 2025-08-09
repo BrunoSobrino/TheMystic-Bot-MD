c5b7cd20eb001a32da4d7fb621d2faa3de60d716
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-09 16:28:19 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -245,12 +245,12 @@ const handler = async (m, { conn, usedPrefix, __dirname, isPrems }) => {
         };
 
         let user = global.db.data.users[m.sender];
-        let exp = user.exp ?? 0;
-        let limit = user.limit ?? 0;
-        let level = user.level ?? 0;
-        let role = user.role ?? 'Nuevo';
-        let money = user.money ?? 0;
-        let joincount = user.joincount ?? 0;
+        let exp = user.exp ? user.exp : 0
+        let limit = user.limit ? user.limit : 0;
+        let level = user.level ? user.level : 0;
+        let role = user.role ? user.role : 'Nuevo';
+        let money = user.money ? user.money : 0;
+        let joincount = user.joincount ? user.joincount : 0;
 
         const defaultMenu = {
             before: (tradutor.menu_header || '')
