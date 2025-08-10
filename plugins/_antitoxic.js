e88cbe6b03269520b478aa45111cffe46de958de
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-09 23:53:14 -0600

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,10 +1,5 @@
 // TheMystic-Bot-MD@BrunoSobrino - _antitoxic.js
 
- // Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-  // Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-  // To set the language, in the root of the project, modify the config.json file.
-
-
 const toxicRegex = /\b(puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon)\b/i;
 
 export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
