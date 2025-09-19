f4eb50f9f65d032ddb2528163bafcdaa2be341d9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-09-19 12:08:10 -0600

    Update uploadImage.js

--- src/libraries/uploadImage.js
+++ src/libraries/uploadImage.js
@@ -1,15 +1,38 @@
-import { fileTypeFromBuffer } from "file-type"
+import fetch from 'node-fetch';
+import { FormData, Blob } from 'formdata-node';
+import { fileTypeFromBuffer } from 'file-type';
 
 /**
- * Upload file to UploadF
+ * Upload file to Catbox
  * Supported mimetypes:
  * - `image/jpeg`
  * - `image/jpg`
  * - `image/png`
  * - `video/mp4`
+ * - `video/webm`
+ * - `audio/mpeg`
+ * - `audio/wav`
  * @param {Buffer} buffer File Buffer
  * @return {Promise<string>}
  */
+export default async (buffer) => {
+  const { ext, mime } = await fileTypeFromBuffer(buffer);
+  const form = new FormData();
+  const blob = new Blob([buffer.toArrayBuffer()], { type: mime });
+  form.append('fileToUpload', blob, 'tmp.' + ext);
+  form.append('reqtype', 'fileupload');
+  const res = await fetch('https://catbox.moe/user/api.php', {
+    method: 'POST',
+    body: form,
+  });
+  const result = await res.text(); 
+  if (result.startsWith('https://files.catbox.moe/')) {
+    return result;
+  } else {
+    throw new Error('Failed to upload the file to Catbox');
+  }
+};
+/*import { fileTypeFromBuffer } from "file-type"
 
 export default async (buffer) => {
  const f = await fileTypeFromBuffer(buffer)
@@ -25,4 +48,6 @@ export default async (buffer) => {
  const fileId = '/' + r.url.split("/").pop()
  const result = origin + '/file' + fileId;
  return result;
-}
+}*/
+
+
