b566b1dc12cc3b03c8cf16742a1f5c049171c706
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-09-26 02:48:07 -0300

    Update group-unwarn.js

--- plugins/group-unwarn.js
+++ plugins/group-unwarn.js
@@ -1,26 +1,53 @@
+import fs from 'fs'
+
 const handler = async (m, {conn, text, command, usedPrefix}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.gc_unwarn
 
-  const pp = './src/assets/images/menu/main/warn.jpg';
-  let who;
-  if (m.isGroup) who = await await m.mentionedJid[0] ? await await m.mentionedJid[0] : m.quoted ? await m?.quoted?.sender : text;
-  else who = m.chat;
-  const user = global.db.data.users[who];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`;
-  if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
-  if (await m.mentionedJid.includes(conn.user.jid)) return;
-  if (user.warn == 0) throw tradutor.texto2;
-  user.warn -= 1;
-  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`}${tradutor.texto3} ${user.warn}/3*`, null, {mentions: [who]});
-};
-handler.tags = ['group'];
-handler.help = ['unwarn'];
-handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+  const pp = './src/assets/images/menu/main/warn.jpg'
+  let who
+
+  if (m.isGroup) {
+    if (m.mentionedJid && m.mentionedJid[0]) {
+      who = m.mentionedJid[0]
+    } else if (m.quoted) {
+      who = m.quoted.sender
+    } else if (text) {
+      who = text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' // convierte número a JID
+    }
+  } else {
+    who = m.chat
+  }
+
+  const user = global.db.data.users[who]
+  const bot = global.db.data.settings[conn.user.jid] || {}
+
+  const warntext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`
+
+  if (!who || !user) {
+    throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)})
+  }
+
+  if (m.mentionedJid?.includes(conn.user.jid)) return
+
+  if (user.warn === 0) throw tradutor.texto2
+
+  user.warn -= 1
+
+  await m.reply(
+    `${user.warn === 1 ? `*@${who.split('@')[0]}*` : `♻️ *@${who.split('@')[0]}*`} ${tradutor.texto3} ${user.warn}/3*`,
+    null,
+    {mentions: [who]}
+  )
+}
+
+handler.tags = ['group']
+handler.help = ['unwarn']
+handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
