5829beb46fa966696b22e129898ce66f72897bbd
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-09-27 17:28:52 -0300

    Update group-listwarn.js

--- plugins/group-listwarn.js
+++ plugins/group-listwarn.js
@@ -1,29 +1,27 @@
-import fs from 'fs'
-
 const handler = async (m, {conn, isOwner}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.gc_listwarn
 
-  const adv = Object.entries(global.db.data.users).filter(([jid, user]) => user.warn && user.warn > 0)
-
-  const caption = `${tradutor.texto1}\n
+  const adv = Object.entries(global.db.data.users).filter((user) => user[1].warn);
+  const warns = global.db.data.users.warn;
+  const user = global.db.data.users;
+  const imagewarn = './src/assets/images/menu/main/warn.jpg';
+  const caption = `${tradutor.texto1}\n 
 *╔═══════════════════·•*
-║ ${tradutor.texto2[0]} ${adv.length} ${tradutor.texto2[1]} ${adv.length ? '\n' + adv.map(([jid, user], i) => {
-    return `
-║ ${i + 1}.- @${jid.split('@')[0]} *(${user.warn}/6)*
-║ - - - - - - - - -`.trim()
-  }).join('\n') : ''}
-*╚══════════════════·•*`
+║ ${tradutor.texto2[0]} ${adv.length} ${tradutor.texto2[1]} ${adv ? '\n' + adv.map(([jid, user], i) => {
+i++
+return `
+║
+║ ${i}.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/6)*\n║\n║ - - - - - - - - -`.trim()}).join('\n') : ''}
+*╚══════════════════·•*`;
+  await conn.sendMessage(m.chat, {text: caption}, {quoted: m}, {mentions: await conn.parseMention(caption)});
+};
 
-  await conn.sendMessage(m.chat, {text: caption, mentions: adv.map(([jid]) => jid)}, {quoted: m})
-}
-
-handler.help = ['listwarn']
-handler.tags = ['group']
-handler.command = /^(listwarn)$/i
-handler.group = true
-handler.admin = true
-
-export default handler
+handler.help = ['listwarn'];
+handler.tags = ['group'];
+handler.command = /^(listwarn)$/i;
+handler.group = true;
+handler.admin = true;
+export default handler;
