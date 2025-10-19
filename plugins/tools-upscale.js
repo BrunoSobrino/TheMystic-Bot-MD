07c79d30abc77c69ee11af006ffe22bb31b92895
Ell Contrerax <sarmientodieg@gmail.com>
2025-10-18 21:49:43 -0500

    Actualizar tools-upscale.js

--- plugins/tools-upscale.js
+++ plugins/tools-upscale.js
@@ -32,7 +32,7 @@ handler.command = ["remini", "hd", "enhance"]
 export default handler
 
 async function upscaleWithStellar(url) {
-  const endpoint = `https://api.stellarwa.xyz/tools/upscale?url=${url}&apikey=BrunoSobrino`
+  const endpoint = `https://api.stellarwa.xyz/tools/upscale?url=${url}&key=BrunoSobrino`
 
   const { data } = await axios.get(endpoint, {
     responseType: "arraybuffer",
