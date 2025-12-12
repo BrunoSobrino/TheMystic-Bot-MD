bbca803b41f65c4ae8ec2a85aa504c39c0d2311b
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-12-12 17:39:28 -0300

    Update owner-actualizar.js

--- plugins/owner-actualizar.js
+++ plugins/owner-actualizar.js
@@ -1,19 +1,21 @@
 import { execSync } from 'child_process';
+import fs from 'fs';
 
 const handler = async (m, { conn, text }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.propietario_actualizar
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+  const tradutor = _translate.plugins.propietario_actualizar;
 
   try {
-          const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
-          let messager = stdout.toString()
-          if (messager.includes('Already up to date.')) messager = tradutor.texto1
-          if (messager.includes('Updating')) messager = tradutor.texto2 + stdout.toString()
-          conn.reply(m.chat, messager, m);
-  } catch {      
- try {    
+    let messager = stdout.toString();
+    if (messager.includes('Already up to date.')) messager = tradutor.texto1;
+    if (messager.includes('Updating')) messager = tradutor.texto2 + stdout.toString();
+
+    conn.reply(m.chat, messager, m);
+
+  } catch {
+    try {
       const status = execSync('git status --porcelain');
       if (status.length > 0) {
         const conflictedFiles = status
@@ -21,7 +23,13 @@ const handler = async (m, { conn, text }) => {
           .split('\n')
           .filter(line => line.trim() !== '')
           .map(line => {
-            if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('MysticSession/') || line.includes('npm-debug.log')) {
+            if (
+              line.includes('.npm/') ||
+              line.includes('.cache/') ||
+              line.includes('tmp/') ||
+              line.includes('MysticSession/') ||
+              line.includes('npm-debug.log')
+            ) {
               return null;
             }
             return '*→ ' + line.slice(3) + '*';
@@ -29,21 +37,23 @@ const handler = async (m, { conn, text }) => {
           .filter(Boolean);
         if (conflictedFiles.length > 0) {
           const errorMessage = `${tradutor.texto3} \n\n${conflictedFiles.join('\n')}.*`;
-          await conn.reply(m.chat, errorMessage, m);  
+          await conn.reply(m.chat, errorMessage, m);
         }
       }
-  } catch (error) {
-    console.error(error);
-    let errorMessage2 = tradutor.texto4;
-    if (error.message) {
-      errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+    } catch (error) {
+      console.error(error);
+      let errorMessage2 = tradutor.texto4;
+      if (error.message) {
+        errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+      }
+      await conn.reply(m.chat, errorMessage2, m);
     }
-    await conn.reply(m.chat, errorMessage2, m);
   }
- }
 };
+
 handler.help = ['update'];
 handler.tags = ['owner'];
 handler.command = /^(update|actualizar|gitpull)$/i;
 handler.rowner = true;
+
 export default handler;
